b1d62fcc3d3a073fd847651b659b7d8006098e0e
root <root@domU-12-31-39-0B-5D-25.compute-1.internal>
2010-05-27 20:56:52 +0000

    dbrebuild working

--- application/controller/admin.rb
+++ application/controller/admin.rb
@@ -7,18 +7,18 @@ module XYZ
     def dbrebuild
       #Better encapsulate where case on whetehr production set (meaning that don't need to recreate tables)
       #associate DBinstance with all models
-      XYZ::Model.set_db_for_all_models(DBinstance)
+      Model.set_db_for_all_models(DBinstance)
       
       #setup infra tables if they don't exist already
-      XYZ::Model.setup_infrastructure_tables?(DBinstance)
+      Model.setup_infrastructure_tables?(DBinstance)
       
       # create the domain-related tables if tehy don't exist already
-      XYZ::Model.migrate_all_models(:up) 
+      Model.migrate_all_models(:up) 
       
       # add the top level factorories if they don't exist
       #has to be done after db added to class and models been added
-      XYZ::IDInfoTable.add_top_factories?() unless XYZ::Config[:production]
-
+      IDInfoTable.add_top_factories?() unless Config[:production]
+      "database rebuild finished"
     end
   end
 end

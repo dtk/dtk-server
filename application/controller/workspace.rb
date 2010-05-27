5c4705d0b1f523ebbfee128c368894244d23f7bf
root <root@domU-12-31-39-02-C8-F3.compute-1.internal>
2010-05-27 03:59:08 +0000

    got the js, css and image includes working

--- application/controller/workspace.rb
+++ application/controller/workspace.rb
@@ -4,14 +4,8 @@ module XYZ
     #probably only used typically for index call
     layout :workspace
 
-    #TODO: load the workspace
-    #path to reach this is /xyz/workspace
     def index
       print "reached from /xyz/workspace\n"
-      #TBD: move so only instantiated once
-      @public_js_root = R8::Config[:public_js_root]
-      @public_css_root = R8::Config[:public_css_root]
-      @public_images_root = R8::Config[:public_images_root]
     end
   end
 end

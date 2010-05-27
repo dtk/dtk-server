6de2511589c374d6373a8a1886154fc02321f3bf
root <root@domU-12-31-39-0B-5D-25.compute-1.internal>
2010-05-27 19:30:48 +0000

    work on getting admin controller to create database working

--- application/controller/admin.rb
+++ application/controller/admin.rb
@@ -6,11 +6,11 @@ module XYZ
     #TODO: figure out proper place/naming for function calls for db install/setup steps
     def dbrebuild
       #Better encapsulate where case on whetehr production set (meaning that don't need to recreate tables)
-      #associate DB with all models
-      XYZ::Model.set_db_for_all_models(DB)
+      #associate DBinstance with all models
+      XYZ::Model.set_db_for_all_models(DBinstance)
       
       #setup infra tables if they don't exist already
-      XYZ::Model.setup_infrastructure_tables?(DB)
+      XYZ::Model.setup_infrastructure_tables?(DBinstance)
       
       # create the domain-related tables if tehy don't exist already
       XYZ::Model.migrate_all_models(:up) 

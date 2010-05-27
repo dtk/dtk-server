e8c0474449855822ae59a5f0c9913d2393a5daaf
root <root@domU-12-31-39-0A-92-14.compute-1.internal>
2010-05-27 00:19:43 +0000

    got the workspace template to be called; next step is position teh jpg and js resources so they could be found and used in  workspace template

--- application/controller/workspace.rb
+++ application/controller/workspace.rb
@@ -1,14 +1,13 @@
 module XYZ
-  class WorkspaceController < Controller
-
+  class WorkspaceController < XYZ::Controller
     #have default template/layout set for workspace controller calls
     #probably only used typically for index call
     layout :workspace
 
     #TODO: load the workspace
+    #path to reach this is /xyz/workspace
     def index
-
-
+    print "reached from /xyz/workspace\n"
     end
   end
 end

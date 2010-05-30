cfcadfd00810dcfd3ba90f6295c208d4c3a04bf2
root <root@client3.example.com>
2010-05-30 23:54:36 +0000

    partial work on devtest controller

--- application/controller/devtest.rb
+++ application/controller/devtest.rb
@@ -10,7 +10,12 @@ module XYZ
       href_prefix = "http://" + http_host() + "/list" 
       c = ret_session_context_id()
       @title = uri
-      objs = Object.get_instance_or_factory(IDHandle[:c => c,:uri => uri],href_prefix,opts)
+	id_info = IDInfoTable.get_row_from_id_handle id_handle,  :raise_error => true 
+
+	#check if instance or factory
+	return get_factory(href_prefix,id_info,opts) if id_info[:is_factory]
+	get_instance(href_prefix,id_info,true,opts)      
+objs = Object.get_instance_or_factory(IDHandle[:c => c,:uri => uri],href_prefix,opts)
       return @results = objs
 require 'pp'; pp objs
 #      def get_objects_wrt_parent(relation_type,parent_id_handle,where_clause={})

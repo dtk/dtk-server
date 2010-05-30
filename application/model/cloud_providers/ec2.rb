b92e999a017d79e5b0371c78f6c14ab260df0734
root <root@domU-12-31-39-00-68-04.compute-1.internal>
2010-05-30 00:22:48 +0000

    update on ec2 node discover and update model

--- application/model/cloud_providers/ec2.rb
+++ application/model/cloud_providers/ec2.rb
@@ -7,20 +7,48 @@ module XYZ
             @@connection ||= CloudConnect::EC2.new
           end
 
-          #TBD: move higher so appliers to all cloud providers
-          def create_object(container_id_handle,vendor_attr_hash)
-            new_objs = {relation_type() => {ref(vendor_attr_hash) =>
-                {:vendor_key => vendor_key_value(vendor_attr_hash),
-                  :vendor_attributes => vendor_attr_hash}}}
-            Object.create_multiple_children_from_hash(container_id_handle,new_objs)          
+          #TBD: move higher so applies to all cloud providers
+          def sync_with_discovered(container_id_handle,vendor_attr_hash_list)
+            marked = Array.new
+            vendor_attr_hash_list.each do |vendor_attr_hash|
+              sync_with_discovered_aux(container_id_handle,vendor_attr_hash,base_attr_fn,marked)
+            end
+            delete_unmarked(container_id_handle,marked)
           end
-          def update_object(container_id_handle,vendor_attr_hash)
+
+         private
+          def sync_with_discovered_aux(container_id_handle,vendor_attr_hash,base_attr_fn,marked)
+            marked << vendor_key_value(vendor_attr_hash)
+            id_handle = find_object(container_id_handle,vendor_attr_hash)
+            if id_handle
+              update_object(id_handle,vendor_attr_hash,base_attr_fn)
+            else
+              create_object(container_id_handle,vendor_attr_hash,base_attr_fn)
+            end
+          end
+        
+          def delete_unmarked(container_id_handle,marked)
+          end
+
+          def create_object(container_id_handle,vendor_attr_hash,base_attr_fn)
+            obj = {:vendor_key => vendor_key_value(vendor_attr_hash)}
+            if should_federate()
+               obj.merge! :is_federated => true
+            else 
+               obj.merge! :vendor_attributes => vendor_attr_hash
+               obj.merge! base_attr_fn.call(vendor_attr_hash)
+            end
+            Object.create_multiple_children_from_hash(container_id_handle,
+                     {relation_type() => {ref(vendor_attr_hash) => obj}})
+          end
+          def update_object(container_id_handle,attr_hash,vendor_attr_hash)
+            return nil if should_federate()
           end
           def find_object(container_id_handle,vendor_attr_hash)
             where_clause = {:vendor_key => vendor_key_value(vendor_attr_hash)}
             Object.get_object_ids_wrt_parent(relation_type(),container_id_handle,where_clause).first
           end
-         private
+
           def vendor_key_value(vendor_attr_hash)
             unique_key_fields().map{|k|vendor_attr_hash[k]}.inspect
           end
--- application/model/cloud_providers/ec2/node.rb
+++ application/model/cloud_providers/ec2/node.rb
@@ -7,21 +7,22 @@ module XYZ
           def discover_and_update(container_id_handle,filter={})
             nodes = connection().servers_all()
             require 'pp'; pp nodes
-           nodes.each do |hash|
-             id_handle = find_object(container_id_handle,hash)
-             if id_handle
-               update_object(id_handle,hash)
-             else
-               create_object(container_id_handle,hash)
-             end
-           end
+            sync_with_discovered(container_id_handle,nodes)
           end
          private
+          #below is effectively dsl
+          def base_attr_fn
+            lambda{|v|{:node_interface => {"eth0" => {}}}}
+          end
           def unique_key_fields
             [:id]
           end
           def name_fields
             [:id]
+          end
+          #TBD: whether should federate might be more granular than just on a class level
+          def should_federate
+            false
           end
         end
       end

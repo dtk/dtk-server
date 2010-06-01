75f024383e6c91ed2d2243cf6b22e6dce6ea41be
root <root@ubuntu910-host2.(none)>
2010-06-01 13:58:50 -0700

    some notes on where data source object(s) are going

--- application/model/data_source.rb
+++ application/model/data_source.rb
@@ -1,3 +1,4 @@
+#TBD: just skeleton of more complex set of lasses; which wil factored in what, how, where: what objects to gather, how to do it and where to put them in the project
 module XYZ
   class DataSource < Model
     set_relation_name(:data_source,:data_source)
@@ -5,10 +6,10 @@ module XYZ
       def up()
         column :source_handle, :json
         column :update_policy, :varchar, :size =>25 #indicates whether inventory is updated onmanual, automatic, user_approval_for_delete, user_approval_for_add, user_approval_for_change (plus automitcally delete objects not put in thru our tool  
-        column :filter, :json
+        column :filter, :json #intended to capture the "what"
         column :polling_policy, :json
         foreign_key :polling_task_id, :task, FK_SET_NULL_OPT
-        column :objects_location, :json
+        column :objects_location, :json #intnded top capture the where such as put in project top level or in container or associate with some group or tag
         many_to_one :project
       end
       #actions

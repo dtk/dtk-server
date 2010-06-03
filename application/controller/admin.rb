08afe24417893a1512f73d2e87c6f4a2884ed773
root <root@ubuntu910-host2.(none)>
2010-06-03 14:01:18 -0700

    adding new test cases, spec routine for dbrebuild and some cleanup

--- application/controller/admin.rb
+++ application/controller/admin.rb
@@ -5,18 +5,7 @@ module XYZ
 
     #TODO: figure out proper place/naming for function calls for db install/setup steps
     def dbrebuild
-      #associate database handle DBInstance with all models
-      Model.set_db_for_all_models(DBinstance)
-
-      #setup infra tables if they don't exist already
-      Model.setup_infrastructure_tables?(DBinstance)
-      
-      # create the domain-related tables if tehy don't exist already
-      Model.migrate_all_models(:up) 
-      
-      # add the top level factorories if they don't exist
-      #has to be done after db added to class and models been added
-      IDInfoTable.add_top_factories?() unless Config[:production]
+      Model.db_rebuild(DBinstance)
       "database rebuild finished"
     end
   end
--- system/core/model/schema/migration_methods.rb
+++ system/core/model/schema/migration_methods.rb
@@ -2,6 +2,22 @@ module XYZ
   #class methods
   module MigrationMethods #methods that can be called within a migration
 
+    def db_rebuild(db)
+      #associate database handle DBInstance with all models
+      set_db_for_all_models(db)
+
+      #setup infra tables if they don't exist already
+      setup_infrastructure_tables?(db)
+      
+      # create the domain-related tables if tehy don't exist already
+      migrate_all_models(:up) 
+      
+      # add the top level factorories if they don't exist
+      #has to be done after db added to class and models been added
+      IDInfoTable.add_top_factories?() 
+
+    end
+
     def set_relation_as_top()
       @is_top = true
     end

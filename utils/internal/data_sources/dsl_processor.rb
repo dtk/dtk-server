b006961dd16584c307725182662b6c944205cc02
root <root@ubuntu910-host2.(none)>
2010-06-09 22:00:04 -0700

    refining data translation dsl

--- utils/internal/data_sources/dsl_processor.rb
+++ utils/internal/data_sources/dsl_processor.rb
@@ -1,5 +1,28 @@
+#TBD: temp for testing
+require "../auxiliary.rb"
 module XYZ
   class DSLTop
+    def self.apply(source_obj)
+      #TBD: ignoring conditions
+      test = Hash.new
+      class_rules.each do |cond,assigns|
+        assigns.each do |lhs,rhs|
+          test[lhs] = apply_to_term(rhs,source_obj) 
+        end
+      end
+      test
+    end
+
+    def self.apply_to_term(term,source_obj) 
+      if term.kind_of?(Source)
+        term.apply(source_obj)
+      elsif term.kind_of?(Function)
+        term.apply(source_obj)
+      else
+        term
+      end
+    end
+
    private
     #top level "conditionals"
     def self.if_exists(condition,&block)
@@ -48,7 +71,7 @@ module XYZ
     def self.class_rules()
       @class_rules
     end
-
+    
     class Condition
      attr_reader :relation,:condition
      def initialize(relation=:no_condition,condition=nil)
@@ -61,14 +84,23 @@ module XYZ
      end
     end
 
-    class Source < String
+    class Source 
       def initialize()
-        replace('source')
+        @path = Array.new
       end
 
       def [](a)
-        term = a.kind_of?(Symbol) ? ":#{a}" : "'#{a}'"
-        replace("#{self}[#{term}]")
+        @path << a
+        self
+      end
+
+      def apply(source_obj)
+        Aux.nested_value(source_obj,@path)
+      end
+
+      #TBD: just for debugging
+      def to_s()
+        "source[#{@path.join("][")}]"
       end
     end
     class Function
@@ -79,6 +111,15 @@ module XYZ
           @function_ref = func_name_or_def
         end
         @args = args.first
+      end
+      
+      def apply(source_obj)
+        evaluated_args = @args.map{|term|DSLTop.apply_to_term(term,source_obj)}
+        if @function_name 
+          send(@function_name,*evaluated_args)
+        elsif @function_ref
+          @function_ref.call(*evaluated_args)
+        end
       end
     end
   end
@@ -103,17 +144,19 @@ module XYZ
   class Leaf2 < DSLTop
     @class_rules ||= create_auto_vivification_hash()
     no_conditions do
-      target[:test4] = fn(:test_fn,source[:private_ip_address],1,2)
+      target[:test4] = fn(:test_fn,source["foo"][:private_ip_address],1,2)
     end
    pp class_rules
   end
-  class SampleLeafClass < DSLTop
-   no_conditions do
-     target[:test5] = fn(:test5,source[:private_ip_address],1,2)
-   end
+  class SampleLeafClass3 < DSLTop
+    @class_rules ||= create_auto_vivification_hash()
     if_exists(source[:private_ip_address]) do
-      target[:test6][:type] = "test6"
+      target[:eth0][:type] = 'ethernet' 
+      target[:eth0][:family] = 'ipv4' 
+      target[:eth0][:address] =  source[:private_ip_address] 
     end
-   pp class_rules
   end
+  pp "----------------------------"
+  source_obj = {:private_ip_address => "10.22.2.3"}
+  pp SampleLeafClass3.apply(source_obj)
 end

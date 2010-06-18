3e14c4e1cc26a6a977d57014abe311c14cd8d116
root <root@ubuntu910-host2.(none)>
2010-06-18 16:58:11 -0700

    refining  treatment of foreign keys in data translation

--- application/controller/node.rb
+++ application/controller/node.rb
@@ -15,17 +15,18 @@ module XYZ
       nodes = DB.list
 
       #render to template
-      R8.Template.set_view('node/workspace_list')
+      template_reference = R8.Template.set_view('node/workspace_list')
+    
 
       #guess these would be class definitions for Controller that would include?
       #require_js('somejsfile')
       #require_css('somecss')
 
       #assign the node list to the proper var to be populated in the template
-      R8.Template.assign(@nodeList,nodes)
+      bindings = R8.Template.assign(@nodeList,nodes)
       #process the template and assign/render the output to the 'toolbar' variable assignment
       #in parent template or bundled and returned in the JSON response
-      R8.Template.render('toolbar')
+      R8.Template.render(template_reference,bindings,'toolbar')
     end
   end
 end

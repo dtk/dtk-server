f15c12fb8ed37fa4ccf6a280424ae0e96ab32afe
unknown <Dell Studio XPS@.(none)>
2010-07-29 01:07:02 -0700

    more todo cleanup notes

--- application/model/model.rb
+++ application/model/model.rb
@@ -3,6 +3,8 @@
 #TBD: will seperate into seperate files
 #TBD: looks like much shared on relationship between Library, Project, and Deployment (they are all containers; so might refactor as one object that has type)
 
+#TODO: whatever is going on here, very unclear why ClassMixinDataSourceExtensions is def inside of Model.rb 
+
 module XYZ
   module ClassMixinDataSourceExtensions
     @@ds_class_objects ||= Hash.new
@@ -17,6 +19,7 @@ module XYZ
     end
   end
 
+#TODO: why is this called object?
   #TBD: re-examine whether current scheme is best way to implement relationship between model top, specfic model classes and the XYZ::model utility class
   class Object < Model
     extend ImportObject
@@ -39,47 +42,53 @@ module XYZ
         hash_assigns = Aux.ret_hash_assignments(hash)
         update_from_hash_assignments(id_handle,hash_assigns,opts)
       end
-  
 
       #not idempotent
       def clone(id_handle,target_id_handle,relation_type,clone_helper=nil,opts={})
         clone_helper = CloneHelper.new(@db) if no_clone_helper_provided = clone_helper.nil?
-
-	obj = get_instance_or_factory(id_handle,nil,{:depth => :deep, :no_hrefs => true})
+        obj = get_instance_or_factory(id_handle,nil,{:depth => :deep, :no_hrefs => true})
 	             
         raise Error.new("clone source (#{id_handle}) not found") if obj.nil? 
 
         tgt_factory_id_handle = get_factory_id_handle(target_id_handle,relation_type)
-	raise Error.new("clone target (#{target_id_handle}) not found") if tgt_factory_id_handle.nil?
-	new_uris = create_from_hash(tgt_factory_id_handle,obj, clone_helper,opts.merge({:shift_id_to_ancestor => true}))
-	clone_helper.set_foreign_keys_to_right_values() if no_clone_helper_provided
+
+        raise Error.new("clone target (#{target_id_handle}) not found") if tgt_factory_id_handle.nil?
+
+        new_uris = create_from_hash(tgt_factory_id_handle,obj, clone_helper,opts.merge({:shift_id_to_ancestor => true}))
+        clone_helper.set_foreign_keys_to_right_values() if no_clone_helper_provided
+
         new_uris
       end
 
       def get_contained_attribute_ids(id_handle,opts={})
-	cmps = get_objects_wrt_parent(:component,id_handle)
-	nodes = get_objects_wrt_parent(:node,id_handle)
+        cmps = get_objects_wrt_parent(:component,id_handle)
+        nodes = get_objects_wrt_parent(:node,id_handle)
         (cmps||[]).map{|cmp|cmp.get_contained_attribute_ids(opts)}.flatten() +
         (nodes||[]).map{|node|node.get_contained_attribute_ids(opts)}.flatten()
       end
 
+#TODO: this seems like generic function but specifically works with nodes?
       #type can be :asserted, :derived or :value
       def get_contained_attributes(type,id_handle,opts={})
-	ret = {}
-	cmps = get_objects_wrt_parent(:component,id_handle)
-	cmps.each{|cmp|
-	  values = cmp.get_contained_attribute_values(type,opts)
-	  if values
-	    ret[:component]||= {}
-	    ret[:component][cmp.get_qualified_ref.to_sym] = values 
+        ret = {}
+        cmps = get_objects_wrt_parent(:component,id_handle)
+
+        cmps.each{|cmp|
+          values = cmp.get_contained_attribute_values(type,opts)
+  
+          if values
+            ret[:component]||= {}
+            ret[:component][cmp.get_qualified_ref.to_sym] = values 
           end
         }
-	nodes = get_objects_wrt_parent(:node,id_handle)
-	nodes.each{|node|
-	  values = node.get_direct_attribute_values(type,opts)
-	  if values
-	    ret[:node]||= {}
-	    ret[:node][node.get_qualified_ref.to_sym] = values 
+
+        nodes = get_objects_wrt_parent(:node,id_handle)
+
+        nodes.each{|node|
+      	  values = node.get_direct_attribute_values(type,opts)
+      	  if values
+      	    ret[:node]||= {}
+      	    ret[:node][node.get_qualified_ref.to_sym] = values 
           end
         }
         ret

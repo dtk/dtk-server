2878732a522976092ce728384022e52c1f8b9bf6
unknown <Dell Studio XPS@.(none)>
2010-08-17 16:52:53 -0700

    added example display meta views for node and component

---
+++ application/meta/component/view.default.display.rb
@@ -0,0 +1,51 @@
+R8View::Views[:component] = {
+  :default => {
+    :display => {
+      :action => 'index.php',
+      :hidden_fields => [
+        {
+          :obj => {
+            :required => true,
+            :type => 'hidden',
+            :value => 'component',
+          },
+        },
+        {
+          :action => {
+            :required => true,
+            :type => 'hidden',
+            :value => 'save',
+          },
+        },
+        {
+          :id => {
+            :required => true,
+            :type => 'hidden',
+          },
+        },
+      ],
+      :field_groups => [
+        {
+          :num_cols => 1,
+          :display_labels => true,
+          :fields => [
+            {:display_name => {
+             :type => 'text',
+             :help => '',
+             :obj_link => true,
+             :obj_link_view => 'display',
+       :rows => 1,
+             :cols => 40,
+            }},
+            {:description => {
+              :type => 'text',
+              :help => '',
+        :rows => 1,
+              :cols => 40,
+            }},
+        ],
+      },
+    ],
+  }
+  }
+}

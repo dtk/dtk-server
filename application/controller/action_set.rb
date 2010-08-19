da2eca59ddd333c46727e382230cc03e52595cb6
root <root@domU-12-31-39-00-CE-42.compute-1.internal>
2010-08-19 01:12:45 +0000

    intermediate step to treat action sets

--- application/controller/action_set.rb
+++ application/controller/action_set.rb
@@ -1,17 +1,22 @@
 module XYZ
+  module ActionSet
+    Delim = '__'
+  end
+
   class ActionsetController < MainController
     def process(*route)
-      pp route
+      action_set_key = route.join("/").gsub(Regexp.new("/#{ActionSet::Delim}.*$"),"")
+      pp [:route_info,R8::Routes[:action_set][action_set_key]]
       "test"
     end
   end
-end
 
-#enter the routes defined in config into Ramaze
-(R8::Routes[:action_set]||[]).each_key do |route|
-  Ramaze::Route["/xyz/#{route}"] = lambda{ |path, request|
-    if path =~ Regexp.new("^/xyz/#{route}")
-      path.gsub(/xyz/,"xyz/actionset/process")
-    end
-  }
+  #enter the routes defined in config into Ramaze
+  (R8::Routes[:action_set]||[]).each_key do |route|
+    Ramaze::Route["/xyz/#{route}"] = lambda{ |path, request|
+      if path =~ Regexp.new("^/xyz/#{route}")
+        path.gsub(Regexp.new("^/xyz/#{route}"),"/xyz/actionset/process/#{route}/#{ActionSet::Delim}")
+      end
+    }
+  end
 end

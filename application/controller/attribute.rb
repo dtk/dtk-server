fc7f7de73c9c035b7723885f5484af536f8be432
root <root@domU-12-31-39-00-B6-27.compute-1.internal>
2010-08-25 16:50:23 +0000

    in midst of refactor for collapsing init, main and action set controllers

--- application/controller/attribute.rb
+++ application/controller/attribute.rb
@@ -15,15 +15,13 @@ module XYZ
 
     def component_display(parsed_query_string=nil)
       where_clause = parsed_query_string || ret_parsed_query_string()
-      #getting model_name by looking at self.class, (e.g., self.class can = XYZ::NodeController)
-      model_name = Aux.demodulize(self.class.to_s).gsub(/Controller$/,"").downcase
-      model_list = get_objects(model_name.to_sym,field_set,where_clause)
+      model_list = get_objects(@model_name.to_sym,field_set,where_clause)
 
       action_name = :list #TODO: automatically determine this
 
       user_context = UserContext.new #TODO: stub
-      tpl = R8Tpl::TemplateR8.new("#{model_name}/#{action_name}",user_context)
-      tpl.assign("#{model_name}_list",model_list)
+      tpl = R8Tpl::TemplateR8.new("#{@model_name}/#{action_name}",user_context)
+      tpl.assign("#{@model_name}_list",model_list)
       tpl.assign(:list_start_prev, 0)
       tpl.assign(:list_start_next, 0)
 

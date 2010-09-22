f13b54c709248c59504fb711bff16d876dae201d
unknown <Dell Studio XPS@.(none)>
2010-09-22 15:46:22 -0700

    quick tweak to cleanup js/css includes in bundle/return now that we are passing variables as pointers to the child actions calls

--- application/public/js/plugins/search.cmdhandler.js
+++ application/public/js/plugins/search.cmdhandler.js
@@ -36,226 +36,249 @@ R8.Cmdbar.cmdHandlers['search'] = {
 			var tabIndex = R8.Cmdbar.getTabIndexByName(tabName);
 			R8.Cmdbar.loadedTabs[tabIndex].clearSlider();
 		} else {
-			var newTab = R8.Cmdbar.cloneTabObj(this.tabDef);
+			var newTab = R8.Cmdbar.cloneTabObj(this.tabDefs[tabName]);
 			newTab.name = tabName;
 			newTab['i18n'] = tabI18n;
 			var tabIndex = R8.Cmdbar.addTab(newTab);
 			R8.Cmdbar.registerTabEvents(tabIndex);
-							}
+		}
 //TODO: needed registerTabEvents seperated out from addTab b/c of some weird timing issues or something, need to revisit and hopefully consolidate
-			R8.Cmdbar.changeTabFocus(tabIndex);
-			if(!R8.Cmdbar.tabsPaneOpen) R8.Cmdbar.toggleTabs();
+		R8.Cmdbar.changeTabFocus(tabIndex);
+		if(!R8.Cmdbar.tabsPaneOpen) R8.Cmdbar.toggleTabs();
 
-			for(term in qList) {
-				if(queryTerm !='') queryTerm +='&';
-					queryTerm += qList[term]['name']+'='+qList[term]['value'];
-			}
-			var tabName = this.name;
-			var renderCompleteCallback = function() {
-				R8.Cmdbar.loadedTabs[tabIndex].initSlider(R8.Cmdbar.loadedTabs[tabIndex].name);
-			}
-			var callbacks = {
-				'io:start' : R8.Cmdbar.loadedTabs[tabIndex].startSearch,
-				'io:end' : R8.Cmdbar.loadedTabs[tabIndex].endSearch,
-				'io:renderComplete' : renderCompleteCallback,
-			};
-			R8.Ctrl.call('workspace/search',queryTerm,callbacks);
-		},
+		for(term in qList) {
+			if(queryTerm !='') queryTerm +='&';
+				queryTerm += qList[term]['name']+'='+qList[term]['value'];
+		}
+		var tabName = this.name;
+		var renderCompleteCallback = function() {
+			R8.Cmdbar.loadedTabs[tabIndex].initSlider(R8.Cmdbar.loadedTabs[tabIndex].name);
+		}
+		var callbacks = {
+			'io:start' : R8.Cmdbar.loadedTabs[tabIndex].startSearch,
+			'io:end' : R8.Cmdbar.loadedTabs[tabIndex].endSearch,
+			'io:renderComplete' : renderCompleteCallback,
+		};
+		R8.Ctrl.call('workspace/search',queryTerm,callbacks);
+	},
 
-		'tabDef': {
-			'name':'',
-			'i18n':'',
-			'status':'',
-			'node':null,
-			'events':{},
-			'contentLoader': function() {
+	'tabDefs': {
+		'node-search': {
+			'name': '',
+			'i18n': '',
+			'status': '',
+			'node': null,
+			'events': {},
+			'contentLoader': function(){
 				var name = this.name;
 				var width = R8.Workspace.viewPortRegion['width'] - 65;
 				var contentFraming = '<div class="slider-top"></div>';
-				contentFraming += '<div id="'+name+'-slider-wrapper" class="slider-wrapper">';
+				contentFraming += '<div id="' + name + '-slider-wrapper" class="slider-wrapper">';
 				contentFraming += '<div id="lbutton"></div>';
-				contentFraming += '<div id="'+name+'-list-container" class="slide-list-container" style="width: '+width+'px;">';
-				contentFraming += '<div id="'+name+'-slider"></div>';
+				contentFraming += '<div id="' + name + '-list-container" class="slide-list-container" style="width: ' + width + 'px;">';
+				contentFraming += '<div id="' + name + '-slider"></div>';
 				contentFraming += '</div>';
 				contentFraming += '<div id="rbutton"></div>';
 				contentFraming += '</div>';
 				contentFraming += '<div class="slider-btm"></div>';
-				document.getElementById('cmdbar-'+this.name+'-tab-content').innerHTML = contentFraming;
-
-				var nodeId = '#'+name+'-list-container';
+				document.getElementById('cmdbar-' + this.name + '-tab-content').innerHTML = contentFraming;
+					
+				var nodeId = '#' + name + '-list-container';
 				var resizeCallback = {
-					'nodeId' : nodeId,
-					'lambda' : function(height,width) {
+					'nodeId': nodeId,
+					'lambda': function(height, width){
 						var width = width - 65;
-						return {'width':width};
+						return {
+							'width': width
+						};
 					}
 				};
 				R8.Workspace.addResizeCallback(resizeCallback);
 			},
-
-			'focus': function() {
+				
+			'focus': function(){
 				var tIndex = R8.Cmdbar.getTabIndexByName(this.name);
 
-				YUI().use("node", function(Y) {
-					R8.Cmdbar.loadedTabs[tIndex]['events']['slider_key_press'] = Y.get('document').on("keypress", function(e) {
+				YUI().use("node", function(Y){
+					R8.Cmdbar.loadedTabs[tIndex]['events']['slider_key_press'] = Y.get('document').on("keypress", function(e){
 						if (e.keyCode == 37) {
 							R8.Cmdbar.loadedTabs[tIndex].slideLeft();
-			//								e.halt();
-						} else if(e.keyCode == 39) {
+//								e.halt();
+						}
+						else if (e.keyCode == 39) {
 							R8.Cmdbar.loadedTabs[tIndex].slideRight();
-			//								e.halt();
+//								e.halt();
 						}
 					});
 				});
 			},
-
-			'blur' : function() {
+				
+			'blur': function(){
 				var tIndex = R8.Cmdbar.getTabIndexByName(this.name);
-//DEBUG
-//console.log('Blurring for:'+this.name);
-//console.log(R8.Cmdbar.loadedTabs[tIndex]['events']);
-				if(typeof(R8.Cmdbar.loadedTabs[tIndex]['events']['slider_key_press']) != 'undefined') {
+				//DEBUG
+				//console.log('Blurring for:'+this.name);
+				//console.log(R8.Cmdbar.loadedTabs[tIndex]['events']);
+				if (typeof(R8.Cmdbar.loadedTabs[tIndex]['events']['slider_key_press']) != 'undefined') {
 					R8.Cmdbar.loadedTabs[tIndex]['events']['slider_key_press'].detach();
 				}
 			},
-
-			'clearContent':function() {},
-
-			deleteCleanup : function() {
-				var nodeId = '#'+this.name+'-list-container';
+				
+			'clearContent': function(){
+			},
+				
+			deleteCleanup: function(){
+				var nodeId = '#' + this.name + '-list-container';
 				R8.Workspace.cancelResizeCallback(nodeId);
 				this.sliderAnim = null;
 				this.slideBarNode = null;
 			},
-
+				
 			//------------Search Specific Functions/Callbacks---------
-			startSearch : function(ioId,arguments) {
+			startSearch: function(ioId, arguments){
 			},
-			
-			endSearch : function(ioId,arguments) {
+				
+			endSearch: function(ioId, arguments){
 			},
-
-			clearSlider : function() {
-				if(this.slideBarNode === null) return;
-//DEBUG
-//console.log('Going to clear everything out...');
+				
+			clearSlider: function(){
+				if (this.slideBarNode === null) 
+					return;
+				//DEBUG
+				//console.log('Going to clear everything out...');
 				this.events['slider_anim'].detach();
-				delete(this.events['slider_anim']);
+				delete (this.events['slider_anim']);
 				this.events['lbtn_click'].detach();
-				delete(this.events['lbtn_click']);
+				delete (this.events['lbtn_click']);
 				this.events['rbtn_click'].detach();
-				delete(this.events['rbtn_click']);
-
-//TODO: figure out why slider_key_press throws undefined error after a 2nd search is run
+				delete (this.events['rbtn_click']);
+					
+				//TODO: figure out why slider_key_press throws undefined error after a 2nd search is run
 				this.events['slider_key_press'].detach();
 				delete (this.events['slider_key_press']);
-
+					
 				this.sliderAnim = null;
 				this.slideBarNode = null;
 				this.sliderSetup = false;
-				document.getElementById(this.name+'-list-container').innerHTML = '';
+				document.getElementById(this.name + '-list-container').innerHTML = '';
 			},
-
-			initSlider: function(tabName) {
+				
+			initSlider: function(tabName){
 				var tIndex = R8.Cmdbar.getTabIndexByName(tabName);
-				if(document.getElementById(tabName+'-slide-bar') == null) {
-					var initSliderCallback = function() { R8.Cmdbar.loadedTabs[tIndex].initSlider(tabName); }
-					setTimeout(initSliderCallback,100);
+				if (document.getElementById(tabName + '-slide-bar') == null) {
+					var initSliderCallback = function(){
+						R8.Cmdbar.loadedTabs[tIndex].initSlider(tabName);
+					}
+					setTimeout(initSliderCallback, 100);
 					return;
 				}
 				R8.Cmdbar.loadedTabs[tIndex].setupDD();
-
+					
 				YUI().use('anim', function(Y){
-					R8.Cmdbar.loadedTabs[tIndex].setupSliderAnim(Y,tIndex);
+					R8.Cmdbar.loadedTabs[tIndex].setupSliderAnim(Y, tIndex);
 				});
 				R8.Cmdbar.loadedTabs[tIndex].sliderSetup = true;
 			},
-
-			setupDD : function() {
+				
+			setupDD: function(){
 				var name = this.name;
 				var tIndex = R8.Cmdbar.getTabIndexByName(name);
-				YUI().use('dd-delegate', 'dd-proxy', 'dd-drop','dd-drop-plugin','node', function(Y){
-				R8.Cmdbar.loadedTabs[tIndex].compDDel = new Y.DD.Delegate({
-					cont: '#'+name+'-slide-bar',
-		//			nodes: 'div.component',
-					nodes: 'div.avail_item',
-//					dragMode: 'intersect',
-				});
-									
-				R8.Cmdbar.loadedTabs[tIndex].compDDel.dd.plug(Y.Plugin.DDProxy, {
-					moveOnEnd: false,
-					cloneNode: true
-				});
-
-				R8.Cmdbar.loadedTabs[tIndex].compDDel.on('drag:start', function(e){
-					var drag = this.get('dragNode'), c = this.get('currentNode');
-					drag.setAttribute('class', c.getAttribute('class'));
-					this.dd.addToGroup('workspace_drop');
-					drag.setStyles({
-						opacity: .5,
-						zIndex: 1000
+				YUI().use('dd-delegate', 'dd-proxy', 'dd-drop', 'dd-drop-plugin', 'node', function(Y){
+					R8.Cmdbar.loadedTabs[tIndex].compDDel = new Y.DD.Delegate({
+						cont: '#' + name + '-slide-bar',
+			//			nodes: 'div.component',
+						nodes: 'div.avail_item',
+	//					dragMode: 'intersect',
+					});
+						
+					R8.Cmdbar.loadedTabs[tIndex].compDDel.dd.plug(Y.Plugin.DDProxy, {
+						moveOnEnd: false,
+						cloneNode: true
+					});
+						
+					R8.Cmdbar.loadedTabs[tIndex].compDDel.on('drag:start', function(e){
+						var drag = this.get('dragNode'), c = this.get('currentNode');
+						drag.setAttribute('class', c.getAttribute('class'));
+						this.dd.addToGroup('workspace_drop');
+						drag.setStyles({
+							opacity: .5,
+							zIndex: 1000
+						});
+					});
+						
+					R8.Cmdbar.loadedTabs[tIndex].drop = Y.one('#viewspace').plug(Y.Plugin.Drop);
+					R8.Cmdbar.loadedTabs[tIndex].drop.drop.addToGroup(['workspace_drop']);
+					//TODO: come back and add in clean up of DD objects and events
+					R8.Cmdbar.loadedTabs[tIndex].compDDel.on('drag:drophit', function(e){
+						var drop = e.drop.get('node'), drag = this.get('dragNode');
+						var item_id = drag.getAttribute('data-id');
+						var model_name = drag.getAttribute('data-model-name');
+							
+						var dragChild = drag.get('children').item(0).cloneNode(true);
+						var d = new Date();
+						var new_comp_id = d.getTime();
+						dragChild.set('id', 'wi_' + new_comp_id);
+							
+						var wspaceElem = R8.Utils.Y.one('#viewspace');
+						var wspaceXY = wspaceElem.getXY();
+						var dragXY = drag.getXY();
+						var dragLeft = dragXY[0] - (wspaceXY[0]);
+						var dragTop = dragXY[1] - (wspaceXY[1]);
+						dragChild.setStyles({
+							'top': dragTop + 'px',
+							'left': dragLeft + 'px'
+						});
+						drop.append(dragChild);
+							
+						R8.Workspace.addItemToViewSpace(dragChild, dragTop, dragLeft);
 					});
 				});
-							
-				R8.Cmdbar.loadedTabs[tIndex].drop = Y.one('#viewspace').plug(Y.Plugin.Drop);
-				R8.Cmdbar.loadedTabs[tIndex].drop.drop.addToGroup(['workspace_drop']);
-//TODO: come back and add in clean up of DD objects and events
-				R8.Cmdbar.loadedTabs[tIndex].compDDel.on('drag:drophit', function(e){
-					var drop = e.drop.get('node'), drag = this.get('dragNode');
-					var item_id = drag.getAttribute('data-id');
-					var model_name = drag.getAttribute('data-model-name');
-							
-					var dragChild = drag.get('children').item(0).cloneNode(true);
-					var d = new Date();
-					var new_comp_id = d.getTime();
-					dragChild.set('id','wi_'+new_comp_id);
-							
-					var wspaceElem = R8.Utils.Y.one('#viewspace');
-					var wspaceXY = wspaceElem.getXY();
-					var dragXY = drag.getXY();
-					var dragLeft = dragXY[0] - (wspaceXY[0]);
-					var dragTop = dragXY[1] - (wspaceXY[1]);
-					dragChild.setStyles({'top':dragTop+'px','left':dragLeft+'px'});
-					drop.append(dragChild);
-
-					R8.Workspace.addItemToViewSpace(dragChild,dragTop,dragLeft);
+			},
+				
+			setupSliderAnim: function(Y, tIndex){
+				var name = R8.Cmdbar.loadedTabs[tIndex]['name'];
+				R8.Cmdbar.loadedTabs[tIndex].slideBarNode = Y.one('#' + name + '-slide-bar');
+				R8.Cmdbar.loadedTabs[tIndex].sliderAnim = new Y.Anim({
+					node: R8.Cmdbar.loadedTabs[tIndex].slideBarNode,
+					duration: 0.3,
 				});
-			});
+				//TODO: fix bug around non uniqueness on l/r buttons for scrolling
+				R8.Cmdbar.loadedTabs[tIndex]['events']['slider_anim'] = R8.Cmdbar.loadedTabs[tIndex].sliderAnim.on('end', function(){
+					R8.Cmdbar.loadedTabs[tIndex].sliderInMotion = false;
+				});
+				R8.Cmdbar.loadedTabs[tIndex]['events']['lbtn_click'] = Y.one('#lbutton').on('click', R8.Cmdbar.loadedTabs[tIndex].slideLeft);
+				R8.Cmdbar.loadedTabs[tIndex]['events']['rbtn_click'] = Y.one('#rbutton').on('click', R8.Cmdbar.loadedTabs[tIndex].slideRight);
+			},
+			slideLeft: function(e){
+				var tIndex = R8.Cmdbar.getTabIndexByName(this.name);
+				if (R8.Cmdbar.loadedTabs[tIndex].sliderInMotion) 
+					return;
+				else 
+					R8.Cmdbar.loadedTabs[tIndex].sliderInMotion = true;
+					
+				R8.Cmdbar.loadedTabs[tIndex].sliderAnim.set('to', {
+					xy: [R8.Cmdbar.loadedTabs[tIndex].slideBarNode.getX() - 510, R8.Cmdbar.loadedTabs[tIndex].slideBarNode.getY()]
+				});
+				R8.Cmdbar.loadedTabs[tIndex].sliderAnim.run();
+			},
+			slideRight: function(e){
+				var tIndex = R8.Cmdbar.getTabIndexByName(this.name);
+				if (R8.Cmdbar.loadedTabs[tIndex].sliderInMotion) 
+					return;
+				else 
+					R8.Cmdbar.loadedTabs[tIndex].sliderInMotion = true;
+				//TODO: figure out how to make the x param dynamic based on component width
+				R8.Cmdbar.loadedTabs[tIndex].sliderAnim.set('to', {
+					xy: [R8.Cmdbar.loadedTabs[tIndex].slideBarNode.getX() + 510, R8.Cmdbar.loadedTabs[tIndex].slideBarNode.getY()]
+				});
+				R8.Cmdbar.loadedTabs[tIndex].sliderAnim.run();
+			},
+				
+			slideBarNode: null,
+			sliderAnim: null,
+			sliderInMotion: false,
+			sliderSetup: false,
 		},
-
-		setupSliderAnim : function(Y,tIndex) {
-			var name = R8.Cmdbar.loadedTabs[tIndex]['name'];
-			R8.Cmdbar.loadedTabs[tIndex].slideBarNode = Y.one('#'+name+'-slide-bar');
-			R8.Cmdbar.loadedTabs[tIndex].sliderAnim = new Y.Anim({
-				node: R8.Cmdbar.loadedTabs[tIndex].slideBarNode,
-				duration: 0.3,
-			});
-//TODO: fix bug around non uniqueness on l/r buttons for scrolling
-			R8.Cmdbar.loadedTabs[tIndex]['events']['slider_anim'] = R8.Cmdbar.loadedTabs[tIndex].sliderAnim.on('end',function(){ R8.Cmdbar.loadedTabs[tIndex].sliderInMotion = false;});
-			R8.Cmdbar.loadedTabs[tIndex]['events']['lbtn_click'] = Y.one('#lbutton').on('click', R8.Cmdbar.loadedTabs[tIndex].slideLeft);
-			R8.Cmdbar.loadedTabs[tIndex]['events']['rbtn_click'] = Y.one('#rbutton').on('click', R8.Cmdbar.loadedTabs[tIndex].slideRight);
-		},
-		slideLeft : function(e) {
-			var tIndex = R8.Cmdbar.getTabIndexByName(this.name);
-			if(R8.Cmdbar.loadedTabs[tIndex].sliderInMotion) return;
-			else R8.Cmdbar.loadedTabs[tIndex].sliderInMotion = true;
-								
-			R8.Cmdbar.loadedTabs[tIndex].sliderAnim.set('to', { xy: [R8.Cmdbar.loadedTabs[tIndex].slideBarNode.getX()-510, R8.Cmdbar.loadedTabs[tIndex].slideBarNode.getY()] });
-			R8.Cmdbar.loadedTabs[tIndex].sliderAnim.run();
-		},
-		slideRight : function(e) {
-			var tIndex = R8.Cmdbar.getTabIndexByName(this.name);
-			if(R8.Cmdbar.loadedTabs[tIndex].sliderInMotion) return;
-			else R8.Cmdbar.loadedTabs[tIndex].sliderInMotion = true;
-//TODO: figure out how to make the x param dynamic based on component width
-			R8.Cmdbar.loadedTabs[tIndex].sliderAnim.set('to', { xy: [R8.Cmdbar.loadedTabs[tIndex].slideBarNode.getX()+510, R8.Cmdbar.loadedTabs[tIndex].slideBarNode.getY()] });
-			R8.Cmdbar.loadedTabs[tIndex].sliderAnim.run();
-		},
-
-		slideBarNode : null,
-		sliderAnim : null,
-		sliderInMotion : false,
-		sliderSetup : false,
-	},
-	//end tabDef
+		//end node tabDef
+	}
+	//end tabDefs
 };

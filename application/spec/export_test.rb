ec4a91a405dd0f100115082c9cc0e5d4c631e51d
root <root@domU-12-31-39-02-69-57.compute-1.internal>
2010-10-07 19:13:48 +0000

    refinement on code to import/export multiple datacenters/libraries at same time

--- application/spec/export_test.rb
+++ application/spec/export_test.rb
@@ -6,5 +6,4 @@ container_uri_list = ARGV[1]
 
 target_id_handles = container_uri_list.split(",").map{|uri|XYZ::IDHandle[:c => 2, :uri => uri]}
 opts={}
-opts.merge!({:nest => true}) if target_id_handles.size > 1
 XYZ::Object.export_objects_to_file(target_id_handles,export_file,opts)
--- utils/internal/import_export/export.rb
+++ utils/internal/import_export/export.rb
@@ -1,24 +1,26 @@
 module XYZ
   #class mixin
   module ExportObject
-    #not idempotent
     #target_id_handle_x can either be an id_handle or an array of id_handles
     def export_objects_to_file(target_id_handle_x,json_file,opts={})
-      #TODO: more efficient way of doing this since this causes fle to be read multiple times
-      if target_id_handle_x.kind_of?(Array)
-        fm = "w"
-        target_id_handle_x.each do |target_id_handle| 
-          opts_x = opts.merge({:file_open_mode => fm})
-          opts_x.merge!({:container_uri => target_id_handle[:uri]}) if opts[:nest]
-          export_objects_to_file_single_target(target_id_handle,json_file,opts_x)
-          fm = "a"
+      hash_content = 
+        if target_id_handle_x.kind_of?(Array)
+          if target_id_handle_x.size == 1
+            hash_content_for_single_target(target_id_handle_x.first,opts)
+          else
+            target_id_handle_x.inject({}) do |res,target_id_handle| 
+              hash = hash_content_for_single_target(target_id_handle,opts)
+              path = target_id_handle[:uri].gsub(Regexp.new("^/"),"").split("/")
+              res.merge!(path.reverse.inject(hash){|h,key|{key => h}}) 
+            end
+          end
+        else
+          export_objects_to_file_single_target(target_id_handle_x,json_file,opts)
         end
-      else
-        export_objects_to_file_single_target(target_id_handle_x,json_file,opts)
-      end
+      write_to_file(hash_content,json_file)
     end
 
-    def export_objects_to_file_single_target(target_id_handle,json_file,opts={})
+    def hash_content_for_single_target(target_id_handle,opts={})
       target_id_info = get_row_from_id_handle(target_id_handle)
       raise Error.new("Target given (#{target_id_handle}) does not exist") unless target_id_info
       prefix = nil
@@ -32,18 +34,16 @@ module XYZ
       objects = get_instance_or_factory(target_id_handle,nil,get_objs_opts)
 
       #stripping off "key" which would be the containing object
-      hash_content = objects.values.first
-      #nest if opts[:container_uri] set
-      if opts[:container_uri]
-        path = opts[:container_uri].gsub(Regexp.new("^/"),"").split("/")
-        hash_content = path.reverse.inject(hash_content){|h,key|{key => h}} 
-      end
+      objects.values.first
+    end
+
+    def write_to_file(hash_content,json_file)
       begin
-        f = File.open(json_file,opts[:file_open_mode]||"w")
+        f = File.open(json_file,"w")
         f.puts(JSON.pretty_generate(hash_content))
-       rescue Exception => err 
+      rescue Exception => err 
         raise Error.new("Error writing exported data to file #{json_file}: #{err}")
-       ensure
+      ensure
        	f.close
       end
     end

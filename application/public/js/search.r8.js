0ed9fdc142be96b6728763729d2b3cec5a0d97af
unknown <Dell Studio XPS@.(none)>
2010-10-18 01:56:59 -0700

    changed up search links to use js calls to submit search form, needs to be massaged more to be more generic

---
+++ application/public/js/search.r8.js
@@ -0,0 +1,20 @@
+if (!R8.Search) {
+
+	(function(R8) {
+		R8.Search = function() {
+			return {
+
+				page : function(model_name,start) {
+					var searchForm = document.getElementById(model_name+'-search-form');
+					var queryParamsElem = R8.Utils.Y.one('#query_params');
+					var query_params = {'start':start};
+
+					YUI().use("json", function(Y) {
+						queryParamsElem.set('value',Y.JSON.stringify(query_params));
+						searchForm.submit();
+					});
+				},
+			}
+		}();
+	})(R8);
+}

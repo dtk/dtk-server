63cd32daaf514f24ccca68517d57bc4252f146cf
root <root@domU-12-31-39-10-05-42.compute-1.internal>
2010-10-22 19:29:43 +0000

    added start of search pattern functionality

--- utils/internal.rb
+++ utils/internal.rb
@@ -1,10 +1,10 @@
+require File.expand_path('internal/errors', File.dirname(__FILE__))
 require File.expand_path('internal/auxiliary', File.dirname(__FILE__))
 require File.expand_path('internal/sql', File.dirname(__FILE__))
 require File.expand_path('internal/dataset_from_search_pattern', File.dirname(__FILE__))
 require File.expand_path('internal/hash_object', File.dirname(__FILE__))
 require File.expand_path('internal/array_object', File.dirname(__FILE__))
 require File.expand_path('internal/rest_uri', File.dirname(__FILE__))
-require File.expand_path('internal/errors', File.dirname(__FILE__))
 
 require File.expand_path('internal/import_export', File.dirname(__FILE__))
 #just load base classes; specific models dynamically loaded on need basis
--- utils/internal/dataset_from_search_pattern.rb
+++ utils/internal/dataset_from_search_pattern.rb
@@ -5,10 +5,12 @@ module XYZ
         sequel_ds = ret_sequel_ds_from_json(db.dataset(),json_search)
         super(model_handle,sequel_ds)
       end
+     private
+
       def ret_sequel_ds_from_json(ds,json_search)
         ret_sequel_ds_from_hash!(ds,SON.parse(json_search))
       end
-      private
+
       def ret_sequel_ds_from_hash!(ds,hash_dataset)
         ds_add = ret_sequel_ds_with_relation(ds,hash_dataset)
         return ds unless ds_add; ds = ds_add
@@ -41,15 +43,23 @@ module XYZ
           elsif col.kind_of?(Hash) and col.size = 1
             {convert_symbol(col.its_key) => convert_symbol(col.its_value)}
           else
-            raise ErrorNotImplemented.new("json col with form #{col.inspect}") 
+            raise ErrorPatternNotImplemented.new(:column,col)
           end
         end
         ds.select(*sequel_cols)
       end
 
       def ret_sequel_ds_with_filters(ds,hash_dataset)
-        filter = find(:filters,hash_dataset)
-        return nil unless filter
+        filters = find(:filters,hash_dataset)
+        return ds unless filter
+        #TODO: just treating some subset of patterns
+        sequel_where_clause =
+          if filters.kind_of?(Hash)
+            filters.inject({}){|h.kv|h.merge(convert_symbol(kv[0]) => convert_symbol(kv[1]))}
+          else
+            raise ErrorPatternNotImplemented.new(:filters,filters)
+          end
+        ds.where(sequel_where_clause)
       end
 
       def find(type,hash_dataset)
@@ -70,6 +80,14 @@ module XYZ
       def convert_symbol(term_in_json)
         return term_in_json if term_in_json.kind_of?(Symbol)
         term_in_json =~ /^:/ ? term_in_json[1..term_in_json.size-1].to_sym : term_in_json 
+      end
+
+      class ErrorParsing < Error
+      end
+      class ErrorPatternNotImplemented < ErrorNotImplemented
+        def initialize(type,obj)
+          super("parsing item #{type} is not supported; it has form: #{object.inspect}")
+        end
       end
     end
   end

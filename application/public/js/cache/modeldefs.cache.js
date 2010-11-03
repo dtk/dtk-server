692b2506813bc328b4426c815a020263c00bbfd4
root <root@domU-12-31-39-02-BD-D2.compute-1.internal>
2010-11-03 00:26:18 +0000

    example hard coded modeldef cache

---
+++ application/public/js/cache/modeldefs.cache.js
@@ -0,0 +1,24 @@
+R8.Model.fieldDefs =  {
+    'node' : {
+	'field_defs' :{
+	    'display_name': {
+		'i18n': 'Display Name',
+		'type' : 'text',
+	    },
+	    'operational_status': {
+		'i18n':'Operational Status',
+		'type' : 'select',
+	    },
+	    'external_ref': {
+		'i18n':'External Referance',
+		'type' : 'text',
+	    },
+	    'image_size' : {
+		'i18n' : 'Image Size',
+		'type' : 'text'
+	    }
+	},
+    }
+}
+    
+

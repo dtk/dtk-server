b8a34f4bc733caa457c18249960b368c4a2fd269
unknown <Dell Studio XPS@.(none)>
2010-11-09 11:32:55 -0800

    added base logging config in application/start.rb

--- application/start.rb
+++ application/start.rb
@@ -5,7 +5,8 @@
 
 require File.expand_path('app', File.dirname(__FILE__))
 
+rotating_logger = Logger.new('r8server.log', 'daily')
+Ramaze::Log.loggers = [rotating_logger]
+Ramaze::Log.level = Logger::WARN
+
 Ramaze.start(:adapter => :thin, :port => 7000, :file => __FILE__)
-
-
-

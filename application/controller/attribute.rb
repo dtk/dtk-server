7f886f0da4d473305fa8631fcbd270e6ab023276
root <root@domU-12-31-39-0B-1C-F6.compute-1.internal>
2010-11-18 05:28:08 +0000

    default.test_node_level_edit.rtpl

--- application/controller/attribute.rb
+++ application/controller/attribute.rb
@@ -23,6 +23,22 @@ pp get_base_object_dataset_needs_to_be_set(:component).ppsql
       tpl.assign("attribute_list",attribute_list)
       return {:content => tpl.render()}
     end
+
+    def edit_under_node(node_id=nil)
+      filter = nil
+      cols = [:id,:display_name,:base_object_node,:needs_to_be_set,:value_actual,:value_derived,:data_type,:semantic_type]
+      field_set = Model::FieldSet.new(model_name,cols)
+      ds = SearchObject.create_from_field_set(field_set,ret_session_context_id(),filter).create_dataset()
+      ds = ds.where(:param_node_id => node_id.to_i) if node_id
+
+      raw_attribute_list = ds.all
+      attribute_list = AttributeComplexType.flatten_attribute_list(raw_attribute_list)
+
+      action_name = "test_node_level_edit"
+      tpl = R8Tpl::TemplateR8.new("#{model_name()}/#{action_name}",user_context())
+      tpl.assign("attribute_list",attribute_list)
+      return {:content => tpl.render()}
+    end
    
     
     def list_under_datacenter(datacenter_id=nil)

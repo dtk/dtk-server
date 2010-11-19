6320f90e6be6081af0b8ccf8b077339b2524e228
root <root@domU-12-31-39-02-21-02.compute-1.internal>
2010-11-19 03:43:40 +0000

    update

--- application/spec/chef/site-cookbooks/redis/metadata.rb
+++ application/spec/chef/site-cookbooks/redis/metadata.rb
@@ -5,6 +5,11 @@ license          "Apache 2.0"
 description      "Installs/Configures redis"
 recipe            "redis", "Installs/Configures redis"
 version           "0.0.1"
+###TODO may put in seprate file
+attribute "_meta_info",
+  :basic_types => {
+    "redis" => "service" 
+   }
 
 require File.expand_path('metadata_aux.rb',::Chef::Config[:cucumber_path]);__t(__FILE__,self)
 

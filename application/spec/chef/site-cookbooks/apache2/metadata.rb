dd538270fed4e26aa85b9692388aedee9896481f
root <root@domU-12-31-39-0B-0D-02.compute-1.internal>
2010-12-03 18:25:12 +0000

    update to apache2 component

--- application/spec/chef/site-cookbooks/apache2/metadata.rb
+++ application/spec/chef/site-cookbooks/apache2/metadata.rb
@@ -47,153 +47,153 @@ recipe            "apache2::mod_status", "Apache module 'status' with config fil
   supports os
 end
 
-attribute "apache",
+attribute "apache2",
   :display_name => "Apache Hash",
   :description => "Hash of Apache attributes",
   :type => "hash"
 
-attribute "apache/dir",
+attribute "apache2/dir",
   :display_name => "Apache Directory",
   :description => "Location for Apache configuration",
   :default => "/etc/apache2"
 
-attribute "apache/log_dir",
+attribute "apache2/log_dir",
   :display_name => "Apache Log Directory",
   :description => "Location for Apache logs",
   :default => "/etc/apache2"
 
-attribute "apache/user",
+attribute "apache2/user",
   :display_name => "Apache User",
   :description => "User Apache runs as",
   :default => "www-data"
 
-attribute "apache/binary",
+attribute "apache2/binary",
   :display_name => "Apache Binary",
   :description => "Apache server daemon program",
   :default => "/usr/sbin/apache2"
 
-attribute "apache/icondir", 
+attribute "apache2/icondir", 
   :display_name => "Apache Icondir",
   :description => "Directory location for icons",
   :default => "/usr/share/apache2/icons"
 
-attribute "apache/listen_ports",
+attribute "apache2/listen_ports",
   :display_name => "Apache Listen Ports",
   :description => "Ports that Apache should listen on",
   :type => "array",
   :default => [ "80", "443" ]
 
-attribute "apache/contact",
+attribute "apache2/contact",
   :display_name => "Apache Contact",
   :description => "Email address of webmaster",
   :default => "ops@example.com"
 
-attribute "apache/timeout",
+attribute "apache2/timeout",
   :display_name => "Apache Timeout",
   :description => "Connection timeout value",
   :default => "300"
 
-attribute "apache/keepalive",
+attribute "apache2/keepalive",
   :display_name => "Apache Keepalive",
   :description => "HTTP persistent connections",
   :default => "On"
 
-attribute "apache/keepaliverequests",
+attribute "apache2/keepaliverequests",
   :display_name => "Apache Keepalive Requests",
   :description => "Number of requests allowed on a persistent connection",
   :default => "100"
 
-attribute "apache/keepalivetimeout",
+attribute "apache2/keepalivetimeout",
   :display_name => "Apache Keepalive Timeout",
   :description => "Time to wait for requests on persistent connection",
   :default => "5"
 
-attribute "apache/servertokens",
+attribute "apache2/servertokens",
   :display_name => "Apache Server Tokens",
   :description => "Server response header",
   :default => "Prod"
 
-attribute "apache/serversignature",
+attribute "apache2/serversignature",
   :display_name => "Apache Server Signature",
   :description => "Configure footer on server-generated documents",
   :default => "On"
 =begin
-attribute "apache/traceenable",
+attribute "apache2/traceenable",
   :display_name => "Apache Trace Enable",
   :description => "Determine behavior of TRACE requests",
   :default => "On"
 
-attribute "apache/allowed_openids",
+attribute "apache2/allowed_openids",
   :display_name => "Apache Allowed OpenIDs",
   :description => "Array of OpenIDs allowed to authenticate",
   :default => ""
 
-attribute "apache/prefork",
+attribute "apache2/prefork",
   :display_name => "Apache Prefork",
   :description => "Hash of Apache prefork tuning attributes.",
   :type => "hash"
 
-attribute "apache/prefork/startservers",
+attribute "apache2/prefork/startservers",
   :display_name => "Apache Prefork MPM StartServers",
   :description => "Number of MPM servers to start",
   :default => "16"
 
-attribute "apache/prefork/minspareservers",
+attribute "apache2/prefork/minspareservers",
   :display_name => "Apache Prefork MPM MinSpareServers",
   :description => "Minimum number of spare server processes",
   :default => "16"
 
-attribute "apache/prefork/maxspareservers",
+attribute "apache2/prefork/maxspareservers",
   :display_name => "Apache Prefork MPM MaxSpareServers",
   :description => "Maximum number of spare server processes",
   :default => "32"
 
-attribute "apache/prefork/serverlimit",
+attribute "apache2/prefork/serverlimit",
   :display_name => "Apache Prefork MPM ServerLimit",
   :description => "Upper limit on configurable server processes",
   :default => "400"
 
-attribute "apache/prefork/maxclients",
+attribute "apache2/prefork/maxclients",
   :display_name => "Apache Prefork MPM MaxClients",
   :description => "Maximum number of simultaneous connections",
   :default => "400"
 
-attribute "apache/prefork/maxrequestsperchild",
+attribute "apache2/prefork/maxrequestsperchild",
   :display_name => "Apache Prefork MPM MaxRequestsPerChild",
   :description => "Maximum number of request a child process will handle",
   :default => "10000"
 
-attribute "apache/worker",
+attribute "apache2/worker",
   :display_name => "Apache Worker",
   :description => "Hash of Apache prefork tuning attributes.",
   :type => "hash"
 
-attribute "apache/worker/startservers",
+attribute "apache2/worker/startservers",
   :display_name => "Apache Worker MPM StartServers",
   :description => "Initial number of server processes to start",
   :default => "4"
 
-attribute "apache/worker/maxclients",
+attribute "apache2/worker/maxclients",
   :display_name => "Apache Worker MPM MaxClients",
   :description => "Maximum number of simultaneous connections",
   :default => "1024"
 
-attribute "apache/worker/minsparethreads",
+attribute "apache2/worker/minsparethreads",
   :display_name => "Apache Worker MPM MinSpareThreads",
   :description => "Minimum number of spare worker threads",
   :default => "64"
 
-attribute "apache/worker/maxsparethreads",
+attribute "apache2/worker/maxsparethreads",
   :display_name => "Apache Worker MPM MaxSpareThreads",
   :description => "Maximum number of spare worker threads",
   :default => "192"
 
-attribute "apache/worker/threadsperchild",
+attribute "apache2/worker/threadsperchild",
   :display_name => "Apache Worker MPM ThreadsPerChild",
   :description => "Constant number of worker threads in each server process",
   :default => "64"
 
-attribute "apache/worker/maxrequestsperchild",
+attribute "apache2/worker/maxrequestsperchild",
   :display_name => "Apache Worker MPM MaxRequestsPerChild",
   :description => "Maximum number of request a child process will handle",
   :default => "0"

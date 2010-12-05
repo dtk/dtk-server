a4321424784a405d34651afdf64abb911531ef3a
root <root@domU-12-31-39-02-68-57.compute-1.internal>
2010-12-05 20:40:06 +0000

    update to wordpress chef meta info

--- application/spec/chef/site-cookbooks/wordpress/service_binding.rb
+++ application/spec/chef/site-cookbooks/wordpress/service_binding.rb
@@ -4,6 +4,12 @@ service :wordpress do
     :required => true,
     :type => "hash",
     :description => "wordpress mysql service access point reference for client",
-    :semantic_type => {"sap_ref" => {"application" => {"type" => "sql::mysql"}}}
-
+    :semantic_type => {"sap_ref" => {"application" => {"type" => "sql::mysql"}}},
+    :transform => {
+      "application" => {
+        "username" => {"__ref" =>  "node[wordpress][db][user]"},
+        "database" => {"__ref" =>  "node[wordpress][db][database]"},
+        "password" => {"__ref" =>  "node[wordpress][db][password]"}
+    }
+  }
 end

5adddf3ce7eda0a2bf293ec6740343594cfc6847
root <root@domU-12-31-39-04-72-21.compute-1.internal>
2010-12-08 23:44:40 +0000

    get_under_context_list and get_under_context

--- application/controller/attribute_link.rb
+++ application/controller/attribute_link.rb
@@ -36,7 +36,45 @@ module XYZ
     end
 
     def list_on_node_ports(node_id=nil)
-      filter = node_id ? [:and, [:eq, :id, node_id.to_i]] : nil
+      aux_list_on_node_ports(node_id ? [node_id] : nil)
+    end
+    def get_under_context_list(explicit_hash=nil)
+      hash = explicit_hash || request.params
+=begin
+      Params
+            -<query filters>
+            -context_list[
+                          {'id':9493llskc393,'model':'node'},
+                          {'id':3448dkwkr5,'model':'component'}
+              ]
+=end
+      #TODO: ignoring query filters no
+      context_list = hash["context_list"]
+      
+      raise Error.new("did not get context_list") unless context_list
+      #TODO: only handling nodes
+      ids = context_list.reject{|x|not x["model"] == "node"}.map{|y|y["id"] ? y["id"].to_i : nil}.compact
+      raise Error.new("no valid ids given") if ids.empty?
+      aux_list_on_node_ports(ids)
+    end
+
+    def get_under_context(explicit_hash=nil)
+      hash = explicit_hash || request.params
+=begin
+2) Get all links from a single object: <base uri>/attribute_link/get_under_context
+      Params
+            -<query filters>
+            -id = 9493llskc393
+            -model = node
+=end
+      raise Error.new("id not given") unless hash["id"]
+      raise Error.new("only node type treated at this time") unless hash["model"] == "node"
+      aux_list_on_node_ports([hash["id"].to_i])
+    end
+
+    #TODO: temp
+    def aux_list_on_node_ports(node_ids)
+      filter = node_ids ? [:and, [:oneof, :id, node_ids]] : nil
       cols = [:id,:display_name,:port_links]
       field_set = Model::FieldSet.new(:node,cols)
       ds = SearchObject.create_from_field_set(field_set,ret_session_context_id(),filter).create_dataset()
@@ -50,18 +88,20 @@ module XYZ
         type = (el[:attribute_link]||{})[:type]||(el[:attribute_link2]||{})[:type]
         hidden = (el[:attribute_link]||{})[:hidden].nil? ? (el[:attribute_link2]||{})[:hidden] : (el[:attribute_link]||{})[:hidden]
         other_end_id = (el[:attribute_link]||{})[:other_end_output_id]||(el[:attribute_link2]||{})[:other_end_input_id]
+        port_dir = el[:attribute_link] ? "input" : "output"
         link_list << {
           :node_id => el[:id],
           :node_name => el[:display_name],
           :port_id => el[:attribute][:id],
           :port_name => port_name,
           :type => type,
+          :port_dir => port_dir,
           :hidden => hidden,
           :other_end_id => other_end_id
         }
       end
-
-      tpl = R8Tpl::TemplateR8.new("#{model_name()}/#{default_action_name()}",user_context())
+      action_name = "list_on_node_ports"
+      tpl = R8Tpl::TemplateR8.new("#{model_name()}/#{action_name}",user_context())
       tpl.assign("link_list",link_list)
       return {:content => tpl.render()}
     end

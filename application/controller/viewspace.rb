ee4a19af77f2615b07aca2688c837f8460f3b3fc
root <root@domU-12-31-39-02-65-A2.compute-1.internal>
2010-12-10 21:59:36 +0000

    slight change to viewsapce controller

--- application/controller/viewspace.rb
+++ application/controller/viewspace.rb
@@ -3,7 +3,8 @@ module XYZ
 
     def update_pos(id)
 pp 'Updating items positions in viewspace:'
-pp request.params('items')
+#pp request.params('items')
+pp request.params
 p '==================================================='
       return {}
     end

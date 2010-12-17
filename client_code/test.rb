ed4184934065c686eba61622814c87319c2a8bf1
root <root@domU-12-31-39-06-BD-33.compute-1.internal>
2010-12-17 22:25:17 +0000

    working on client (node) code

--- client_code/test.rb
+++ client_code/test.rb
@@ -4,7 +4,7 @@ require 'mcollective'
 include MCollective::RPC
   
 mc = rpcclient("chef_client")
-msg_content = {:run_list => ["recipe[ser_account]"]}
+msg_content = {:run_list => ["recipe[user_account]"]}
 printrpc mc.run(msg_content)
  
 printrpcstats

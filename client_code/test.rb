3e521ba7b613b9dca9d0eb7675cd75fcf3dd0387
root <root@domU-12-31-39-06-BD-33.compute-1.internal>
2010-12-17 22:20:57 +0000

    working on client (node) code

---
+++ client_code/test.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'mcollective'
+include MCollective::RPC
+  
+mc = rpcclient("chef_client")
+msg_content = {:run_list => ["recipe[user_account]"]}
+printrpc mc.run(msg_content)
+ 
+printrpcstats
+ 
+mc.disconnect

9990f9101e1bce9a1d1992bcb5375cd5876ab86a
root <root@domU-12-31-39-00-C0-F5.compute-1.internal>
2010-12-28 08:04:46 +0000

    dynamic loadf of adapters for workflow and command-and-control

--- utils/internal/command_and_control.rb
+++ utils/internal/command_and_control.rb
@@ -5,7 +5,7 @@ module XYZ
       begin
         type = R8::Config[:command_and_control][:type]
         require File.expand_path("#{UTILS_DIR}/internal/command_and_control/adapters/#{type}", File.dirname(__FILE__))
-        klass = XYZ.const_get type.capitalize
+        klass = XYZ::CommandAndControlAdapter.const_get type.capitalize
        rescue LoadError
         Log.error("cannot find command_and_control adapter; loading null command_and_control class")
       end
@@ -18,4 +18,6 @@ module XYZ
      def initialize()
      end
   end
+   module CommandAndControlAdapter
+   end
 end
--- utils/internal/command_and_control/adapters/mcollective.rb
+++ utils/internal/command_and_control/adapters/mcollective.rb
@@ -2,24 +2,25 @@ require 'mcollective'
 include MCollective::RPC
 
 module XYZ
-  class Mcollective < CommandAndControl
-    #TODO: should take parameter, which includes whether client is chef or puppet
-    def dispatch_to_client() 
-      mc = rpcclient("chef_client",:options => Options)
-      msg_content = {:run_list => ["recipe[user_account]"]}
-      results =  mc.run(msg_content)
-
-      data = results.map{|result|result.results[:data]} 
-      mc.disconnect
-      data 
+  module CommandAndControlAdapter
+    class Mcollective < CommandAndControl
+      #TODO: should take parameter, which includes whether client is chef or puppet
+      def dispatch_to_client() 
+        mc = rpcclient("chef_client",:options => Options)
+        msg_content = {:run_list => ["recipe[user_account]"]}
+        results =  mc.run(msg_content)
+        
+        data = results.map{|result|result.results[:data]} 
+        mc.disconnect
+        data 
+      end
+     private
+      Options = {
+        :disctimeout=>2,
+        :config=>"/etc/mcollective/client.cfg",
+        :filter=>{"identity"=>[], "fact"=>[], "agent"=>[], "cf_class"=>[]},
+        :timeout=>500000000
+      }  
     end
- private
-    Options = {
-      :disctimeout=>2,
-      :config=>"/etc/mcollective/client.cfg",
-      :filter=>{"identity"=>[], "fact"=>[], "agent"=>[], "cf_class"=>[]},
-      :timeout=>500000000
-    }  
-
   end
 end

98d0ff4a0be8082f6203ce56a232fe07c09097ce
root <root@domU-12-31-39-00-A1-06.compute-1.internal>
2010-12-29 18:39:45 +0000

    stub routine totake action list and group by node and set order

--- application/model/ordered_actions.rb
+++ application/model/ordered_actions.rb
@@ -1,38 +1,64 @@
 #TODO: should this be in model dircetory?
 module XYZ
-  class OrderedActions < Array
+  class OrderedActions 
     def self.create(action_list)
-      self.new(action_list)
+      obj=self.new()
+      obj.set_top_level(action_list)
+      obj
     end
     def is_single_action?()
-      first == :single_action
+      @type == :single_action
     end
     def is_concurrent?()
-      first == :concurrent
+      @type == :concurrent
     end
     def is_sequential?()
-      first == :sequential
+      @type == :sequential
     end
     
     def single_action()
-      self[1]
+      elements.first
     end
     def elements()
-      self[1..size-1]
+      @elements
+    end
+
+    #### 'private' methods fro 'this'
+    def set_top_level(action_list)
+      if action_list.size == 1
+        @type = :single_action
+        @elements = action_list
+        return self
+      end
+      actions_by_node = group_by_node(action_list)
+      #TODO: stub where all actions cross-node are concurrent
+      @type = :concurrent
+      @elements = actions_by_node
     end
 
    private
-    def initialize(action_list)
-      #TODO: need to group by nodes
-        #TODO stub
-      if action_list.size == 1
-        replace([:single_action,action_list.first])
-      elsif Action.actions_are_concurrent?(action_list)
-        replace([:concurrent] + action_list)
-      else
-        replace([:sequential] + action_list)
+    attr_writer :type, :elements
+    def initialize()
+      @type = nil
+      @elements = nil
+    end
+
+    def group_by_node(action_list)
+      node_ids = action_list.map{|a|a[:node][:id]}.uniq
+      node_ids.map do |node_id| 
+        order_actions_in_node(action_list.reject{|a|not a[:node][:id] == node_id}) 
       end
-      self
+    end
+
+    def order_actions_in_node(action_list)
+      if action_list.size == 1
+        return action_list.first
+      end
+      #TODO: stub to determine appropriate sequential order on node
+      obj = self.new()
+      obj.type = :sequential
+      obj.elements = action_list
+      obj
     end
   end
 end

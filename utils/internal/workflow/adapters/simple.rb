0bd5ca06035265efa8c9f197c5fb1664fded1794
root <root@domU-12-31-39-00-A1-06.compute-1.internal>
2010-12-29 17:54:37 +0000

    put mutex in simple workflw adapter

--- utils/internal/workflow/adapters/simple.rb
+++ utils/internal/workflow/adapters/simple.rb
@@ -2,12 +2,16 @@ module XYZ
   module WorkflowAdapter
     class Simple < XYZ::Workflow
       def execute()
-        results = Array.new
+        results = Hash.new
         if @type == :sequential
           @elements.each{|action| results << execute_on_node(action)}
         elsif @type == :concurrent
-          #TODO: do we need mutex for setting value on results
-          threads = @elements.map{|action| Thread.new{results << execute_on_node(action)}}
+          threads = @elements.map do |action| 
+            Thread.new do 
+              result = execute_on_node(action)
+              @lock.synchronize{results[action[:id]] = result}
+            end
+          end
           threads.each{|t| t.join}
         end
         pp [:results, results]
@@ -23,8 +27,9 @@ module XYZ
       end
 
       def initialize(ordered_actions)
-       @type = ordered_actions.is_concurrent?() ? :concurrent : :sequential
-       @elements = ordered_actions.is_single_action?() ? [ordered_actions.single_action()] : ordered_actions.elements
+        @type = ordered_actions.is_concurrent?() ? :concurrent : :sequential
+        @elements = ordered_actions.is_single_action?() ? [ordered_actions.single_action()] : ordered_actions.elements
+        @lock = Mutex.new
         #TODO: put in max threads
      end
     end

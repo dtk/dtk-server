dc6ec4dcc682124f4d345c14386229ef4e475334
root <root@domU-12-31-39-02-5C-A4.compute-1.internal>
2010-12-30 17:42:48 +0000

    fixed up concurrent requests

--- utils/internal/command_and_control/adapters/mcollective.rb
+++ utils/internal/command_and_control/adapters/mcollective.rb
@@ -1,30 +1,51 @@
 require 'mcollective'
 include MCollective::RPC
-
 module XYZ
   module CommandAndControlAdapter
     class Mcollective < CommandAndControl
+      def initialize()  
+        @mc = rpcclient("chef_client",:options => Options)
+      end
       def dispatch_to_client(action) 
-        opts = Options.merge(:filter => Filter.merge("fact"=>[{:value=>pbuilderid(action), :fact=>"pbuilderid"}]))
-        mc = rpcclient("chef_client",:options => opts)
+        identity = mcollective_id(action[:node])
+        unless identity
+          Log.error("cannot find identity for node #{action[:node].inspect}")
+          return nil
+        end
         msg_content = {:run_list => ["recipe[user_account]"]}
-        results = mc.run(msg_content)
+        filter = {"identity" => [identity], "agent" => ["chef_client"]}
+        results = @mc.custom_request("run",msg_content,identity,filter)
         data = results.map{|result|result.results[:data]} 
-        #TODO: where do we do mc.disconnect
+        #TODO: where do we do @mc.disconnect; since @mcs shaer connection cannot do it here
         data 
       end
      private
-      #TODO: adapters for chef, puppet etc on payload
-      def pbuilderid(action)
-        action[:node][:external_ref][:instance_id]
+
+      def mcollective_id(node)
+        return DiscoveredNodes[node[:id]] if DiscoveredNodes[node[:id]]
+        identity = discover_mcollective_id(node)
+        Lock.synchronize{DiscoveredNodes[node[:id]] = identity}
+        identity
       end
+      def discover_mcollective_id(node)
+        filter = Filter.merge("fact" => [{:fact=>"pbuilderid", :value=>pbuilderid(node)}])
+        @mc.client.discover(filter,Options[:disctimeout]).first
+      end
+
+      #TODO: adapters for chef, puppet etc on payload
+      def pbuilderid(node)
+        node[:external_ref][:instance_id]
+      end
+
       Filter = {"identity"=>[], "fact"=>[], "agent"=>[], "cf_class"=>[]}
       Options = {
-        :disctimeout=>4,
+        :disctimeout=>2,
         :config=>"/etc/mcollective/client.cfg",
         :filter=> Filter,
         :timeout=>200
       }  
+      DiscoveredNodes = Hash.new
+      Lock = Mutex.new
    end
   end
 end

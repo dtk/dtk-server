bdd0752f38844d4b662571fa5716e69a5d024f8a
root <root@domU-12-31-39-14-F0-C9.compute-1.internal>
2010-12-31 21:21:22 +0000

    chef config agent parsing

--- utils/internal/config_agent/adapters/chef.rb
+++ utils/internal/config_agent/adapters/chef.rb
@@ -32,22 +32,22 @@ end
         def initialize()
           @recipes = Array.new
           @common_attr_index = Hash.new
-          @attributes = Array.new
+          @attributes = Hash.new
         end
         def add_action(action)
-          component_id = action[:component][:id]
-          if @common_attr_index[component_id]
-            common_attr_val_list = @common_attr_index[component_id]
-            common_attr_val_list << attributes(action)
+          recipe_name = recipe(action)
+          if @common_attr_index[recipe_name]
+            common_attr_val_list = @common_attr_index[recipe_name]
+            common_attr_val_list << attributes(action, :strip_off_recipe_name => true)
           elsif action[:component][:only_one_per_node]
-            @recipes << recipe(action)
-            @attributes += attributes(action)
+            @recipes << recipe_name
+            @attributes.merge!(attributes(action))
           else
-            @recipes << recipe(action)
+            @recipes << recipe_name
             list = Array.new
-            @common_attr_index[component_id] = list
-            list << attributes(action)
-            @attributes << {:list => list}
+            @common_attr_index[recipe_name] = list
+            list << attributes(action, :strip_off_recipe_name => true)
+            @attributes.merge!(recipe_name => {"list" => list})
           end
           self
         end
@@ -55,13 +55,22 @@ end
         def recipe(action)
           ((action[:component]||{})[:external_ref]||{})[:recipe_name]
         end
-        def attributes(action)
-          (action[:attributes]||[]). map do |attr|
-            key = (attr[:external_ref]||{})[:path]
+        def attributes(action,opts={})
+          ret = Hash.new
+          (action[:attributes]||[]).each do |attr|
+            var_name_path = (attr[:external_ref]||{})[:path]
             val = attr[:attribute_value]
             #TODO: after testing remove nils
-            {key => val} if key
+            ret.merge!(attribute_name(var_name_path,opts) => val) if var_name_path
           end.compact
+          ret
+        end
+        def attribute_name(var_name_path,opts={})
+          #TODO: assume form is node[recipe][x1] or node[recipe][x1][x2] or ..
+          reverse_keys = var_name_path.gsub(/^node\[/,"").gsub(/\]$/,"").split("][").reverse
+          reverse_keys.pop if opts[:strip_off_recipe_name]
+          f = reverse_keys.shift
+          reverse_keys.inject(f){|h,k|{k => h}}
         end
       end
     end

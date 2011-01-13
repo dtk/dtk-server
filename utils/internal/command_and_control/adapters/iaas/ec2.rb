e6daed06e70e6807f5f3817b4b26fa4896f069ba
root <root@domU-12-31-39-14-F4-DD.compute-1.internal>
2011-01-13 04:26:00 +0000

    working on using persisted tasks

--- utils/internal/command_and_control/adapters/iaas/ec2.rb
+++ utils/internal/command_and_control/adapters/iaas/ec2.rb
@@ -2,28 +2,38 @@ module XYZ
   module CommandAndControlAdapter
     class Ec2 < CommandAndControlIAAS
       def self.execute(create_node)
-        ami = ((create_node[:image]||{})[:external_ref]||{})[:image_id]
-        raise ErrorCannotCreateNode.new unless ami
-        create_options = {:image_id => ami}
-        #TODO: right now hardcoding size and groups
-        create_options.merge!(:flavor_id => "t1.micro",:groups => ["basic"])
-        response = @@conn.server_create(create_options)
-        instance_id = response[:id]
-        state = response[:state]
-        external_ref = {
-          :instance_id => instance_id,
-          :type => "ec2_instance"
-        }
-        Log.info("node created with instance id #{instance_id}; waiting for it to be avaialble")
-        pp [:node_created,response]
-        create_node[:node].merge!(:external_ref => external_ref)
-        create_node.save_new_node_info()
-
+        #handle case where node has been created already (and eeror mayu have been time out waiting for node to be up
+        instance_id = ((create_node[:node]||{})[:external_ref]||{})[:nstance_id]
+        if instance_id.nil?
+          ami = ((create_node[:image]||{})[:external_ref]||{})[:image_id]
+          raise ErrorCannotCreateNode.new unless ami
+          create_options = {:image_id => ami}
+          #TODO: right now hardcoding size and groups
+          create_options.merge!(:flavor_id => "t1.micro",:groups => ["basic"])
+          response = @@conn.server_create(create_options)
+          instance_id = response[:id]
+          state = response[:state]
+          external_ref = {
+            :instance_id => instance_id,
+            :type => "ec2_instance"
+          }
+          Log.info("node created with instance id #{instance_id}; waiting for it to be avaialble")
+          pp [:node_created,response]
+          create_node[:node].merge!(:external_ref => external_ref)
+          create_node.save_new_node_info()
+        else
+          Log.info("node already created with instance id #{instance_id}; waiting for it to be avaialble")
+        end
         CommandAndControl.wait_for_node_to_be_ready(create_node[:node])
         Log.info("node #{instance_id} is available")
 
         {:status => "succeeded",
-          :node => {:external_ref => external_ref}
+          :node => {
+            :external_ref => {
+              :instance_id => instance_id,
+              :type => "ec2_instance"
+            }
+          }
         }
       end
      private

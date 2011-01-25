1d6341cf09cf61ecc12a45f7946122d61602bb3a
root <root@domU-12-31-39-09-26-43.compute-1.internal>
2011-01-25 19:58:21 +0000

    working on mocking

--- application/config/config.rb
+++ application/config/config.rb
@@ -78,7 +78,7 @@ R8::Config[:command_and_control][:node_config][:type] = "mcollective"
 R8::Config[:command_and_control][:iaas] = Hash.new
 #TODO: put in provisions to have multiple iias providers at same time
 #R8::Config[:command_and_control][:iaas][:type] = "ec2__mock" 
-R8::Config[:command_and_control][:iaas][:type] = "ec2" 
+R8::Config[:command_and_control][:iaas][:type] = "ec2__mock" 
 
 #optional timer plug
 #R8::Config[:timer] = Hash.new
--- utils/internal/command_and_control/adapters/iaas/ec2__mock.rb
+++ utils/internal/command_and_control/adapters/iaas/ec2__mock.rb
@@ -3,10 +3,6 @@ module XYZ
   module CommandAndControlAdapter
     class Ec2__mock < Ec2
      private
-      #TODO: may put this in Config file
-      CacheFile = "#{R8::Config[:sys_root_path]}/cache/ec2_mock.json"
-      @@cache_contents = nil
-
       def self.wait_for_node_to_be_ready(node)
         sleep(5)
       end
@@ -14,57 +10,60 @@ module XYZ
       #TODO: look at fog's mock
       class MockEc2Connection
         def server_create(create_options)
-          ret = {
-            :block_device_mapping=>[],
-            :image_id=>"ami-ee38c987",
-            :flavor_id=>"t1.micro",
-            :private_dns_name=>nil,
-            :created_at=>Tue Jan 25 18:57:04 UTC 2011,
-            :state=>"pending",
-            :monitoring=>[{"state"=>false}],
-            :availability_zone=>"us-east-1a",
-            :groups=>["basic"],
-            :ami_launch_index=>0,
-            :kernel_id=>"aki-3af50453",
-            :product_codes=>[],
-            :dns_name=>nil,
-            :reason=>nil,
-            :id=>"i-3706b75b",
-            :client_token=>nil,
-            :root_device_type=>"ebs"}
-
+          instance_id = generate_unique_instance_id(create_options)
+          CommonFields.merge(:state => "pending", :id => instance_id,:created_at => created_at(instance_id),:block_device_mapping=>[])
         end
+        
+
         def server_get(instance_id)
-          ret = {
-            :image_id=>"ami-ee38c987",
-            :block_device_mapping=>
+          #TODO stub
+          network_attrs = {
+            :ip_address=>"184.73.10.255",
+            :private_dns_name=>"domU-12-31-39-02-D8-05.compute-1.internal",
+            :private_ip_address=>"10.248.223.243",
+            :dns_name=>"ec2-184-73-10-255.compute-1.amazonaws.com",
+          }
+          #TODO: stub
+          block_device_mapping =
             [{"volumeId"=>"vol-34820c5c",
                "deviceName"=>"/dev/sda1",
                "deleteOnTermination"=>true,
-               "attachTime"=>Tue Jan 25 18:57:08 UTC 2011,
-               "status"=>"attached"}],
-            :flavor_id=>"t1.micro",
-            :ip_address=>"184.73.10.255",
-            :private_dns_name=>"domU-12-31-39-02-D8-05.compute-1.internal",
-            :created_at=>Tue Jan 25 18:57:04 UTC 2011,
-            :state=>"running",
-            :private_ip_address=>"10.248.223.243",
-            :tags=>{},
-            :monitoring=>[{"state"=>false}],
-            :availability_zone=>"us-east-1a",
-            :state_reason=>{},
-            :groups=>["basic"],
-            :ami_launch_index=>0,
-            :kernel_id=>"aki-3af50453",
-            :product_codes=>[],
-            :dns_name=>"ec2-184-73-10-255.compute-1.amazonaws.com",
-            :reason=>nil,
-            :id=>"i-3706b75b",
-            :client_token=>nil,
-            :root_device_type=>"ebs"}
+               "attachTime"=>"Tue Jan 25 18:57:08 UTC 2011",
+               "status"=>"attached"}]
 
+          CommonFields.merge(network_attrs).merge(:state => "running", :id => instance_id,:created_at => created_at(instance_id), :block_device_mapping => block_device_mapping)
         end
+
+      private
+      def created_at(instance_id)
+        #TODO: stub
+        "Tue Jan 25 18:57:04 UTC 2011"
       end
+
+      def generate_unique_instance_id(create_options)
+        #TODO: stub
+        "i-3706b75b"
+      end
+      #TODO: may put this in Config file
+      CacheFile = "#{R8::Config[:sys_root_path]}/cache/ec2_mock.json"
+      @@cache_contents = nil
+
+      CommonFields = {
+        :image_id=>"ami-ee38c987",
+        :flavor_id=>"t1.micro",
+        :private_dns_name=>nil,
+        :state=>"pending",
+        :monitoring=>[{"state"=>false}],
+        :availability_zone=>"us-east-1a",
+        :groups=>["basic"],
+        :ami_launch_index=>0,
+        :kernel_id=>"aki-3af50453",
+        :product_codes=>[],
+        :reason=>nil,
+        :client_token=>nil,
+        :root_device_type=>"ebs"
+      }
+    end
 
       def self.conn()
         Conn[0] ||= MockEc2Connection.new

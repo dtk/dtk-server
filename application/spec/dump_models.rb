d719a01bdf4e0b0e38f6ac143a5c5b99ec9225f9
root <root@domU-12-31-39-03-A9-28.compute-1.internal>
2011-02-01 03:05:30 +0000

    start of work on deriving real and virtual objects from meta info

--- application/spec/dump_models.rb
+++ application/spec/dump_models.rb
@@ -3,31 +3,48 @@ root = File.expand_path('../', File.dirname(__FILE__))
 require root + '/app'
 #utils/internal/serialize_to_json.rb
 include XYZ
+#monkey patch
+module ActiveSupport
+  class OrderedHash < ::Hash
+    def pretty_print(q)
+#      q.group(0, "#<OrderedHash", "}>") {
+      q.group(0,"","}") {
+        q.breakable " "
+        q.text "{"
+        q.group(1) {
+          q.seplist(self) {|pair|
+            q.pp pair.first
+            q.text "=>"
+            q.pp pair.last
+          }
+        }
+      }
+    end
+  end
+end
+
 ordered_top_level_keys = 
   [
    :schema,
    :table,
-   :model_class,
    :columns,
    :virtual_columns,
    :many_to_one,
-   :one_to_many,
-
-   :relation_type,
-   :has_ancestor_field,
-   :parent_id,
-   :local_id,
-   :id
+   :one_to_many
   ]
 
 
 #pp DB_REL_DEF.each_value.map{|x|x.keys}.flatten.uniq
-=begin
+
 DB_REL_DEF.each do |model_name,info|
- # pp [:model_name,model_name];pp info; puts "------------------------------------------------------"
+  pp [:model_name,model_name];
+  ordered_hash = ActiveSupport::OrderedHash.new()
+  ordered_top_level_keys.each{|k|ordered_hash[k] = info[k] if info.has_key?(k)}
+  pp ordered_hash
+  puts "------------------------------------------------------"
 end
-=end
-#ordered_hash = ActiveSupport::OrderedHash.new()
+
+
 
 
 

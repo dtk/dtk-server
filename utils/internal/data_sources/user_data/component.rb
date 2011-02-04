3bdaae1590eca47ee55bea82bb482e5ad637297a
root <root@domU-12-31-39-17-28-AB.compute-1.internal>
2011-02-04 21:47:41 +0000

    fixing up 02-04 test case

--- utils/internal/data_sources/user_data/component.rb
+++ utils/internal/data_sources/user_data/component.rb
@@ -5,6 +5,12 @@ module XYZ
         definitions do
           target[:display_name] = source["ref"]
           target[:ui] = source["ui"]
+          if_exists(source["only_one_per_node"]) do
+            target[:only_one_per_node] = source["only_one_per_node"]
+          end
+          if_exists(source["basic_type"]) do
+            target[:basic_type] = source["basic_type"]
+          end
         end
          class << self
             def unique_keys(source)

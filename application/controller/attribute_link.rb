659f5af6af94ada68c815319590a6ba3471c04e7
root <root@domU-12-31-39-04-05-97.compute-1.internal>
2011-03-02 16:33:15 +0000

    hooked up error processing

--- application/controller/attribute_link.rb
+++ application/controller/attribute_link.rb
@@ -1,21 +1,18 @@
 module XYZ
   class Attribute_linkController < Controller
     helper :ports
-    #TODO: right now just for testing
-
-#=begin
-#TODO: testing of port constraints
     def save(explicit_hash=nil,opts={})
-      hash = explicit_hash || request.params.dup
-      constraints = create_object_from_id(hash["input_id"],:attribute).get_constraints()
-      if constraints
-        target = {:target_port_id_handle => id_handle(hash["output_id"],:attribute)}
-        constraints.evaluate_given_target(target, :raise_error_when_violation => true)
+      handle_errors do
+        hash = explicit_hash || request.params.dup
+        constraints = create_object_from_id(hash["input_id"],:attribute).get_constraints()
+        if constraints
+          target = {:target_port_id_handle => id_handle(hash["output_id"],:attribute)}
+          constraints.evaluate_given_target(target, :raise_error_when_violation => true)
+        end
+        super(hash,opts)
       end
-      super(hash,opts)
     end
-#=end
-
+    #TODO: right now just for testing
     def list_legal_connections(*parent_uri_array) #TODO stub
       parent_id = nil
       parent_uri = (parent_uri_array.empty? ? "/datacenter/dc1" : "/" + parent_uri_array.join("/"))

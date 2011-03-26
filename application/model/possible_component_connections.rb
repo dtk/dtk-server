e728e66882f8e411f183b9040cdcd9cb13b1b397
root <root@domU-12-31-39-00-88-C8.compute-1.internal>
2011-03-26 15:05:26 +0000

    refinement of possible_component_connections.rb

--- application/model/possible_component_connections.rb
+++ application/model/possible_component_connections.rb
@@ -1,11 +1,21 @@
 module XYZ
+  #This representation captures what input components can be connected to which output components and 
+  #how there attributes are functionally related
+  #input and output components are specfied by a component_type or a memeber of the component type hierarcy.
+  #When a user tries to link to comomponents this datra structure is searched to find a match (and if no match)
+  #a link constraint violation raised saying they cannot connect. Matching is based by finding most specfic match (wrt type hierrachy of
+  #input component then most specific match of output component
+  #NOTE: need to flesh out details related to "inheritance override issues"
   PossibelComponentConnections = {
     :mysql__slave => { #input component; can be component_type or member of component hiererachy
-      #connection type is so can specify for exampel this is a db connection, monitoring connection, etc; there will be a standard set
-      #plus ones that are more special purpose (like db_slave_to_master) and can be user defined like 
+
+      #connection type is so can specify, for example 'this is a db connection', monitoring connection, etc; 
+      #there will be a standard set of connections plus ones that are more special purpose 
+      #(like db_slave_to_master) and can be user defined like 
+      #NOTE: may allow these to also be at the connection_mapping level
       :connection_type => :db_slave_to_master,
 
-      #whether connection to this input is required; by default may make this true, but listed here for illustration
+      #:required captures whether connection to this input is required; by default may make this true, but listed here for illustration
       #this can be at this level or connection mapping level (if for example there is multiple attributes associated
       #with connection and only certain ones are required
       :required => true, 
@@ -13,18 +23,32 @@ module XYZ
       :mysql__master => { #output component: can be component_type or member of component hiererachy
 
         ###contraints
-        #this is optional and for contarints beyond the implicit constraint that input x cannot connect to output y unless they match
-        #a rule here (match taking into account component type hierarchy); may also provide for 
-        #"inheritence" override (if multiple matches most specfic one is used; where match is first by input component (tehn output component)
+        #this is optional and needed to be specified only for constraints beyond the implicit constraint 
+        #that input x cannot connect to output y unless they match a 'rule' here (match taking into account 
+        #component type hierarchy); 
         :constraints => 
 
-        [], #will put in example later, such as constraint on particular mysql version oor that master and slaev isnatnces must be same major version
+        [], #will put in example later, such as constraint on particular mysql version or that master and slave components must be same major version
 
-        ### (additional) attribute mappaings
-        #this is optional; by default dont need l4 sap_ref to sap connection; unless it needs to be overwritten as show in this example
-        #use a directory style naming convention tied into our path names for unravvled attributes; 
-        #plus special symbol :__parent, which in unix directory woudl be euqivalent to ../
-        :connection_mappings =>
+        ### (additional) attribute mappings
+        #this is optional; by default dont need l4 sap_ref to sap connection; unless it needs to be overwritten as shown in this example
+        #the basic building blocks for these attribute mapapings is path specification that identifies a component or node 
+        #attribute and functions that can combine paths (and also can be nested) 
+        #for paths a directory style naming convention tied into our path names for unravvled attributes; 
+        #plus a few special symbols 
+        #   :__parent, which in unix directory woudl be euqivalent to ../
+        # :__node - represents the node that component is on and used to access node attributes
+        # :__input_node and __output_node which as wil be illustarettd below used when a relationship between components need links
+        # that point in both directions
+        #functions will be close in concept to what is used in cloud formation
+        #NOTE: in hash rep may represent fuinctions like {:fn_name => 'fn_name', :args => 'array of args}
+        ##
+        ## When mapping refernces attributes that do not exist, tehy are automatcally added when link added
+        ## Simimilarly when mapping refernces componenst that dont exist, tehy are autoamtically created
+        #NOTE: open issue is best way to handle what will call the 'load balancer keep alive issue' 
+        # which is when add for example a new branch to load balancer need config for this new instance (will explain better later)
+        #so need a var naming convention to refer to all teh attributes associated one of possible many connected attributes
+        :attribute_mappings =>
 
         [
          #the input to output mappings are with respect to teh input and output components in rule unless __parent is used (on output side)

559e76fc8c8673d08302cfd35864d4a10301e91b
root <root@domU-12-31-39-04-05-64.compute-1.internal>
2011-04-03 21:38:33 +0000

    updated possible_component_connections.new.rb

--- application/model/possible_component_connections.new.rb
+++ application/model/possible_component_connections.new.rb
@@ -76,7 +76,7 @@ module XYZ
                [:eq, "mysql__slave.version", "mysql__server.version"],
 
                #this captures that the master extension must be instantiated already
-               [:component_extended, :master]
+               [:component_extended, "master"]
 
                #the other alternative would be to omit this constraint and
                #include an event that instantiated the mysql master extension if it did not exist
@@ -98,6 +98,44 @@ module XYZ
           }
         ]
        }]
+    },
+    #variant of first example where rather than creating a db, java_webapp2 requires an existing db
+    #this serves to also illusrate how to handle extsnions (like db) where for one base there can be multiple
+    #instances
+    :java_webapp2 => {
+      :attributes => [],
+      :link_defs => 
+      [
+       {
+         :type => :database, 
+         :required => true, 
+
+         :possible_links => 
+         [
+          {:mysql__server => {
+              :constraints => 
+              [
+               #this constraint says that mysql__server must be exetnded with a database having property
+               #that its db name is 'java_wa2' (cannot simply say that mysql__server is extended with a database extension
+               #since theer can be a number of database extensions
+               #using a syntax that is similar to what is used in xpath to 'constrain teh path' when multiple branches
+               #note that db_params is a hash with three keys (database, user_name, password)
+               [:component_extended, "database[db_params.database=java_wa2]"]
+              ],
+              :attribute_mappings => 
+              [
+               {"mysql__server.sap__l4" => "java_webapp.sap_ref__l4"},
+
+               #I know this starting to look complicated, but issue is that cannot simply identify left hand side
+               #by mysql__server.db_params since mysql__server can have multiple "db_params"
+               #This picks out db_params associated with database with name 'java_wa2'
+               #It serves to make sure that java_webapp2 has the password and username associated with db with name java_wa2
+               {"mysql__server.db_params[database=java_wa2]"  => "java_webapp2.db_params_ref"}
+              ]
+            }
+          }]
+       }]
     }
   }
+
 end

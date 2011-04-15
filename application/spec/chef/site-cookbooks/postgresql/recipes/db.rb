50419d323aaeaf9eb0bda32ec7349ab70521f9a9
root <root@domU-12-31-39-05-58-56.compute-1.internal>
2011-04-15 20:45:39 +0000

    update to postgres db recipe

--- application/spec/chef/site-cookbooks/postgresql/recipes/db.rb
+++ application/spec/chef/site-cookbooks/postgresql/recipes/db.rb
@@ -1,1 +1,9 @@
 Chef::Log.info("in postgresql::db")
+require 'pp'
+pp node[:postgresql][:db][:list].to_hash
+=begin
+process_db_instances "instances" do
+  elements node[:postgresql][:db][:list]
+end
+=end
+

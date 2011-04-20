363e488568e8a73ec8f17b19f07b171cd1d373c5
root <root@domU-12-31-39-00-DE-14.compute-1.internal>
2011-04-20 23:06:23 +0000

    working on config file upload

--- application/meta/file_asset/new/model_def.rb
+++ application/meta/file_asset/new/model_def.rb
@@ -2,6 +2,7 @@
   :schema=>:file_asset,
   :table=>:file_asset,
   :columns=>{
+    :file_name => {:type=>:varchar, :size=>50},
     :path => {:type=>:varchar},
     :content=> {:type=>:varchar}
   },

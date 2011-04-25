017c92b5568575f10e4abc558fe4bb4146271bf6
rich <root@ubuntu.gateway.2wire.net>
2011-04-24 20:38:27 -0700

    update

--- application/config/config.rb
+++ application/config/config.rb
@@ -73,18 +73,18 @@ R8::Config[:workflow] = Hash.new
 R8::Config[:workflow][:type] = "simple"
 
 #Command and control related parameters
-R8::Config[:command_and_control] = Hash.new
-R8::Config[:command_and_control][:node_config] = Hash.new
-R8::Config[:command_and_control][:node_config][:type] = "mcollective"
-#R8::Config[:command_and_control][:node_config][:type] = "mcollective__mock"
-R8::Config[:command_and_control][:node_config][:mcollective] = Hash.new
-R8::Config[:command_and_control][:node_config][:mcollective][:agent] = "chef_solo"
+R8::Config[:command_and_control] ||= Hash.new
+R8::Config[:command_and_control][:node_config] ||= Hash.new
+R8::Config[:command_and_control][:node_config][:type] ||= "mcollective"
+#R8::Config[:command_and_control][:node_config][:type] ||= "mcollective__mock"
+R8::Config[:command_and_control][:node_config][:mcollective] ||= Hash.new
+R8::Config[:command_and_control][:node_config][:mcollective][:agent] ||= "chef_solo"
 
 
-R8::Config[:command_and_control][:iaas] = Hash.new
+R8::Config[:command_and_control][:iaas] ||= Hash.new
 #TODO: put in provisions to have multiple iias providers at same time
-R8::Config[:command_and_control][:iaas][:type] = "ec2" 
-#R8::Config[:command_and_control][:iaas][:type] = "ec2__mock" 
+R8::Config[:command_and_control][:iaas][:type] ||= "ec2" 
+#R8::Config[:command_and_control][:iaas][:type] ||= "ec2__mock" 
 
 #optional timer plug
 #R8::Config[:timer] = Hash.new

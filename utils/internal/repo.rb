50d526724ec3f3a9a1cc5b218a5de569989cf6a6
root <root@ip-10-196-243-224.ec2.internal>
2011-05-04 17:27:15 +0000

    working on repo

--- utils/internal/repo.rb
+++ utils/internal/repo.rb
@@ -9,91 +9,57 @@ module XYZ
       @index = @grit_repo.index #creates new object so use @index, not grit_repo
     end
 
-    def add_branch(branch_name)
-      
-      @index.read_tree("master")
-      @index.commit("Adding branch #{branch_name}", [@grit_repo.commits.first], nil, nil, branch_name)
+    def add_branch(branch_name,start="master")
+      start ||= "master"
+      checkout(start)
+      @index.commit("Adding branch #{branch_name}", [@grit_repo.commit(start)], nil, nil, branch_name)
     end
 
-    def checkout_branch(branch_name)
+    def checkout(branch_name,&block)
        @index.read_tree(branch_name)
+      #TODO: when get index mechanisms to work dont need below
+      current_head = @grit_repo.head.name
+      git_command.checkout({},branch_name)
+      return unless block
+      yield
+      unless current_head == branch_name
+        git_command.checkout({},current_head)
+      end
     end
 
-    def checkout_master()
-       @index.read_tree("master")
-    end
-
-    def add_file_to_master(file_name,content)
-      checkout_master()
-      message = "Adding #{file_name} to master"
+    def add_file(file_name,content,branch_name="master")
+      message = "Adding #{file_name} to #{branch_name}"
+      ret = nil
       Dir.chdir(@path) do
         File.open(file_name,"w"){|f|f << content}
-        @grit_repo.add(file_name)
-        @grit_repo.commit_index(message)
+        checkout(branch_name) do 
+          @grit_repo.add(file_name)
+          ret = @grit_repo.commit_index(message)
+        end
       end  
 
       #TODO: new form not working
       # @index.add(file_name,content)
-      # @ index.commit("Adding #{file_name} to master", [@grit_repo.commits.first])
+      # @ index.commit(message, [@grit_repo.commit(branch_name)])
+      ret
     end
 
-
-    def add_file_to_branch(branch_name,file_name,content)
-      @index.add(file_name,content)
- #     @index.commit("Adding #{file_name} to branch #{branch_name}", [commit(branch_name)], nil, nil, branch_name)
+    def branch_exists?(branch_name)
+      @grit_repo.heads.find{|h|h.name == branch_name} ? true : nil
     end
-   private
-    def git()
+
+#   private
+    attr_reader :grit_repo
+    def git_command()
       @grit_repo.git
     end
   end
 end
 
-def add_multiple_commits_same_file_different_content(repo)
-  Dir.chdir("/root/Repo") do
-    previous_commit = repo.commits.first && repo.commits.first.id
-    (0...5).each do |count|
-      i1 = repo.i
-      i1.read_tree('master')
-      file_name = "foo#{count.to_s}.txt"
-      content = "hello new foo2, count is #{count}.\n"
-      File.open(file_name,"w"){|f|f << content}
-      repo.add(file_name)
-      repo.commit_index("my commit - #{count}")
-=begin      
-     i1.add("foo#{count.to_s}.txt", "hello foo, count is #{count}.\n")
-      previous_commit =  i1.commit("my commit - #{count}",
-                             previous_commit,
-                                 Grit::Actor.new("j#{count}", "e@e#{count}.zz"),
-                             previous_commit.nil? ? nil : repo.commits(previous_commit).first.tree)
-=end
-    end
-  end
-end
-
+file_name = ARGV[0]
+branch_name = ARGV[1]
+branch_start = ARGV[2]
 r = XYZ::Repo.new("/root/Repo")
-r.add_file_to_master("hij.txt","new")
-=begin
-#add_multiple_commits_same_file_different_content(r)
+r.add_branch(branch_name,branch_start) unless r.branch_exists?(branch_name)
+pp r.add_file(file_name,"initial text",branch_name)
 
-
-index = r.i
-#index.add('myfile2.txt', 'This is the content')
-Dir.chdir("/root/Repo") do
-  file_name = 'myfile2.txt'
-  content = "hello new foo5\n"
-#  File.open(file_name,"w"){|f|f << content}
-  index.read_tree("master")
-  index.add(file_name,content)
-  index.commit('commit x',[r.commits.first])
-end
-
-
-index.add('myfile2.txt', 'This is the content2')
-index.commit('2nd commit')
-
-#r.checkout_branch("b1")
-#r.add_file_to_branch("b1","foo2.text","sample text")
-#r.add_file_to_master("foo3.text","foo3 text")
-#pp r.commit_all("foo3")
-=end

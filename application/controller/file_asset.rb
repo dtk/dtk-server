9baa70cf525d94f572d7ea6e78ceabaa45d89e62
root <root@ip-10-35-99-213.ec2.internal>
2011-05-07 00:56:13 +0000

    test stub to get file info

--- application/controller/file_asset.rb
+++ application/controller/file_asset.rb
@@ -8,6 +8,22 @@ module XYZ
 
       return {:data=>file_asset}
     end
-  end
 
+    def test_get(*path_array)
+      path = path_array.join("/")
+      repo,af_path = (path =~ Regexp.new("(^[^/]+)/(.+$)"); [$1,$2])
+      sp_hash = {
+        :filter => [:eq, :path, af_path],
+        :cols => [:id,:path,:implementation_info]
+      }
+      mh  = ModelHandle.new(ret_session_context_id(),:file_asset)
+      file_asset = Model.get_objects_from_sp_hash(mh,sp_hash).find{|x|x[:implementation][:repo] == repo}
+      raise "file asset #{path} not found" unless file_asset
+      pp file_asset
+      contents = file_asset.get_content()
+      contents.each_line{|l|STDOUT << l}
+      STDOUT << "\n"
+      {:content=>"<pre>#{contents}</pre>"}
+    end
+  end
 end

b45f1f520b170fb000d9f85e303d0f7bd53813f8
root <root@ip-10-32-153-23.ec2.internal>
2011-05-12 21:31:49 +0000

    hack to show real log

--- application/controller/task.rb
+++ application/controller/task.rb
@@ -12,13 +12,29 @@ module XYZ
       end
       assoc_nodes = task.get_associated_nodes()
 
-      unless R8::Config[:command_and_control][:node_config][:type] == "mcollective"
+      chef_logging = nil
+      if R8::Config[:command_and_control][:node_config][:type] == "mcollective"
+        logs = CommandAndControl.get_logs(task,assoc_nodes)
+        #if multiple nodes present error otehrwise present first
+        hash_form = nil
+        logs.each do |node_id,result|
+          pp "log for node_id #{node_id.to_s}"
+          parsed_log = ParseLog.parse(result[:data])
+          hash_form = parsed_log.hash_form()
+          break if hash_form[:log_segments].find{|seg|seg[:type] == :error}
+        end
+        if hash_form
+          chef_logging = convert_symbols(hash_form)
+        end
+      else
         chef_logging = get_logs_mock(assoc_nodes)
-        tpl = R8Tpl::TemplateR8.new("task/chef_log_view",user_context())
-        tpl.assign(:logging,chef_logging)
-
-        return {:content => tpl.render()}
       end
+
+      chef_logging ||= get_logs_mock(assoc_nodes)
+      tpl = R8Tpl::TemplateR8.new("task/chef_log_view",user_context())
+      tpl.assign(:logging,chef_logging)
+      
+      return {:content => tpl.render()}
 
       logs = CommandAndControl.get_logs(task,assoc_nodes)
       pp "----------------"
@@ -35,6 +51,16 @@ module XYZ
       {:content => {}}
     end
   private
+
+    def convert_symbols(obj)
+      if obj.kind_of?(Hash)
+        obj.inject({}){|h,kv| h.merge(kv[0].to_s => convert_symbols(kv[1]))}
+      elsif obj.kind_of?(Array)
+        obj.map{|x|convert_symbols(x)}
+      else
+        obj
+      end
+    end
     def get_logs_mock(assoc_nodes)
       i = 0
       assoc_nodes.each do |node|

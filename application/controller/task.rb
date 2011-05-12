57569a2797d727dc7d1d60dc050d1c17e9c13df0
root <root@ip-10-32-153-23.ec2.internal>
2011-05-12 17:47:23 +0000

    mocking patch

--- application/controller/task.rb
+++ application/controller/task.rb
@@ -11,26 +11,36 @@ module XYZ
         raise Error.new("not implemented yet get_logs with task id given")
       end
       assoc_nodes = task.get_associated_nodes()
+
+      unless R8::Config[:command_and_control][:node_config][:type] == "mcollective"
+        get_logs_mock(assoc_nodes)
+        return {:content => {}}
+      end
+
       logs = CommandAndControl.get_logs(task,assoc_nodes)
       pp "----------------"
       i = 0
       logs.each do |node_id,result|
         pp "log for node_id #{node_id.to_s}"
-        if R8::Config[:command_and_control][:node_config][:type] == "mcollective"
           parsed_log = ParseLog.parse(result[:data])
           hash_form = parsed_log.hash_form()
           #File.open("/tmp/t#{node_id.to_s}.json","w"){|f|f << JSON.pretty_generate(hash_form)}
           STDOUT << parsed_log.pp_form_summary
           pp [:file_asset_if_error,parsed_log.ret_file_asset_if_error(model_handle)]
           STDOUT << "----------------\n"
-        else
-          file = File.expand_path(SampleSets[i], File.dirname(__FILE__))
-          hash_form = File.open(file){|f|JSON.parse(f.read)}
-          i += i
-          break if i >= SampleSets.size
-        end
       end
       {:content => {}}
+    end
+  private
+    def get_logs_mock(assoc_nodes)
+      i = 0
+      assoc_nodes.each do |node|
+        pp "log for node_id #{node[:id].to_s}"
+        file = File.expand_path(SampleSets[i], File.dirname(__FILE__))
+        hash_form = File.open(file){|f|JSON.parse(f.read)}
+        i += i
+        break if i >= SampleSets.size
+      end
     end
     SampleSets = ["temp/error_example1.json","temp/ok_example1.json"]
   end

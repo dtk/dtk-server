4c29a494aae1be4c2da6645a44b6510a861dc63a
root <root@ip-10-83-18-30.ec2.internal>
2011-05-28 15:55:44 +0000

    working on reciever that gets events indicating a task step is finished

--- utils/internal/workflow/adapters/ruote/receiver.rb
+++ utils/internal/workflow/adapters/ruote/receiver.rb
@@ -1,6 +1,24 @@
 module XYZ
   module WorkflowAdapter
-    class RuoteReceiver
+    class RuoteReceiver < ::Ruote::Receiver
+      def initialize(engine,listener)
+        super(engine)
+        @stop = nil
+        @listener = listener
+        @thread = Thread.new { listen }
+        @thread.join
+      end
+
+      def stop()
+        @stop = true
+      end
+      private
+        def listen
+          while not @stop
+            @listener.process_event()
+          end
+        end
+      end
     end
   end
 end

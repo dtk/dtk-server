d7e985d97a61b1e51ea032b5b631bb616b59d1f0
root <root@ip-10-32-157-110.ec2.internal>
2011-05-29 23:14:46 +0000

    modifying ruote workflow adapter

--- utils/internal/workflow/adapters/ruote.rb
+++ utils/internal/workflow/adapters/ruote.rb
@@ -13,24 +13,31 @@ module XYZ
         Engine.wait_for(wfid)
       end
      private 
+      ObjectStore = Hash.new
+      ObjectStoreLock = Mutex.new
+      #TODO: make sure task id is globally unique
+      def self.push_on_object_store(task_id,task_info)
+        ObjectStoreLock.synchronize{ObjectStore[task_id] = task_info}
+      end
+      
       module Participant
         class Top
           include ::Ruote::LocalParticipant
          private 
-          def idh_from_serialized_idh(s_idh)
-            IDHandle[s_idh.inject({}){|h,(k,v)|h.merge(k.to_sym => v)}]
+          def get_and_delete_from_object_store(task_id)
+            ret = nil
+            ObjectStoreLock.synchronize{ret = ObjectStore.delete(task_id)}
+            ret
           end
         end
         class ExecuteOnNode < Top
           def consume(workitem)
-            # _s means getting back in serialized form
-            action_s = workitem.fields["params"]["action"]
-            top_task_idh_s = workitem.fields["params"]["top_task_idh"]
-            task_idh_s = workitem.fields["params"]["task_idh"]
+            task_id = workitem.fields["params"]["task_id"]
+            task_info = get_and_delete_from_object_store(task_id)
+            action = task_info["action"]
+            top_task_idh = task_info["top_task_idh"]
+            task = task_info["task"]
 
-            top_task_idh = idh_from_serialized_idh(top_task_idh_s)
-            task = idh_from_serialized_idh(task_idh_s).create_object
-            action = action_s #TODO: stub
             result = Workflow.process_executable_action(task,action,top_task_idh)
             workitem.fields[workitem.fields["params"]["action"]["id"]] = result
             reply_to_engine(workitem)
@@ -54,7 +61,7 @@ module XYZ
       end
 
       def compute_process_def()
-        #TODO: see if we need to leep geenrating new ones or whether we can (delete) and reuse
+        #TODO: see if we need to keep geenrating new ones or whether we can (delete) and reuse
         @@count += 1
         top_task_idh = @task.id_handle()
         name = "process-#{@@count.to_s}"
@@ -66,10 +73,17 @@ module XYZ
       def compute_process_body(task,top_task_idh)
         executable_action = task[:executable_action]
         if executable_action
+          task_info = {
+            "action" => executable_action,
+            "task" => task,
+            "top_task_idh" => top_task_idh
+          }
+          task_id = task.id()
+          Ruote.push_on_object_store(task_id,task_info)
+
           ["participant", 
            {"ref" => "execute_on_node", 
-             "action" => executable_action,
-             "task_idh" => task.id_handle,
+            "task_id" => task_id,
              "top_task_idh" => top_task_idh
            },
            []]

98f42ae496ec7c2423d65136cb4407ef8cfa961a
root <root@ip-10-32-157-110.ec2.internal>
2011-05-29 18:02:52 +0000

    modifying ruote workflow adapter

--- utils/internal/workflow/adapters/ruote.rb
+++ utils/internal/workflow/adapters/ruote.rb
@@ -30,51 +30,37 @@ module XYZ
       end
       
       def process_def()
-        @process_def ||= compute_process_definition()
+        @process_def ||= compute_process_def()
       end
 
-      def compute_process_definition()
+      def compute_process_def()
+        #TODO: see if we need to leep geenrating new ones or whether we can (delete) and reuse
         @@count += 1
         top_task_idh = @task.id_handle()
-        ::Ruote.process_definition :name => "process-#{@@count.to_s}" do
-          sequence(compute_process_def_body(top_task_idh))
-        end
+        name = "process-#{@@count.to_s}"
+        ["define", 
+         {"name" => name},
+         [compute_process_body(task,top_task_idh)]]
       end
 
-      def compute_process_def_body(top_task_idh)
-        executable_action = @task[:executable_action]
+      def compute_process_body(task,top_task_idh)
+        executable_action = task[:executable_action]
         if executable_action
-          participant :ref => :execute_on_node, :action => executable_action,:top_task_idh => top_task_idh
-        elsif @task[:temporal_order] == "sequential"
-          process_sequential(top_task_idh)
-        elsif @task[:temporal_order] == "concurrent"
-          process_concurrent(top_task_idh)
+          ["participant", {"ref" => "execute_on_node", "action" => executable_action,"top_task_idh" => top_task_idh},[]]
+        elsif task[:temporal_order] == "sequential"
+          compute_process_body_sequential(task.subtasks,top_task_idh)
+        elsif task[:temporal_order] == "concurrent"
+          compute_process_body_concurrent(task.subtasks,top_task_idh)
         else
           Log.error("do not have rules to process task")
         end
       end
-    end
-  end
-end
-
-=begin      
-            if ordered_actions.is_single_state_change?()
-              participant :ref => :execute_on_node, :action => ordered_actions.single_state_change()
-            elsif ordered_actions.is_concurrent?()
-              concurrence :merge_type => :mix do
-                ordered_actions.subtasks.each{|action|participant :ref => :execute_on_node, :action => action}
-              end
-            elsif ordered_actions.is_sequential?()
-              sequence do
-                ordered_actions.subtasks.each{|action|participant :ref => :execute_on_node, :action => action}
-              end
-            end
-            participant :return_results
-          end
-        end
+      def compute_process_body_sequential(subtasks,top_task_idh)
+        ["sequence", {}, subtasks.map{|t|compute_process_body(t,top_task_idh)}]
+      end
+      def compute_process_body_concurrent(subtasks,top_task_idh)
+        ["concurrence", {"merge_type"=>"mix"}, subtasks.map{|t|compute_process_body(t,top_task_idh)}]
       end
     end
   end
 end
-=end
-

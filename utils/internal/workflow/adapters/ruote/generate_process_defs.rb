aea15364c4a30bfabc8bfed24a061194fa4376c6
root <root@ip-10-243-110-191.ec2.internal>
2011-06-03 00:38:52 +0000

    cleaning up ruote so that can be more flexible in translating between r8 tasks and ruote processes

--- utils/internal/workflow/adapters/ruote/generate_process_defs.rb
+++ utils/internal/workflow/adapters/ruote/generate_process_defs.rb
@@ -9,28 +9,21 @@ module XYZ
         name = "process-#{count.to_s}"
         ["define", 
          {"name" => name},
-         [["sequence", {}, 
-           [compute_process_body(task,top_task_idh),
-            participant(:end_of_task)]]]]
+         [sequence(compute_process_body(task,top_task_idh),
+                   participant(:end_of_task))]]
       end
       private
-      
-      def compute_process_body(task,top_task_idh)
-        executable_action = task[:executable_action]
-        if executable_action
-          task_info = {
-            "action" => executable_action,
-            "workflow" => self,
-            "top_task_idh" => top_task_idh
-          }
-          task_id = task.id()
-          Ruote::TaskInfo.set(task_id,task_info)
 
-          
-          participant(:execute_on_node,{:task_id => task_id,:top_task_idh => top_task_idh})
+      ####semantic processing
+
+      ####synactic processing
+      def compute_process_body(task,top_task_idh)
+        if task[:executable_action]
+          process_executable_action(:execute_on_node,task,top_task_idh)
 
 =begin
 #test
+TODO: put in semabic based whether theer are prefix or postfix actions
 Ruote::TaskInfo.set(task_id,task_info,"test")
           ["sequence", {},
             [["participant",
@@ -56,15 +49,29 @@ Ruote::TaskInfo.set(task_id,task_info,"test")
         concurrence(subtasks.map{|t|compute_process_body(t,top_task_idh)})
       end
 
+      def process_executable_action(name,task,top_task_idh,args={})
+        executable_action = task[:executable_action]
+        task_info = {
+          "action" => executable_action,
+          "workflow" => self,
+          "top_task_idh" => top_task_idh
+        }
+        task_id = task.id()
+        Ruote::TaskInfo.set(task_id,task_info,args[:task_type])
+        participant(name,{:task_id => task_id,:top_task_idh => top_task_idh}.merge(args))
+      end
+
       #formatting fns
       def participant(name,opts={})
         ["participant",to_str_form({"ref" => name}.merge(opts)),[]]
       end
 
-      def sequence(subtask_array)
+      def sequence(*subtask_array_x)
+        subtask_array = subtask_array_x.size == 1 ? subtask_array_x.first : subtask_array_x
         ["sequence", {}, subtask_array]
       end
-      def concurrence(subtask_array)
+      def concurrence(*subtask_array_x)
+        subtask_array = subtask_array_x.size == 1 ? subtask_array_x.first : subtask_array_x
         ["concurrence", {"merge_type"=>"stack"}, subtask_array]
       end
 

bebb645c8ba631b7b5a4c2af814b80fcd84fadd0
root <root@ip-10-32-155-100.ec2.internal>
2011-06-13 21:38:51 +0000

    now group attributes get qualified i18n name

--- application/controller/project.rb
+++ application/controller/project.rb
@@ -4,6 +4,7 @@ module XYZ
     helper :create_tasks_from_pending_changes
 
     def test_group_attrs(datacenter_id)
+      redirect = "/xyz/project/test_group_attrs/#{datacenter_id.to_s}"
       pending_changes = flat_list_pending_changes_in_datacenter(datacenter_id.to_i)
       commit_task = create_task_from_pending_changes(pending_changes)
 
@@ -11,18 +12,21 @@ module XYZ
       grouped_attrs = Attribute.ret_grouped_attributes(augmented_attr_list)
       ##pp grouped_attrs.map{|attr|[attr[:display_name],attr[:attr_val_type]]}
 
-      i18n = get_i18n_mappings_for_models(:attribute)
+      i18n_mapping = get_i18n_mappings_for_models(:attribute,:component)
       attr_list = grouped_attrs.map do |a|
         name = a[:display_name]
+        attr_i18n = i18n_string(i18n_mapping,:attribute,name)
+        component_i18n = i18n_string(i18n_mapping,:component,a[:component][:display_name])
+        node_i18n = a[:node][:display_name]
+        qualified_attr_i18n = "#{node_i18n}/#{component_i18n}/#{attr_i18n}"
         {
           :id => a[:unraveled_attribute_id],
           :name =>  name,
           :value => a[:attribute_value],
-          :i18n => i18n_string(i18n,:attribute,name),
+          :i18n => qualified_attr_i18n
         }
       end
 
-      redirect = "/xyz/project/test_group_attrs/#{datacenter_id.to_s}"
       tpl = R8Tpl::TemplateR8.new("project/attributes_edit",user_context())
       tpl.assign("redirect",redirect)
       tpl.assign("field_list",attr_list)
@@ -33,6 +37,7 @@ module XYZ
       hash = explicit_hash || request.params.dup
       redirect = hash.delete("redirect")
       pp hash
+      #TODO: stub to update and propagate vars
       redirect redirect
     end
   end

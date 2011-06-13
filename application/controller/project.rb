d07ea690d915479c8deec1763b530c49a359338c
root <root@ip-10-32-155-100.ec2.internal>
2011-06-13 21:18:13 +0000

    working on attribute groups

--- application/controller/project.rb
+++ application/controller/project.rb
@@ -9,8 +9,31 @@ module XYZ
 
       augmented_attr_list = Attribute.augmented_attribute_list_from_task(commit_task)
       grouped_attrs = Attribute.ret_grouped_attributes(augmented_attr_list)
-      pp grouped_attrs.map{|attr|[attr[:display_name],attr[:attr_val_type]]}
-      return {:content => nil}
+      ##pp grouped_attrs.map{|attr|[attr[:display_name],attr[:attr_val_type]]}
+
+      i18n = get_i18n_mappings_for_models(:attribute)
+      attr_list = grouped_attrs.map do |a|
+        name = a[:display_name]
+        {
+          :id => a[:unraveled_attribute_id],
+          :name =>  name,
+          :value => a[:attribute_value],
+          :i18n => i18n_string(i18n,:attribute,name),
+        }
+      end
+
+      redirect = "/xyz/project/test_group_attrs/#{datacenter_id.to_s}"
+      tpl = R8Tpl::TemplateR8.new("project/attributes_edit",user_context())
+      tpl.assign("redirect",redirect)
+      tpl.assign("field_list",attr_list)
+      return {:content => tpl.render()}
+    end
+
+    def save_attributes(explicit_hash=nil)
+      hash = explicit_hash || request.params.dup
+      redirect = hash.delete("redirect")
+      pp hash
+      redirect redirect
     end
   end
 end

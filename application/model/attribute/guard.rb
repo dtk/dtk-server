fefc3765cf9d71a82998c74943130b2245c4c8bd
root <root@ip-10-196-230-9.ec2.internal>
2011-07-03 21:30:43 +0000

    refactored so can compute guards using same dependency logic used to find indirectly required attributes

--- application/model/attribute/guard.rb
+++ application/model/attribute/guard.rb
@@ -4,30 +4,60 @@ module XYZ
       ret = Array.new
       augmented_attr_list = augmented_attribute_list_from_task(top_level_task)
       dependency_analysis(augmented_attr_list) do |attr_in,link,attr_out|
-        if guard = ret_attribute_guard(attr_in,link,attr_out)
+        if guard = GuardedAttribute.create(attr_in,link,attr_out)
           ret << guard
         end
       end
       ret
     end
-   private
-    def ret_attribute_guard(guarded_attr,link,guard_attr=nil)
-      #TODO: wil be used or leveraged when need guards
+  end
+  class GuardedAttribute < HashObject
+    def self.create(guarded_attr,link,guard_attr)
       unless guard_attr
-        #TODO: lock up the info
-        guard_attr = {:node => "Need to compute"}
+        Log.error("not treated when guard attribute is null")
+          return nil
       end
-      task_guard = {
-        :condition => {
-          :task_type => :create_node, 
-          :node => guard_attr[:node]
+      #gurading attributes that are unset and 
+      #TODO: should we assume that what gets heer are only requierd attributes
+      unless guard_attr[:dynamic] and (not guard_attr[:attribute_value]) and (not guarded_attr[:attribute_value])
+        return nil
+      end
+      guarded = {
+        :task_type => :config_node
+      }.merge(attr_info(guarded_attr))
+
+      #need to case on whether teh dynamic attribute set by config_node or create_node
+      if guarded_attr[:semantic_type_summary] == "sap_ref__l4" and (guarded_attr[:item_path]||[]).include?(:host_address)
+        task_type = :create_node
+        attr_info_keys = [:node]
+      else
+        task_type = :config_node
+        attr_info_keys = nil
+      end
+
+      guard = {
+        :task_type => task_type
+      }.merge(attr_info(guard_attr,attr_info_keys))
+      new(:guarded => guarded, :guard => guard)
+    end
+   private
+    def self.attr_info(attr,keys=nil)
+      ret = {
+        :node => {
+          :id => attr[:node][:id],
+          :display_name =>  attr[:node][:display_name]
         },
-        :guarded_task => {
-          :task_type => :config_node,
-          :node => guarded_attr[:node]
+        :component => {
+          :id => attr[:component][:id],
+          :display_name =>  attr[:component][:display_name]
+        },
+        :attribute => {
+          :id => attr[:id],
+          :display_name =>  attr[:display_name]
         }
       }
-      task_guard
+      return ret unless keys
+      keys.inject({}){|h,(k,v)| keys.include?(k) ? h.merge(k => v) : h}
     end
   end
 end

cbd455b4ec732bdb347c2797fe11d7e99ff0aa58
root <root@ip-10-118-55-128.ec2.internal>
2011-08-29 18:14:16 +0000

    refactoring link defs

--- application/meta/link_def_attribute_mapping/new/model_def.rb
+++ application/meta/link_def_attribute_mapping/new/model_def.rb
@@ -2,7 +2,21 @@
   :schema=>:link_def,
   :table=>:attribute_mapping,
   :columns=>{
-    
+    :output_attribute_id => {
+      :type=>:bigint,
+      :foreign_key_rel_type=>:attribute,
+      :on_delete=>:cascade,
+      :on_update=>:cascade
+    },
+    :output_path => {:type => :varchar, :size => 25},
+    :output_contant => {:type => :varchar}, #if this is non null then meaning is to set input to contant value (and output_attribute_id and output_path will be null
+    :input_attribute_id => {
+      :type=>:bigint,
+      :foreign_key_rel_type=>:attribute,
+      :on_delete=>:cascade,
+      :on_update=>:cascade
+    },
+    :input_path => {:type => :varchar, :size => 25},
   },
   :many_to_one=>[:link_def_possible_link]
 }

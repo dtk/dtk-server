aeb199c0fde23f1363e340aecbe0d31ef7b28c76
root <root@ip-10-32-71-98.ec2.internal>
2011-09-19 17:18:46 +0000

    working on code for ui interface to attribute defs

--- application/model/attribute/datatype.rb
+++ application/model/attribute/datatype.rb
@@ -11,9 +11,38 @@ module XYZ
     end
 
     def ret_default_info()
-      self[:value_asserted]
+      default = self[:value_asserted]
+      return nil unless default
+      #TODO: temparily unraveling arrays
+      if is_array?()
+        ret = Hash.new
+        hash_semantic_type = semantic_type[:array]
+        default.each_with_index do |d,i|
+          el = ret_default_info__hash(hash_semantic_type,d)
+          el.each{|k,v|ret.merge!("#{k}[#{i.to_s}]" => v)}
+        end
+        ret
+      else
+        ret_default_info__hash(semantic_type,default)
+      end
     end
+
    private
+
+    def ret_default_info__hash(hash_semantic_type,default)
+      hash_semantic_type.inject({}) do |h,(k,v)|
+        if v[:dynamic]
+          h
+        else
+          info = Hash.new
+          info.merge!(:required=> v[:required]) if v.has_key?(:required)
+          info.merge!(:type => v[:type])
+          info.merge!(:default_value => default[k]) if default.has_key?(k)
+          h.merge(k => info)
+        end
+      end
+    end
+
     def semantic_type()
       @semantic_type ||= SemanticTypeSchema.create_from_attribute(self)
     end

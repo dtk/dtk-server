892ca4f1e280b77c1ad1ee2829cdf066025c8da1
root <root@ip-10-34-145-126.ec2.internal>
2011-10-03 21:47:42 +0000

    working on puppet parser

--- utils/internal/config_agent/adapters/puppet/parser.rb
+++ utils/internal/config_agent/adapters/puppet/parser.rb
@@ -1,24 +1,60 @@
 require 'rubygems'
 require 'pp'
 require 'puppet'
-=begin
-require "puppet/application/parser"
 
-command_line = 
-#<Puppet::Util::CommandLine:0xb72e1758
- @args=["validate", "master.pp"],
- @argv=["parser", "validate", "master.pp"],
- @stdin=#<IO:0xb75de578>,
- @subcommand_name="parser",
- @zero="/usr/bin/puppet">
+module XYZ
+  class Puppet
+    class ParseStructure < Hash
+    end
+    class ComponentPS < ParseStructure
+      def initialize(ast_item)
+        type =
+          if ast_item.kind_of?(::Puppet::Parser::AST::Hostclass)
+            "puppet_class"
+          elsif ast_item.kind_of?(::Puppet::Parser::AST::Definition)
+            "puppet_definition"
+          else
+            raise Error.new("unexpected type for ast_item")
+          end
+          self[:type] = type
+          self[:name] = ast_item.name
+          self[:attributes] = (ast_item.context[:arguments]||[]).map{|arg|AttributePS.new(arg)}
+      end
+    end 
 
+    class AttributePS < ParseStructure
+      def initialize(arg)
+        self[:name] = arg[0]
+        self[:default] =  default_value(arg[1]) if arg[1]
+      end
+     private
+      def default_value(default_obj)
+        if default_obj.kind_of?(::Puppet::Parser::AST::String)
+          default_obj.value
+        elsif default_obj.kind_of?(::Puppet::Parser::AST::Name)
+          default_obj.value
+        else
+          raise Error.new("unexpected type for an attribute default")
+        end
+      end
+    end
+  end
+end
 
-app = Puppet::Application::Parser.new(command_line)
-app.run
-=end
+#monkety patches
+class Puppet::Parser::AST::Definition
+  attr_reader :name
+end
+
+####
+
 file = ARGV[0]
-file = "/root/r8server-repo/puppet-mysql/manifests/classes/master.pp"
+file ||= "/root/r8server-repo/puppet-mysql/manifests/classes/master.pp"
 Puppet[:manifest] = file
 environment = "production"
 krt = Puppet::Node::Environment.new(environment).known_resource_types
-pp krt
+krt_code = krt.hostclass("").code
+krt_code.children.each do |ast_item|
+    pp XYZ::Puppet::ComponentPS.new(ast_item)
+end
+#pp krt_code

4637704f3a29d00ac5d0bb9ba73eadb05c63d897
root <root@ip-10-99-47-191.ec2.internal>
2011-10-11 18:03:42 +0000

    working on convert from refinement hash to version dependent yaml form

--- application/spec/parser_test.rb
+++ application/spec/parser_test.rb
@@ -32,5 +32,7 @@ meta_generator = XYZ::GenerateMeta.create("1.0")
 refinement_hash = meta_generator.generate_refinement_hash(r8_parse,module_name)
 #in between here refinement has would have through user interaction the user set the needed unknowns
 render_hash = refinement_hash.render_hash_form()
-pp render_hash
+require 'YAML'
+YAML::dump(render_hash,STDOUT)
+
 end
--- utils/internal/generate_meta/versions/V1.0.rb
+++ utils/internal/generate_meta/versions/V1.0.rb
@@ -5,7 +5,7 @@ module XYZ
     class ModuleMeta < ::XYZ::ModuleMeta
       def render_hash_form(opts={})
         ret = SimpleOrderedHash.new
-        ret[:version] = "1.0"
+        ret["version"] = "1.0"
         self[:components].each do |cmp|
           unless (not value(:include).nil?) and not value(:include)
             hash_key = cmp.required_value(:hash_key)
@@ -18,10 +18,10 @@ module XYZ
     class ComponentMeta < ::XYZ::ComponentMeta
       def render_hash_form(opts={})
         ret = SimpleOrderedHash.new
-        ret[:display_name] = required_value(:display_name)
-        ret.set?(:description,value(:description))
+        ret["display_name"] = required_value(:display_name)
+        ret.set?("description",value(:description))
         ext_ref = required_value(:external_ref)
-        ret[:external_ref] = convert_external_ref(ext_ref)
+        ret["external_ref"] = convert_external_ref(ext_ref)
         ret
       end
       private
@@ -29,13 +29,13 @@ module XYZ
         ret = SimpleOrderedHash.new
         ext_ref_key = 
           case ext_ref[:type]
-            when "puppet_class" then :class_name
-            when "puppet_definition" then :definition_name
+            when "puppet_class" then "class_name"
+            when "puppet_definition" then "definition_name"
             else raise Error.new("unexpected component type (#{ext_ref[:type]})")
           end
         #TODO: may need to append module name
         ret[ext_ref_key] = ext_ref[:name]
-        ret[:type] = ext_ref[:type]
+        ret["type"] = ext_ref[:type]
         ret
       end
     end

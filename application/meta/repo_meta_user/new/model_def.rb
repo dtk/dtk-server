81291469f68941c4990dc8d717c6b5eaab1d09c7
root <root@ip-10-212-91-49.ec2.internal>
2011-11-19 02:11:38 +0000

    add seed_repo_meta.rb

--- application/meta/repo_meta_user/new/model_def.rb
+++ application/meta/repo_meta_user/new/model_def.rb
@@ -3,6 +3,5 @@
   :table=>:user,
   :columns=>{
     :user_name => {:type=>:varchar, :size => 50}
-  },
-  :one_to_many => [:repo_meta_user_acl]
+  }
 }
--- application/meta/repo_meta_user_acl/new/model_def.rb
+++ application/meta/repo_meta_user_acl/new/model_def.rb
@@ -2,13 +2,13 @@
   :schema=>:repo,
   :table=>:user_acl,
   :columns=>{
-    :repo_id =>{
+    :repo_meta_user_id =>{
       :type=>:bigint,
-      :foreign_key_rel_type=>:repo_meta,
+      :foreign_key_rel_type=>:repo_meta_user,
       :on_delete=>:cascade,
       :on_update=>:cascade
     },
     :access_rights => {:type=>:json}
   },
-  :many_to_one=> [:repo_meta_user]
+  :many_to_one=> [:repo_meta]
 }

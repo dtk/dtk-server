283e79b0d9eb7b0013c9696851b666f25e1688c9
root <root@domU-12-31-39-09-AC-D5.compute-1.internal>
2011-11-22 01:47:48 +0000

    code to extract from compressed file into a target dir

--- utils/internal/extract/adapters/tar_gz.rb
+++ utils/internal/extract/adapters/tar_gz.rb
@@ -1,21 +1,33 @@
 require 'zlib'
 require 'archive/tar/minitar'
+require 'fileutils'
 module XYZ
   class ExtractTarGz < Extract
-    class << self
-      def into_directory(compressed_file,target_dir,opts={})
-        Zlib::GzipReader.open(compressed_file) do |gzip|
-          Archive::Tar::Minitar::Reader.open(gzip).each do |entry|
-            relative_path = ret_relative_path(entry.name,opts)
-            unless skip(relative_path,opts)
-              pp [:foo,relative_path]
-              #entry.read
-              #entry.close
+    def self.into_directory(compressed_file,target_dir,opts={})
+      Zlib::GzipReader.open(compressed_file) do |gzip|
+        Archive::Tar::Minitar::Reader.open(gzip).each do |entry|
+          relative_path = ret_relative_path(entry.name,opts)
+          next if skip(relative_path,opts)
+          full_path = "#{target_dir}/#{relative_path}"
+          if entry.directory?()
+            #TODO:if can be sure that subdirectories allwos appear first and with pruning than dont need _p version
+            FileUtils.mkdir_p(full_path)
+          elsif entry.file?()
+            begin
+             copy_file(entry,full_path)
+             ensure
+              entry.close
             end
           end
         end
       end
-     private
+    end
+   private
+    def self.copy_file(entry,out_file_path)
+      #TODO: for larger files need more increemntal way of doing this
+      File.open(out_file_path,"w") do |out_file|
+        out_file << entry.read
+      end
     end
   end
 end

edfa62ec644ecbc25d8bb18578c42ca2de7a6ca3
root <root@domU-12-31-39-14-F5-32.compute-1.internal>
2011-12-03 02:23:42 +0000

    bootstrap that adds super user, selected user

--- application/model/user_group.rb
+++ application/model/user_group.rb
@@ -1,4 +1,11 @@
 module XYZ
   class UserGroup < Model
+    def self.get_from_groupname(model_handle,groupname)
+      sp_hash = {
+        :cols => [:id, :groupname],
+        :filter => [:eq, :groupname,groupname]
+      }
+      get_obj(model_handle,sp_hash)
+    end
   end
 end

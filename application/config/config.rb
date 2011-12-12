ac4f34eb1dae34639b704d44f3909aa72634e1c0
unknown <Dell Studio XPS@.(none)>
2011-12-12 09:09:49 -0800

    login screen updated, logout button/route added

--- application/config/config.rb
+++ application/config/config.rb
@@ -29,7 +29,7 @@ R8::Config[:avatar_base_uri] = R8::Config[:base_uri] + "/images/user_avatars"
 R8::Config[:login][:path] = "/xyz/user/login"
 R8::Config[:login][:resgister] = "/xyz/user/register"
 #TODO: below is stub
-R8::Config[:login][:redirect] = "/xyz/workspace/index"
+R8::Config[:login][:redirect] = "/xyz/ide/index"
 
 
 #Database related config params

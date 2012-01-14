3acb19b6d7ce6584d28ed408465ec200487a4709
Rich Pelavin <rich.pelavin@gmail.com>
2012-01-14 05:56:16 +0000

    some restructuring before putting back in node groups

---
+++ application/model/component/clone.rb
@@ -0,0 +1,133 @@
+module XYZ
+  module ComponentClone
+    def determine_cloned_components_parent(specified_target_idh)
+      #TODO: may deprecate if not using; previously mapped extensions to parents; now putting them with node as tehir parent
+      return specified_target_idh if SubComponentComponentMapping.empty?
+      cmp_fs = FieldSet.opt([:id,:display_name,:component_type],:component)
+      specified_target_id = specified_target_idh.get_id()
+      cmp_ds = Model.get_objects_just_dataset(model_handle,{:id => id()},cmp_fs)
+      mapping_ds = SQL::ArrayDataset.create(self.class.db,SubComponentComponentMapping,model_handle.createMH(:mapping))
+                          
+      first_join_ds =  cmp_ds.graph(:inner,mapping_ds,{:component => :component_type})
+
+      parent_cmp_ds = Model.get_objects_just_dataset(model_handle,{:node_node_id => specified_target_idh.get_id()},cmp_fs)
+
+      final_join_ds = first_join_ds.graph(:inner,parent_cmp_ds,{:component_type => :parent},{:convert => true})
+      
+      target_info = final_join_ds.all().first
+      return specified_target_idh unless target_info
+      target_info[:component2].id_handle()
+    end
+
+    SubComponentComponentMapping = 
+      [
+#       {:component => "postgresql__db", :parent => "postgresql__server"}
+      ]
+
+    def clone_post_copy_hook(clone_copy_output,opts={})
+      component_idh = clone_copy_output.id_handles.first
+      add_needed_sap_attributes(component_idh)
+      parent_action_id_handle = id_handle().get_top_container_id_handle(:datacenter)
+      StateChange.create_pending_change_item(:new_item => component_idh, :parent => parent_action_id_handle)
+    end
+
+    #handles copying over if needed component template and implementation into project
+    def clone_post_copy_hook_into_node(node)
+      #processing so that component's implementation and template are cloned to project
+      #self will have implementation_id set to library implementation and ancestor_id set to library template
+      #need to search project to see if has implementation that matches (same repo)
+      #if match then set new_cmps impelemntation_id to this otehrwise need to clone implementaion in library to project
+      proj = node.get_project()
+      proj_idh = proj.id_handle()
+
+      #create new project implementation if needed
+      library_impl = id_handle(:model_name => :implementation,:id => self[:implementation_id]).create_object()
+      new_impl_id = library_impl.clone_into_project_if_needed(proj).get_id()
+
+      #find new ancestor_id
+      library_cmp_tmpl_idh = id_handle(:id => self[:ancestor_id])
+      #ok to do below because self and library_cmp_tmpl share attribute values
+      library_cmp_tmpl =  library_cmp_tmpl_idh.create_object
+      proj_cmp_tmpl_idh = find_match_in_project(proj_idh)
+      new_ancestor_id = proj_cmp_tmpl_idh ? proj_cmp_tmpl_idh.get_id() : proj.clone_into(library_cmp_tmpl,{:implementation_id => new_impl_id,:extended_base => self[:extended_base]})
+
+      update_from_hash_assignments(:implementation_id => new_impl_id, :ancestor_id => new_ancestor_id)
+    end
+
+    def source_clone_info_opts()
+      raise Error.new("component#source_clone_info_opts is deprecated")
+      {:ret_new_obj_with_cols => [:id,:implementation_id,:component_type,:version,:ancestor_id]}
+    end
+
+    def add_needed_sap_attributes(component_idh)
+      sp_hash = {
+        :filter => [:and, [:oneof, :basic_type, BasicTypeInfo.keys]],
+        :columns => [:id, :display_name,:basic_type]
+      }
+      component = component_idh.get_objects_from_sp_hash(sp_hash).first
+      return nil unless component
+      
+      basic_type_info = BasicTypeInfo[component[:basic_type]]
+      sap_dep = basic_type_info[:sap_dependency]
+
+      sap_info = component.get_objects_from_sp_hash(:columns => [:id, :display_name, sap_dep]).first
+      unless sap_info
+        Log.error("error in finding sap dependencies for component #{component_idh}")
+        return nil
+      end
+
+      sap_config_attr = sap_info[:attribute]
+      par_attr = sap_info[:parent_attribute]
+      node = sap_info[:node]
+
+      sap_val = basic_type_info[:fn].call(sap_config_attr[:attribute_value],par_attr[:attribute_value])
+      sap_attr_row = Aux::hash_subset(basic_type_info,[{:sap => :ref},{:sap => :display_name},:description,:semantic_type,:semantic_type_summary])
+      sap_attr_row.merge!(
+         :component_component_id => component[:id],
+         :value_derived => sap_val,
+         :is_port => true,
+         :hidden => true,
+         :data_type => "json")
+
+      attr_mh = component_idh.createMH(:model_name => :attribute, :parent_model_name => :component)
+      sap_attr_idh = self.class.create_from_row(attr_mh,sap_attr_row, :convert => true)
+
+      return nil unless sap_attr_idh
+      AttributeLink.create_links_sap(basic_type_info,sap_attr_idh,sap_config_attr.id_handle(),par_attr.id_handle(),node.id_handle())
+    end
+   private
+    #TODO: some of these are redendant of whats in sap_dependency_X like "sap__l4" and "sap__db"
+    BasicTypeInfo = {
+      "database" => {
+        :sap_dependency => :sap_dependency_database,
+        :sap => "sap__db",
+        :sap_config => "sap_config__db",
+        :sap_config_fn_name => "sap_config_conn__db",
+        :parent_attr => "sap__l4",
+        :parent_fn_name => "sap_conn__l4__db",
+        :semantic_type => {":array" => "sap__db"}, #TODO: need the  => {"application" => service qualification)
+        :semantic_type_summary => "sap__db",
+        :description => "DB access point",
+        :fn => lambda{|sap_config,par|compute_sap_db(sap_config,par)}
+      }
+    }
+   protected
+    def self.compute_sap_db(sap_config_val,par_vals)
+      #TODO: check if it is this simple; also may not need and propagate as byproduct of adding a link 
+      par_vals.map{|par_val|sap_config_val.merge(par_val)}
+    end
+   private
+    def find_match_in_project(project_idh)
+      sp_hash = {
+        :filter => [:and,
+                    [:eq, :project_project_id, project_idh.get_id()],
+                    [:eq, :component_type, self[:component_type]],
+                    [:eq,:version, self[:version]]
+                   ],
+        :cols => [:id]
+      }
+      row = Model.get_objects_from_sp_hash(model_handle,sp_hash).first
+      row && row.id_handle()
+    end
+  end
+end
--- application/model/node.rb
+++ application/model/node.rb
@@ -1,6 +1,8 @@
+r8_nested_require('node','clone')
 r8_nested_require('node','meta')
 module XYZ
   class Node < Model
+    include NodeClone
     set_relation_name(:node,:node)
     extend NodeMetaClassMixin 
 
@@ -364,71 +366,6 @@ module XYZ
       dangling_links = (ndx_in_links.keys - ndx_out_links.keys).map{|id|ndx_in_links[id]}
       dangling_links += (ndx_out_links.keys - ndx_in_links.keys).map{|id|ndx_out_links[id]}
       [connected_links,dangling_links]
-    end
-
-    ################## cloning related methods
-    def add_model_specific_override_attrs!(override_attrs,target_obj)
-      override_attrs[:type] ||= "staged"
-      override_attrs[:ref] ||= SQL::ColRef.concat("s-",:ref)
-      override_attrs[:display_name] ||= SQL::ColRef.concat{|o|["s-",:display_name,o.case{[[{:ref_num=> nil},""],o.concat("-",:ref_num)]}]}
-    end
-    def source_clone_info_opts()
-      {:ret_new_obj_with_cols => [:id,:external_ref]}
-    end
-
-    def clone_post_copy_hook(clone_copy_output,opts={})
-      component = clone_copy_output.objects.first
-      #handles copying over if needed component template and implementation into project
-      component.clone_post_copy_hook_into_node(self)
-
-      component_idh = clone_copy_output.id_handles.first
-
-      #get the link defs/component_ports associated with components on the node; this is used
-      #to determine if need to add internal links and for port processing
-      node_link_defs_info = get_objs(:cols => [:node_link_defs_info])
-      component_id = component.id()
-      
-      ###create needed component ports
-      ndx_for_port_update = Hash.new
-      component_link_defs = node_link_defs_info.map  do |r|
-        link_def = r[:link_def]
-        if link_def[:component_component_id] == component_id
-          ndx_for_port_update[link_def[:id]] = r
-          link_def 
-        end
-      end.compact
-
-      create_opts = {:returning_sql_cols => [:link_def_id,:id,:display_name,:type,:connected]}
-      new_cmp_ports = Port.create_needed_component_ports(component_link_defs,self,component,create_opts)
-
-      #update node_link_defs_info with new ports
-      new_cmp_ports.each do |port|
-        ndx_for_port_update[port[:link_def_id]].merge!(:port => port)
-      end
-
-      #TODO: more efficient way to do this; instead include all needed columns in :returning_sql_cols above
-      if opts[:outermost_ports] 
-        port_mh = model_handle(:port)
-        external_port_idhs = new_cmp_ports.map do |port_hash|
-          port_mh.createIDH(:id => port_hash[:id]) if ["component_internal_external","component_external"].include?(port_hash[:type])
-        end.compact
-        unless external_port_idhs.empty?
-          new_ports = Model.get_objs_in_set(external_port_idhs, {:cols => Port.common_columns})
-          i18n = get_i18n_mappings_for_models(:component,:attribute)
-          new_ports.map do |port|
-            port.materialize!(Port.common_columns)
-            port[:name] = get_i18n_port_name(i18n,port)
-          end
-          opts[:outermost_ports] += new_ports
-        end
-      end
-
-      #### end create needed component ports ####
-                                                             
-      LinkDef.create_needed_internal_links(self,component,node_link_defs_info)
-
-      parent_action_id_handle = get_parent_id_handle()
-      StateChange.create_pending_change_item(:new_item => component_idh, :parent => parent_action_id_handle)
     end
 
     #TODO: quick hack

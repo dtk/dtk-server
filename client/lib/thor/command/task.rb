50a3cc374404be68258d715a6eb55c9747eb04ff
Rich Pelavin <rich.pelavin@gmail.com>
2012-01-15 07:23:34 +0000

    experimenting with thor for cli parsing

--- client/lib/thor/command/task.rb
+++ client/lib/thor/command/task.rb
@@ -2,7 +2,7 @@ module R8::Client
   class TaskCommand < CommandBase
     desc "status", "task status"
     method_option "detail-level",:default => "summary", :aliases => "-d", :desc => "detail level to report task status"
-    method_option "task-id", :aliases => "-d", :desc => "Task ID"
+    method_option "task-id", :aliases => "-t", :desc => "Task ID"
 
     def status
       detail_level = options["detail-level"]

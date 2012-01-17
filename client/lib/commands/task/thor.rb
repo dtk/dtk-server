5cb5dbbc5b70da5b6a2e6e6b69d8c258dd1848ca
Rich Pelavin <rich.pelavin@gmail.com>
2012-01-17 03:55:31 +0000

    updating Thor descriptions in task

--- client/lib/commands/task/thor.rb
+++ client/lib/commands/task/thor.rb
@@ -1,36 +1,6 @@
 module R8::Client
   class TaskCommand < CommandBaseThor
 
-    desc "status [TASK-ID]", "task status"
-    method_option "detail-level",:default => "summary", :aliases => "-d", :desc => "detail level to report task status"
-    def status(task_id=nil)
-      detail_level = options["detail-level"]
-      body = Hash.new
-      body[:detail_level] = detail_level
-      body[:task_id] = task_id if task_id
-      post rest_url("task/state_info"),body
-    end
-
-    desc "commit_changes", "commit changes"
-    def commit_changes()
-      post rest_url("task/create_task_commit_changes")
-    end
-
-    desc "execute [TASK-ID]", "execute task"
-    def execute(task_id)
-      post rest_url("task/execute"), "task_id" => task_id
-    end
-
-    desc "commit_changes_and_execute", "commit changes and execute task"
-    def commit_changes_and_execute()
-      response = commit_changes()
-      if response.ok?
-        execute(response.data["task_id"])
-      else
-        response
-      end
-    end
-
     desc "list","List tasks"
     def list()
       #TODO: just hard coded params now
@@ -40,6 +10,37 @@ module R8::Client
       search_hash.set_order_by!(:created_at,"DESC")
       post rest_url("task/list"), search_hash.post_body_hash()
     end
+
+    desc "status [TASK-ID]", "Return task status; if no TASK-ID then information about most recent task"
+    method_option "detail-level",:default => "summary", :aliases => "-d", :desc => "detail level to report task status"
+    def status(task_id=nil)
+      detail_level = options["detail-level"]
+      body = Hash.new
+      body[:detail_level] = detail_level
+      body[:task_id] = task_id if task_id
+      post rest_url("task/state_info"),body
+    end
+
+    desc "commit_changes", "Commit changes"
+    def commit_changes()
+      post rest_url("task/create_task_commit_changes")
+    end
+
+    desc "execute TASK-ID", "Execute task"
+    def execute(task_id)
+      post rest_url("task/execute"), "task_id" => task_id
+    end
+
+    desc "commit_changes_and_execute", "Commit changes and execute task"
+    def commit_changes_and_execute()
+      response = commit_changes()
+      if response.ok?
+        execute(response.data["task_id"])
+      else
+        response
+      end
+    end
+
   end
 end
 

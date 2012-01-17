beeb88b9e0965cacf1b3e2e83c447a85cdc40813
Rich Pelavin <rich.pelavin@gmail.com>
2012-01-17 17:47:59 +0000

    so gui delete will delete node groups

--- application/model/node_group.rb
+++ application/model/node_group.rb
@@ -4,6 +4,10 @@ module XYZ
       #TODO: stub
       Model.delete_instance(id_handle())
     end
+
+    def destroy_and_delete
+      delete()
+    end
     private
     #TODO: can we avoid explicitly pacing this here
      def self.db_rel()

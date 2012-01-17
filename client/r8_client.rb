3ffaec28f561f8ef6b0d3b7da273d88905229bc8
Your Name <you@example.com>
2012-01-17 06:17:32 +0000

    update

--- client/r8_client.rb
+++ client/r8_client.rb
@@ -1,5 +1,5 @@
 require File.expand_path("lib/require_first", File.dirname(__FILE__))
-require File.expand_path("lib/aux", File.dirname(__FILE__))
+require File.expand_path("lib/auxiliary", File.dirname(__FILE__))
 require File.expand_path("lib/core", File.dirname(__FILE__))
 require File.expand_path("lib/parser", File.dirname(__FILE__))
 require File.expand_path("lib/view_processor", File.dirname(__FILE__))

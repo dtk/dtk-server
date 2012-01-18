6ec2d0c1c9d33d9a472a75bb37725d2ebcc9ca00
Rich Pelavin <rich.pelavin@gmail.com>
2012-01-18 05:14:58 +0000

    notes on ways in which cloning component to node group can be made more efficient

--- application/model/node_group.rb
+++ application/model/node_group.rb
@@ -10,6 +10,12 @@ module XYZ
     def clone_post_copy_hook(clone_copy_output,opts={})
       super
       clone_source_obj = clone_copy_output.source_object
+      
+      #clone the component in all the nodes taht are a member of this node group
+      #TODO: started with brute force way to do this. There is many different ways that teh computation can be optimized, such s
+      #bulk cloning, flags that idniacet ops to skip when node is being cloned to mirror what is in node group
+      # shortcutting implementation pointers by having it be an overriding attribute
+      #copy to nodes the output object after post processing not source object
       override_attrs = {}
       node_clone_opts = [:ret_new_obj_with_cols].inject({}) do |h,k|
         opts.has_key?(k) ? h.merge(k => opts[k]) : h

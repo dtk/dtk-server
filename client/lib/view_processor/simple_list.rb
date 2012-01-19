118a2ca470c41992aeb516a7f785ee6a6596b7a3
Rich Pelavin <rich.pelavin@gmail.com>
2012-01-19 03:05:20 +0000

    putting in simple list rendering cli fn

--- client/lib/view_processor/simple_list.rb
+++ client/lib/view_processor/simple_list.rb
@@ -2,21 +2,31 @@ require 'erubis'
 module R8
   module Client
     class ViewProcSimpleList < ViewProcessor
-      def render(hash)
+      def render(hash,ident=0)
         pp_adapter = ViewProcessor.get_adapter("hash_pretty_print",@command_class)
-        pp_hash = pp_adapter.render(hash)
+        ordered_hash = pp_adapter.render(hash)
+        #TODO: need to break apart if has nested hashes
+        scalar_value_render(ordered_hash,ident)
+      end
+     private
+      def scalar_value_render(ordered_hash,ident)
         template_bindings = {
-          :pretty_print_hash => pp_hash
+          :ordered_hash => ordered_hash,
+          :ident => ident_str(ident),
+          :identp2 => ident_str(ident+2)
         }
         SimpleListTemplate.result(template_bindings)
 
       end
+      def ident_str(n)
+        Array.new(n, " ").join
+      end
 SimpleListTemplate = Erubis::Eruby.new <<eos
-<% keys = pretty_print_hash.keys %>
+<% keys = ordered_hash.keys %>
 <% first = keys.shift  %>
-<%= pretty_print_hash[first] %>
+<%= ident %><%= ordered_hash[first] %>
 <% keys.each do |k| %>
-  <%= k %>:  <%= pretty_print_hash[k] %>
+<%= identp2 %><%= k %>:  <%= ordered_hash[k] %>
 <% end %>
 eos
 

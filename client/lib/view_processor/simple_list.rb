1820d12776540cb9bb59f080b9de8c8cca32bfa5
Rich Pelavin <rich.pelavin@gmail.com>
2012-01-19 20:15:45 +0000

    making simple list cli viewdef treat nested hashes

--- client/lib/view_processor/simple_list.rb
+++ client/lib/view_processor/simple_list.rb
@@ -5,8 +5,8 @@ module R8
       def render(hash,ident_info={})
         pp_adapter = ViewProcessor.get_adapter("hash_pretty_print",@command_class)
         ordered_hash = pp_adapter.render(hash)
-        #find next value that is type pretty print hash
-        first,nested,rest = find_first_pp_hash_vals(hash)
+        #find next value that is type pretty print hash or array
+        first,nested,rest = find_first_non_scalar(ordered_hash)
         ret = String.new
         unless first.empty?
           ret = scalar_value_render(ordered_hash,ident_info)
@@ -16,7 +16,9 @@ module R8
             :ident => (ident_info[:ident]||0) +IdentAdd,
             :prefix => nested.keys.first
           }
-          ret << render(nested.values.first,ident_info_nested)
+          vals = nested.values.first
+          vals = [vals] unless vals.kind_of?(Array)
+          vals.each{|val|ret << render(val,ident_info_nested)}
         end
         unless rest.empty?
           ret << render(rest,ident_info)
@@ -26,20 +28,20 @@ module R8
      private
 
       IdentAdd = 2
-      def find_first_pp_hash_vals(hash)
+      def find_first_non_scalar(ordered_hash)
         found = nil
-        keys = hash.keys
+        keys = ordered_hash.keys
         keys.each_with_index do |k,i|
-          if hash[k].kind_of?(Hash)
+          if ordered_hash[k].kind_of?(Hash) or ordered_hash[k].kind_of?(Array)
             found = i
             break
           end
         end
         if found.nil?
-          empty_hash = hash.class.new
-          [hash,empty_hash,empty_hash]
+          empty_ordered_hash = ordered_hash.class.new
+          [ordered_hash,empty_ordered_hash,empty_ordered_hash]
         else
-          [keys[0,found],keys[found,1],keys[found+1,keys.size-1]].map{|key_array|hash.slice(*key_array)}
+          [keys[0,found],keys[found,1],keys[found+1,keys.size-1]].map{|key_array|ordered_hash.slice(*key_array)}
         end
       end
 

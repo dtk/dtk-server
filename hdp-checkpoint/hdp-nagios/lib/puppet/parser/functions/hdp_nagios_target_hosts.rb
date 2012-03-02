eee6518b35e035564aa685ccfeff63feefb5dde2
Rich Pelavin <rich.pelavin@gmail.com>
2012-03-02 17:02:31 +0000

    update to hdp

--- hdp-checkpoint/hdp-nagios/lib/puppet/parser/functions/hdp_nagios_target_hosts.rb
+++ hdp-checkpoint/hdp-nagios/lib/puppet/parser/functions/hdp_nagios_target_hosts.rb
@@ -1,7 +1,8 @@
 module Puppet::Parser::Functions
   newfunction(:hdp_nagios_target_hosts, :type => :rvalue) do |args|
     args = [args].flatten(1)
-    types = args[0].split("'")
-    types.map{|t|scope.function_hdp_host(t)}.map{|h|h.empty? ? [] : [h].flatten(1)}.flatten
+    types = args[0].split(",")
+    Puppet::Parser::Functions.autoloader.loadall
+    types.map{|t|function_hdp_host(t)}.map{|h|h.empty? ? [] : [h].flatten(1)}.flatten
   end
 end

806d7942639aa663af85f8fe410d8b3df8bb23e1
Rich Pelavin <rich.pelavin@gmail.com>
2012-03-21 05:48:53 +0000

    update

--- utils/internal/model/clone.rb
+++ utils/internal/model/clone.rb
@@ -322,11 +322,12 @@ module XYZ
 
       def get_nested_objects__assembly(model_handle,target_parent_mh,assembly_objs_info,recursive_override_attrs,opts)
         raise Error.new("Not treating assembly_objs_info with more than 1 element") unless assembly_objs_info.size == 1
-        new_assembly_assign = {:assembly_id => assembly_objs_info.first[:id]}
-        ancestor_id = assembly_objs_info.first[:ancestor_id]
+        assembly_obj_info = assembly_objs_info.first
+        ancestor_id = assembly_obj_info[:ancestor_id]
         target_parent_mn = target_parent_mh[:model_name]
         model_name = model_handle[:model_name]
-        parent_id_col = DB.parent_field(target_parent_mn,model_name)
+        new_assembly_assign = {:assembly_id => assembly_obj_info[:id]}
+        new_par_assign = {DB.parent_field(target_parent_mn,model_name) => assembly_obj_info[:parent_id]}
         AssemblyChildren.map do |nested_model_name|
           nested_mh = model_handle.createMH(:model_name => nested_model_name, :parent_model_name => target_parent_mn)
           override_attrs = new_assembly_assign.merge(ret_child_override_attrs(nested_mh,recursive_override_attrs))
@@ -338,10 +339,10 @@ module XYZ
           end
           create_opts = {:duplicate_refs => :allow, :returning_sql_cols => [:ancestor_id,:assembly_id]}
           #putting in nulls to null-out; more efficient to omit this columns in create
-          parent_rels = (DB_REL_DEF[nested_model_name][:many_to_one]||[]).inject({:old_par_id => ancestor_id}) do |hash,pos_par|
-            hash.merge(matching_models?(pos_par,target_parent_mn) ? {} : {DB.parent_field(pos_par,model_name) => SQL::ColRef.null_id})
+          parent_rel = (DB_REL_DEF[nested_model_name][:many_to_one]||[]).inject({:old_par_id => ancestor_id}) do |hash,pos_par|
+            hash.merge(matching_models?(pos_par,target_parent_mn) ? new_par_assign : {DB.parent_field(pos_par,model_name) => SQL::ColRef.null_id})
           end
-          {:model_handle => nested_mh, :clone_par_col => :assembly_id, :parent_rels => parent_rels, :override_attrs => override_attrs, :create_opts => create_opts}
+          {:model_handle => nested_mh, :clone_par_col => :assembly_id, :parent_rels => [parent_rel], :override_attrs => override_attrs, :create_opts => create_opts}
         end
       end
       AssemblyChildren = [:node,:attribute_link,:port_link]

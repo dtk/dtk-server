92514baa3de7a9870101fcddb7825d4fa26ac6b1
Rich Pelavin <rich.pelavin@gmail.com>
2012-04-16 23:36:08 +0000

    update

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,10 @@
+ ./spec/clear_db.sh ; ./spec/dbrebuild.rb ; ./spec/import/initialize.rb --delete
+ ./spec/import/add_user.rb joe --create-private stdlib,hdp,hdp-hadoop,hdp-zookeeper,hdp-hbase,hdp-nagios,hdp-ganglia,hdp-dashboard,hdp-monitor-webserver
+ ./spec/import/add_user.rb joe --create-private mysql,hdp-mysql,hdp-hcat,hdp-hive,hdp-pig,hdp-sqoop
+
+
+
+
+
+
+

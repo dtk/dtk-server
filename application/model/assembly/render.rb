4bd4aebcdaba12b616d171efb64cece4c84fb0dc
Rich Pelavin <rich.pelavin@gmail.com>
2012-05-10 16:23:41 +0000

    rendering assemblies in serialized form

--- application/model/assembly/render.rb
+++ application/model/assembly/render.rb
@@ -4,6 +4,8 @@ module XYZ
     def render(opts={})
       nested_objs = get_nested_objects_for_render()
       pp nested_objs
+      output_hash = output_hash_form(nested_objs)
+      pp output_hash
     end
    private
     def get_nested_objects_for_render()
@@ -32,11 +34,7 @@ module XYZ
         :cols => [:id,:display_name,:type,:direction,:node_node_id],
         :filter => [:oneof, :node_node_id, nested_node_ids]
       }
-      port_rows = Model.get_objs(model_handle(:port),sp_hash)
-      port_rows.each do |r|
-        node = ndx_nodes[r[:node_node_id]]
-        (node[:ports] ||= Array.new) << r
-      end
+      ports = Model.get_objs(model_handle(:port),sp_hash)
 
       #get port links
       sp_hash = {
@@ -45,8 +43,53 @@ module XYZ
       }
       port_links = Model.get_objs(model_handle(:port_link),sp_hash)
 
-      {:nodes => ndx_nodes.values, :port_links => port_links, :attributes => assembly_attrs, :implementations => ndx_impls.values}
+      {:nodes => ndx_nodes.values, :ports => ports, :port_links => port_links, :attributes => assembly_attrs, :implementations => ndx_impls.values}
     end
 
+    def output_hash_form(nested_objs)
+      ret = SimpleOrderedHash.new()
+      #add modules
+      ret[:modules] = nested_objs[:implementations].map do |impl|
+        #TODO: stub that ignores verion = 1
+        version = impl[:version_num]
+        ((version.nil? or version == 1) ? impl[:module_name] : "#{impl[:module_name]}-#{version}") 
+      end
+
+      #add assembly level attributes
+      #TODO: stub
+
+      #add nodes and components
+      ret[:nodes] = nested_objs[:nodes].inject(SimpleOrderedHash.new()) do |h,node|
+        node_name = node[:display_name]
+        cmp_info = node[:components].map{|cmp|component_name_output_form(cmp[:component_type])}
+        h.merge(node_name => cmp_info)
+      end
+
+      #add port links
+      ndx_ports = nested_objs[:ports].inject(Hash.new){|h,p|h.merge(p[:id] => p)}
+      ret[:port_links] = nested_objs[:port_links].map do |pl|
+        input_port = ndx_ports[pl[:input_id]]
+        output_port = ndx_ports[pl[:output_id]]
+       {port_output_form(input_port,:input) => port_output_form(input_port,:output)}
+      end
+      ret
+    end
+
+    def port_output_form(port,dir)
+      #example internal form component_external___hdp-hadoop__namenode___namenode_conn
+      if port[:display_name] =~ /component_external___(.+)__(.+)___(.+$)/
+        mod = $1;cmp = $2;port_name = $3
+       ret = "#{mod}#{Module_seperator}#{cmp}#{Module_seperator}#{port_name}"
+       ((dir == :input) ? "#{ret}_ref" : ret)
+      else
+        ralse Error.new("unexpected display name #{port[:display_name]}")
+      end
+    end
+   
+    def component_name_output_form(internal_format)
+      internal_format.gsub(/__/,Module_seperator)
+    end
+    
+    Module_seperator = "::"
   end
 end

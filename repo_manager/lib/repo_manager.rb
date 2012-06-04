c9875d8890ed947193d4064cbaa23dfa5bae8a87
root <root@ip-10-212-142-144.ec2.internal>
2012-06-04 01:52:22 +0000

    repo_manager/lib/repo_manager.rb

--- repo_manager/lib/repo_manager.rb
+++ repo_manager/lib/repo_manager.rb
@@ -1,5 +1,5 @@
 #TODO: refering to common r8server apps, which wil be made to a common gem
-require File.expand_path('../../R8Server/application/require_first', File.dirname(__FILE__))
+require File.expand_path('../../application/require_first', File.dirname(__FILE__))
 module R8
 end
 %w{config git_bare_repo gitolite_manager}.each do |f|

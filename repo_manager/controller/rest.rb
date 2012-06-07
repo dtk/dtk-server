abd22281ee32c44bfce8cf3413f53df4cb94e851
root <root@domU-12-31-39-12-39-62.compute-1.internal>
2012-06-07 03:54:52 +0000

    update

--- repo_manager/controller/rest.rb
+++ repo_manager/controller/rest.rb
@@ -1,5 +1,9 @@
 class RestController < Controller
   helper :rest
+  engine :none
+  provide(:html, :type => 'text/html'){|a,s|s.to_json} 
+  provide(:json, :type => 'application/json'){|a,s|s.to_json}
+
   def index
     rest_ok_response
   end

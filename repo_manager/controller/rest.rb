c755d63a0b5e63f37508433b4eabc30189216eae
root <root@domU-12-31-39-0A-14-F7.compute-1.internal>
2012-06-08 01:26:20 +0000

    update

--- repo_manager/controller/rest.rb
+++ repo_manager/controller/rest.rb
@@ -9,27 +9,34 @@ class RestController < Controller
     rest_ok_response :version => "0.1"
   end
 
-  def get_file_content(repo_name,path)
-    content = GitoliteManager::Repo.new(repo_name).file_content(path)
-    rest_ok_response :content => content
+  class AdminController < self
+    map "/rest/admin"
+
+    def add_user()
+      username,rsa_pub_key = ret_non_null_request_params(:username,:rsa_pub_key)
+      GitoliteManager::Repo::Admin.add_user(username,rsa_pub_key)
+      rest_ok_response :usename => username
+    end
+
+    def delete_user()
+      username = ret_non_null_request_params(:username)
+      GitoliteManager::Repo::Admin.delete_user(username)
+      rest_ok_response :usename => username
+    end
+
+    def create_repo_and_user()
+      repo_name,username,rsa_pub_key = ret_non_null_request_params(:repo_name,:username,:rsa_pub_key)
+      GitoliteManager::Repo::Admin.create_repo_and_user(repo_name,username,rsa_pub_key,"RW+")
+      rest_ok_response :repo_name => repo_name
+    end
   end
 
-  def add_user()
-    username,rsa_pub_key = ret_non_null_request_params(:username,:rsa_pub_key)
-    GitoliteManager::Repo::Admin.add_user(username,rsa_pub_key)
-    rest_ok_response :usename => username
-  end
-
-  def delete_user()
-    username = ret_non_null_request_params(:username)
-    GitoliteManager::Repo::Admin.delete_user(username)
-    rest_ok_response :usename => username
-  end
-
-  def create_repo_and_user()
-    repo_name,username,rsa_pub_key = ret_non_null_request_params(:repo_name,:username,:rsa_pub_key)
-    GitoliteManager::Repo::Admin.create_repo_and_user(repo_name,username,rsa_pub_key,"RW+")
-    rest_ok_response :repo_name => repo_name
+  class RepoController < self
+    map "/rest/repo"
+    def get_file_content(repo_name,path)
+      content = GitoliteManager::Repo.new(repo_name).file_content(path)
+      rest_ok_response :content => content
+    end
   end
 
   def error

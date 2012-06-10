d508b54070975edae6fed75b2b1fd2bc00f1a2cd
root <root@domU-12-31-39-07-6E-7F.compute-1.internal>
2012-06-10 01:35:40 +0000

    update

--- repo_manager/lib/repo_manager/gitolite_manager/repo.rb
+++ repo_manager/lib/repo_manager/gitolite_manager/repo.rb
@@ -1,7 +1,5 @@
 module R8::RepoManager; class GitoliteManager
   class Repo < self
-    #TODO: instaed may make child of GitRepo::ObjectAccess; or use missing method and send to @repo
-    #but check in explicit list
     def initialize(repo_name,branch='master')
       #updating and querying from the bare repo   
       @repo = GitRepo::ObjectAccess.new(bare_repo_dir(repo_name),branch)
@@ -17,7 +15,6 @@ module R8::RepoManager; class GitoliteManager
         @repo.add_or_replace_file(file_path,content)
       end
     end
-
 
     def delete_file_and_commit(file_path,commit_msg=nil)
       commit_msg = "updating file #{file_path}"
@@ -38,31 +35,5 @@ module R8::RepoManager; class GitoliteManager
     def respond_to?(name)
       !!(RepoMethods.include?(name) || super)
     end
-
-=begin
-    def add_or_replace_file(file_path,content)
-      @repo.add_or_replace_file(file_path,content)
-    end
-
-    def branches()
-      @repo.branches()
-    end
-    
-    def create_branch(new_branch)
-      @repo.create_branch(new_branch)
-    end
-
-    def file_content(file_path)
-      @repo.file_content(file_path)
-    end
-
-    def ls_r(depth=nil,opts={})
-      @repo.ls_r(depth,opts)
-    end
-
-    def commit_context(commit_msg,&block)
-      @repo.commit_context(commit_msg,&block)
-    end
-=end
   end
 end;end

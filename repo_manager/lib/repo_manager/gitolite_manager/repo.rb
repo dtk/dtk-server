762bfd6f25f8b28f456d2573c286887c3f171d77
root <root@domU-12-31-39-02-D9-72.compute-1.internal>
2012-06-10 01:02:18 +0000

    update

--- repo_manager/lib/repo_manager/gitolite_manager/repo.rb
+++ repo_manager/lib/repo_manager/gitolite_manager/repo.rb
@@ -18,15 +18,30 @@ module R8::RepoManager; class GitoliteManager
       end
     end
 
-    def add_or_replace_file(file_path,content)
-      @repo.add_or_replace_file(file_path,content)
-    end
 
     def delete_file_and_commit(file_path,commit_msg=nil)
       commit_msg = "updating file #{file_path}"
       commit_context(commit_msg||"deleting file #{file_path}") do
         @repo.delete_file(content)
       end
+    end
+
+    #'pass' all these methods to @repo
+    RepoMethods = [:add_or_replace_file,:branches,:create_branch,:file_content,:ls_r,:commit_context]
+    def method_missing(name,*args,&block)
+      if RepoMethods.include?(name)
+        @repo.send(name,*args,&block)
+      else
+        super
+      end
+    end
+    def respond_to?(name)
+      !!(RepoMethods.include?(name) || super)
+    end
+
+=begin
+    def add_or_replace_file(file_path,content)
+      @repo.add_or_replace_file(file_path,content)
     end
 
     def branches()
@@ -48,5 +63,6 @@ module R8::RepoManager; class GitoliteManager
     def commit_context(commit_msg,&block)
       @repo.commit_context(commit_msg,&block)
     end
+=end
   end
 end;end

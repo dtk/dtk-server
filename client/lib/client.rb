948dcb647d651878a3635100774436dc4e73d955
Rich Pelavin <rich.pelavin@gmail.com>
2012-06-13 20:49:27 +0000

    cleaning up client code

--- client/lib/client.rb
+++ client/lib/client.rb
@@ -1,4 +1,9 @@
+require 'rubygems'
 require File.expand_path("require_first", File.dirname(__FILE__))
+#TODO: should be common gem
+dtk_require("../../common/lib/rest_client_wrapper.rb")
+
+########
 dtk_require("auxiliary")
 dtk_require("core")
 dtk_require("commands")

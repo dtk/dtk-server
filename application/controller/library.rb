757e7982fb202c7f226d5be53f0b2bfeb73b3e0c
Rich Pelavin <rich.pelavin@gmail.com>
2012-06-13 03:12:36 +0000

    update

--- application/controller/library.rb
+++ application/controller/library.rb
@@ -2,13 +2,14 @@ module XYZ
   class LibraryController < Controller
     #TODO: stub; this might be part of installation
     def rest__bind_to_repo_manager()
-      library_id = ret_non_null_request_params(:library_id)
+      library_id = ret_request_params(:library_id) || Library.create_public_library?(model_handle()).get_id()
       create_object_from_id(library_id).bind_to_repo_manager()
       rest_ok_response
     end
 
     def rest__import_from_repo_manager()
-      library_id,repo_manager_hostname = ret_non_null_request_params(:library_id,:repo_manager_hostname)
+      library_id = ret_request_params(:library_id) || Library.create_public_library?(model_handle()).get_id()
+      repo_manager_hostname = ret_non_null_request_params(:repo_manager_hostname)
       create_object_from_id(library_id).import_from_repo_manager(repo_manager_hostname)
       rest_ok_response
     end

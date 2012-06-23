9adb936c31a4606878f71c0073ddb2e214e4f04d
Richard Pelavin <rich.pelavin@gmail.com>
2012-06-23 18:29:19 +0000

    update

--- application/controller/component_module.rb
+++ application/controller/component_module.rb
@@ -1,10 +1,12 @@
 module XYZ
   class Component_moduleController < Controller
     def rest__list_from_library()
+      #TODO: update to call ComponentModule method
       rest_ok_response Implementation.list_from_library(model_handle(:implementation))
     end
 
     def rest__list_from_workspace()
+      #TODO: update to call ComponentModule method
       rest_ok_response Implementation.list_from_workspace(model_handle(:implementation))
     end
 
@@ -15,6 +17,7 @@ module XYZ
     def rest__import()
       remote_repo_name = ret_non_null_request_params(:remote_repo_name)
       library_id = ret_request_params(:library_id) 
+      #TODO: may replace with default library being public library; so more sharing by default
       library_idh = (library_id && id_handle(library_id,:library)) || Library.users_private_library(model_handle(:library)).id_handle()
       unless library_idh
         raise Error.new("No library specified and no default can be determined")
@@ -24,6 +27,7 @@ module XYZ
     end
 
     def rest__update_library()
+      #TODO: update to call ComponentModule method
       workspace_impl_id = ret_non_null_request_params(:implementation_id)
       workspace_impl = id_handle(workspace_impl_id,:implementation).create_object()
       workspace_impl.update_library_module_with_workspace()
--- application/model/component_module.rb
+++ application/model/component_module.rb
@@ -1,49 +1,40 @@
 module XYZ
   class ComponentModule < Model
-    class << self
-      def import(library_idh,remote_repo_name)
-        local_repo_name = remote_repo_name
-        if remote_already_imported?(library_idh,remote_repo_name)
-          raise Error.new("Cannot import remote repo (#{remote_repo_name}) which has been imported already")
-        end
-        if conflict_with_local_repo?(library_idh,local_repo_name)
-          raise Error.new("Import conflicts with local repo (#{local_repo_name})")
-        end
-
-        #TODO: this might be done a priori
-        RepoRemote.authorize_dtk_instance(remote_repo_name)
-        #TODO: create repo and implemntations 
-        nil
+    def self.import(library_idh,remote_repo_name)
+      local_repo_name = remote_repo_name
+      if remote_already_imported?(library_idh,remote_repo_name)
+        raise Error.new("Cannot import remote repo (#{remote_repo_name}) which has been imported already")
+      end
+      if conflict_with_local_repo?(library_idh,local_repo_name)
+        raise Error.new("Import conflicts with local repo (#{local_repo_name})")
       end
 
-     private
-      def remote_already_imported?(library_idh,remote_repo_name)
-        ret = nil
-        sp_hash = {
-          :cols => [:id,:display_name],
-          :filter => [:eq, :remote_repo_name, remote_repo_name]
-        }
-        repos = Model.get_objs(library_idh.createMH(:repo),sp_hash)
-        return ret if repos.empty?
-        
-        sp_hash = {
-          :cols => [:id,:display_name],
-          :filter => [:and, [:eq, :library_library_id, library_idh.get_id()],
-                      [:oneof, :repo_id, repos.map{|r|r[:id]}]]
-        }
-        impls = Model.get_objs(library_idh.createMH(:implementation),sp_hash)
-        not impls.empty?
-      end
+      #TODO: this might be done a priori
+      RepoRemote.authorize_dtk_instance(remote_repo_name)
+      #TODO: create repo and implemntations 
+      nil
+    end
 
-      def conflict_with_local_repo?(library_idh,local_repo_name)
-        sp_hash = {
-          :cols => [:id,:display_name],
-          :filter => [:and, [:eq, :library_library_id, library_idh.get_id()],
-                      [:eq, :display_name, local_repo_name]]
-        }
-        impls = Model.get_objs(library_idh.createMH(:implementation),sp_hash)
-        not impls.empty?
-      end
+    private
+    def self.remote_already_imported?(library_idh,remote_repo_name)
+      ret = nil
+      sp_hash = {
+        :cols => [:id,:display_name],
+        :filter => [:and, [:eq, :library_library_id, library_idh.get_id()],
+                    [:eq, :remote_repo, remote_repo_name]]
+      }
+      cms = get_objs(library_idh.createMH(:component_module),sp_hash)
+      not cms.empty?
+    end
+
+    def self.conflict_with_local_repo?(library_idh,local_repo_name)
+      sp_hash = {
+        :cols => [:id,:display_name],
+        :filter => [:and, [:eq, :library_library_id, library_idh.get_id()],
+                    [:eq, :display_name, local_repo_name]]
+      }
+      cms = get_objs(library_idh.createMH(:component_module),sp_hash)
+      not cms.empty?
     end
   end
 end

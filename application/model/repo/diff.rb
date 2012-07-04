dcf1f2ca4860f879dfb09ad480cc0acfa2e5b248
Richard Pelavin <rich.pelavin@gmail.com>
2012-07-04 00:34:22 +0000

    update

--- application/model/repo/diff.rb
+++ application/model/repo/diff.rb
@@ -7,15 +7,19 @@ class DTK::Repo
     end
 
     def file_added?()
-      @new_file or @renamed_file
+      @new_file && {:path => @a_path}
+    end
+
+    def file_renamed?()
+      @renamed_file && {:old_path => @b_path, :new_path => @a_path}
     end
 
     def file_deleted?()
-      @deleted_file or @renamed_file
+      @deleted_file && {:path => @a_path}
     end
 
     def file_modified?()
-      !!@diff and !(@new_file or @deleted_file or @renamed_file)
+      (not (@new_file or @deleted_file or @renamed_file)) && {:path => @a_path} 
     end
 
    private
--- application/model/repo/diffs.rb
+++ application/model/repo/diffs.rb
@@ -4,9 +4,10 @@ class DTK::Repo
       super(array_diff_hashes.map{|hash|Diff.new(hash)})
     end
 
-    #returns a hash with keys :file_added, :file_deleted, :file_modified
+    #returns a hash with keys :file_renamed, :file_added, :file_deleted, :file_modified
     def ret_diff_types_summary()
       ret = Hash.new
+      ret.merge!(:file_renamed => true) if find{|d|d.file_renamed?}
       ret.merge!(:file_added => true) if find{|d|d.file_added?}
       ret.merge!(:file_deleted => true)if find{|d|d.file_deleted?}
       ret.merge!(:file_modified => true) if find{|d|d.file_modified?}

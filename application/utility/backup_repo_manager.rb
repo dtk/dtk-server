35ad408e47a315ef8e2a942a4eb43cd2af4fa80a
Richard Pelavin <rich.pelavin@gmail.com>
2012-08-26 19:00:13 +0000

    fixing repo backup utility to just talk to repo managers not dtk server

--- application/utility/backup_repo_manager.rb
+++ application/utility/backup_repo_manager.rb
@@ -4,49 +4,28 @@ require 'pp'
 require File.expand_path("../require_first",File.dirname(__FILE__))
 r8_require_common_lib("aux","errors","log")
 r8_require("../../repo_manager_client/lib/repo_manager_client.rb")
-#TODO: should move some of routines in dtk-clone core to dtk common or make dtk api package
-r8_require("../../../dtk-client/lib/core")
 
-include DTK
-include DTK::Common
-class API <  Client::Conn
-  def get(path)
-    url = rest_url(path)
-    handle_error do
-      Rest::Response.new(json_parse_if_needed(get_raw(url)))
-    end
-  end
-
-  def post(path,body=nil)
-    url = rest_url(path)
-    handle_error do 
-      Rest::Response.new(json_parse_if_needed(post_raw(url,body)))
-    end
-  end
-  def handle_error(&block)
-    response = yield
-    raise Error.new(response["errors"].inspect) unless response.ok?
-    response
-  end
-end
-###
+remote_repo_base_url = "http://ec2-174-129-28-204.compute-1.amazonaws.com:7000"
 mirror_host = 'ec2-50-16-199-149.compute-1.amazonaws.com' #ARGV[0]
 mirror_base_url = "http://#{mirror_host}:7000"
 
-dtk_conn = API.new()
 
-response =  dtk_conn.post("component_module/list_remote")
-remote_component_modules = response.data().map{|r|r["display_name"]}
-pp [:remote_component_modules,remote_component_modules]
+include DTK
+include DTK::Common
 
 mirror_client = RepoManagerClient.new(mirror_base_url)
 username = Aux.dtk_instance_repo_username()
 rsa_pub_key = Aux.get_ssh_rsa_pub_key()
 pp mirror_client.add_user(username,rsa_pub_key,:noop_if_exists => true)
 
-remote_component_modules.each do |repo_name|
+remote_repo_client = RepoManagerClient.new(remote_repo_base_url)
+response =  remote_repo_client.list_repos()
+remote_modules = response.map{|r|r["repo_name"]}
+pp [:remote_modules,remote_modules]
+
+
+remote_modules.each do |repo_name|
   mirror_client.create_repo(username,repo_name,"RW+") #TODO: may make this part of component/push_to_mirror call
   pp "created or found mirror_repo  #{repo_name}"
-  #TODO: this needs to be changed to talk to remote manager, not the dtk instance
-#  pp response =  dtk_conn.post("component_module/push_to_mirror",{:component_module_id => repo_name, :mirror_host => mirror_host})
+  pp response =  remote_repo_client.push_to_mirror(repo_name,mirror_host)
 end

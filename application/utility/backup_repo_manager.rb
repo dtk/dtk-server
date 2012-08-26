6fc8bc69bae8d9e227a5a430b6a8e70c29140e27
Richard Pelavin <rich.pelavin@gmail.com>
2012-08-26 03:09:08 +0000

    working on backup repo runs through component modules and then creates empty repos on mirror as first step before push mirror

--- application/utility/backup_repo_manager.rb
+++ application/utility/backup_repo_manager.rb
@@ -2,12 +2,37 @@
 require 'rubygems'
 require 'pp'
 require File.expand_path("../require_first",File.dirname(__FILE__))
+r8_require_common_lib("aux")
 r8_require("../../repo_manager_client/lib/repo_manager_client.rb")
-repo_name = ARGV[0]
-rest_base_url = 'http://ec2-23-20-6-192.compute-1.amazonaws.com:7000' #ARGV[1]
+#TODO: should move some of routines in dtk-clone core to dtk common or make dtk api package
+r8_require("../../../dtk-client/lib/core")
+class API <  DTK::Client::Conn
+  def get(path)
+    url = rest_url(path)
+    ::DTK::Common::Rest::Response.new(json_parse_if_needed(get_raw(url)))
+  end
 
-username = 'test_user'
-client = DTK::RepoManagerClient.new(rest_base_url)
-rsa_pub_key = File.open('/root/.ssh/id_rsa.pub').read
-pp client.add_user(username,rsa_pub_key,:noop_if_exists => true)
-pp client.create_repo(username,repo_name,"RW+")
+  def post(path,body=nil)
+    url = rest_url(path)
+    ::DTK::Common::Rest::Response.new(json_parse_if_needed(post_raw(url,body)))
+  end
+end
+###
+
+rest_base_url = 'http://ec2-23-20-6-192.compute-1.amazonaws.com:7000' #ARGV[0]
+dtk_conn = API.new()
+
+response =  dtk_conn.post("component_module/list_remote")
+raise Error.new(response.inspect) unless response.ok?
+remote_component_modules = response.data().map{|r|r["display_name"]}
+pp [:remote_component_modules,remote_component_modules]
+
+mirror_client = ::DTK::RepoManagerClient.new(rest_base_url)
+username = ::DTK::Common::Aux.dtk_instance_repo_username()
+rsa_pub_key = ::DTK::Common::Aux.get_ssh_rsa_pub_key()
+pp mirror_client.add_user(username,rsa_pub_key,:noop_if_exists => true)
+
+remote_component_modules.each do |repo_name|
+  mirror_client.create_repo(username,repo_name,"RW+")
+  pp "created mirror_repo  #{repo_name}"
+end

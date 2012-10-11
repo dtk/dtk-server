aa305bd0d5be19ab035752da919f5b2e3160886f
Rich Pelavin <rich.pelavin@gmail.com>
2012-10-11 02:33:39 +0000

    feature_upgrade_image

--- application/model/node/template.rb
+++ application/model/node/template.rb
@@ -19,21 +19,42 @@ module DTK
         ret
       end
       def self.image_upgrade(model_handle,old_image_id,new_image_id)
-        sp_hash = {
-          :cols => [:id,:ref,:display_name,:rules,:os_type]
-        }
-        node_bindings = get_objs(model_handle.createMH(:node_binding_ruleset),sp_hash)
-        matching_node_bindings = node_bindings.select do |nb| 
+
+        nb_mh = model_handle.createMH(:node_binding_ruleset)
+        matching_node_bindings = get_objs(nb_mh,:cols => [:id,:rules]).select do |nb| 
           nb[:rules].find{|r|r[:node_template][:image_id] == old_image_id}
         end
         if matching_node_bindings.empty?
           raise ErrorUsage.new("Cannot find reference to image_id (#{old_image_id})")
         end
-        pp matching_node_bindings
+
         image_type = matching_node_bindings.first[:rules].first[:node_template][:type].to_sym
-        #TODO: check if new_image_id is a legitimate image id 
         unless CommandAndControl.existing_image?(new_image_id,image_type)
           raise ErrorUsage.new("Image id (#{new_image_id}) does not exist")
+        end
+
+        #update daatstructute than model
+        matching_node_bindings.each do |nb|
+          nb[:rules].each do |r|
+            nt = r[:node_template]
+            if nt[:image_id] == old_image_id
+              nt[:image_id] = new_image_id
+            end
+          end
+        end
+        update_from_rows(nb_mh,matching_node_bindings)
+
+        #find and update nodes that are images
+        sp_hash = {
+          :cols => [:id,:external_ref],
+          :filter => [:eq, :type, "image"]
+        }
+        matching_images = get_objs(model_handle,sp_hash).select do |r|
+          r[:external_ref][:image_id] == old_image_id
+        end
+        unless matching_images.empty?
+          matching_images.each{|r|r[:external_ref][:image_id] = new_image_id}
+          update_from_rows(model_handle,matching_images)
         end
       end
     end

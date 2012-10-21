55e4a1820a2f2a31d1ea0c8810715c9d0e147380
Rich Pelavin <rich.pelavin@gmail.com>
2012-10-21 22:37:05 +0000

    refinement to format/process netstat and other actiosn

--- application/model/assembly/instance/action.rb
+++ application/model/assembly/instance/action.rb
@@ -4,23 +4,50 @@ module DTK
     module ActionMixin
       def initiate_get_netstats(action_results_queue)
         nodes = get_nodes(:id,:display_name,:external_ref)
-        indexes = nodes.map{|r|r[:id]}
-        action_results_queue.set_indexes!(indexes)
-        ndx_pbuilderid_to_node_info =  nodes.inject(Hash.new) do |h,n|
-          h.merge(n.pbuilderid => {:id => n[:id], :display_name => n[:display_name]}) 
-        end
-        callbacks = {
-          :on_msg_received => proc do |msg|
-            response = CommandAndControl.parse_response__execute_action(nodes,msg)
-            #TODO: now ignoring bad results because have time out mechanism; might put errors in queue to terminate earlier
-            if response and response[:pbuilderid] and response[:status] == :ok
-              node_info = ndx_pbuilderid_to_node_info[response[:pbuilderid]]
-              data = response[:data].map{|r|node_info.merge(r)}
-              action_results_queue.push(node_info[:id],data)
+        Action::GetNetstats.initiate(nodes,action_results_queue)
+      end
+      module Action 
+        class GetNetstats < ActionResultsQueue::Result
+          def self.initiate(nodes,action_results_queue)
+            indexes = nodes.map{|r|r[:id]}
+            action_results_queue.set_indexes!(indexes)
+            ndx_pbuilderid_to_node_info =  nodes.inject(Hash.new) do |h,n|
+              h.merge(n.pbuilderid => {:id => n[:id], :display_name => n[:display_name]}) 
             end
+            callbacks = {
+              :on_msg_received => proc do |msg|
+                response = CommandAndControl.parse_response__execute_action(nodes,msg)
+                #TODO: now ignoring bad results because have time out mechanism; might put errors in queue to terminate earlier
+                if response and response[:pbuilderid] and response[:status] == :ok
+                  node_info = ndx_pbuilderid_to_node_info[response[:pbuilderid]]
+                  raw_data = response[:data].map{|r|node_info.merge(r)}
+                  data = process_data_for_ipv4(raw_data)
+                  action_results_queue.push(node_info[:id],new(node_info[:display_name],data))
+                end
+              end
+            }
+            CommandAndControl.request__execute_action(:netstat,:get_tcp_udp,nodes,callbacks)
           end
-        }
-        CommandAndControl.request__execute_action(:netstat,:get_tcp_udp,nodes,callbacks)
+          private
+          def self.process_data_for_ipv4(raw_data)
+            ndx_ret = Hash.new
+            raw_data.each do |r|
+              next unless r[:state] == "LISTEN"
+              if r[:local] =~ /(^.+):([0-9]+$)/
+                address = $1
+                address = "0.0.0.0" if address == "::"
+                port = $2.to_i
+                next unless address =~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/
+                ndx_ret[port] ||= {
+                  :port => port,
+                  :local_address => address,
+                  :protocol => r[:protocol]
+                }
+              end
+            end
+            ndx_ret.values
+          end
+        end
       end
     end
   end
--- utils/internal/action_results_queue.rb
+++ utils/internal/action_results_queue.rb
@@ -1,6 +1,26 @@
 module DTK
   #cross threads may eb seperate requests for new action results queue, but no locking on allocated instance
   class ActionResultsQueue
+
+    #returns :is_complete => is_complete, :results => results
+    def self.get_results(queue_id,ret_only_if_complete)
+      is_complete = results = nil
+      unless ret_only_if_complete
+        results = self[queue_id].ret_whatever_is_complete
+        delete(queue_id)
+        is_complete = true
+      else
+        results = self[queue_id].all_if_complete()
+        if results
+          delete(queue_id)
+          is_complete = true
+        else
+          is_complete = false
+        end
+      end
+      {:is_complete => is_complete, :results => Result.post_process(results)}
+    end
+
     Lock = Mutex.new
     Queues = Hash.new
     @@count = 0
@@ -19,6 +39,7 @@ module DTK
     end
 
     attr_reader :id
+
     def self.delete(queue_id)
       Lock.synchronize do
         Queues.delete(queue_id.to_i)
@@ -40,6 +61,27 @@ module DTK
     
     def ret_whatever_is_complete()
       @indexes.inject(Hash.new){|h,i| h.merge(i => @results[i])} 
+    end
+
+    class Result
+      def initialize(node_name,data)
+        @data = data
+        @node_name = node_name
+      end
+      attr_reader :data, :node_name
+      def self.post_process(results)
+        unless results.kind_of?(Hash) #and results.values.first.kind_of?(self)
+          return results
+        end
+        ret = Array.new
+        results.each do |node_id,result|
+          node_name = result.node_name
+          result.data.each do |r|
+            ret << r.merge(:node_id => node_id,:node_name => node_name)
+          end
+        end
+        ret
+      end
     end
   end
 end

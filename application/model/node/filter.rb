abe7c5a98196b3ec165a41c2833611c4cddf6927
Rich Pelavin <rich.pelavin@gmail.com>
2012-11-04 18:02:24 +0000

    feature_user_components_on_target: working on computing node centric state changes

---
+++ application/model/node/filter.rb
@@ -0,0 +1,18 @@
+module DTK; class Node
+  class Filter
+    def filter(nodes)
+      filter_aux?(nodes)
+    end
+    def include?(node)
+      !filter_aux?([node]).empty?
+    end
+    class NodeList
+      def initialize(node_idhs)
+        @node_ids = node_idhs.map{|n|n.get_id()} 
+      end
+      def filter(nodes)
+        nodes.select{|n|@node_ids.include?(n[:id])}
+      end
+    end
+  end
+end; end

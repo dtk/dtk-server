0bade2ad7a07f43bc95e30305b872cf20ee3a562
Rich Pelavin <rich.pelavin@gmail.com>
2012-11-10 02:20:34 +0000

    feature_node_converge: checkpoint work

--- application/model/task/create.rb
+++ application/model/task/create.rb
@@ -36,10 +36,10 @@ module XYZ
       node_mh = target_idh.create_childMH(:node)
       node_group = node_group_idh.create_object()
 
-      create_nodes_changes = StateChange::NodeGroup.node_state_changes(target_idh,:node_group => node_group)
+      create_nodes_changes = StateChange::SingleNodeGroup.node_state_changes(target_idh,:node_group => node_group)
       create_nodes_task = create_nodes_task(task_mh,create_nodes_changes)
 
-      config_nodes_changes = StateChange::NodeGroup.component_state_changes(node_mh,:node_group => node_group)
+      config_nodes_changes = StateChange::SingleNodeGroup.component_state_changes(node_mh,:node_group => node_group)
       config_nodes_task = config_nodes_task(task_mh,config_nodes_changes)
 
       ret = create_new_task(task_mh,:temporal_order => "sequential",:node_id => node_group_idh.get_id(),:display_name => "node_group_converge", :commit_message => commit_msg)

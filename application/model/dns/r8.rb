6ae6014c101c3c8e4e3826d441a671363bd6ddf6
Rich Pelavin <rich.pelavin@gmail.com>
2012-12-13 04:14:02 +0000

    feature_assembly_sets_dns

--- application/model/dns/r8.rb
+++ application/model/dns/r8.rb
@@ -15,7 +15,9 @@ module DTK
         #TODO: to supports this may want to put in logic that prevents assemblies with explicit names from having same name
         assembly_part = assembly_part(r8_dns_info[:assembly])
         node_part = qualified_name(r8_dns_info)
-        tenant_part = ::R8::Config[:dns][:r8][:tenant_name]
+        unless tenant_part = ::R8::Config[:dns][:r8][:tenant_name]
+          raise Error.new("Server config variable (dns.r8.tenant_name) has not been set")
+        end
         domain = ::R8::Config[:dns][:r8][:domain]
         "#{assembly_part}.#{node_part}.#{tenant_part}.#{domain}"
       end

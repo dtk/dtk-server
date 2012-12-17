218b44da9d797ca98f7e54f959e851b8fa2b275d
Rich Pelavin <rich.pelavin@gmail.com>
2012-12-17 18:39:34 +0000

    fixes to current session

--- utils/internal/current_session.rb
+++ utils/internal/current_session.rb
@@ -1,24 +1,38 @@
-module XYZ
+module DTK
   # Wrapper for Ramaze::Session object, user credentials are set in 
   # user_controller#process_login
   # Also timeout logic can be found in AuthController
   class CurrentSession
     class << self
+      def method_missing(name,*args)
+        instance = Instance.new
+        instance.respond_to?(name) ? instance.send(name,*args) : super
+      end
+      def respond_to?(name)
+        instance = Instance.new
+        instance.respond_to?(name)||super
+      end
+    end
+   private
+    class Instance
+      extend Innate::StateAccessor
+      state_accessor :user_object, :auth_filters
+      def get_user_object()
+        user_object
+      end
       def get_username()
         get_user_object()[:username]
       end
-
-      def get_user_object()
-        session[:USER][:credentials]
+      
+      def set_user_object(user_object)
+        @user_object = user_object
       end
-
-      def set_user_object()
-        # [Haris] - Sorry if this broke the code but I did not understand way that user is being set in session
-        raise "This is not a proper way to set session user please look into using user_controller#process_login."
-      end
-
       def get_auth_filters()
-        # TODO: Rich needs to implement this, with previous implemntation it did not work
+        auth_filters
+      end
+
+      def set_auth_filters(*array_auth_filters)
+        @auth_filters = array_auth_filters
       end
     end
   end

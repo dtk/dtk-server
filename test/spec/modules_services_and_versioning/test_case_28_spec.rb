5ad5e2773346afcdaeb7f176d2365eb452137f27
bakir.jusufbegovic <bakir.jusufbegovic@gmail.com>
2013-02-14 09:16:05 +0100

    'Merge' new test scripts from 'master' branch

---
+++ test/spec/modules_services_and_versioning/test_case_28_spec.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+#Test Case 28: Import component module from remote and use this component in assembly
+
+require 'rubygems'
+require 'rest_client'
+require 'pp'
+require 'json'
+require 'awesome_print'
+require './test/lib/dtk_common'
+require './test/lib/shared_spec'
+
+assembly_name = 'test_case_28_instance'
+assembly_template = 'bootstrap::node_with_params'
+os = 'natty'
+node_name = 'node1'
+module_name = "mysql"
+module_filesystem_location = "~/component_modules"
+$assembly_id = 0
+#Initial empty module components list, will be populated after "Get module components list" context call
+$module_components_list = Array.new()
+
+dtk_common = DtkCommon.new(assembly_name, assembly_template)
+
+describe "Test Case 28: Import component module from remote and use this component in assembly" do
+
+	context "Import module #{module_name} function" do
+		include_context "Import remote module", dtk_common, module_name
+	end
+
+	context "Get module components list" do
+		include_context "Get module components list", dtk_common, module_name
+	end
+
+	context "Module existence on local filesystem check" do
+		it "verifies module exists" do
+			list_response = system("ls #{module_filesystem_location}/#{module_name}")
+			list_response.should eq(true)
+		end
+	end
+
+	context "Stage assembly function" do
+		include_context "Stage", dtk_common
+	end
+
+	context "List assemblies after stage" do		
+		include_context "List assemblies after stage", dtk_common
+	end
+
+	context "Set OS attribute function" do
+		include_context "Set attribute", dtk_common, 'os_identifier', os
+	end
+
+	context "Add components to assembly node" do
+		$module_components_list.each do |component_id|
+			include_context "Add component to assembly node", dtk_common, node_name, component_id
+		end
+	end
+
+	context "Converge function" do
+		include_context "Converge", dtk_common
+	end
+
+	context "Delete and destroy assemblies" do
+		include_context "Delete assemblies", dtk_common
+	end
+
+	context "Delete module" do
+		include_context "Delete module", dtk_common, module_name
+	end
+
+	context "Delete module from local filesystem" do
+		it "deletes module" do
+			delete_response = system("rm -rf #{module_filesystem_location}/#{module_name}")
+			delete_response.should eq(true)
+		end
+	end
+end
---
+++ test/spec/modules_services_and_versioning/test_case_32_spec.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+#Test Case 32: Get list of all assembly templates for particular service
+
+require 'rubygems'
+require 'rest_client'
+require 'pp'
+require 'json'
+require 'awesome_print'
+require './test/lib/dtk_common'
+require './test/lib/shared_spec'
+
+assembly_name = 'test_case_32_instance'
+assembly_template = 'bootstrap::test1'
+new_assembly_template = 'test_case_32_instance_assembly_template'
+service_name = "new_service"
+$assembly_id = 0
+
+dtk_common = DtkCommon.new(assembly_name, assembly_template)
+
+describe "Test Case 32: Get list of all assembly templates for particular service" do
+
+	context "Create new service function" do
+		include_context "Create service", dtk_common, service_name
+	end
+
+	context "Stage assembly function" do
+		include_context "Stage", dtk_common
+	end
+
+	context "List assemblies after stage" do		
+		include_context "List assemblies after stage", dtk_common
+	end
+
+	context "Create assembly template in #{service_name} service from existing assembly" do
+		it "missing test step implementation"
+	end
+
+	context "Check if #{new_assembly_template} assembly_template belongs to #{service_name} service" do
+		include_context "Check if assembly template belongs to the service", dtk_common, service_name, new_assembly_template
+	end
+
+	context "Delete assembly template" do
+		it "missing test step implementation"
+	end
+
+	context "Delete and destroy assemblies" do
+		include_context "Delete assemblies", dtk_common
+	end
+
+	context "List assemblies after delete" do
+		include_context "List assemblies after delete", dtk_common
+	end
+
+	context "Delete service function" do
+		include_context "Delete service", dtk_common, service_name
+	end
+end

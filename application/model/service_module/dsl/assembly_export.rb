83a3fe99e1209c26a568565bcb068de1915521ca
Rich Pelavin <rich.pelavin@gmail.com>
2013-02-25 23:19:55 +0000

    feature_dsl_v2: working on assembly export in v2 syntax

--- application/model/service_module/dsl/assembly_export.rb
+++ application/model/service_module/dsl/assembly_export.rb
@@ -11,10 +11,9 @@ module DTK
       end
 
       def serialize_and_save_to_repo()
-        hash_to_serialize = serialize()
-        ordered_hash_content = ret_ordered_hash_content(hash_to_serialize)
         path = assembly_meta_filename_path()
-        @service_module_branch.serialize_and_save_to_repo(path,ordered_hash_content)
+        ordered_hash_serialized_content = serialize()
+        @service_module_branch.serialize_and_save_to_repo(path,ordered_hash_serialized_content)
         path
       end
 
@@ -42,6 +41,22 @@ module DTK
 
       def assembly_meta_filename_path()
         ServiceModule::assembly_meta_filename_path(assembly_hash()[:display_name])
+      end
+
+      def assembly_hash()
+        self[:component].values.first
+      end
+
+      def component_output_form(component_hash)
+        name = component_name_output_form(component_hash[:component_type])
+        if attr_overrides = component_hash[:attribute_override]
+          {name => attr_overrides.values.inject(Hash.new){|h,a|h.merge(a[:display_name] => a[:attribute_value])}}
+        else
+          name 
+        end
+      end
+      def component_name_output_form(internal_format)
+        internal_format.gsub(/__/,Seperators[:module_component])
       end
 
     end

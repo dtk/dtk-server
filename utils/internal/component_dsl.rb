d7a7ee74705111748be867761e86f0ed47a7bf85
Rich Pelavin <rich.pelavin@gmail.com>
2013-03-06 00:53:00 +0000

    feature_dsl_v2: cleanup

--- utils/internal/component_dsl.rb
+++ utils/internal/component_dsl.rb
@@ -96,8 +96,8 @@ module DTK
       #parse_check raises errors if any errors found
       klass.parse_check(version_specific_input_hash)
       ret = klass.normalize(version_specific_input_hash)
-pp ret
-raise Error.new("got here")
+pp [:input_hash,version_specific_input_hash]
+pp [:normalize,ret]
       #version below refers to component version not metafile version
       ret.each_value{|cmp_info|cmp_info["version"] ||= Component.default_version()}
       ret

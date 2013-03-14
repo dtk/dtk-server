5b8a8825399907b9f6c91d2f4148d0650b3ed930
bakir.jusufbegovic <bakir.jusufbegovic@gmail.com>
2013-03-14 15:18:34 +0100

    Fixed test_case_23

--- test/spec/use_of_parameters/test_case_23_spec.rb
+++ test/spec/use_of_parameters/test_case_23_spec.rb
@@ -13,7 +13,6 @@ assembly_name = 'test_case_23_instance'
 assembly_template = 'bootstrap::node_with_params'
 OS = 'natty'
 MEMORY_SIZE = 't1.micro'
-HOST_ADDRESSES_IPV4 = '127.0.0.1'
 NODE_NAME = 'node1'
 
 $assembly_id = 0
@@ -37,10 +36,6 @@ describe "Test Case 23: Add optional params on existing attributes in assembly n
 		include_context "Set attribute", dtk_common, 'memory_size', MEMORY_SIZE
 	end
 
-	context "Set HOST_ADDRESSES_IPV4 attribute function" do
-		include_context "Set attribute", dtk_common, 'host_addresses_ipv4', HOST_ADDRESSES_IPV4
-	end
-
 	context "Converge function" do
 		include_context "Converge", dtk_common
 	end
@@ -51,10 +46,6 @@ describe "Test Case 23: Add optional params on existing attributes in assembly n
 
 	context "Check MEMORY_SIZE attribute function after converge" do
 		include_context "Check attribute", dtk_common, NODE_NAME, 'memory_size', MEMORY_SIZE
-	end
-
-	context "Check HOST_ADDRESSES_IPV4 attribute function" do
-		include_context "Check attribute", dtk_common, NODE_NAME, 'host_addresses_ipv4', HOST_ADDRESSES_IPV4
 	end
 
 	context "Delete and destroy assemblies" do

4cbef77aafa4931c6efc118b1c616f858a34d9ed
Rich Pelavin <rich.pelavin@gmail.com>
2013-04-04 00:36:41 +0000

    for_amar_4_3_13

--- application/model/task/create.rb
+++ application/model/task/create.rb
@@ -257,18 +257,26 @@ module XYZ
       #FOR_AMAR
       #rich: here is dump of internode dependencies; this is caleld after tsort (intranode) processing (which is within get_executable_action_from_state_change
       #so  shoudl restructure so this is called before intra_node processing
-=begin
+
       inter_node_deps = Array.new
       component_actions = all_actions.map{|a|a[:component_actions]}.flatten(1) 
       augmented_attr_list = Attribute.aug_attr_list_from_component_actions(component_actions)
-       dependency_analysis(augmented_attr_list) do |attr_in,link,attr_out|
-        if guard = GuardedAttribute.create(attr_in,link,attr_out)
-         inter_node_deps << guard
+       Attribute.dependency_analysis(augmented_attr_list) do |attr_in,link,attr_out|
+        if attr_guard = GuardedAttribute.create(attr_in,link,attr_out)
+          guard = attr_guard[:guard]
+          guarded = attr_guard[:guarded]
+          cmp_dep = {
+            :guard => {:component => guard[:component], :node => guard[:node]},
+            :guarded => {:component => guarded[:component], :node => guarded[:node]}
+          }
+          #guarded has to go after guard
+          #flat list of dependencies as oppossed to collecting all deps for one component
+          #attr_guard has relationship between attributes; stripping out attr info; consequently same compoennt relationship can be in more than once
+         inter_node_deps << cmp_dep
         end
       end
       pp inter_node_deps
-      raise "got here"
-=end
+
       ret
     end
 

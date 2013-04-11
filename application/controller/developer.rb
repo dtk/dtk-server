8584d21c58a130c08291e762f4468e3c2dab2bc0
Aldin Milanovic <aldin@atlantbh.com>
2013-04-11 10:12:33 +0200

    Upload-agent feature for upload on multiple nodes. no_build

--- application/controller/developer.rb
+++ application/controller/developer.rb
@@ -5,12 +5,22 @@ module XYZ
   	helper :node_helper
 
   	def rest__inject_agent()
-  		params = ret_params_hash(:agent_files, :node_pattern)
-  		node = create_node_obj(:node_pattern)
-      queue = ActionResultsQueue.new
+  		params = ret_params_hash(:agent_files, :node_pattern, :node_list)
+      node_list = params[:node_list]||[]
+      nodes     = get_nodes_from_params(node_list)
+      queue     = ActionResultsQueue.new
 
-      DeveloperMixin.initiate_inject_agent(queue, [node], params)
+      DeveloperMixin.initiate_inject_agent(queue, nodes, params)
       rest_ok_response :action_results_id => queue.id
     end
+
+    def get_nodes_from_params(node_list)
+      nodes = []
+      node_list.each do |n|
+        nodes << get_objects(:node, { :id => n.to_i}).flatten
+      end
+      return nodes.flatten
+    end
+
   end
 end

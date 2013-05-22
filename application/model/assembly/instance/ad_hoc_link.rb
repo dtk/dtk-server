774ef02f62abc6c97a395cf7d66c6bea1c46cd9a
Rich Pelavin <rich.pelavin@gmail.com>
2013-05-22 16:55:31 +0000

    feature_ad_hoc_service_links: working on add_ad_hoc_service_link

--- application/model/assembly/instance/ad_hoc_link.rb
+++ application/model/assembly/instance/ad_hoc_link.rb
@@ -1,7 +1,7 @@
 module DTK
   class Assembly::Instance
    module AdHocLinkMixin
-     def add_ad_hoc_service_link(server_type,input_cmp_idh,output_cmp_idh)
+     def add_ad_hoc_service_link(service_type,input_cmp_idh,output_cmp_idh)
        AdHocLink.new(self,service_type).add_service_link(input_cmp_idh,output_cmp_idh)
      end
    end
@@ -11,22 +11,20 @@ module DTK
        @assembly_instance = assembly_instance
        @service_type = service_type
      end
-     def add_service_link(server_type,input_cmp_idh,output_cmp_idh)
-       ndx_matching_ports = find_matching_ports?([input_cmp_idh,output_cmp_idh]).injec(Hash.new){|h,p|h.merge(p[:id] => p)} 
+     def add_service_link(input_cmp_idh,output_cmp_idh)
+       ndx_matching_ports = find_matching_ports?([input_cmp_idh,output_cmp_idh]).inject(Hash.new){|h,p|h.merge(p[:id] => p)} 
        input_port = ndx_matching_ports[input_cmp_idh.get_id()]
        output_port = ndx_matching_ports[output_cmp_idh.get_id()]
+       @assembly_instance.id_handle() #TODO: stub
      end
     private
      def find_matching_ports?(cmp_idhs)
-       ret = Array.new
        sp_hash = {
          :cols => Port.common_columns(),
-         :filter => [:oneof,:compoennt_id,cmp_idhs.map{|idh|idh.get_id()}]
+         :filter => [:oneof,:component_id,cmp_idhs.map{|idh|idh.get_id()}]
        }
        cmp_mh = cmp_idhs.first.createMH()
-       ports = Model.get_obj(cmp_mh,sp_hash)
-       return ret if ports.empty?()
-       ports
+       Model.get_objs(cmp_mh,sp_hash).select{|p|p.link_def_name() == @service_type}
      end
    end
   end

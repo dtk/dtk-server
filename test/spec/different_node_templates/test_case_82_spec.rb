36ec2d07fd438491436cd25a670430fcf734da30
bakir.jusufbegovic <bakir.jusufbegovic@gmail.com>
2013-05-23 15:19:41 +0200

    Added new test cases for node templates

---
+++ test/spec/different_node_templates/test_case_82_spec.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+#Test Case 82: Stage node template and converge it (simple)
+
+require 'rubygems'
+require 'rest_client'
+require 'pp'
+require 'json'
+require 'awesome_print'
+require './lib/dtk_common'
+require './lib/node_operations_spec'
+
+STDOUT.sync = true
+
+node_name = 'natty micro'
+staged_node_name = 'test_case_82_instance'
+
+dtk_common = DtkCommon.new('', '')
+$node_id = 0
+
+describe "Test Case 82: Stage node template and converge it (simple)" do
+
+  before(:all) do
+    puts "**********************************************************"
+    puts "Test Case 82: Stage node template and converge it (simple)"
+    puts "**********************************************************"
+    puts ""
+  end
+
+  context "Stage node template #{node_name} with name #{staged_node_name}" do
+    include_context "Stage node template", dtk_common, node_name, staged_node_name
+  end
+
+  context "List nodes after stage" do
+    include_context "List nodes after stage", dtk_common, staged_node_name
+  end   
+
+  context "Converge node function" do
+    include_context "Converge node", dtk_common, staged_node_name
+  end
+  
+  context "Delete and destroy node function" do
+    include_context "Destroy node", dtk_common, staged_node_name
+  end
+
+  after(:all) do
+    puts "", ""
+  end
+end

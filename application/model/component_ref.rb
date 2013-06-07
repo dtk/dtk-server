49e23254b1504d17daaffcba2e1566fbd4d681dd
Haris KRAJINA <haris@atlantbh.com>
2013-06-07 12:32:58 +0200

    Included version in component ref, no_build

--- application/model/component_ref.rb
+++ application/model/component_ref.rb
@@ -15,13 +15,15 @@ module DTK
 
     def self.get_referenced_component_modules(project,component_refs)
       template_ids_to_get = Array.new
-      component_types = Array.new
+      component_types = Hash.new
+      component_versions = Hash.new
+
       component_refs.each do |r|
         unless r[:component_type] or r[:component_template_id]
           update_object(:component_type,:component_template_id)
         end
         if r[:component_type]
-          component_types << r[:component_type]
+          component_types.store(r[:component_type], r[:version])
         else
           template_ids_to_get << r[:component_template_id]
         end
@@ -33,18 +35,29 @@ module DTK
           :filter => [:oneof, :id, template_ids_to_get]
         }
         get_objs(project.model_handle(:component),sp_hash).each do |r|
-          component_types << r[:component_type]
+          component_types.store(r[:component_type], r[:version])
         end
       end
 
       return ret if component_types.empty?
-      module_names = component_types.uniq.map{|r|Component.module_name(r)}
+      module_names = component_types.keys.uniq.map do |r|
+        module_name = Component.module_name(r)
+        component_versions[module_name] = component_types[r]
+        module_name
+      end
+
       sp_hash = {
         :cols => [:id,:group_id,:display_name],
         :filter => [:and,[:eq,:project_project_id,project[:id]],[:oneof,:display_name,module_names]]
       }
       
-      get_objs(project.model_handle(:component_module),sp_hash)
+      response = get_objs(project.model_handle(:component_module),sp_hash)
+      response.each do |element|
+        # we take previusly saved version and return it to the map
+        element.merge!( :version => component_versions[element[:display_name]])
+      end
+
+      response
     end
 
     def self.print_form(cmp_ref__obj_or_hash)

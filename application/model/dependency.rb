fd9e0b264a15bcdf485320d3a45e6c8028979c73
Rich Pelavin <rich.pelavin@gmail.com>
2013-06-10 20:38:40 +0000

    cleaning relationship between simple dependencies, component order, dereived dependencies, and derived order

--- application/model/dependency.rb
+++ application/model/dependency.rb
@@ -1,56 +1,23 @@
 module DTK
   class Dependency < Model
-    def self.add_component_dependency(component_idh, type, hash_info)
-      #TODO: bug problem may be need to get parent of component to use craete rows
-      #TODO: stubbed
-#      cmp = component_idh.create_object.update_object!(:display_name,:library_library_id,:group_id)
-      cmp = component_idh.create_object.update_object!(:display_name)
-      other_cmp_idh = component_idh.createIDH(:id => hash_info[:other_component_id])
-      other_cmp = other_cmp_idh.create_object.update_object!(:display_name,:component_type)
-      search_pattern = {
-        ":filter" => [":eq", ":component_type",other_cmp[:component_type]]
-      }
-      create_row = {
-        :ref => other_cmp[:component_type],
-        :component_component_id => component_idh.get_id(),
-        :description => "#{other_cmp[:display_name]} #{type} #{cmp[:display_name]}",
-        :search_pattern => search_pattern,
-        :severity => "warning",
-        :library_library_id => cmp[:library_library_id]
-      }
-      dep_mh = component_idh.createMH(:dependency)
-      Model.create_from_row(dep_mh,create_row)
+    r8_nested_require('dependency','simple')
+    r8_nested_require('dependency','derived')
+
+    #if its simple component type match returns component type
+    def is_simple_component_type_match?()
+      #TODO: hack until we have macros which will stamp the dependency to make this easier to detect
+      #looking for signature where dependency has
+      #:search_pattern=>{:filter=>[:and, [:eq, :component_type, <component_type>]
+      if filter = (deps[:search_pattern]||{})[":filter".to_sym]
+        if deps[:type] == "component" and filter[0] == ":eq" and filter[1] == ":component_type"
+          filter[2]
+        end
+      end
     end
 
-    #assumption that this is called with components having keys :id,:dependencies, :extended_base, :component_type
-    #this can be either component template or component instance with :dependencies joined in from associated template
     def self.find_in_depends_on_form(components)
       raise Error.new("write this in terms of def Dependency#depends_on_form")
     end
-    def self.find_ndx_cmp_type_and_derived_order(components)
-      ret = Hash.new
-      components.each do |cmp|
-        unless pntr = ret[cmp[:id]]
-          pntr = ret[cmp[:id]] = {:component_type => cmp[:component_type], :component_dependencies => Array.new}
-        end
-        if cmp[:extended_base]
-          pntr[:component_dependencies] << cmp[:extended_base]
-        elsif deps = cmp[:dependencies]
-          #process dependencies
-          #TODO: hack until we have macros which will stamp the dependency to make this easier to detect
-          #looking for signature where dependency has
-          #:search_pattern=>{:filter=>[:and, [:eq, :component_type, <component_type>]
-          filter = (deps[:search_pattern]||{})[":filter".to_sym]
-          if filter and deps[:type] == "component"
-            if filter[0] == ":eq" and filter[1] == ":component_type"
-              pntr[:component_dependencies] << filter[2]
-            end
-          end
-        end
-      end
-      ret
-    end
-
   end
 end
 

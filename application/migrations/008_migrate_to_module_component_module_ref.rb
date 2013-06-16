553032888f638f65046bd2f4b93345b63fbce9ee
Rich Pelavin <rich.pelavin@gmail.com>
2013-06-16 18:55:49 +0000

    feature_data_migrations_in_sequel_migrations: starting to experiment with how to do data migrations in sequel migrations

---
+++ application/migrations/008_migrate_to_module_component_module_ref.rb
@@ -0,0 +1,9 @@
+require 'pp'
+Sequel.migration do
+  up do 
+    cmp_module_refs = DB[:module__component_module_refs].select(:id,:content,:branch_id).all()
+    info_table = DB[:top__id_info].filter(:relation_id=>cmp_module_refs.map{|r|r[:id]}).all()
+pp info_table
+    raise "forced error"
+  end
+end

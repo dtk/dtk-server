eeaa69f61181790ae8dce39cd128dfb6cdf11a26
Rich Pelavin <rich.pelavin@gmail.com>
2013-06-17 15:42:07 +0000

    feature_data_migrations_in_sequel_migrations

--- application/migrations/008_migrate_to_module_component_module_ref.rb
+++ application/migrations/008_migrate_to_module_component_module_ref.rb
@@ -3,11 +3,11 @@ require 'pp'
 Sequel.migration do
   up do 
    #TODO: this is just testing a number of things now
-    ::XYZ::Migration.db_rebuild(:component_module_ref,:module_branch)
+    DTKMigration.db_rebuild(:component_module_ref,:module_branch)
     pp [:test_new_table_there,DB[:module__component_module_ref].select(:id,:version_info,:branch_id,:component_component_id).all()]
     cmp_module_refs = DB[:module__component_module_refs].select(:id,:content,:branch_id).all()
     info_table = DB[:top__id_info].filter(:relation_id=>cmp_module_refs.map{|r|r[:id]}).all()
-pp info_table
+#pp info_table
     raise "forced error"
   end
 end
--- application/migrations/common.rb
+++ application/migrations/common.rb
@@ -1,32 +1,29 @@
 require 'sequel'
 require 'singleton'
 require 'pp'
-module XYZ
-  class Migration
-    #this does an insert and also updates appropriately the top.id_info table
-    def self.insert(schema,table,row)
-      sequel_db(schema,table).insert(row) do |db_ret|
-        pp db_ret
-      end
+module DTKMigration
+  #this does an insert and also updates appropriately the top.id_info table
+  def self.insert(schema,table,row)
+    sequel_db(schema,table).insert(row) do |db_ret|
+      pp db_ret
     end
+  end
 
-    def self.db_rebuild(*model_names)
-      DBRebuild.instance.db_rebuild(model_names)
+  def self.db_rebuild(*model_names)
+    DTKModel.instance.db_rebuild(model_names)
+  end
+ private
+  class DTKModel
+    include Singleton
+    def initialize()
+      require File.expand_path('../app_migration', File.dirname(__FILE__))
     end
-  private
-    class DBRebuild
-      include Singleton
-      def initialize()
-        #TODO: rather than including app, can have stripped down version taht just includes necssary items 
-        require File.expand_path('../app', File.dirname(__FILE__))
-      end
-      def db_rebuild(model_names)
-        ::DTK::Model.db_rebuild(DBinstance,model_names,::DTK::Opts.new(:raise_error => true))
-      end
+    def db_rebuild(model_names)
+      ::DTK::Model.db_rebuild(DBinstance,model_names,::DTK::Opts.new(:raise_error => true))
     end
-
-    def self.sequel_db(schema,table)
-      ::Sequel::DB["#{schema}__#{table}".to_sym]
-    end
+  end
+  
+  def self.sequel_db(schema,table)
+    ::Sequel::DB["#{schema}__#{table}".to_sym]
   end
 end

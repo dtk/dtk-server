ef2619e6bd98daf0381f46ab8835b733856e7d14
Rich Pelavin <rich.pelavin@gmail.com>
2013-06-19 15:50:54 +0000

    feature_data_migrations_in_sequel_migrations: working on basis for data migrations

--- application/migrations/008_migrate_to_module_component_module_ref.rb
+++ application/migrations/008_migrate_to_module_component_module_ref.rb
@@ -3,39 +3,39 @@ require 'pp'
 Sequel.migration do
   up do 
    #TODO: this is just testing a number of things now
-    m = DTKMigration.new
-    m.db_rebuild(:component_module_ref,:module_branch)
-    cmp_mod_refs = m.get_objs(:component_module_refs,:cols => [:id,:ref,:content,:branch_id])
-    pp cmp_mod_refs
-    new_rows = Array.new
-    cmp_mod_refs.each do |r|
-      content = r[:content]
-      unless content.nil? or content.empty?
-        unless content.keys == [:component_modules]
-          raise "content not treated: #{content.inspect}"
-        end
+    DTKMigration.dtk_model_context do
+      dtk_db_rebuild(:component_module_ref,:module_branch)
+      cmp_mod_refs = dtk_get_objs(:component_module_refs,:cols => [:id,:ref,:content,:branch_id])
+      pp cmp_mod_refs
+      new_rows = Array.new
+      cmp_mod_refs.each do |r|
+        content = r[:content]
+        unless content.nil? or content.empty?
+          unless content.keys == [:component_modules]
+            raise "content not treated: #{content.inspect}"
+          end
 
-        content[:component_modules].each do |cmp_mod_sym,ver|
-          cmp_mod = cmp_mod_sym.to_s
-          new_row = {
-            :old_id => r[:id],
-            :ref => cmp_mod,
+          content[:component_modules].each do |cmp_mod_sym,ver|
+            cmp_mod = cmp_mod_sym.to_s
+            new_row = {
+              :old_id => r[:id],
+              :ref => cmp_mod,
             :branch_id => r[:branch_id],
-            :version_info => ver,
-            :component_module => cmp_mod
+              :version_info => ver,
+              :component_module => cmp_mod
             }
-          new_rows << new_row
+            new_rows << new_row
+          end
         end
       end
-    end
-    m.create_objs(:component_module_ref,:module_branch,new_rows,:component_module_refs)
-    pp m.get_objs(:component_module_ref,:cols => [:id,:ref,:component_module,:version_info,:branch_id])
-
+      dtk_create_objs(:component_module_ref,:module_branch,new_rows,:component_module_refs)
+pp      dtk_get_objs(:component_module_ref,:cols => [:id,:ref,:component_module,:version_info,:branch_id])
 #    pp  DB[:module__component_module_ref].all()
    # pp [:test_new_table_there,DB[:module__component_module_ref].select(:id,:version_info,:branch_id,:component_component_id).all()]
    # cmp_module_refs = DB[:module__component_module_refs].select(:id,:content,:branch_id).all()
    # info_table = DB[:top__id_info].filter(:relation_id=>cmp_module_refs.map{|r|r[:id]}).all()
 #pp info_table
-    raise "forced error"
+      raise "forced error"
+    end
   end
 end
--- application/migrations/common.rb
+++ application/migrations/common.rb
@@ -1,56 +1,63 @@
-raise "put the DTKMigration operations under a transaction"
 require 'sequel'
 require 'singleton'
 require 'pp'
 class DTKMigration
-  require File.expand_path('../app_migration', File.dirname(__FILE__))
-  include DTK
-  def initialize()
-    c = 2
-    @user_mh = ModelHandle.new(c,:user)
-    user_superuser = User.create_user_in_groups?(ModelHandle.new(c,:user),"superuser")
-    CurrentSession.new.set_user_object(user_superuser)
+  def self.dtk_model_context(&block)
+    DTKModel.instance.instance_eval &block
   end
-  
-  def db_rebuild(*model_names)
-    Model.db_rebuild(DBinstance,model_names,Opts.new(:raise_error => true))
-  end
-  
-  def get_objs(model_name,sp_hash)
-    Model.get_objs(mh(model_name),sp_hash,:keep_ref_cols => true)
-  end
-  
-  def create_objs(model_name,parent_model_name,rows,old_model_name=nil)
-    ndx_user_date_ref_info = (old_model_name ? get_ndx_user_date_ref_info(old_model_name,rows.map{|r|r[:old_id]}.compact) : {})
-    create_columns = rows.first.keys - [:old_id] #taking first row because they all have same columns
-    create_rows = rows.map do |r|
-      el = nil
-      if ndx = r[:old_id]
-        if user_date_ref_info = ndx_user_date_ref_info[ndx]
-          #info in create_columns can over write info in user_date_ref_info
-          el = user_date_ref_info.merge(Aux.hash_subset(r,create_columns))
-        end
-      end
-      el || r
+
+  class DTKModel
+    include Singleton
+
+    def dtk_db_rebuild(*model_names)
+      Model.db_rebuild(DBinstance,model_names,Opts.new(:raise_error => true))
     end
-    mh = mh(model_name).merge(:parent_model_name => parent_model_name)
-    Model.create_from_rows_for_migrate(mh,create_rows)
-  end
+  
+    def dtk_get_objs(model_name,sp_hash)
+      Model.get_objs(mh(model_name),sp_hash,:keep_ref_cols => true)
+    end
+    
+    def dtk_create_objs(model_name,parent_model_name,rows,old_model_name=nil)
+      ndx_user_date_ref_info = (old_model_name ? get_ndx_user_date_ref_info(old_model_name,rows.map{|r|r[:old_id]}.compact) : {})
+      create_columns = rows.first.keys - [:old_id] #taking first row because they all have same columns
+      create_rows = rows.map do |r|
+        el = nil
+        if ndx = r[:old_id]
+          if user_date_ref_info = ndx_user_date_ref_info[ndx]
+            #info in create_columns can over write info in user_date_ref_info
+          el = user_date_ref_info.merge(Aux.hash_subset(r,create_columns))
+          end
+        end
+        el || r
+      end
+      mh = mh(model_name).merge(:parent_model_name => parent_model_name)
+      Model.create_from_rows_for_migrate(mh,create_rows)
+    end
+    
+   private
+    def initialize()
+      require File.expand_path('../app_migration', File.dirname(__FILE__))
+      self.class.class_eval{include DTK}
+      c = 2
+      @user_mh = ModelHandle.new(c,:user)
+      user_superuser = User.create_user_in_groups?(ModelHandle.new(c,:user),"superuser")
+      CurrentSession.new.set_user_object(user_superuser)
+    end
 
-  private
-  def mh(model_name)
-    @user_mh.createMH(model_name)
+    def mh(model_name)
+      @user_mh.createMH(model_name)
+    end
+    
+    def get_ndx_user_date_ref_info(model_name,ids)
+      ret = Hash.new
+      return ret if ids.empty?
+      sp_hash = {
+        :cols => [:id]+UserDateRefCols,
+        :filter => [:oneof,:id,ids]
+      }
+      dtk_get_objs(model_name,sp_hash).inject(Hash.new){|h,r|h.merge(r[:id] => Aux.hash_subset(r,UserDateRefCols))}
+    end
+    #  UserDateRefCols = [:ref,:ref_num,:owner_id,:group_id,:created_at,:updated_at]
+    UserDateRefCols = [:ref,:owner_id,:group_id]
   end
-
-  def get_ndx_user_date_ref_info(model_name,ids)
-    ret = Hash.new
-    return ret if ids.empty?
-    sp_hash = {
-      :cols => [:id]+UserDateRefCols,
-      :filter => [:oneof,:id,ids]
-    }
-    get_objs(model_name,sp_hash).inject(Hash.new){|h,r|h.merge(r[:id] => Aux.hash_subset(r,UserDateRefCols))}
-  end
-#  UserDateRefCols = [:ref,:ref_num,:owner_id,:group_id,:created_at,:updated_at]
-  UserDateRefCols = [:ref,:owner_id,:group_id]
 end

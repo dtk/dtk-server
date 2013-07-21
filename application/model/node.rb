c978cdc26704452b46a4d2fe89ed3db00768090c
Rich Pelavin <rich@reactor8.com>
2013-07-21 04:09:42 +0000

    put in logic so that staged nodes get unique names

--- application/model/node.rb
+++ application/model/node.rb
@@ -32,6 +32,36 @@ module XYZ
       false
     end
 
+    class Instance < self
+      def self.get(mh,opts={})
+        sp_hash = {
+          :cols => ([:id,:group_id,:display_name]+(opts[:cols]||[])).uniq,
+          :filter => [:neq,:datacenter_datacenter_id,nil]
+        }
+        get_objs(mh,sp_hash)
+      end
+
+      def self.get_unique_instance_name(mh,display_name)
+        display_name_regexp = Regexp.new("^#{display_name}")
+        matches = get(mh,:cols=>[:display_name]).select{|r|r[:display_name] =~ display_name_regexp}
+        if matches.empty?
+          return display_name
+        end
+        index = 2
+        matches.each do |r|
+          instance_name = r[:display_name]
+          if instance_name =~ /-([0-9]+$)/
+            instance_index = $1.to_i
+            if instance_index >= index
+              index += 1
+            end
+          end
+        end
+        "#{display_name}-#{index.to_s}"
+      end
+    end
+
+
 #TODO: end stub for feature_node_admin_state
 
     ### virtual column defs
@@ -57,7 +87,6 @@ module XYZ
 
     #######################
     # standard get methods
-
     def get_target(additional_columns = [])
       sp_hash = {
         :cols => [:id,:group_id,:display_name] + additional_columns,

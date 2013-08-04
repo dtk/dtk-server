4ab6aa9b956d9e1898db4feb3b36e7512c54df79
Rich Pelavin <rich@reactor8.com>
2013-08-04 02:24:09 +0000

    feature-task-refinement: small bug fix

--- application/model/task/template/action_list/config_components.rb
+++ application/model/task/template/action_list/config_components.rb
@@ -7,9 +7,18 @@ module DTK; class Task; class Template
           opts.merge!(:filter_proc => lambda{|el|el[:basic_type] == "smoketest"}) 
         end
         assembly_cmps = assembly.get_component_list(:seed => new())
-        ret = NodeGroup.get_component_list(assembly_cmps.map{|r|r[:node]},:add_on_to => assembly_cmps)
+        ret = NodeGroup.get_component_list(assembly_cmps.nodes(),:add_on_to => assembly_cmps)
         ret.set_action_indexes!()
       end
+    end
+
+    def nodes()
+      ndx_ret = Hash.new
+      each do |r|
+        node = r[:node]
+        ndx_ret[node[:id]] ||= node
+      end
+      ndx_ret.values()
     end
   end
 end; end; end

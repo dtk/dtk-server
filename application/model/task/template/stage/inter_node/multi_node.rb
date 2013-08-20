0d9a3fe4dd32cd6f64714f08b646f6ee403f1c4c
Rich Pelavin <rich@reactor8.com>
2013-08-20 03:33:46 +0000

    putting in logic that allows task templates with quantification over components

--- application/model/task/template/stage/inter_node/multi_node.rb
+++ application/model/task/template/stage/inter_node/multi_node.rb
@@ -12,17 +12,22 @@ module DTK; class Task; class Template; class Stage
       #This is used to include all applicable classes
       class Applicable < self
         def self.parse_and_reify(serialized_node_actions,action_list)
-          if serialized_node_actions[:ordered_components].size == 1
-            #TODO: if there is a title then we match on title
-            cmp_type = serialized_node_actions[:ordered_components].first
+          info_per_node = Hash.new #indexed by node_id
+          serialized_node_actions[:ordered_components].each do |cmp_ref|
+            #TODO: if there is a title then we need to match on title
+            cmp_type = cmp_ref
             matching_actions = action_list.select{|a|a.match_component_type?(cmp_type)}
-            matching_actions.inject(Hash.new) do |h,a|
-              h.merge(InterNode.parse_and_reify_node_actions(serialized_node_actions,a.node_name,a.node_id,action_list))
+            matching_actions.each do |a|
+              node_id = a.node_id
+              pntr = info_per_node[node_id] ||= {:actions => Array.new, :name => a.node_name, :id => node_id}
+              pntr[:actions] << cmp_ref
             end
-          else
-            raise Error.new("Stub does not treat (#{serialized_node_actions[:ordered_components].inspect})")
+          end
+          info_per_node.values.inject(Hash.new) do |h,n|
+            h.merge(InterNode.parse_and_reify_node_actions({:ordered_components => n[:actions]},n[:name],n[:id],action_list))
           end
         end
+
       end
     end
   end

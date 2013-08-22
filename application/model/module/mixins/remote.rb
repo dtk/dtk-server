1c0a57f8c69a1cdacd2773c3ee15c39da6e7de70
Aldin Milanovic <aldin@atlantbh.com>
2013-08-22 14:01:20 +0200

    Small fix in error message when delete-remote without namespace specified no_build

--- application/model/module/mixins/remote.rb
+++ application/model/module/mixins/remote.rb
@@ -213,7 +213,7 @@ module DTK
         module_obj.get_repos().each do |repo|
           # we remove remote repos
           unless repo_remote_db = RepoRemote.get_remote_repo(repo.model_handle(:repo_remote), repo.id, remote_params[:module_name], remote_params[:module_namespace])
-            raise ErrorUsage.new("R8 remote repo record '#{remote_params[:module_namespace]}/#{remote_params[:module_name]}' does not exist") 
+            raise ErrorUsage.new("Remote component/service (#{remote_params[:module_namespace]}/#{remote_params[:module_name]}) does not exist") 
           end
 
           repo.unlink_remote(remote_params[:repo])

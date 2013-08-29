852f0818bc2a58487962e510fcecc66eab37ef8a
Aldin Milanovic <aldin@atlantbh.com>
2013-08-29 13:38:44 +0200

    Fix for DTK-875 AssemblyController#initaite_grep not in current branch no_build

--- application/model/assembly/instance/action.rb
+++ application/model/assembly/instance/action.rb
@@ -41,7 +41,7 @@ module DTK
             indexes = nodes.map{|r|r[:id]}
             action_results_queue.set_indexes!(indexes)
             ndx_pbuilderid_to_node_info =  nodes.inject(Hash.new) do |h,n|
-              h.merge(n.pbuilderid => {:id => "test", :display_name => n[:display_name]}) 
+              h.merge(n.pbuilderid => {:id => n[:id], :display_name => n[:display_name]}) 
             end
 
             callbacks = {
@@ -72,20 +72,18 @@ module DTK
             indexes = nodes.map{|r|r[:id]}
             action_results_queue.set_indexes!(indexes)
             ndx_pbuilderid_to_node_info =  nodes.inject(Hash.new) do |h,n|
-              h.merge(n.pbuilderid => {:id => "test", :display_name => n[:display_name]}) 
+              h.merge(n.pbuilderid => {:id => n[:id], :display_name => n[:display_name]}) 
             end
 
             callbacks = {
               :on_msg_received => proc do |msg|
                 response = CommandAndControl.parse_response__execute_action(nodes,msg)
-
                 if response and response[:pbuilderid] and response[:status] == :ok
                   node_info = ndx_pbuilderid_to_node_info[response[:pbuilderid]]
                   action_results_queue.push(node_info[:id],response[:data])
                 end
               end
             }
-
             CommandAndControl.request__execute_action(:tail,:grep,nodes,callbacks,params)
           end
         end        

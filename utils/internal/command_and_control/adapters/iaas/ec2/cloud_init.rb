f943ec135ce1e249e74feb326f2bd9af6c7a3927
Rich Pelavin <rich@reactor8.com>
2013-09-06 04:29:20 +0000

    putting the mcollective auth info needed in cloud init under mcollective/config

--- utils/internal/command_and_control/adapters/iaas/ec2/cloud_init.rb
+++ utils/internal/command_and_control/adapters/iaas/ec2/cloud_init.rb
@@ -17,72 +17,25 @@ module DTK; module CommandAndControlAdapter
       end
       private
       def self.embed_in_os_specific_wrapper(os_type,cloud_init_user_data)
-        pp [:cloud_init_user_data,cloud_init_user_data]
-        raise ErrorUsage.new("got here")
+        unless header =  UserDataTemplates[os_type]
+          raise Error.new("No user data template for OS of type (#{os_type})")
+        end
+         header + cloud_init_user_data + "\n"
       end
-    end
-  end
-end; end
-=begin
-      
-pp [:foo_debug,CommandAndControl.cloud_init_user_data()]
-
-
-        mcollective_ssh_remote_public_key=File.open(R8::Config[:mcollective][:ssh][:remote][:public_key], 'rb') { |f| f.read }
-        mcollective_ssh_remote_private_key=File.open(R8::Config[:mcollective][:ssh][:remote][:private_key], 'rb') { |f| f.read }
-        mcollective_ssh_local_public_key=File.open(R8::Config[:mcollective][:ssh][:local][:public_key], 'rb') { |f| f.read }
-        template_bindings = {
-          :node_config_server_host => node_config_server_host,
-          :git_server_url => git_server_url, 
-          :git_server_dns => git_server_dns,
-          :fingerprint => fingerprint,
-          :mcollective_ssh_remote_public_key => mcollective_ssh_remote_public_key,
-          :mcollective_ssh_remote_private_key => mcollective_ssh_remote_private_key,
-          :mcollective_ssh_local_public_key => mcollective_ssh_local_public_key
-        }
-        unbound_bindings = template_bindings.reject{|k,v|v}
-        raise Error.new("Unbound cloudint var(s) (#{unbound_bindings.values.join(",")}") unless unbound_bindings.empty?
-        UserDataTemplates[os_type.to_sym] && UserDataTemplates[os_type.to_sym].result(template_bindings)
-      end
-    #TODO: put this as boothook because if not get race condition with start of mcollective
-#need to check if this now runs on every boot; if so might want to put provision in so only runs on first boot
-
-
-UserDataTemplates = Hash.new 
-# Ubuntu template
-UserDataTemplates[:ubuntu] = Erubis::Eruby.new <<eos
-#cloud-boothook
-#!/bin/sh 
-
-#{USER_DATA_SH}
-
-eos
-
-# Red Hat template
-UserDataTemplates[:redhat] = Erubis::Eruby.new <<eos
-#!/bin/sh 
-
-#{USER_DATA_SH}
-
-eos
-
-# CentOS template
-UserDataTemplates[:centos] = Erubis::Eruby.new <<eos
+      UserDataTemplateDefault = <<eos
 #!/bin/sh
 
-#{USER_DATA_SH}
-
 eos
-
-
-UserDataTemplates[:debian] = Erubis::Eruby.new <<eos
+      UserDataTemplateUbuntu = <<eos
+#cloud-boothook
 #!/bin/sh 
-
-#{USER_DATA_SH}
-
 eos
-
+      UserDataTemplates = {
+        :ubuntu => UserDataTemplateUbuntu,
+        :redhat => UserDataTemplateDefault,
+        :centos => UserDataTemplateDefault,
+        :debian => UserDataTemplateDefault
+      }
     end
   end
 end; end
-=end

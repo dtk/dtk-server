6e6eecdeba407350af19c8ab99530e82a90364d4
Rich Pelavin <rich@reactor8.com>
2013-09-11 20:18:38 +0000

    updated create workspace call in add_user utility so only creates a workspace assembly, not its ancestor

--- application/model/workspace.rb
+++ application/model/workspace.rb
@@ -1,12 +1,9 @@
 module DTK
   class Workspace < Assembly::Instance
-    #creates both a workspace template and a workspace instance
-    def self.create?(target_idh,project_idh)
+    def self.create?(target_idh)
       cmp_mh = target_idh.createMH(:component)
-      workspace_template_idh = create_aux?(:template,cmp_mh,:project_project_id => project_idh.get_id())
       instance_assigns = {
-        :datacenter_datacenter_id => target_idh.get_id(),
-        :ancestor_id => workspace_template_idh.get_id()
+        :datacenter_datacenter_id => target_idh.get_id()
       }
       create_aux?(:instance,cmp_mh,instance_assigns)
     end

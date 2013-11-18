c64a20a7184c8ba977de7be66c2b1c8ac0f9a4ce
Rich Pelavin <rich@reactor8.com>
2013-11-18 21:44:50 +0000

    cleaning up cancel task; putting it on assembly

--- application/config/routes.rb
+++ application/config/routes.rb
@@ -57,6 +57,7 @@ R8::ReactorRoute.draw do
   post  'assembly/create_component_dependency' => 'assembly#create_component_dependency'
   post  'assembly/promote_module_updates' => 'assembly#promote_module_updates'
   post  'assembly/clear_tasks' => 'assembly#clear_tasks'
+  post  'assembly/cancel_task' => 'assembly#cancel_task'
 
    # ATTRIBUTE
   post  'attribute/set' => 'attribute#set'
--- application/helper/task_helper.rb
+++ application/helper/task_helper.rb
@@ -1,8 +1,17 @@
 module Ramaze::Helper
   module TaskHelper
-    def get_most_recent_task()
-      tasks = XYZ::Task.get_top_level_tasks(model_handle(:task)).sort{|a,b| b[:updated_at] <=> a[:updated_at]}
-      tasks.first
+    def cancel_task(top_task_id)
+      task = ::DTK::Task.get_hierarchical_structure(id_handle(top_task_id,:task))
+      ::DTK::Workflow.cancel(top_task_id, task)
+    end
+
+    def get_most_recent_executing_task(filter=nil)
+      aug_filter = [:and,filter,[:eq,:status,'executing']].compact
+      get_most_recent_task(aug_filter)
+    end
+
+    def get_most_recent_task(filter=nil)
+      ::DTK::Task.get_top_level_most_recent_task(model_handle(:task),filter)
     end
   end
 end

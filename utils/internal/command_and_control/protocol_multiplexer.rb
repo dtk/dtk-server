d727d7fc82df1e81f2ef6eec34cc3169b62faa7f
Rich Pelavin <rich@reactor8.com>
2014-01-11 23:36:44 +0000

    bumped up timeout to wait on mcolelctive replies, no_build

--- utils/internal/command_and_control/protocol_multiplexer.rb
+++ utils/internal/command_and_control/protocol_multiplexer.rb
@@ -24,7 +24,7 @@ module XYZ
         trigger[:send_message].call(@protocol_handler,request_id)
       end
      private
-      DefaultTimeout = 360
+      DefaultTimeout = 30 * 60
       ExpectedCountDefault = 1
      public
 

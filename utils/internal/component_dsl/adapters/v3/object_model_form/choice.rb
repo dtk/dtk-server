150065449e974663e1064aed024274debe826da7
Rich Pelavin <rich@reactor8.com>
2014-01-25 00:47:37 +0000

    working on interaction of component deps and link_defs

--- utils/internal/component_dsl/adapters/v3/object_model_form/choice.rb
+++ utils/internal/component_dsl/adapters/v3/object_model_form/choice.rb
@@ -33,9 +33,10 @@ module DTK; class ComponentDSL; class V3
       #returns [dependencies,link_defs]
       def self.deps_and_link_defs(input_hash,base_cmp,opts={}) 
         ndx_dep_choices = Dependency.ndx_dep_choices(input_hash["dependencies"],base_cmp,opts)
-        spliced_ndx_link_def_links = LinkDef.spliced_ndx_link_def_links(input_hash["link_defs"],base_cmp,ndx_dep_choices,opts)
-        integrate_deps_and_link_defs!(ndx_dep_choices,spliced_ndx_link_def_links)
-pp [:after_add_dependent_components,ndx_dep_choices,spliced_ndx_link_def_links]
+        ndx_link_def_links = LinkDef.ndx_link_def_links(input_hash["link_defs"],base_cmp,opts)
+pp [:before_integrate,:ndx_dep_choices,ndx_dep_choices]
+        spliced_ndx_link_def_links = integrate_deps_and_link_defs!(ndx_dep_choices,ndx_link_def_links)
+pp [:after_integrate,:ndx_dep_choices,ndx_dep_choices]
         dependencies = Dependency.dependencies?(ndx_dep_choices.values,base_cmp,opts)
         link_defs = LinkDef.link_defs?(spliced_ndx_link_def_links)
         [dependencies,link_defs]
@@ -80,7 +81,20 @@ pp [:after_add_dependent_components,ndx_dep_choices,spliced_ndx_link_def_links]
         self.class.new(@raw,@dep_cmp_name,@base_cmp)
       end
 
-      def self.integrate_deps_and_link_defs!(ndx_dep_choices,spliced_ndx_link_def_links)
+      def matches_on_keys?(choice_with_single_pl)
+        ret = nil
+        pl_single = choice_with_single_pl.possible_link
+        unless pl_single.size == 1
+          raise Error.new("Unexepected that (#{pl_single.print_form}) has size > 1")
+        end
+        possible_link[pl_single.keys.first]
+      end
+
+      #returns spliced_ndx_link_def_links
+      def self.integrate_deps_and_link_defs!(ndx_dep_choices,ndx_link_def_links)
+        #first splice
+        spliced_ndx_link_def_links = splice_link_def_and_dep_info(ndx_link_def_links,ndx_dep_choices)
+pp [:spliced_ndx_link_def_links,spliced_ndx_link_def_links]
         #throw error if there are any unmatched ndx_dep_choices that have a remote location
         #remove any simple dependencies that match a link def
         ndx_dep_choices.each do |ndx,dep_choices|
@@ -97,18 +111,48 @@ pp [:after_add_dependent_components,ndx_dep_choices,spliced_ndx_link_def_links]
             end
           end
         end
-      end
-      
-      
-      def matches_on_keys?(choice_with_single_pl)
-        ret = nil
-        pl_single = choice_with_single_pl.possible_link
-        unless pl_single.size == 1
-          raise Error.new("Unexepected that (#{pl_single.print_form}) has size > 1")
-        end
-        possible_link[pl_single.keys.first]
+        spliced_ndx_link_def_links
       end
 
+      def self.splice_link_def_and_dep_info(ndx_link_def_links,ndx_dep_choices)
+        ret = Hash.new
+        ndx_link_def_links.each do |link_def_ndx,link_def_link_choices|
+          pruned_ndx_dep_choices = ndx_dep_choices
+          dep_name_match = false
+          if dep_choices = ndx_dep_choices[link_def_ndx]
+            pruned_ndx_dep_choices = {link_def_ndx => dep_choices}
+            dep_name_match = true
+          end
+          link_def_link_choices.each do |ldl_choice|
+            if dn = ldl_choice.dependency_name
+              unless dep_name_match
+                base_cmp_name = ldl_choice.base_cmp_print_form()
+                dep_cmp_name = ldl_choice.dep_cmp_print_form()
+                error_msg = "The link def segment on ?1: ?2\nreferences a dependency name (?3) that does not exist.\n"
+                raise ParsingError.new(error_msg,base_cmp_name,{dep_cmp_name => ldl_choice.print_form},dn)
+              end
+            end
+            unless ndx = matching_dep_index?(ldl_choice,pruned_ndx_dep_choices)
+              ldl_choice.required = false
+              ndx = ldl_choice.dep_cmp_ndx()
+            end
+            (ret[ndx] ||= Array.new) << ldl_choice
+          end
+        end
+        ret
+      end
+
+      def self.matching_dep_index?(link_def_link_choice,ndx_dep_choices)
+        ret = nil
+        ndx_dep_choices.each do |dep_ndx,dep_choices|
+          dep_choices.each do |dep_choice|
+            if dep_choice.matches?(link_def_link_choice)
+              return dep_ndx
+            end
+          end
+        end
+        ret
+      end
     end
   end
 end; end; end
--- utils/internal/component_dsl/adapters/v3/object_model_form/link_def.rb
+++ utils/internal/component_dsl/adapters/v3/object_model_form/link_def.rb
@@ -1,11 +1,18 @@
 module DTK; class ComponentDSL; class V3
   class ObjectModelForm
     class LinkDef < OMFBase
-      def self.spliced_ndx_link_def_links(in_link_defs,base_cmp,ndx_dep_choices,opts={})
+      def self.ndx_link_def_links(in_link_defs,base_cmp,opts={})
         ret = Hash.new
         return ret unless in_link_defs
-        ndx_link_def_links = ndx_link_def_links(in_link_defs,base_cmp,opts)
-        splice_link_def_and_dep_info(ndx_link_def_links,ndx_dep_choices)
+        convert_to_hash_form(in_link_defs) do |dep_cmp_name,link_def_links|
+          link_def_links = [link_def_links] unless link_def_links.kind_of?(Array)
+          link_def_links = convert_link_def_links(dep_cmp_name,link_def_links,base_cmp,opts)
+          link_def_links.each do |ldl|
+            ndx = ldl.dependency_name || dep_cmp_name
+            (ret[ndx] ||= Array.new) << ldl
+          end
+        end
+        ret
       end
 
       def self.link_defs?(spliced_ndx_link_def_links)
@@ -43,19 +50,6 @@ module DTK; class ComponentDSL; class V3
       end
 
       #------ begin: related to ndx_link_def_links
-      def self.ndx_link_def_links(in_link_defs,base_cmp,opts={})
-        ret = Hash.new
-        convert_to_hash_form(in_link_defs) do |dep_cmp_name,link_def_links|
-          link_def_links = [link_def_links] unless link_def_links.kind_of?(Array)
-          link_def_links = convert_link_def_links(dep_cmp_name,link_def_links,base_cmp,opts)
-          link_def_links.each do |ldl|
-            ndx = ldl.dependency_name || dep_cmp_name
-            (ret[ndx] ||= Array.new) << ldl
-          end
-        end
-        ret
-      end
-
       def self.convert_link_def_links(dep_cmp_name,link_def_links,base_cmp,opts={})
         link_def_links.inject(Array.new) do |a,link|
           unless link.kind_of?(Hash)
@@ -68,48 +62,6 @@ module DTK; class ComponentDSL; class V3
 
       def self.convert_link_def_link(link_def_link,dep_cmp_name,base_cmp,opts={})
         Choice::LinkDefLink.new(link_def_link,dep_cmp_name,base_cmp).convert(link_def_link,opts)
-      end
-
-      #------ end: related to ndx_link_def_links
-
-      def self.splice_link_def_and_dep_info(ndx_link_def_links,ndx_dep_choices)
-        ret = Hash.new
-        ndx_link_def_links.each do |link_def_ndx,link_def_link_choices|
-          pruned_ndx_dep_choices = ndx_dep_choices
-          dep_name_match = false
-          if dep_choices = ndx_dep_choices[link_def_ndx]
-            pruned_ndx_dep_choices = {link_def_ndx => dep_choices}
-            dep_name_match = true
-          end
-          link_def_link_choices.each do |ldl_choice|
-            if dn = ldl_choice.dependency_name
-              unless dep_name_match
-                base_cmp_name = ldl_choice.base_cmp_print_form()
-                dep_cmp_name = ldl_choice.dep_cmp_print_form()
-                error_msg = "The link def segment on ?1: ?2\nreferences a dependency name (?3) that does not exist.\n"
-                raise ParsingError.new(error_msg,base_cmp_name,{dep_cmp_name => ldl_choice.print_form},dn)
-              end
-            end
-            unless ndx = matching_dep_index?(ldl_choice,pruned_ndx_dep_choices)
-              ldl_choice.required = false
-              ndx = ldl_choice.dep_cmp_ndx()
-            end
-            (ret[ndx] ||= Array.new) << ldl_choice
-          end
-        end
-        ret
-      end
-
-      def self.matching_dep_index?(link_def_link_choice,ndx_dep_choices)
-        ret = nil
-        ndx_dep_choices.each do |dep_ndx,dep_choices|
-          dep_choices.each do |dep_choice|
-            if dep_choice.matches?(link_def_link_choice)
-              return dep_ndx
-            end
-          end
-        end
-        ret
       end
 
     end

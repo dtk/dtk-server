a73b783bc1915e17d532fdfae6656668f9162558
Rich Pelavin <rich@reactor8.com>
2014-02-12 17:01:16 +0000

    Fixed bug taht blocked converge

--- utils/internal/config_agent.rb
+++ utils/internal/config_agent.rb
@@ -5,20 +5,24 @@ module DTK
     r8_nested_require('config_agent','parse_errors_cache')
 
     def self.parse_given_module_directory(type,dir)
-      Adapter.load(type).parse_given_module_directory(dir)
+      load(type).parse_given_module_directory(dir)
     end
     def self.parse_given_filename(type,filename)
-      Adapter.load(type).parse_given_filename(filename)
+      load(type).parse_given_filename(filename)
     end
     def self.parse_given_file_content(type,file_path,file_content)
-      Adapter.load(type).parse_given_file_content(file_path,file_content)
+      load(type).parse_given_file_content(file_path,file_content)
     end
 
     def self.parse_external_ref?(type,impl_obj)
-      processor = Adapter.load(type)
+      processor = load(type)
       if processor.respond_to?('parse_external_ref?'.to_sym)
         processor.parse_external_ref?(impl_obj)
       end
+    end
+
+    def self.load(type)
+      Adapter.load(type)
     end
 
     def node_name(node)

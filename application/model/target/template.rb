605f4bf54189b8f15c88823bd2c57acb7e460f05
Rich Pelavin <rich@reactor8.com>
2014-02-20 05:32:31 +0000

    put check in provider/delete-provider to make sure that not deleting provider contaning default target

--- application/model/target/template.rb
+++ application/model/target/template.rb
@@ -42,8 +42,16 @@ module DTK
         unless assembly_instances.empty?
           assembly_names = assembly_instances.map{|a|a[:display_name]}.join(',')
           provider_name = provider.get_field?(:display_name)
-          raise ErrorUsage.new("Cannot delete provide #{provider_name} because service instance(s) (#{assembly_names}) are using one of its targets") 
+          raise ErrorUsage.new("Cannot delete provider '#{provider_name}' because service instance(s) (#{assembly_names}) are using one of its targets") 
         end
+        
+        target_instances = provider.get_target_instances(:cols => [:display_name,:is_default_target])
+        if default_target = target_instances.find{|t|t[:is_default_target]}
+          provider_name = provider.get_field?(:display_name)
+          default_target_name = default_target[:display_name]
+          raise ErrorUsage.new("Cannot delete provider '#{provider_name}' because it contains the default target (#{default_target_name})")
+        end 
+
         delete_instance(provider.id_handle())
       end
 

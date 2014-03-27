1ca37d001e81d1dbbb81b64db0aaadad6d4261e0
Rich Pelavin <rich@reactor8.com>
2014-03-27 23:32:08 +0000

    feature-multiple-remotes

--- application/model/repo/remote.rb
+++ application/model/repo/remote.rb
@@ -80,35 +80,14 @@ module DTK
 
     class Remote
 
+      r8_nested_require('remote','with_namespace')
+
       CREATE_MODULE_PERMISSIONS = { :user => 'RWDP', :user_group => 'RWDP', :other => 'R'}
-
-      class RemoteModuleRepoInfo < Hash
-        #has keys
-        #  :remote_repo_url
-        #  :remote_repo 
-        #  :remote_branch
-        #  :module_name
-        #  :version
-        def initialize(parent,branch_obj,remote_params)
-          super()
-          remote_repo = @remote_repo||parent.default_remote_repo()
-          hash = {
-            :module_name => remote_params[:module_name],
-            :remote_repo => remote_repo.to_s,
-            :remote_repo_url => parent.repo_url_ssh_access(remote_params[:remote_repo_name]),
-            :remote_branch => parent.version_to_branch_name(remote_params[:version]),
-            :workspace_branch => branch_obj.get_field?(:branch)
-          }
-          hash.merge!(:version => remote_params[:version]) if remote_params[:version]
-          replace(hash)
-        end
-      end
-
       r8_nested_require('remote','auth')
       include AuthMixin
 
       def get_remote_module_info(branch_obj,remote_params)
-        RemoteModuleRepoInfo.new(self,branch_obj,remote_params)
+        WithNamespace.new(self,branch_obj,remote_params)
       end
 
       def get_remote_module_components(module_name, type, version, namespace)
---
+++ application/model/repo/remote/with_namespace.rb
@@ -0,0 +1,25 @@
+module DTK; class Repo
+  class Remote
+    class WithNamespace < Hash
+      #has keys
+      #  :remote_repo_url
+      #  :remote_repo 
+      #  :remote_branch
+      #  :module_name
+      #  :version
+      def initialize(parent,branch_obj,remote_params)
+        super()
+        remote_repo = @remote_repo||parent.default_remote_repo()
+        hash = {
+          :module_name => remote_params[:module_name],
+          :remote_repo => remote_repo.to_s,
+          :remote_repo_url => parent.repo_url_ssh_access(remote_params[:remote_repo_name]),
+          :remote_branch => parent.version_to_branch_name(remote_params[:version]),
+          :workspace_branch => branch_obj.get_field?(:branch)
+        }
+        hash.merge!(:version => remote_params[:version]) if remote_params[:version]
+        replace(hash)
+      end
+    end
+  end
+end; end

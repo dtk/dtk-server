ee6dd8f6ef1e92241f8815b79ab54378672f460e
Rich Pelavin <rich@reactor8.com>
2014-05-13 22:42:19 +0000

    feature-node-groups: working on node attribute meta definitions

--- application/model/node/attribute/declarations.rb
+++ application/model/node/attribute/declarations.rb
@@ -1,10 +1,12 @@
 module DTK; class Node
   class NodeAttribute
-    Def::Type.attribute 'node_agent.puppet.version' do
+    Def::Type :foo do
+      attribute 'node_agent.puppet.version' do
 #      aliases ['puppet_version']
       data_type :string
       types lambda{|x|ConfigAgent.treated_version?(:puppet,x)}
       #TODO: put in meta attribute cannot_change_after_converge
     end
+end
   end
 end; end

044a6240be9413916bc856d3f7bca8fe822738ee
Rich Pelavin <rich@reactor8.com>
2014-06-09 06:05:51 +0000

    install-agent-workflow-task: moved processing of install agent to node_config,ssh

---
+++ utils/internal/command_and_control/adapters/node_config/ssh.rb
@@ -0,0 +1,10 @@
+module DTK
+  module CommandAndControlAdapter
+    class Ssh < CommandAndControlNodeConfig
+      def self.execute(task_idh,top_task_idh,task_action)
+        Log.error('ssh processing of actions needs to be written')
+      end
+    end
+  end
+end
+

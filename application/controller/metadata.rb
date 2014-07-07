56647a5ccc6a7b81821a0fa5b63b33d493437b42
Haris Krajina <haris@atlantbh.com>
2014-07-07 14:40:35 +0200

    added relative checks when reading file

--- application/controller/metadata.rb
+++ application/controller/metadata.rb
@@ -1,7 +1,7 @@
 module XYZ
   class MetadataController < Controller
   	def rest__get_metadata(file_name)
-  		file = File.open(File.expand_path("meta/tables_metadata/#{file_name}.json")).read
+  		file = File.open(File.expand_path("../meta/tables_metadata/#{file_name}.json", File.dirname(__FILE__))).read
       rest_ok_response file
   	end
   end

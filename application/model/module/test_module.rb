fe8989b5f0bf49eb4757b4cb44a57951b4f17328
Aldin Milanovic <aldin@atlantbh.com>
2014-07-07 11:27:27 +0200

    First part of changes for test_module

---
+++ application/model/module/test_module.rb
@@ -0,0 +1,27 @@
+module DTK
+  class TestModule < Model
+    r8_nested_require('component','dsl_mixin')
+    r8_nested_require('component','dsl')
+
+    r8_nested_require('component','version_context_info')
+    r8_nested_require('component','delete_mixin')
+
+    # include DeleteMixin
+    extend ModuleClassMixin
+    include ModuleMixin
+    # include DSLMixin
+
+    def self.model_type()
+      :test_module
+    end
+
+    def self.component_type()
+      :test #hardwired
+    end
+
+    def component_type()
+      :test #hardwired
+    end
+
+  end
+end

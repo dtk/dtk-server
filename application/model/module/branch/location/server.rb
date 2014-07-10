37c458ff080b989f661064f9a553a5e56cb8b006
Aldin Milanovic <aldin@atlantbh.com>
2014-07-10 14:56:44 +0200

    Added prefix test for test_modules in r8server-repo

--- application/model/module/branch/location/server.rb
+++ application/model/module/branch/location/server.rb
@@ -22,7 +22,15 @@ module DTK; class ModuleBranch
           username = CurrentSession.new.get_username()
           repo_name = "#{username}-local-#{module_name()}"
           # component_type can be :service_module, :puppet or :chef
-          @component_type == :service_module ? "sm-#{repo_name}" : repo_name
+          # @component_type == :service_module ? "sm-#{repo_name}" : repo_name
+          case @component_type
+            when :service_module
+              return "sm-#{repo_name}"
+            when :test_module
+              return "test-#{repo_name}"
+            else
+             repo_name
+           end
         end
 
         #===== helper methods

f7ae3f7ebd3de5a967b2e644a371aad62bd6b22a
Rich Pelavin <rich@reactor8.com>
2014-07-23 21:32:18 +0000

    refactor of state change create object; catalyst being treament of service node groups

--- application/model/state_change.rb
+++ application/model/state_change.rb
@@ -1,7 +1,9 @@
 module DTK
   class StateChange < Model
     r8_nested_require('state_change','get_pending_changes')
+    r8_nested_require('state_change','create')
     extend GetPendingChangesClassMixin
+    extend CreateClassMixin
 
     def self.list_pending_changes(target_idh)
       # TODO: may pass in options so dont get all fields that are returned in flat_list_pending_changes
@@ -28,22 +30,6 @@ module DTK
       end
     end
 
-    def self.create_converge_state_changes(node_idhs)
-      sample_idh = node_idhs.first()
-      sp_hash = {
-        :cols => [:id,:datacenter_datacenter_id,:components]
-      }
-      new_item_hashes = Model.get_objs_in_set(node_idhs,sp_hash).map do |r|
-        {
-          :new_item => r[:component].id_handle(), 
-          :parent => sample_idh.createIDH(:model_name => :datacenter, :id=> r[:datacenter_datacenter_id]),
-          :type => "converge_component"
-        }
-      end
-      create_pending_change_items(new_item_hashes)
-    end
-
-
     # object processing and access functions
     #######################
     def on_node_config_agent_type()
@@ -63,63 +49,7 @@ module DTK
       true
     end
 
-    def self.create_pending_change_item(new_item_hash,opts={})
-      create_pending_change_items([new_item_hash],opts).first
-    end
-    # assumption is that all parents are of same type and all changed items of same type
-    def self.create_pending_change_items(new_item_hashes,opts={})
-      ret = Array.new
-      return ret if new_item_hashes.empty? 
-      parent_model_name = new_item_hashes.first[:parent][:model_name]
-
-      # workaround referenced in PBUILDER-161
-      unless [:target,:datacenter].include?(parent_model_name)
-        Log.info("workaround for PBUILDER-161: changing parent_model_name (#{parent_model_name}) to be target (datacenter)")
-        parent_idh = new_item_hashes.first[:parent].get_top_container_id_handle(:datacenter)
-        parent_model_name = parent_idh[:model_name]
-        new_item_hashes.each{|r|r[:parent] = parent_idh}
-      end
-
-      new_item_obj = new_item_hashes.first[:new_item]
-      new_item_obj.get_field?(:group_id)  #to make sure model handle has group_id
-      model_handle = new_item_obj.createMH({:model_name => :state_change, :parent_model_name => parent_model_name})
-
-      object_model_name = new_item_hashes.first[:new_item][:model_name]
-      object_id_col = "#{object_model_name}_id".to_sym
-      parent_id_col = model_handle.parent_id_field_name()
-      type = new_item_hashes.first[:type] ||  
-        case object_model_name
-          when :attribute then "setting"
-          when :component then "install_component"
-          when :node then "create_node"
-          else raise Error::NotImplemented.new("when object type is #{object_model_name}")
-      end 
-      
-      ref_prefix = "state_change"
-      i=0
-      rows = new_item_hashes.map do |item| 
-        ref = "#{ref_prefix}#{(i+=1).to_s}"
-        id = item[:new_item].get_id()
-        parent_id = item[:parent].get_id()
-        display_name = ret_stub_display_name(object_model_name,item[:new_item][:display_name])
-        hash = {
-          :ref => ref,
-          :display_name => display_name,
-          :status => "pending",
-          :type => type,
-          :object_type => object_model_name.to_s,
-          object_id_col => id,
-          parent_id_col => parent_id
-        }
-        hash.merge!(:change => item[:change]) if item[:change]
-        hash.merge!(:change_paths => item[:change_paths]) if item[:change_paths]
-        hash
-      end
-      create_from_rows(model_handle,rows,{:convert => true})
-    end
-
     def self.ret_display_name(flat_pending_ch)
-
       type = flat_pending_ch[:type]
       node_name = flat_pending_ch[:node][:display_name]
       suffix = 
@@ -134,17 +64,6 @@ module DTK
           nil
         end
       suffix ? "#{type}(#{suffix})" : type
-    end
-
-    # called 'stub'  because objects refernced can change
-    def self.ret_stub_display_name(object_model_name,item_display_name)
-      display_name_prefix = 
-        case object_model_name
-         when :attribute then "setting-attribute"
-         when :component then "install-component"
-         when :node then "create-node"
-      end
-      display_name_prefix + (item_display_name ? "(#{item_display_name})" : "")
     end
   end
 end

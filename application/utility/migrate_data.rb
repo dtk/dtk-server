5761886e83a18986c9f366f180ece52a4ea4743d
Haris Krajina <haris@atlantbh.com>
2014-08-18 15:38:39 +0200

    FIX

--- application/utility/migrate_data.rb
+++ application/utility/migrate_data.rb
@@ -1,9 +1,19 @@
 #!/usr/bin/env ruby
 
-unless ARGV[0]
-  puts "You need to pass tenant name! (e.g. dtk16)"
+unless ARGV[0] && ARGV[1]
+  puts "You need to pass tenant name and tenant DB ID! (e.g. ruby utility/migrate_data.rb dtk16 2)"
   exit
 end
+
+puts "WARNING!"
+puts
+puts "************* PROVIDED DATA *************"
+puts " TENANT-ID:    #{ARGV[0]}"
+puts " TENANT-DB-ID: #{ARGV[1]}"
+puts "*****************************************"
+puts "Make sure that provided data is correct, and press any key to continue OR CTRL^C to stop"
+system('read')
+
 
 root = File.expand_path('../', File.dirname(__FILE__))
 

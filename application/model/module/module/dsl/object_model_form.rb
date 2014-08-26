971a1c4ce31825c4cf205b2211c6b59694555869
Rich Pelavin <rich@reactor8.com>
2014-08-26 02:47:06 +0000

    making component parsing of dependencies more robust, no_build

--- application/model/module/module/dsl/object_model_form.rb
+++ application/model/module/module/dsl/object_model_form.rb
@@ -65,14 +65,16 @@ module DTK; class ModuleDSL
       @fields
     end
 
-    def convert_to_hash_form(hash_or_array,&block)
-      self.class.convert_to_hash_form(hash_or_array,&block)
+    def convert_to_hash_form(obj,&block)
+      self.class.convert_to_hash_form(obj,&block)
     end
-    def self.convert_to_hash_form(hash_or_array,&block)
-      if hash_or_array.kind_of?(Hash)
-        hash_or_array.each_pair{|k,v|block.call(k,v)}
-      else #hash_or_array.kind_of?(Array)
-        hash_or_array.each do |el|
+    def self.convert_to_hash_form(obj,&block)
+      if obj.kind_of?(Hash)
+        obj.each_pair{|k,v|block.call(k,v)}
+      elsif obj.kind_of?(String)
+        block.call(obj,Hash.new)
+      else #obj.kind_of?(Array)
+        obj.each do |el|
           if el.kind_of?(Hash)
             block.call(el.keys.first,el.values.first)
           else #el.kind_of?(String)

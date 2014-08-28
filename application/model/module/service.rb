dtk/dtk-server

commit 685544a507963d4e5168d47fd3f8f66b15573df2

fixed problem that ywo component modules with same naem in different ns are trampling each other on push -f, no_build

application/model/module/service/dsl/assembly_import.rb
@@ -25,7 +25,7 @@ module DTK; class ServiceModule
       version_proc_class.assembly_iterate(module_name,hash_content) do |assemblies_hash,node_bindings_hash|
         dangling_errors = ParsingError::DanglingComponentRefs::Aggregate.new()
         assemblies_hash.each do |ref_without_ns,assem|
-          ref = "#{@module_namespace}--#{ref_without_ns}"
+          ref = Namespace.join_namespace(@module_namespace,ref_without_ns)
           if file_path = opts[:file_path]
             @ndx_assembly_file_paths[ref] = file_path
           end

application/model/module/module/dsl/update_model.rb
@@ -24,7 +24,7 @@ module DTK; class ModuleDSL
       end
       attr_reader :components_hash,:stored_components_hash
 
-      def parse_components!(config_agent_type,dsl_hash,namespace=nil)
+      def parse_components!(config_agent_type,dsl_hash,namespace)
         impl_id = impl_idh.get_id()
         module_branch_id = module_branch_idh.get_id()
         
@@ -48,9 +48,6 @@ module DTK; class ModuleDSL
           info.merge!("implementation_id" => impl_id, "module_branch_id" => module_branch_id)
           h.merge(cmp_ref => info)
         end
-        # TODO: think this is no longer needed
-        # process the link defs for remote components
-        # process_remote_link_defs!(container_idh)
       end
 
      private
@@ -59,13 +56,9 @@ module DTK; class ModuleDSL
       def component_ref_from_cmp_type(config_agent_type,component_type)
         "#{config_agent_type}-#{component_type}"
       end
-      def component_ref(config_agent_type,r8_hash_cmp_ref,namespace=nil)
-        # TODO: may be better to have these prefixes already in r8 dsl file
-        # "#{config_agent_type}-#{r8_hash_cmp_ref}"
-        ret = "#{config_agent_type}-#{r8_hash_cmp_ref}"
-        ret = Namespace.join_namespace(namespace, ret) if namespace
-
-        ret
+      def component_ref(config_agent_type,hash_cmp_ref,namespace)
+        ref_wo_ns = "#{config_agent_type}-#{hash_cmp_ref}"
+        Namespace.join_namespace(namespace, ref_wo_ns)
       end
 
     end

application/model/module/module/dsl_mixin.rb
@@ -135,6 +135,7 @@ module DTK; class BaseModule
     def update_model_objs_or_create_dsl?(diffs_summary,module_branch,version,opts={})
       ret = Hash.new
       dsl_created_info = Hash.new
+      module_namespace = module_namespace()
       impl_obj = module_branch.get_implementation()
       # TODO: make more robust to handle situation where diffs dont cover all changes; think can detect by looking at shas
       impl_obj.modify_file_assets(diffs_summary)
@@ -148,7 +149,7 @@ module DTK; class BaseModule
         AssemblyModule::Component.finalize_edit(assembly,self,module_branch)
       elsif ModuleDSL.contains_dsl_file?(impl_obj)
         if opts[:force_parse] or diffs_summary.meta_file_changed?() or (get_field?(:dsl_parsed) == false)
-          if e = ModuleDSL::ParsingError.trap{parse_dsl_and_update_model(impl_obj,module_branch.id_handle(),version,nil,opts)}
+          if e = ModuleDSL::ParsingError.trap{parse_dsl_and_update_model(impl_obj,module_branch.id_handle(),version,module_namespace,opts)}
             ret.merge!(:dsl_parsed_info => e)
           end
         end

commit bd3b911f1f877e03a0958eec906a4de9883cc1d3

fixed bug introduced by commit 685544a507963d4e5168d47fd3f8f66b15573df2

application/model/module/service.rb
@@ -29,6 +29,10 @@ module DTK
       ndx_ret.values
     end
 
+    def assembly_ref(assembly_name)
+      Namespace.join_namespace(module_namespace(),"#{module_name()}-#{assembly_name}")
+    end
+
     def self.get_required_and_missing_modules(project, remote_params, client_rsa_pub_key=nil)
       remote = remote_params.create_remote(project)
       response = Repo::Remote.new(remote).get_remote_module_components(client_rsa_pub_key)
@@ -330,12 +334,6 @@ pp [:debug_publish_preprocess_raise_error,:module_info,module_info]
       mb_mh = model_handle(:module_branch)
       cmp_module_branch_idhs = component_templates.map{|r|r[:module_branch_id]}.uniq.map{|id|mb_mh.createIDH(:id => id)}
       ModuleBranch.get_component_modules_info(cmp_module_branch_idhs)
-    end
-
-    def self.assembly_ref(module_name,assembly_name)
-      # TODO: right now cannot change because node bdings in assembly.json hard coded to this. Need to check if any ambiguity
-      # if have module name with hyphen
-      "#{module_name}-#{assembly_name}"
     end
   end
 end

application/model/module/service/dsl/assembly_import.rb
@@ -22,10 +22,9 @@ module DTK; class ServiceModule
     def process(module_name,hash_content,opts={})
       integer_version = determine_integer_version(hash_content,opts)
       version_proc_class = load_and_return_version_adapter_class(integer_version)
-      version_proc_class.assembly_iterate(module_name,hash_content) do |assemblies_hash,node_bindings_hash|
+      version_proc_class.assembly_iterate(@service_module,hash_content) do |assemblies_hash,node_bindings_hash|
         dangling_errors = ParsingError::DanglingComponentRefs::Aggregate.new()
-        assemblies_hash.each do |ref_without_ns,assem|
-          ref = Namespace.join_namespace(@module_namespace,ref_without_ns)
+        assemblies_hash.each do |ref,assem|
           if file_path = opts[:file_path]
             @ndx_assembly_file_paths[ref] = file_path
           end
@@ -75,15 +74,21 @@ module DTK; class ServiceModule
       @db_updates_assemblies["component"]
     end
 
-    def self.import_assembly_top(serialized_assembly_ref,assembly_hash,module_branch,module_name,opts={})
+    def self.import_assembly_top(assembly_ref,assembly_hash,module_branch,module_name,opts={})
       if assembly_hash.empty?
         raise ParsingError.new("Empty assembly dsl file",opts_file_path(opts))
       end
       unless assembly_name = assembly_hash["name"]||opts[:default_assembly_name]
         raise ParsingError.new("No name associated with assembly dsl file",opts_file_path(opts))
       end
-      version_field = module_branch.get_field?(:version)
-      assembly_ref = internal_assembly_ref__with_version(serialized_assembly_ref,version_field)
+
+      if version_field = module_branch.get_field?(:version)
+        unless version_field == 'master'
+          Log.error("this probably should be done same time module name and module namesapce put in to form ref, i.e., in ServiceModule#assembly_ref")
+          raise Error.new("Treatment of versions in import is not yet implemented")
+          # assembly_ref = internal_assembly_ref__with_version(assembly_ref,version_field)
+        end
+      end
       {
         assembly_ref => {
           "display_name" => assembly_name,
@@ -162,6 +167,7 @@ module DTK; class ServiceModule
       end
     end
 
+    # TODO: deprecate or move to within ServiceModule.assembly_ref
     def self.internal_assembly_ref__add_version(assembly_ref,version_field)
       Assembly.internal_assembly_ref__add_version(assembly_ref,version_field)
     end
@@ -198,6 +204,7 @@ module DTK; class ServiceModule
       nil
     end
 
+    # TODO: deprecate or move to within ServiceModule.assembly_ref
     def self.internal_assembly_ref__with_version(serialized_assembly_ref,version_field)
       module_name,assembly_name = parse_serialized_assembly_ref(serialized_assembly_ref)
       Assembly.internal_assembly_ref(module_name,assembly_name,version_field)
@@ -213,6 +220,7 @@ module DTK; class ServiceModule
         raise Error.new("Unexpected form for serialized assembly ref (#{ref})")
       end
     end
+    ### end: TODO: deprecate or move to within ServiceModule.assembly_ref
 
     def self.import_component_refs(container_idh,assembly_name,components_hash,component_module_refs,opts={})
       cmps_with_titles = Array.new

application/model/module/service/dsl/assembly_import/adapters/v1.rb
@@ -1,9 +1,9 @@
 module DTK; class ServiceModule
   class AssemblyImport
     class V1 < self
-      def self.assembly_iterate(module_name,hash_content,&block)
+      def self.assembly_iterate(service_module,hash_content,&block)
         assemblies_hash = hash_content["assemblies"].values.inject(Hash.new) do |h,assembly_info|
-          h.merge(ServiceModule.assembly_ref(module_name,assembly_info["name"]) => assembly_info)
+          h.merge(service_module.assembly_ref(assembly_info["name"]) => assembly_info)
         end
         node_bindings_hash = hash_content["node_bindings"]
         block.call(assemblies_hash,node_bindings_hash)

application/model/module/service/dsl/assembly_import/adapters/v2.rb
@@ -1,16 +1,16 @@
 module DTK; class ServiceModule
   class AssemblyImport
     class V2 < self
-      def self.assembly_iterate(module_name,hash_content,&block)
+      def self.assembly_iterate(service_module,hash_content,&block)
         assembly_hash = (hash_content["assembly"]||{}).merge(Aux::hash_subset(hash_content,["name","workflow"]))
-        assembly_ref = ServiceModule.assembly_ref(module_name,hash_content["name"])
+        assembly_ref = service_module.assembly_ref(hash_content["name"])
         assemblies_hash = {assembly_ref => assembly_hash}
         node_bindings_hash = hash_content["node_bindings"]
         block.call(assemblies_hash,node_bindings_hash)
       end
 
-      def self.import_assembly_top(serialized_assembly_ref,assembly_hash,module_branch,module_name,opts={})
-        ret = super(serialized_assembly_ref,assembly_hash,module_branch,module_name,opts)
+      def self.import_assembly_top(assembly_ref,assembly_hash,module_branch,module_name,opts={})
+        ret = super(assembly_ref,assembly_hash,module_branch,module_name,opts)
         ret_assembly_hash = ret.values.first
         ret_assembly_hash.merge!("task_template" => import_task_templates(assembly_hash))
         ret

application/model/module/service/service_add_on/import.rb
@@ -101,7 +101,8 @@ module DTK
         unless assembly = @assemblies.find{|a|a[:display_name] == name}
           Log.error("Field (#{field}) has value (#{name}) which is not a valid assembly reference")
         end
-        [assembly,ServiceModule.assembly_ref(module_name,name)]
+        raise Error.new("if use need to pass in service_module and call service_module.assembly_ref(name)")
+#        [assembly,ServiceModule.assembly_ref(module_name,name)]
       end
     end
   end
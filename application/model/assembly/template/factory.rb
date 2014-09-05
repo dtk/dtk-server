84a138ea7a7a89c208d583500adeb54cc8fcfc4a
Aldin Milanovic <aldin@atlantbh.com>
2014-09-05 14:32:12 +0200

    Removed comment, no_build

--- application/model/assembly/template/factory.rb
+++ application/model/assembly/template/factory.rb
@@ -47,10 +47,6 @@ module DTK
         local_params = ModuleBranch::Location::LocalParams::Server.new(
           :module_type => :service_module,
           :module_name => service_module_name,
-          # :module_namespace => service_module.module_namespace(),
-          # changed module_namespace to namespace because getting error message error: [#<XYZ::Error: Illegal key(s): module_namespace>
-          # when do service/service_name> create-assembly
-          # revert if breaks anything
           :namespace => service_module.module_namespace(),
           :version => opts[:version]
         )

ac4d709ea561d626ef5176fba953c48ecaccd972
Aldin Milanovic <aldin@atlantbh.com>
2014-09-19 15:20:43 +0200

    DTK-1666 Raise ErrorUsage when import a component module where the dtk.model.yaml name does not match module name, no_build

--- application/model/module/service/dsl.rb
+++ application/model/module/service/dsl.rb
@@ -201,6 +201,10 @@ module DTK
             
             hash_content = Aux.convert_to_hash(file_content,format_type,opts)||{}
             return hash_content if ParsingError.is_error?(hash_content)
+
+            assembly_name = validate_name_for_assembly(meta_file,hash_content['name'])
+            return assembly_name if ParsingError.is_error?(assembly_name)
+
             imported = assembly_import_helper.process(module_name,hash_content,opts)
             return imported if ParsingError.is_error?(imported)
           end
@@ -261,6 +265,13 @@ module DTK
           end
         end
         ret
+      end
+
+      def validate_name_for_assembly(file_path,name)
+        return unless (name || file_path)
+        file_name = file_path.split('/').last
+        assembly_name = file_name.split('.').first
+        return ParsingError::BadAssemblyReference.new(:file_path => file_path, :name => name) unless assembly_name.eql?(name)
       end
 
       def meta_file_format_type(path)

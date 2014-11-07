d1b188f173006566ceb090ca7f383f6255475188
Haris Krajina <haris@atlantbh.com>
2014-11-07 10:28:38 +0000

    remove old deployment modules

--- dtk_deployment_modules/component_modules/dtk_nginx/manifests/base.pp
+++
@@ -1,5 +0,0 @@
-class dtk_nginx::base(
-  $listen_port = $default_listen_port
-) inherits dtk_nginx::params
-{
-}
--- dtk_deployment_modules/component_modules/dtk_postgresql/manifests/client.pp
+++
@@ -1,6 +0,0 @@
-define dtk_postgresql::hostname(
-  $server_hostname
-)
-{
-  notify{ "server_hostname = ${server_hostname}":}
-}

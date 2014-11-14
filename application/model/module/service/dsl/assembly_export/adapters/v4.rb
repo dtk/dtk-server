df5762d3c3320d7d0b78d0686368347416b7b8fd
Rich Pelavin <rich@reactor8.com>
2014-11-14 04:38:03 +0000

    feature-attribute-tagging: refinment to render tag info when promoting assembly instance

--- application/model/module/service/dsl/assembly_export/adapters/v4.rb
+++ application/model/module/service/dsl/assembly_export/adapters/v4.rb
@@ -2,6 +2,39 @@ module DTK
   class ServiceModule; class AssemblyExport
     r8_require('v3')
     class V4 < V3
+      def attr_overrides_output_form(non_def_attrs)
+        ret = nil
+        return ret unless non_def_attrs
+        value_overrides = Array.new
+        attribute_info = Array.new
+        non_def_attrs.values.each do |attr|
+          if attr.isa_value_override?()
+            unless attr.is_title_attribute()
+              value_overrides << {attr[:display_name] => attr_value_output_form(attr,:attribute_value)}
+            end
+          end
+          if tags = attr.tags?()
+            attribute_info << {attr[:display_name] => attr_tags_setting(tags)}
+          end
+        end
+
+        ret = attribute_info_ouput_form(:attributes,value_overrides).merge(
+               attribute_info_ouput_form(:attribute_info,attribute_info))
+        !ret.empty? && ret
+      end
+
+      def attr_tags_setting(tags)
+        tags.size == 1 ? {:tag => tags.first} : {:tags => tags}
+      end
+
+      def attribute_info_ouput_form(key,array)
+        if array.empty?
+          Hash.new
+        else
+          sorted = array.sort{|a,b|a.keys.first <=> b.keys.first}
+          SimpleOrderedHash.new(key => SimpleOrderedHash.new(sorted))
+        end
+      end
     end
   end; end
 end

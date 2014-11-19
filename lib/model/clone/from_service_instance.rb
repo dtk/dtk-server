2e7af15f824f8b2ee136c5c95f017d64240af2ac
Rich Pelavin <rich@reactor8.com>
2014-11-19 17:57:00 +0000

    feature-module-update-in-service: putting in code to increemntally update componet object from service insatnce compoennt module changes

---
+++ lib/model/clone/from_service_instance.rb
@@ -0,0 +1,8 @@
+module DTK
+  class Clone
+    # This class is responsible for incremntal clone (incremental update) when component module 
+    # in a service instance are updated the compoennt isnatnce needs to be updated
+    class FromServiceInstance
+    end
+  end
+end

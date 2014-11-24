3f8a55e2ecbb801fa40614df763893bd05cc4e54
Rich Pelavin <rich@reactor8.com>
2014-11-24 19:52:38 +0000

    feature-module-update-in-service: refining add, modify, delete

--- lib/model/clone/incremental_update.rb
+++ lib/model/clone/incremental_update.rb
@@ -2,8 +2,10 @@ module DTK; class Clone
   # These mdouels explicitly have class to the sub object type in contrast to 
   # initial clone, which does not              
   module IncrementalUpdate
+    # Helper classes
     r8_nested_require('incremental_update','instance_template_links')
     r8_nested_require('incremental_update','instances_templates_links')
+    # classes for processing specific object model types
     r8_nested_require('incremental_update','component')
     r8_nested_require('incremental_update','dependency')
   end

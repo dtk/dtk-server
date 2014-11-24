dc9d3446c1f6fde4058f80fc7065702d0970b856
Rich Pelavin <rich@reactor8.com>
2014-11-24 00:37:15 +0000

    feature-module-update-in-service: clean up

---
+++ lib/model/clone/incremental_update/instance_template_links.rb
@@ -0,0 +1,41 @@
+module DTK; class Clone
+  module IncrementalUpdate
+    class InstanceTemplateLinks < Array
+      Link = Struct.new(:instance,:template)
+      def add(instance,template)
+        self << Link.new(instance,template)
+      end
+
+      def template(instance)
+        match = match_instance(instance)
+        match[:template] || raise(Error.new("Cannot find matching template for instance (#{instance.inspect})"))
+      end
+
+      def match_instance(instance)
+        instance_id = instance.id
+        unless match = find{|l|l.instance and l.instance.id == instance_id}
+          raise(Error.new("Cannot find match for instance (#{instance.inspect})"))
+        end
+        match
+      end
+
+      def all_id_handles()
+        templates().map{|r|r.id_handle()} + instances().map{|r|r.id_handle()}
+      end
+      
+     private
+      def templates()
+        #removes dups
+        inject(Hash.new) do |h,l|
+          template = l.template
+          h.merge(template ? {template.id => template} : {})
+        end.values
+      end
+     
+      def instances()
+        #does not have dups
+        map{|l|l.instance}.compact
+      end
+    end
+  end
+end; end

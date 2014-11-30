419fe6bdc9d83eee6e1fdacf2c2856fa3865c3fa
Rich Pelavin <rich@reactor8.com>
2014-11-30 20:52:26 +0000

    feature-link-defs-on-node-groups: refactoring attribute_mapping class to have multiple sub objects in own files

--- application/model/link_def_link/attribute_mapping.rb
+++ application/model/link_def_link/attribute_mapping.rb
@@ -1,6 +1,10 @@
 module DTK
   class LinkDefLink
     class AttributeMapping < HashObject
+      r8_nested_require('attribute_mapping','augmented_link_context')
+      r8_nested_require('attribute_mapping','augmented_link')
+      r8_nested_require('attribute_mapping','parse_helper')
+                        
       def self.reify(object)
         if object.kind_of?(AttributeMapping)
           object
@@ -11,52 +15,23 @@ module DTK
         end
       end
 
-      def match_attribute_patterns?(dep_attr_pattern,antec_attr_pattern)
-        if dep_attr_pattern.match_attribute_mapping_endpoint?(self[:input]) and
-            antec_attr_pattern.match_attribute_mapping_endpoint?(self[:output])
-          self
-        end
-      end
-
+      # returns array of AugmentedLink elements
       def self.ret_links(attribute_mappings,context,opts={})
         attribute_mappings.inject(Array.new) do |ret,am|
           ret + am.ret_links(context,opts)
         end
       end
-
-      class AugmentedLink < Hash
-        def initialize(attribute_mapping)
-          super()
-          @attribute_mapping = attribute_mapping
-        end
-        def parse_function_with_args?()
-          @attribute_mapping.parse_function_with_args?()
-        end
-      end
-
-      # returns array of AugmentedLink elements
       def ret_links(context,opts={})
         ret = Array.new
         err_msgs = Array.new
         input_attr,input_path = get_aug_attr_with_unravel_path(err_msgs,:input,context)
         output_attr,output_path = get_aug_attr_with_unravel_path(err_msgs,:output,context)
-
         unless err_msgs.empty?
           process_ret_links_error(err_msgs,opts)
           # above might raise an exception in which case below is never reached
           return ret
         end
-# TODO: case to see if need to break into multiple links
-input_attr_node = input_attr[:node]
-output_attr_node = output_attr[:node]
-input_node_group = input_attr_node.is_node_group?()
-output_node_group = output_attr_node.is_node_group?()
-pp [input_attr_node,output_attr_node,input_node_group,output_node_group]
-
-        am_link = AugmentedLink.new(self).merge(:input_id => input_attr[:id],:output_id => output_attr[:id])
-        am_link.merge!(:input_path => input_path) if input_path
-        am_link.merge!(:output_path => output_path) if output_path
-        [am_link]
+        AugmentedLinkContext.new(self,input_attr,input_path,output_attr,output_path).ret_links()
       end
 
       # returns a hash with args if this is a function that takes args
@@ -65,37 +40,14 @@ pp [input_attr_node,output_attr_node,input_node_group,output_node_group]
       def parse_function_with_args?()
         ParseHelper::VarEmbeddedInText.isa?(self) # || other ones we add
       end
-      module ParseHelper
-        module VarEmbeddedInText
-          def self.isa?(am)
-            if output_term_index = (am[:output]||{})[:term_index] 
-              if output_var = output_term_index.split('.').last
-                # example abc${output_var}def",
-                if output_var =~ /(^[^\$]*)\$\{[^\}]+\}(.*$)/
-                  text_parts = [$1,$2]
-                  {
-                    :name => :var_embedded_in_text,
-                    :constants  =>  {:text_parts => text_parts}
-                  }
-                end
-              end
-            end
-          end
+
+      def match_attribute_patterns?(dep_attr_pattern,antec_attr_pattern)
+        if dep_attr_pattern.match_attribute_mapping_endpoint?(self[:input]) and
+            antec_attr_pattern.match_attribute_mapping_endpoint?(self[:output])
+          self
         end
       end
           
-      def pp_form(direction)
-        ret = 
-          if attr = self[direction]
-            cmp_type = attr[:component_type]
-            attr_name = attr[:attribute_name]
-            if cmp_type and attr_name
-              "#{Component.component_type_print_form(cmp_type)}.#{attr_name}"
-            end
-          end
-        ret||""
-      end
-
      private
       # returns [attribute,unravel_path] and updates error if any error
       def get_aug_attr_with_unravel_path(err_msgs,dir,context)
@@ -116,6 +68,17 @@ pp [input_attr_node,output_attr_node,input_node_group,output_node_group]
         end
       end
 
+      def pp_form(direction)
+        ret = 
+          if attr = self[direction]
+            cmp_type = attr[:component_type]
+            attr_name = attr[:attribute_name]
+            if cmp_type and attr_name
+              "#{Component.component_type_print_form(cmp_type)}.#{attr_name}"
+            end
+          end
+        ret||""
+      end
     end
   end
 end

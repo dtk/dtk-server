39096c1820b47963bf16b92ba4a9ffcc481d06c7
Rich Pelavin <rich@reactor8.com>
2014-12-02 02:20:41 +0000

    feature-link-defs-on-node-groups: working on processing link defs on node groups

--- application/model/link_def_link/attribute_mapping/augmented_link_context.rb
+++ application/model/link_def_link/attribute_mapping/augmented_link_context.rb
@@ -58,9 +58,13 @@ module DTK; class LinkDefLink
         if @output_attr_obj.is_node_attribute?()
           node_group_attrs = @output_attr_obj.get_node_group_member_attributes()
           pp(:attr => @output_attr_obj.value, :node_group_attrs => node_group_attrs)
+          input_attr = input_attr()
+          node_group_attrs.map do |output_attr|
+            ret_single_link(input_attr(),output_attr)
+          end
         end
-        nil
       end
+
     end
   end
 end; end

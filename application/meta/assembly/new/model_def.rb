0d029156cafd4baeaea59fdeedd6a655474ed234
Rich Pelavin <rich@reactor8.com>
2014-12-05 18:38:47 +0000

    feature-link-defs-on-node-groups: bug fixing around workflow

--- application/meta/assembly/new/model_def.rb
+++ application/meta/assembly/new/model_def.rb
@@ -312,7 +312,7 @@ lambda__instance_nodes_components_assembly_template =
       :type=>:json,
       :hidden=>true,
       :remote_dependencies=>
-        lambda__segments_nodes_and_components.call([:id,:display_name,:group_id],[:id,:display_name,:component_type,:group_id,:only_one_per_node]) +
+        lambda__segments_nodes_and_components.call([:id,:display_name,:group_id,:type],[:id,:display_name,:component_type,:group_id,:only_one_per_node]) +
       [{
         :model_name=>:attribute,
         :convert => true,

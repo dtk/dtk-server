9216bca10aa87550cdf2306b51cb1b9a38332861
bjusufbe <bakir@atlantbh.com>
2015-01-14 14:53:45 +0100

    added changes back for add_user.rb

--- application/model/namespace.rb
+++ application/model/namespace.rb
@@ -68,7 +68,7 @@ module DTK
     def self.find_by_name(namespace_mh, namespace_name)
       sp_hash = {
         :cols => common_columns(),
-        :filter => [:eq, :name, namespace_name.downcase]
+        :filter => [:eq, :name, namespace_name.to_s.downcase]
       }
 
       results = Model.get_objs(namespace_mh, sp_hash)
@@ -77,7 +77,7 @@ module DTK
     end
 
     def self.find_or_create(namespace_mh, namespace_name)
-      #namespace_name = namespace_name.is_a?(Namespace) ? namespace_name.display_name : namespace_name
+      namespace_name = namespace_name.is_a?(Namespace) ? namespace_name.display_name : namespace_name
       raise Error, "You need to provide namespace name where creating object" if namespace_name.nil? || namespace_name.empty?
       namespace = self.find_by_name(namespace_mh, namespace_name)
 

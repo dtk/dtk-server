40f373ad870e0bbb3d1dd7858cdb8a2470e5be94
Rich Pelavin <rich@reactor8.com>
2015-01-28 22:44:40 +0000

    updated find model ref violations to look at assembly branch rather than the base service branch

--- application/model/module/assembly_module/service.rb
+++ application/model/module/assembly_module/service.rb
@@ -10,9 +10,15 @@ module DTK; class AssemblyModule
     end
     private :initialize
 
-    def self.get_or_create_assembly_branch(assembly)
-      new(assembly).get_or_create_assembly_branch()
+    # This checks if an assembly specfic branch has been made and returns this otherwise gives teh base branch
+    def self.get_assembly_branch(assembly)
+      new(assembly).get_assembly_branch()
     end
+    def get_assembly_branch()
+      module_branches = @service_module.get_module_branches() 
+      module_branches.find{|mb|mb.matches_version?(@am_version)} || module_branches.find{|mb|mb.matches_base_version?()}
+    end
+
     def get_or_create_assembly_branch()
       @service_module.get_module_branch_matching_version(@am_version) || create_assembly_branch()
     end

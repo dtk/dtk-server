a095f1013eb85280f526805cbcb33c5fdee6f973
Rich Pelavin <rich@reactor8.com>
2015-02-10 23:36:16 +0000

    feature-action-dsl: cleanup of config_agent types

---
+++ utils/internal/config_agent/type.rb
@@ -0,0 +1,16 @@
+module DTK
+  class ConfigAgent
+    module Type
+      module Symbol
+        All = [:puppet,:dtk_provider,:serverspec,:test,:node_module]
+        Default = :puppet
+        All.each do |type|
+          class_eval("def self.#{type}();:#{type};end")
+        end
+      end
+      def self.default_symbol()
+        Symbol::Default
+      end
+    end
+  end
+end

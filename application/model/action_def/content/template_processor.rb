59f0db906fda4c02d945584833fa88ebff2716c9
Aldin Milanovic <aldin@atlantbh.com>
2015-02-17 15:01:06 +0100

    DTK-1930 Treat Mustache variables in dtk action syscall

--- application/model/action_def/content/template_processor.rb
+++ application/model/action_def/content/template_processor.rb
@@ -1,9 +1,10 @@
 module DTK; class ActionDef; class Content
   class TemplateProcessor
     # TODO: hard wired in mustache
-    r8_nested_require('template_processor/adapter','mustache')
+    r8_nested_require('template_processor/adapter','mustache_template')
     def self.default()
-      Mustache.new()
+      # For Rich: needed to change from Mustache to MustacheTemplate because Mustache class name is reserved by mustache gem
+      MustacheTemplate.new()
     end
   end
 end; end; end

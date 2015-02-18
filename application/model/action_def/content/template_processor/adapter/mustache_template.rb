05009cbf2a2ea16030027ec47bbf85bdeaeb565d
Rich Pelavin <rich@reactor8.com>
2015-02-18 19:26:30 +0000

    updateing template processing

--- application/model/action_def/content/template_processor/adapter/mustache_template.rb
+++ application/model/action_def/content/template_processor/adapter/mustache_template.rb
@@ -2,17 +2,21 @@ require 'mustache'
 
 module DTK; class ActionDef; class Content
   class TemplateProcessor
-    # For Rich: needed to change class name from Mustache to MustacheTemplate because Mustache class name is reserved by mustache gem
     class MustacheTemplate < self
       def needs_template_substitution?(command_line)
         # will return true if command_line has mustache template attributes '{{ variable }}'
-        return command_line.match(/\{\{.+\}\}/)
+        command_line.match(HasMustacheVarsRegExp)
       end
+      HasMustacheVarsRegExp = /\{\{.+\}\}/
 
       def bind_template_attributes(command_line, attr_val_pairs)
         # using Mustache gem to extract attribute values; raise error if unbound attributes
-        Mustache.raise_on_context_miss = true
-        Mustache.render(command_line, attr_val_pairs)
+        begin 
+          ::Mustache.raise_on_context_miss = true
+          ::Mustache.render(command_line, attr_val_pairs)
+         rescue ::Mustache::ContextMiss => e
+          raise ErrorUsage.new(e.message)
+        end
       end
     end
   end

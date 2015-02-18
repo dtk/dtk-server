65c1061a09cad93cb0b308635de00165b3267dff
Rich Pelavin <rich@reactor8.com>
2015-02-18 20:55:35 +0000

    updateing template processing

--- application/model/action_def/content/template_processor/adapter/mustache_template.rb
+++ application/model/action_def/content/template_processor/adapter/mustache_template.rb
@@ -5,7 +5,7 @@ module DTK; class ActionDef; class Content
     class MustacheTemplate < self
       def needs_template_substitution?(command_line)
         # will return true if command_line has mustache template attributes '{{ variable }}'
-        command_line.match(HasMustacheVarsRegExp)
+        command_line =~ HasMustacheVarsRegExp
       end
       HasMustacheVarsRegExp = /\{\{.+\}\}/
 
@@ -15,7 +15,18 @@ module DTK; class ActionDef; class Content
           ::Mustache.raise_on_context_miss = true
           ::Mustache.render(command_line, attr_val_pairs)
          rescue ::Mustache::ContextMiss => e
-          raise ErrorUsage.new(e.message)
+          raise ErrorUsage.new(normalize_mustache_context_miss_error(e,command_line))
+        end
+      end
+
+      def normalize_mustache_context_miss_error(mustache_gem_err,command_line)
+        str_err = mustache_gem_err.message
+        if str_err =~ /^Can't find ([^\s]+) in/
+          missing_var = $1
+          ident = 4
+          "The mustache variable '#{missing_var}' in the following command is not set:\n#{' '*ident}#{command_line}"
+        else
+          "Template error in command (#{command_line}): #{str_err}"
         end
       end
     end

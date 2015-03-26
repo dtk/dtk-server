ab04981043b442d9a02de70469c6640f87b1d7af
Rich Pelavin <rich@reactor8.com>
2015-03-26 02:05:56 +0000

    DTK-2009: got rid of the code that copied no change attributes to template and also fixed typo on fields not copied over in attributes

--- lib/model/clone/incremental_update/attribute.rb
+++ lib/model/clone/incremental_update/attribute.rb
@@ -1,7 +1,7 @@
 module DTK; class Clone
   class IncrementalUpdate
     class Attribute < self
-      FieldsThatDontChange = [:id,:display_name,:group_id,:component_component_id,:attribute_derived]
+      FieldsThatDontChange = [:id,:display_name,:group_id,:component_component_id,:value_derived,:value_asserted]
       # instance_template_links has type InstanceTemplate::Links
       def self.modify_instances(model_handle,instance_template_links)
         parent_id_col = model_handle.parent_id_field_name()
@@ -15,12 +15,6 @@ module DTK; class Clone
       # TODO: put in equality test so that does not need to do the modify equal objects
       def equal_so_dont_modify?(instance,template)
         false
-      end
-      def sync_no_copy_fields!(instance,template)
-        template[:value_derived] = instance[:value_derived]
-        # TODO: DTK-1939; this does not handle where instance[:value_asserted] came from default and now default is changing
-        template[:value_asserted] = instance[:value_asserted]
-        nil
       end
       
       def update_opts()

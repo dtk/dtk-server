1c22f9aa8d9ea4dd397a6b08c0fea1e5cf71e0ee
Rich Pelavin <rich@reactor8.com>
2015-04-07 18:57:26 +0000

    put in updates had in feature branch for log format

--- utils/internal/log.rb
+++ utils/internal/log.rb
@@ -1,43 +1,67 @@
 # TODO: bring in a production quality ruby logging capability that gets wrapped here
 # TODO: would put this in config
-module XYZ
+# TODO: out is no longer used adn should be removed after checking no calls use it
+module DTK
   module Log
     Config = Hash.new
     Config[:print_time] = false
     Config[:print_method] = false
+    Config[:include_caller] = [:error]
+    Config[:include_caller_depth] = 5
   end
 end
 
-module XYZ
+module DTK
   module Log
     def self.info(msg, out = $stdout)
-      ::Ramaze::Log.info(msg)
+      simple_form(:info,msg)
     end
     def self.debug(msg, out = $stdout)
-      ::Ramaze::Log.debug(msg)
+      simple_form(:debug,msg)
     end
     def self.error(msg, out = $stdout)
-      ::Ramaze::Log.error(msg)
-    end
-    def self.info_pp(obj, out = $stdout)
-      out << "info: "
-      out << Aux::pp_form(obj)
-      obj
-    end
-    def self.debug_pp(obj, out = $stdout)
-      out << "debug: "
-      out << Aux::pp_form(obj)
-      obj
+      simple_form(:error,msg)
     end
     def self.warn(msg, out = $stdout)
-      ::Ramaze::Log.warn(msg)
+      simple_form(:warn,msg)
+    end
+
+    def self.info_pp(obj, out = $stdout)
+      pp_form(:info,obj)
+    end
+    def self.debug_pp(obj, out = $stdout)
+      pp_form(:debug,obj)
     end
     def self.error_pp(obj, out = $stdout)
-      out << "error: "
-      out << Aux::pp_form(obj)
+      pp_form(:error,obj)
+    end
+
+   private
+    def self.simple_form(type,msg)
+      msg = include_caller_info?(type,msg)
+      ramaze_log(type,msg)
+    end
+
+    def self.pp_form(type,obj)
+      msg = Aux::pp_form(obj)
+      msg = include_caller_info?(type,msg)
+      ramaze_log(type,msg)
       obj
     end
-   private
+
+    def self.ramaze_log(type,msg)
+      ::Ramaze::Log.send(type,msg)
+    end
+  
+    def self.include_caller_info?(type,msg)
+      if (Config[:include_caller]||[]).include?(type)
+        caller_depth = Config[:include_caller_depth] || 1
+        msg += "\n#{Aux::pp_form(caller[OffsetDepth...OffsetDepth+caller_depth])}\n" 
+      end
+      msg
+    end
+    OffsetDepth = 2 #so does not give caller info for errors itself
+
     def self.format(msg)
       ret = String.new
       ret << "#{Time.now}: " if Config[:print_time]

343123f9dd7589d9d30a36975055743413625bc5
Aldin Milanovic <aldin@atlantbh.com>
2015-04-09 14:58:11 +0200

    SMall fic

--- application/model/module/assembly_module/component.rb
+++ application/model/module/assembly_module/component.rb
@@ -23,24 +23,7 @@ module DTK; class AssemblyModule
       raise ErrorNoChangesToModule.new(@assembly, component_module) unless base_branch
 
       local_branch = component_module.get_workspace_module_branch(am_version)
-      raise ErrorUsage.new("Unable to find local branch") unless local_branch
-
-      base_branch.merge!(:version => am_version, :local_branch => local_branch[:display_name], :current_branch_sha => local_branch[:current_sha])
-      base_branch
-    end
-
-    def self.component_module_workspace_info(assembly, component_module)
-      new(assembly).component_module_workspace_info(component_module)
-    end
-    def component_module_workspace_info(component_module)
-      get_applicable_component_instances(component_module,:raise_error_if_empty => true)
-      am_version = assembly_module_version()
-
-      base_branch = component_module.get_workspace_branch_info()
-      raise ErrorNoChangesToModule.new(@assembly, component_module) unless base_branch
-
-      local_branch = component_module.get_workspace_module_branch(am_version)
-      raise ErrorUsage.new("Unable to find local branch") unless local_branch
+      raise ErrorNoComponentsInModule.new(@assembly, component_module) unless local_branch
 
       base_branch.merge!(:version => am_version, :local_branch => local_branch[:display_name], :current_branch_sha => local_branch[:current_sha])
       base_branch

1e358ed33ce685bfde68a3d9daa0f9882ff231fd
Rich Pelavin <rich@reactor8.com>
2015-05-08 17:07:30 +0000

    fixing bug in how device mapping is set when overrides

--- utils/internal/command_and_control/adapters/iaas/ec2/create_node.rb
+++ utils/internal/command_and_control/adapters/iaas/ec2/create_node.rb
@@ -111,6 +111,7 @@ module DTK; module CommandAndControlAdapter
           create_options.update_availability_zone!()
           create_options.update_vpc_info?()
           create_options.update_block_device_mapping!(image(ami))
+raise Error.new()
           create_options.update_user_data!()
 
           begin
@@ -195,19 +196,14 @@ module DTK; module CommandAndControlAdapter
           end
 
           def update_block_device_mapping!(image)
+            root_device_override_attrs = {'deleteOnTermination' => 'true'}
+            if root_device_size = @node.attribute.root_device_size() 
+              root_device_override_attrs.merge!('volumeSize' => root_device_size)
+            end
             # only add block_device_mapping if it was fully generated
-            if block_device_mapping_from_image = image.block_device_mapping_with_delete_on_termination()
-              block_device_mapping = self[:block_device_mapping] = block_device_mapping_from_image
-              # if root_device_size explicity given
-              
-              if root_device_size = @node.attribute.root_device_size() 
-                if device_name = image.block_device_mapping_device_name()
-                  block_device_mapping.first.merge!({'DeviceName' => device_name, 'Ebs.VolumeSize' => root_device_size})
-                else
-                  Log.error("Cannot determine device name for ami (#{ami})")
-                end
-              end
-            end  
+            if block_device_mapping = image.block_device_mapping?(root_device_override_attrs)
+              merge!(:block_device_mapping => block_device_mapping)
+            end
           end
 
           def update_user_data!()
--- utils/internal/command_and_control/adapters/iaas/ec2/image.rb
+++ utils/internal/command_and_control/adapters/iaas/ec2/image.rb
@@ -22,34 +22,53 @@ module DTK; module CommandAndControlAdapter
       def root_device_name()
         value(:root_device_name)
       end
-      def block_device_mapping_device_name()
-        if bdm = single_block_device_mapping?()
-          bdm['deviceName']
+
+      def block_device_mapping?(root_device_override_attrs={})
+        if default_block_device_mappings = value(:block_device_mapping)
+          BlockDeviceMapping.ret(default_block_device_mapping,root_device_override_attrs)
         end
       end
 
-      def block_device_mapping_with_delete_on_termination()
-        return nil unless value(:block_device_mapping)
-        block_device_mapping = create_block_device_mapping(value(:block_device_mapping))
-        block_device_mapping.first["Ebs.DeleteOnTermination"] = "true"
-        block_device_mapping
-      end
-
-      private
-
+     private
       def value(attr)
         (@ami||{})[attr]
       end
 
+      module BlockDeviceMapping
+        def self.ret(default_block_device_mapping,root_device_override_attrs={})
+          block_device_mapping = update_root_device(block_device_mapping,root_device_override_attrs)
+          convert_keys(block_device_mapping)
+        end
+       private
+        def self.update_root_device(block_device_mapping,root_device_override_attrs={})
+          ret = block_device_mapping
+          unless root_device_override_attrs.empty?
+            size = root_device_override_attrs.size
+            # TODO: assuming route device is first element in array default_block_device_mapping; need to further validate
+            [root_device_override_attrs.first.merge(root_device_override_attrs)] + root_device_override_attrs[1..size-1]
+          else
+            ret
+          end
+        end
+
+        def self.convert_keys(block_device_mapping)
+          block_device_mapping.map do |one_mapping|
+            one_mapping.inject(Hash.new) do |h,(k,v)|
+            
+          end
+        end
+        KeyMapping = {
+          'deviceName'          => 'DeviceName',
+          'snapshotId'          => 'Ebs.SnapshotId',
+          'volumeSize'          => 'Ebs.VolumeSize',
+          'deleteOnTermination' => 'Ebs.DeleteOnTermination',
+          'virtualName'         => 'VirtualName',
+        }
+
+
       def create_block_device_mapping(image_mappings)
         block_device_mapping = []
-        name_mapping = {
-          'deviceName' => 'DeviceName',
-          'snapshotId' => 'Ebs.SnapshotId',
-          'volumeSize' => 'Ebs.VolumeSize',
-          'deleteOnTermination' => 'Ebs.DeleteOnTermination',
-          'virtualName' => 'VirtualName',
-        }
+
         image_mappings.each do |image_mapping|
           mapping = {}
           name_mapping.each do |key, value|
@@ -60,17 +79,15 @@ module DTK; module CommandAndControlAdapter
         block_device_mapping
       end
 
-      def single_block_device_mapping?()
-        if bdm = value(:block_device_mapping)
-          case bdm.size
-            when 0 then nil
-            when 1 then bdm.first
-            else 
-              Log.error("Call to single_block_device_mapping? when more than one blocks defined")
-              nil
-          end
+
+          block_device_mapping = create_block_device_mapping(block_device_mappingvalue(:block_device_mapping))
+          block_device_mapping.first["Ebs.DeleteOnTermination"] = "true"
+          block_device_mapping
         end
       end
+    
+
+
     end
   end
 end; end

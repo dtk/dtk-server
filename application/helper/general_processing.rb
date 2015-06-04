298d690c7424f744eab0e48f99e630aa53f5b2ff
Haris Krajina <haris@atlantbh.com>
2015-06-04 16:43:04 +0200

    FIX

--- application/helper/general_processing.rb
+++ application/helper/general_processing.rb
@@ -7,16 +7,19 @@ module Ramaze::Helper
 
     ### user processing
     def login_first
-      auth_violation_response() unless logged_in? 
+      auth_violation_response() unless logged_in?
     end
 
     def auth_violation_response()
+      # DEBUG SNIPPET >>> REMOVE <<<
+      require 'ap'
+      ap caller
       rest_request? ? respond('Forbidden',403) : redirect(R8::Config[:login][:path])
     end
 
     def auth_unauthorized_response(message)
       rest_request? ? respond(message,401) : redirect(R8::Config[:login][:path])
-    end    
+    end
 
     def auth_forbidden_response(message)
       rest_request? ? respond(message,403) : redirect(R8::Config[:login][:path])
@@ -36,7 +39,7 @@ module Ramaze::Helper
       @json_response = controller.json_response?
         @controller = controller
       end
-      
+
       def create_object_from_id(id)
         @controller.create_object_from_id(id)
       end
@@ -73,10 +76,10 @@ module Ramaze::Helper
     end
 
     def json_response?()
-      @json_response ||= rest_request?() or ajax_request?() 
+      @json_response ||= rest_request?() or ajax_request?()
     end
     def rest_request?()
-      # TODO: needs to be fixed up; issue is different envs (linux versus windows) give different values for request.env["REQUEST_URI"] 
+      # TODO: needs to be fixed up; issue is different envs (linux versus windows) give different values for request.env["REQUEST_URI"]
       @rest_request ||= (request.env["REQUEST_URI"] =~ Regexp.new("/rest/") ? true : nil)
     end
     def ajax_request?

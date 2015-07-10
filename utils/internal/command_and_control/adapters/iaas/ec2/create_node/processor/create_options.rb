d77766bf37e6384662e5428e46980d1361a3c4f1
Haris Krajina <haris@atlantbh.com>
2015-07-10 10:31:49 +0200

    Resolved merge with --ours strategy to master's HEAD

--- utils/internal/command_and_control/adapters/iaas/ec2/create_node/processor/create_options.rb
+++ utils/internal/command_and_control/adapters/iaas/ec2/create_node/processor/create_options.rb
@@ -9,34 +9,34 @@ module DTK; module CommandAndControlAdapter
         @node         = parent.node
         @external_ref = parent.external_ref||{}
       end
-      
+
       def update_security_group!
-        security_group = @target.get_security_group() || 
-          @target.get_security_group_set() || 
+        security_group = @target.get_security_group() ||
+          @target.get_security_group_set() ||
           @external_ref[:security_group_set] ||
           [R8::Config[:ec2][:security_group]] ||
           'default'
         merge!(groups: security_group)
       end
-      
+
       def update_tags!
         merge!(tags: {"Name" => ec2_name_tag()})
       end
-      
+
       def update_key_name
         merge!(key_name: @target.get_keypair() || R8::Config[:ec2][:keypair])
       end
-      
+
       def update_availability_zone!
         target_availability_zone = (@target[:iaas_properties]||{})[:availability_zone]
-        avail_zone = @external_ref[:availability_zone] || 
-          (@target[:iaas_properties]||{})[:availability_zone] || 
+        avail_zone = @external_ref[:availability_zone] ||
+          (@target[:iaas_properties]||{})[:availability_zone] ||
           R8::Config[:ec2][:availability_zone]
         unless avail_zone.nil? || avail_zone == 'automatic'
           merge!(availability_zone: avail_zone)
         end
       end
-      
+
       def update_vpc_info?
         if @target.is_builtin_target?()
           #TODO: we wil get rid of this special case and just put the info in builtin target
@@ -47,29 +47,29 @@ module DTK; module CommandAndControlAdapter
             return
           end
         end
-        
+
         unless iaas_properties = @target[:iaas_properties]
           Log.error_pp(["Unexpected that @target does not have :iaas_properties",@target])
           return
         end
-        
+
         unless iaas_properties[:ec2_type] == 'ec2_vpc'
           return
         end
-        
+
         unless subnet = iaas_properties[:subnet]
           Log.error_pp(["Unexpected that @target does not have :iaas_properties",@target])
           return
         end
-        
+
         subnet_id = @conn.check_for_subnet(subnet)
         associate_public_ip = true #TODO: stub vale
-        merge!(subnet_id: subnet_id, associate_public_ip: associate_public_ip) 
+        merge!(subnet_id: subnet_id, associate_public_ip: associate_public_ip)
       end
-      
+
       def update_block_device_mapping!(image)
         root_device_override_attrs = {'Ebs.DeleteOnTermination' => 'true'}
-        if root_device_size = @node.attribute.root_device_size() 
+        if root_device_size = @node.attribute.root_device_size()
           root_device_override_attrs.merge!('Ebs.VolumeSize' => root_device_size)
         end
         # only add block_device_mapping if it was fully generated
@@ -77,23 +77,23 @@ module DTK; module CommandAndControlAdapter
           merge!(block_device_mapping: block_device_mapping)
         end
       end
-      
+
       def update_user_data!
         self[:user_data] ||= CommandAndControl.install_script(@node)
         self
       end
 
-      def update_client_token?()
+      def update_client_token?
         if client_token = @external_ref[:client_token]
           self[:client_token] ||= client_token
         else
-          Log.error_pp(["Unexpected that @external_ref does not have client_token",@node,@external_ref])
+          Log.error_pp(["Unexpected that @external_ref does not have client_token", @node,@external_ref])
         end
         self
       end
 
       private
-      
+
       def ec2_name_tag
         # TO-DO: move the tenant name definition to server configuration
         tenant = ::DtkCommon::Aux::running_process_user()
@@ -111,11 +111,12 @@ module DTK; module CommandAndControlAdapter
         end
         ret
       end
+
       Ec2NameTag = {
         vars: [:assembly, :node, :tenant, :target, :user],
         tag: R8::Config[:ec2][:name_tag][:format]
       }
-      
+
       def ec2_name_tag__get_assembly_name
         if assembly = @node.get_assembly?()
           assembly.get_field?(:display_name)

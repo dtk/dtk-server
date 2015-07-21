3e55ee434d7cc8a2c949dd53df8d815c13ee6972
dduvnjak <dario@atlantbh.com>
2015-07-21 13:56:32 +0000

    Squashed 'dtk_modules/common_user/' content from commit 9801950
    git-subtree-dir: dtk_modules/common_user
    git-subtree-split: 98019504cbeccfff1ed310537ed891b98592f45c

---
+++ manifests/init.pp
@@ -0,0 +1,29 @@
+define common_user($user)
+{
+  user {$user:
+    home       => "/home/${user}",
+    shell      => '/bin/bash',
+    managehome => true,
+    ensure     => present
+  }
+}
+
+define common_user::common_user_ssh_config($user)
+{
+  if ! defined(File["/home/${user}/.ssh"]) {
+    file { "/home/${user}/.ssh":
+      ensure => directory,
+      mode   => '0700',
+      owner  => $user,
+      group  => $user,
+    }
+  }
+
+  file { "/home/${user}/.ssh/config":
+    ensure  => 'present',
+    mode    => '0644',
+    owner   => $user,
+    content => template('common_user/ssh/config.erb'),
+    require => File["/home/${user}/.ssh"],
+  }
+}

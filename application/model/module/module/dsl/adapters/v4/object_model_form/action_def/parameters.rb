e4cb825a59b703d2229e57406b013c4be1e04625
Rich Pelavin <rich.reactor8.com>
2015-07-26 02:11:59 +0000

    DTK-2167: working on parsing action def parameters

--- application/model/module/module/dsl/adapters/v4/object_model_form/action_def/parameters.rb
+++ application/model/module/module/dsl/adapters/v4/object_model_form/action_def/parameters.rb
@@ -1,13 +1,26 @@
 module DTK; class ModuleDSL::V4::ObjectModelForm
   class ActionDef
-    class Parameters
+    class Parameters < self
       def self.create?(input_hash, context = {})
         ret = nil
         unless parameters = Constant.matches?(input_hash, :Parameters)
           return ret
         end
-        pp [:debug,parameters]
-        nil
+        pp [:input_params, parameters]
+        ParsingError.raise_error_if_not(parameters, Hash)
+
+        ret = parameters.inject(OutputHash.new) do |h, (attr_name, attr_info)|
+          if attr_info.is_a?(Hash)
+            opts_attr = { component_type: cmp_type }.merge(opts)
+            h.merge(attr_name => attribute_fields(attr_name, attr_info, opts_attr))
+          else
+            fail ParsingError.new('TODO: need to write this')
+#            cmp_name = component_print_form(cmp_type)
+#            fail ParsingError.new('Ill-formed attributes section for component (?1): ?2', cmp_name, 'attributes' => parameters)
+          end
+        end
+        pp [:transformed, ret]
+        ret
       end
     end
   end

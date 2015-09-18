3191380bc5dca03130d68026f6a67a084d00342f
adnanabh <adnan.muslija@atlantbh.com>
2015-09-18 10:25:30 +0200

    added script for docker tenant intialization

---
+++ scripts/utility/docker_init.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+        echo "No public address provided."
+        exit 1
+fi
+
+IMAGE=getdtk/server-full
+REPO_HOST=repoman1.internal.r8network.com
+REPO_PORT=443
+MCO_PORT=61663
+CONTAINER=dtk
+
+rm -rf /${CONTAINER}
+mkdir /${CONTAINER}
+
+ADDRESS=${1}
+USER=${2:-dtk33}
+PASS=${3:-r8server}
+NAME="dtk-docker-${RANDOM}"
+
+echo -e "USERNAME=${USER}\nPASSWORD=${PASS}\nPUBLIC_ADDRESS=${ADDRESS}\nINSTANCE_NAME=${NAME}" > /${CONTAINER}/dtk.config
+
+echo "Creating dtk-server: ${NAME}"
+
+docker ps | grep dtk > /dev/null
+RUNNING=$?
+if [[ $RUNNING -eq 0 ]]; then
+        echo "Stoping Docker Container: ${CONTAINER}..."
+        docker stop ${CONTAINER} > /dev/null
+fi
+
+
+docker ps -a | grep dtk > /dev/null
+EXISTS=$?
+if [[ $EXISTS -eq 0 ]]; then
+        echo "Removing Docker Container: ${CONTAINER}..."
+        docker rm ${CONTAINER} > /dev/null
+fi
+
+echo -e "Pulling the latest DTK - Server image \n"
+docker pull ${IMAGE}
+echo -e "\nStarting a new Docker Container: ${CONTAINER}"
+#docker run -e REMOTE_REPO_HOST=${REPO_HOST} -e REMOTE_REPO_REST_PORT=${REPO_PORT} -e MCOLLECTIVE_PORT=${MCO_PORT} --name dtk -v /dtk:/host_volume -p 8080:80 -p ${MCO_PORT}:6163 -p 2222:22 -d getdtk/server-full

b7e764b6a3ededb86a6fad925f740c5a5af469e2
adnanabh <adnan.muslija@atlantbh.com>
2015-09-21 16:06:54 +0200

    docker setup script fix 2

--- test/functional/rspec/lib/docker_dtk_setup.rb
+++ test/functional/rspec/lib/docker_dtk_setup.rb
@@ -20,7 +20,9 @@ provider_id=provider_name
 target_name="test_target"
 service_module="r8/bootstrap"
 local_module="bootstrap"
-
+catalog_user="docker-credentials"
+catalog_pass="r8server"
+repo_user="dtk17-docker-client"
 
 # Create new provider
 provider_status=dtk_common.send_request('/rest/target/create_provider',  iaas_properties: {keypair: keypair_name, security_group: sg_name, key: config['properties']['aws_access_key_id'], secret: config['properties']['aws_secret_access_key']}, provider_name: provider_name, iaas_type: iaas_type, no_bootstrap: true)
@@ -48,9 +50,13 @@ else
 end
 
 # Pull r8:bootstrap component
-module_status=dtk_common.send_request('/rest/service_module/import', remote_module_name: service_module, local_module_name: local_module, rsa_pub_key: dtk_common.ssh_key)
+module_status=dtk_common.send_request('/rest/service_module/import', remote_module_name: service_module, local_module_name: local_module, rsa_pub_key: dtk_common.ssh_key, do_not_raise: true)
 if module_status['status']=='ok' 
 	puts "Module #{service_module} pulled from remote repo!"
 else
 	puts "Failed to pull #{service_module} from remote repo."
 end
+
+credentials_status=dtk.common.send_request('/rest/account/set_catalog_credentials', username: catalog_user, password: catalog_pass, validate: true)
+
+ssh_key_status=dtk_common.send_request('/rest/account/add_user_direct_access', rsa_pub_key: dtk_common.ssh_key, username: repo_user, first_registration: false)

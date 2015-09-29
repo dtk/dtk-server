08bf35dcc461e69d1725cb9ba30ea56a07b5ca8c
adnanabh <adnan.muslija@atlantbh.com>
2015-09-29 09:15:21 +0200

    DTK-2249: cosmetic changes for docker_init

--- scripts/utility/docker_init.sh
+++ scripts/utility/docker_init.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 if [[ -z $1 ]]; then
-        echo "No public address provided."
-        exit 1
+    echo "No public address provided."
+    exit 1
+fi
+
+if [[ -z $2 ]]; then
+	echo "Upgrade status not specified."
+    exit 1
 fi
 
 IMAGE=getdtk/server-full
 REPO_HOST=repoman1.internal.r8network.com
 REPO_PORT=443
 MCO_PORT=61663
-
-
-
+SSH_PORT=2222
+HTTP_PORT=8080
 
 ADDRESS=${1}
 UPGRADE=${2}
@@ -27,28 +31,29 @@ if [[ $UPGRADE -eq 0 ]]; then
 fi
 
 echo -e "USERNAME=${USER}\nPASSWORD=${PASS}\nPUBLIC_ADDRESS=${ADDRESS}\nINSTANCE_NAME=${NAME}" > "/${CONTAINER}/dtk.config"
-
 echo "Creating dtk-server: ${NAME}"
 
 docker ps | grep dtk > /dev/null
 RUNNING=$?
 if [[ $RUNNING -eq 0 ]]; then
-        echo "Stoping Docker Container: ${CONTAINER}..."
-        docker stop ${CONTAINER} > /dev/null
+    echo -e "Stoping Docker Container: ${CONTAINER}...\n"
+    docker stop ${CONTAINER} > /dev/null
 fi
-
 
 docker ps -a | grep dtk > /dev/null
 EXISTS=$?
 if [[ $EXISTS -eq 0 ]]; then
-        echo "Removing Docker Container: ${CONTAINER}..."
-        docker rm ${CONTAINER} > /dev/null
+    echo -e "Removing Docker Container: ${CONTAINER}...\n"
+    docker rm ${CONTAINER} > /dev/null
 fi
 
-echo -e "Pulling the latest DTK - Server image \n"
-docker pull ${IMAGE}
+echo -e "Pulling the latest DTK - Server image. \n"
+docker pull ${IMAGE} > /dev/null
+PULLED=$?
+if [[ $PULLED -eq 1 ]]; then
+	echo -e "Error while pulling image. \n"
+	exit 1
+fi
+
 echo -e "\nStarting a new Docker Container: ${CONTAINER}"
-docker run -e REMOTE_REPO_HOST=${REPO_HOST} -e REMOTE_REPO_REST_PORT=${REPO_PORT} -e MCOLLECTIVE_PORT=${MCO_PORT} --name ${CONTAINER} -v /${CONTAINER}:/host_volume -p 8080:80 -p ${MCO_PORT}:6163 -p 2222:22 -d getdtk/server-full
-
-
-
+docker run -e REMOTE_REPO_HOST=${REPO_HOST} -e REMOTE_REPO_REST_PORT=${REPO_PORT} -e MCOLLECTIVE_PORT=${MCO_PORT} --name ${CONTAINER} -v /${CONTAINER}:/host_volume -p ${HTTP_PORT}:80 -p ${MCO_PORT}:6163 -p ${SSH_PORT}:22 -d ${IMAGE}

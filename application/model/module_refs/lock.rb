2aac8612238f8e90a465217c910f96eb9b4aa2bb
Rich Pelavin <rich.reactor8.com>
2015-10-18 23:20:11 +0000

    Updating collapse module_refs lock so that it handles versions

--- application/model/module_refs/lock.rb
+++ application/model/module_refs/lock.rb
@@ -112,7 +112,7 @@ module DTK
       def self.compute_elements(assembly_instance, types, opts = {})
         module_refs_tree = ModuleRefs::Tree.create(assembly_instance)
         collapsed = module_refs_tree.collapse(Aux.hash_subset(opts, [:raise_errors]))
-        collapsed.choose_namespaces!()
+        collapsed.choose_namespaces_and_versions!()
         collapsed.add_implementations!(assembly_instance)
 
         ret = new(assembly_instance)

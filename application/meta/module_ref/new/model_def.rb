39613070d7ff0552f97053ce390760731f1f3412
Aldin Milanovic <aldin@atlantbh.com>
2015-11-06 14:03:24 +0100

    Do not allow to delete component module if it's dependency to other component or service module

--- application/meta/module_ref/new/model_def.rb
+++ application/meta/module_ref/new/model_def.rb
@@ -8,5 +8,64 @@
     namespace_info: { type: :json },
     external_ref: { type: :json }
   },
+  virtual_columns: {
+    is_dependency_to_component_modules: {
+      type: :json,
+      hidden: true,
+      remote_dependencies:
+      [
+        {
+          model_name: :module_branch,
+          convert: true,
+          join_type: :inner,
+          join_cond: { id: :module_ref__branch_id },
+          cols: [:id, :display_name, :branch, :version, :component_id]
+         },
+        {
+          model_name: :component_module,
+          convert: true,
+          join_type: :inner,
+          join_cond: { id: :module_branch__component_id },
+          cols: [:id, :display_name, :namespace_id]
+        },
+        {
+          model_name: :namespace,
+          convert: true,
+          join_type: :inner,
+          join_cond: { id: :component_module__namespace_id },
+          cols: [:id, :display_name]
+        }
+      ]
+    },
+    is_dependency_to_service_modules: {
+      type: :json,
+      hidden: true,
+      remote_dependencies:
+      [
+        {
+          model_name: :module_branch,
+          convert: true,
+          join_type: :inner,
+          join_cond: { id: :module_ref__branch_id },
+          cols: [:id, :display_name, :branch, :version, :service_id]
+         },
+        {
+          model_name: :service_module,
+          convert: true,
+          join_type: :inner,
+          join_cond: { id: :module_branch__service_id },
+          cols: [:id, :display_name, :namespace_id]
+        },
+        {
+          model_name: :namespace,
+          convert: true,
+          join_type: :inner,
+          join_cond: { id: :service_module__namespace_id },
+          cols: [:id, :display_name]
+        }
+      ]
+    }
+  },
   many_to_one: [:module_branch]
 }
+         # join_cond: { node_node_id: :node__id, component_type: :nested_component__component_type },

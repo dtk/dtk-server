2c1dc6e43254377d0b76d2e6ca32ce15bd19cc1b
bjusufbe <bakir@atlantbh.com>
2015-11-12 15:40:17 +0100

    added methods for service module versioning tests

--- test/functional/rspec/lib/mixins/service_modules_version_mixin.rb
+++ test/functional/rspec/lib/mixins/service_modules_version_mixin.rb
@@ -68,18 +68,17 @@ module ServiceModulesVersionMixin
 
     		delete_remote_version_resposne = send_request('/rest/service_module/delete_remote', {rsa_pub_key: self.ssh_key, remote_module_name: service_module_name, remote_module_namespace: service_module_namespace, force_delete: false, version: service_module_version})
 
- 			if (delete_remote_version_resposne['status'] == 'ok')
+ 			  if (delete_remote_version_resposne['status'] == 'ok')
     			puts "Service module #{service_module_name} version #{service_module_version} deleted successfully"
 				service_module_version_deleted = true
-			else
-				puts "Service module #{service_module_name} version #{service_module_version} was not deleted successfully"
-				service_module_version_deleted = false
-			end
-		else
-			puts "Service module #{service_module_name} does not exist in service module list and therefore service module version cannot be deleted from remote"
-			service_module_version_deleted = false
-     	end
-
+  			else
+  				puts "Service module #{service_module_name} version #{service_module_version} was not deleted successfully"
+  				service_module_version_deleted = false
+  			end
+  		else
+  			puts "Service module #{service_module_name} does not exist in service module list and therefore service module version cannot be deleted from remote"
+  			service_module_version_deleted = false
+      end
      	service_module_version_deleted
     end
 
@@ -123,5 +122,51 @@ module ServiceModulesVersionMixin
 
   		puts ''
   		service_module_versions_deleted
-    end  
+    end
+
+    def check_service_module_version(service_module_name, version_name)
+      puts "List service module versions: ", "----------------------------------"
+      version_found = false
+      versions = send_request('/rest/service_module/list_versions', {:service_module_id => service_module_name})
+      ap versions
+      if versions['data'][0]['versions'].include? version_name
+        puts "Version #{version_name} exists on service module #{service_module_name}"
+        version_found = true
+      else
+        puts "Version #{version_name} does not exist on service module #{service_module_name}"
+      end
+      puts ""
+      version_found
+    end
+
+    def check_service_module_remote_versions(service_module_name, version_name)
+      puts "List remote service module versions: ", "-------------------------------------"
+      remote_version_found = false
+      remote_versions = send_request('/rest/service_module/list_remote_versions', {:service_module_id=>service_module_name, :rsa_pub_key => @ssh_key})
+      ap remote_versions
+
+      if remote_versions['data'][0]['versions'].include? version_name
+        puts "Version #{version_name} exists on service module #{service_module_name} remote"
+        remote_version_found = true
+      else
+        puts "Version #{version_name} does not exist on service module #{service_module_name} remote"
+      end
+      puts ""
+      remote_version_found
+    end
+
+    def publish_service_module_version(service_module_name, version_name)
+      puts "Publish service module version: ", "-----------------------------------"
+      module_published = false
+      publish_response = send_request('/rest/service_module/export', {:service_module_id=>service_module_name, :rsa_pub_key=>ssh_key, :version=>version_name})
+      ap publish_response
+      if publish_response['status'] == 'ok' && publish_response['data']['remote_repo_name'] == service_module_name.split(":").last
+        puts "Service module #{service_module_name} has been published successfully!"
+        module_published = true
+      else
+        puts "Service module #{service_module_name} has not been published successfully!"
+      end
+      puts ""
+      module_published
+    end 
 end

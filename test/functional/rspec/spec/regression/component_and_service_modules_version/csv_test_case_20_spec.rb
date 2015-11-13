a804d9cceab73f51443fc96c127625ca8d11a55e
bjusufbe <bakir@atlantbh.com>
2015-11-13 11:56:37 +0100

    added test scripts for versioning service module

---
+++ test/functional/rspec/spec/regression/component_and_service_modules_version/csv_test_case_20_spec.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+# Test Case 20: NEG - Create version of a service module that already exists
+
+require 'rubygems'
+require 'rest_client'
+require 'pp'
+require 'json'
+require 'awesome_print'
+require './lib/dtk_common'
+require './lib/assembly_and_service_operations_spec'
+require './lib/parameters_setting_spec'
+require './lib/service_modules_spec'
+require './lib/service_module_versions_spec'
+
+service_module_name = 'temp_service'
+service_module_namespace = 'version'
+imported_service_module_name = 'version:temp_service'
+service_module_filesystem_location = '~/dtk/service_modules/version'
+version = '1.0.0'
+
+dtk_common = Common.new('', '')
+
+describe '(Component, service and versioning) NEG - Test Case 20: Create version of a service module that already exists' do
+  
+  before(:all) do
+    puts '********************************************************************************************************', ''
+  end
+
+  context 'Create service module version' do
+    include_context 'Create service module version', dtk_common, service_module_name, version_name
+  end
+
+  context 'Create same service module version' do
+    include_context 'NEG - Create service module version', dtk_common, service_module_name, version_name
+  end
+
+  context "Delete service module version" do
+    include_context 'Delete service module version', dtk_common, service_module_name, version_name
+  end
+
+  after(:all) do
+    puts '', ''
+  end
+end

0cad04177858de739562cb85a3461d02e6268532
adnanabh <adnan.muslija@atlantbh.com>
2015-11-16 16:00:23 +0100

    add new test script for service and module versioning

--- test/functional/rspec/spec/regression/component_and_service_modules_version/csv_test_case_25_spec.rb
+++ test/functional/rspec/spec/regression/component_and_service_modules_version/csv_test_case_25_spec.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# Test Case 24: Install service module, add cmp module version as dependency to module_refs.yaml, publish it, delete modules and install service module again and check that cmp module version is also installed
+# Test Case 25: Install service module, add cmp module version as dependency to module_refs.yaml, create new version of service module, publish it, delete modules and install service module version again and check that cmp module version is also installed
 
 require 'rubygems'
 require 'rest_client'
@@ -15,25 +15,26 @@ require './lib/service_modules_spec'
 
 service_module_namespace = 'version'
 component_module_namespace = 'version'
-service_module_name = 'temp_service_24'
-component_module_name = 'temp24'
+service_module_name = 'temp_service_25'
+component_module_name = 'temp25'
 assembly_name = 'test'
 full_service_module_name = "#{service_module_namespace}:#{service_module_name}"
 full_component_module_name = "#{component_module_namespace}:#{component_module_name}"
 service_module_filesystem_location = "~/dtk/service_modules/#{service_module_namespace}"
 component_module_filesystem_location = "~/dtk/component_modules/#{component_module_namespace}"
 component_module_version = '0.0.1'
+service_module_version = '0.0.1'
 file_for_change = 'module_refs.yaml'
 file_for_add = 'module_refs.yaml'
 file_for_remove = 'module_refs.yaml'
-file_for_change_location = 'spec/regression/component_and_service_modules_version/resources/csv_test_case_24_first_module_refs.yaml'
-file_for_restore_location = 'spec/regression/component_and_service_modules_version/resources/csv_test_case_24_second_module_refs.yaml'
+file_for_change_location = 'spec/regression/component_and_service_modules_version/resources/csv_test_case_25_first_module_refs.yaml'
+file_for_restore_location = 'spec/regression/component_and_service_modules_version/resources/csv_test_case_25_second_module_refs.yaml'
 assembly_yaml_file_location = "~/dtk/service_modules/#{service_module_namespace}/#{service_module_name}/assemblies/#{assembly_name}/assembly.yaml"
 module_refs_file_location = "~/dtk/service_modules/#{service_module_namespace}/#{service_module_name}/module_refs.yaml"
 
 dtk_common = Common.new('', '')
 
-describe '(Component, service and versioning) Test Case 24: Install service module, add cmp module version as dependency to module_refs.yaml, publish it, delete modules and install service module again and check that cmp module version is also installed' do
+describe '(Component, service and versioning) Test Case 25: Install service module, add cmp module version as dependency to module_refs.yaml, create new version of service module, publish it, delete modules and install service module version again and check that cmp module version is also installed' do
 	before(:all) do
 		puts '************************************************************************************************************************************************************************************************************', ''
 	end
@@ -62,16 +63,20 @@ describe '(Component, service and versioning) Test Case 24: Install service modu
   	include_context 'Push local service module changes to server', full_service_module_name, file_for_change
   end
 
-  context 'Push service module changes to remote repo' do
-  	include_context 'Export service module', full_service_module_name, service_module_namespace
+  context 'Create new service module version' do
+    include_context 'Create service module version', dtk_common, full_service_module_name, service_module_version
+  end
+=begin
+  context 'Publish service module version to remote repo' do
+  	include_context 'Publish versioned service module', dtk_common, full_service_module_name, service_module_version
   end
 
-  context 'Delete service module from server' do
-  	include_context 'Delete service module', dtk_common, full_service_module_name
+  context 'Delete all service module version from server' do
+  	include_context 'Delete all service module versions', dtk_common, full_service_module_name
   end
 
-  context 'Delete local service module files' do
-  	include_context 'Delete service module from local filesystem', service_module_filesystem_location, service_module_name
+  context 'Delete all local service module version files' do
+  	include_context 'Delete all local service module versions', service_module_filesystem_location, service_module_name
   end
 
   context "Delete all component module versions from server" do
@@ -82,32 +87,24 @@ describe '(Component, service and versioning) Test Case 24: Install service modu
     include_context 'Delete all local component module versions', component_module_filesystem_location, component_module_name
   end
 
-  context 'Install service module' do
-    include_context 'Import remote service module', service_module_namespace + '/' + service_module_name
+  context 'Install service module version from remote' do
+    include_context 'Install service module version', service_module_name, service_module_namespace, service_module_version
   end
 
   context "Check if the component module version exists on server" do
     include_context "Check if component module version exists on server", dtk_common, full_component_module_name, component_module_version
   end
 
-  context 'Remove component module as dependency for service module' do
-  	include_context 'Add module_refs.yaml file', service_module_name, file_for_restore_location, file_for_add, service_module_filesystem_location
+  context 'Delete service module version from remote repo' do
+  	include_context 'Delete remote service module version', dtk_common, service_module_name, service_module_namespace, service_module_version
   end
 
-  context 'Push service module changes to server' do
-  	include_context 'Push local service module changes to server', full_service_module_name, file_for_change
+  context 'Delete all service module version from server' do
+    include_context 'Delete all service module versions', dtk_common, full_service_module_name
   end
 
-  context 'Push service module changes to remote repo' do
-  	include_context 'Export service module', full_service_module_name, service_module_namespace
-  end
-
-  context 'Delete service module from server' do
-  	include_context 'Delete service module', dtk_common, full_service_module_name
-  end
-
-  context 'Delete local service module files' do
-  	include_context 'Delete service module from local filesystem', service_module_filesystem_location, service_module_name
+  context 'Delete all local service module version files' do
+    include_context 'Delete all local service module versions', service_module_filesystem_location, service_module_name
   end
 
   context "Delete all component module versions from server" do
@@ -117,7 +114,7 @@ describe '(Component, service and versioning) Test Case 24: Install service modu
   context "Delete all component module versions from local filesystem" do
     include_context 'Delete all local component module versions', component_module_filesystem_location, component_module_name
   end
-
+=end
   after(:all) do
     puts '', ''
   end

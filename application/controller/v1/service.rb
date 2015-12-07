9d8417fc5748141d80807be9400f731080d795e4
Haris Krajina <haris@atlantbh.com>
2015-12-07 12:13:24 +0100

    V1 namespace, adding additional endpoints

--- application/controller/v1/service.rb
+++ application/controller/v1/service.rb
@@ -5,6 +5,56 @@ module DTK
       helper :assembly_helper
       helper :task_helper
 
+
+      def rest__create
+        target_id = ret_request_param_id_optional(:target_id, Target::Instance)
+        target = target_idh_with_default(target_id).create_object(model_name: :target_instance)
+
+        service_module_obj = create_obj(:service_module_name, ServiceModule)
+
+        # Special case to support Jenikins CLI orders, since we are not using shell we do not have access
+        # to element IDs. This "workaround" helps with that.
+        if service_module_id = service_module_obj.id
+          # this is name of assembly template
+          assembly_id = ret_request_params(:assembly_name)
+          service_module = ServiceModule.find(model_handle(:service_module), service_module_id)
+          assembly_template = service_module.get_assembly_templates().find { |template| template[:display_name].eql?(assembly_id) || template[:id] == assembly_id.to_i }
+          fail ErrorUsage, "We are not able to find assembly '#{assembly_id}' for service module '#{service_module_id}'" unless assembly_template
+        else
+          assembly_template = ret_assembly_template_object()
+        end
+
+        opts = {}
+        if assembly_name = ret_request_params(:name)
+          opts[:assembly_name] = assembly_name
+        end
+
+        if service_settings = ret_settings_objects(assembly_template)
+          opts[:service_settings] = service_settings
+        end
+
+        if node_size = ret_request_params(:node_size)
+          opts[:node_size] = node_size
+        end
+
+        if os_type = ret_request_params(:os_type)
+          opts[:os_type] = os_type
+        end
+
+        new_assembly_obj = assembly_template.stage(target, opts)
+
+        response = {
+          new_service_instance: {
+            name: new_assembly_obj.display_name_print_form,
+            id: new_assembly_obj.id()
+          },
+          display_name: new_assembly_obj.display_name,
+          id: new_assembly_obj.id
+        }
+
+        rest_ok_response(response)
+      end
+
       def rest__info
         service = service_object()
         rest_ok_response service.info
@@ -114,7 +164,7 @@ module DTK
         end
 
         project = get_default_project()
-        opts = {mode: :create, local_clone_dir_exists: false }
+        opts = { mode: :create, local_clone_dir_exists: false }
 
         if namespace = ret_request_params(:namespace)
           opts.merge!(namespace: namespace)

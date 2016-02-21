85720e87e7c9aff8598e83c8f8ab44de963380c6
Rich Pelavin <rich@reactor8.com>
2016-02-21 19:20:34 +0000

    DTK-2452 Refined treatment of node targets that are image/with_iaas_properties

--- application/model/node_bindings/node_target/image.rb
+++ application/model/node_bindings/node_target/image.rb
@@ -18,6 +18,7 @@
 module DTK; class NodeBindings
   class NodeTarget
     class Image < self
+      r8_nested_require('image', 'with_iaas_properties')
       attr_reader :image
       def initialize(hash)
         super(Type)
@@ -47,53 +48,45 @@ module DTK; class NodeBindings
         { type: type().to_s, image: @image, size: @size }
       end
 
+      def match_or_create_node?(_target)
+        :create
+      end
+
       Type = :image
-      Fields = {
-        image: {
-          key: 'image',
-          required: true
-        },
-        size: {
-          key: 'size'
-        }
-      }
-      InputFormToInternal = Fields.inject({}) { |h, (k, v)| h.merge(v[:key] => k) }
-      Allkeys = Fields.values.map { |f| f[:key] }
-      RequiredKeys =  Fields.values.select { |f| f[:required] }.map { |f| f[:key] }
 
       def self.parse_and_reify(parse_input, _opts = {})
         if parse_input.type?(ContentField)
           input = parse_input.input
           if input[:type].to_sym == Type
-            new(input)
+            WithIAASProperties.create_if_matches?(input) || new(input)
           end
         elsif parse_input.type?(Hash)
           input = parse_input.input
-          if Aux.has_only_these_keys?(input, Allkeys) && !RequiredKeys.find { |k| !input.key?(k) }
+          ImageSizeFields.create_if_matches?(input) || WithIAASProperties.create_if_matches?(input)
+        end
+      end
+
+      module ImageSizeFields
+        def self.create_if_matches?(input)
+          if Aux.has_only_these_keys?(input, AllDSLFields) && !RequiredDSLFields.find { |k| !input.key?(k) }
             internal_form_hash = input.inject({}) { |h, (k, v)| h.merge(InputFormToInternal[k] => v) }
-            new(internal_form_hash)
-          elsif Aux.has_only_these_keys?(input, [WithIAASProperties::Key])
-            WithIAASProperties.new(input[WithIAASProperties::Key])
+            Image.new(internal_form_hash)
           end
         end
+        Fields = {
+          image: {
+            key: 'image',
+            required: true
+          },
+          size: {
+            key: 'size'
+          }
+        }
+        InputFormToInternal = Fields.inject({}) { |h, (k, v)| h.merge(v[:key] => k) }
+        AllDSLFields = Fields.values.map { |f| f[:key] }
+        RequiredDSLFields =  Fields.values.select { |f| f[:required] }.map { |f| f[:key] }
       end
 
-      def match_or_create_node?(_target)
-        :create
-      end
-
-      class WithIAASProperties < self
-        Key = 'iaas_properties'
-
-        def initialize(iaas_properties)
-          super({})
-          @iaas_properties = iaas_properties
-        end
-
-        def hash_form
-          super.merge(iaas_properties: @iaas_properties)
-        end
-      end
     end
   end
 end; end

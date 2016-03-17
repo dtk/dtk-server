e8482479c12cd3349421166068c368cb44632e6a
Aldin Milanovic <aldin@atlantbh.com>
2016-03-17 23:45:25 +0100

    Additional changes for stage-target

--- application/model/target/instance.rb
+++ application/model/target/instance.rb
@@ -43,6 +43,51 @@ module DTK
         Node::TargetRef.get_target_running_nodes(self)
       end
 
+      def self.update_target_from_converge(vpc_cmp, vpc_subnet_cmp, s_group_cmp, provider, project, target)
+        region            = ''
+        key               = ''
+        secret            = ''
+        security_group    = ''
+        availability_zone = ''
+
+        vpc_cmp_attributes        = vpc_cmp.get_component_with_attributes_unraveled({})[:attributes]
+        vpc_subnet_cmp_attributes = vpc_subnet_cmp.get_component_with_attributes_unraveled({})[:attributes]
+        s_group_cmp_attributes    = s_group_cmp.get_component_with_attributes_unraveled({})[:attributes]
+
+        vpc_cmp_attributes.each do |attribute|
+          if attribute[:display_name].eql?('reqion')
+            region = attribute[:value_asserted] || attribute[:value_derived] || region
+          elsif attribute[:display_name].eql?('aws_access_key_id')
+            key = attribute[:value_asserted] || attribute[:value_derived] || key
+          elsif attribute[:display_name].eql?('aws_secret_access_key')
+            secret = attribute[:value_asserted] || attribute[:value_derived] || secret
+          end
+        end
+
+        vpc_subnet_cmp_attributes.each do |attribute|
+          if attribute[:display_name].eql?('availability_zone')
+            availability_zone = attribute[:value_asserted] || attribute[:value_derived] || availability_zone
+          end
+        end
+
+        s_group_cmp_attributes.each do |sgcmp|
+          if sgcmp[:display_name].eql?('group_name')
+            security_group = sgcmp[:value_asserted] || sgcmp[:value_derived] || security_group
+            break
+          end
+        end
+
+        iaas_properties = {
+          :region => region,
+          :key => key,
+          :secret => secret,
+          :security_group => security_group,
+          :availability_zone => availability_zone
+        }
+
+        target.update({iaas_properties: iaas_properties, parent_id: provider.id()})
+      end
+
       # These properties are inherited ones for target instance: default provider -> target's provider -> target instance (most specific)
       InheritedProperties = [:iaas_type, :iaas_properties, :type, :description]
 
@@ -114,21 +159,8 @@ module DTK
           :security_group => security_group,
           :availability_zone => availability_zone
         }
-# [:and, [:eq, :parent_id, r[:parent_id]],
-#            [:eq, :display_name, r[:display_name]]]
-        sp_hash = {
-          cols: [:id, :display_name, :ref],
-          filter: [:and, [:eq, :ref, service_instance.get_field?(:ref)], [:eq, :display_name, service_instance.get_field?(:display_name)]]
-        }
-        target_mh = project_idh.createMH(:target)
-        if existing_target = get_obj(target_mh, sp_hash)
-          existing_target.update_from_row(target_mh, iaas_properties)
-          target = existing_target
-        else
-        target = create_target(target_type, project_idh, provider, iaas_properties)
-        end
 
-        target
+        create_target(target_type, project_idh, provider, iaas_properties)
       end
 
       def self.create_targets?(project_idh, provider, iaas_properties_array, opts = {})
@@ -189,7 +221,8 @@ module DTK
           display_name: target_name,
           type: 'instance',
           iaas_type: 'ec2',
-          iaas_properties: {}
+          iaas_properties: {},
+          project_id: project_idh.id()
         }
         create_opts = { convert: true, ret_obj: { model_name: :target_instance } }
         create_from_rows(target_mh, [create_rows], create_opts)

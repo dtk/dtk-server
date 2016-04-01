adf315224f004efedd8ce852ff0ab6f1af73f589
Rich Pelavin <rich@reactor8.com>
2016-04-01 19:19:31 +0000

    DTK-2515: putting in utility function to automatically create a node template module from info in server nodes_info/current/nodes_info.json

--- application/utility/create_node_template_module.rb
+++ application/utility/create_node_template_module.rb
@@ -16,11 +16,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-require 'erubis'
-require 'pp'
-
+require 'yaml'
 require File.expand_path('require_first_minimal', File.dirname(__FILE__))
 dtk_require('library_nodes')
 
-hash_content = DTK::LibraryNodes.get_hash(in_library: 'public')
-pp hash_content
+output_file = ARGV[0]
+module DTK
+  module Utility
+    module CreateNodeTemplateModule
+      def self.create(output_file)
+        node_info = LibraryNodes.ret_nodes_info_content_from_config_file
+        images_attribute_value = images_attribute_value(node_info)
+        yaml_content = YAML.dump(hash_content(images_attribute_value))
+        if output_file
+          File.open(output_file, 'w') { |f| f << yaml_content }
+        else
+          STDOUT << yaml_content
+        end
+      end
+      
+      
+      private
+      
+      def self.images_attribute_value(node_info)
+      end
+
+      def self.hash_content(images_attribute_value)
+        ComponentModuleHeader 
+      end
+        
+      ComponentModuleName = 'image_aws'
+      DslVersion = '1.0.0' 
+      ImagesAttributeName = 'images'
+      ComponentModuleHeader = {
+        'module' => ComponentModuleName,
+        'dsl_version' => DslVersion,
+        'components' => {
+          ComponentModuleName => {
+            'attributes' =>  {
+              ImagesAttributeName => {
+                'description'  =>  'Mapping of logical image names to amis',
+                'type'  =>  'hash'
+              }
+            }
+          }
+        }
+      }
+      
+
+    end
+  end
+end
+DTK::Utility::CreateNodeTemplateModule.create(output_file)

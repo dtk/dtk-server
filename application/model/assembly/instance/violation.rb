04e6876f9ed41f85db8b3bae8fd5cd0193074c25
Rich Pelavin <rich.reactor8.com>
2016-04-16 21:35:08 +0000

    DTK-2525: cleanup/refactor

--- application/model/assembly/instance/violation.rb
+++ application/model/assembly/instance/violation.rb
@@ -18,6 +18,22 @@
 module DTK
   class Assembly::Instance
     class Violation
+
+      private
+
+      def self.inherited(child_class) 
+        (@klasses ||= []) << child_class
+      end
+
+      def self.impacted_by(*types)
+        pp [self, types]
+      end
+
+      # methods above need to be before including the violation subclasses
+      r8_nested_require('violation', 'sub_classes')
+    end
+
+    class Violation
       def table_form
         type_and_display
       end
@@ -27,11 +43,15 @@ module DTK
         fail Error, "Missing method '#{self.class}#hash_form'"
       end
 
+      # could be overwritten
+      def self.type
+        Aux.underscore(Aux.demodulize(self.class.to_s)).to_sym
+      end
+
       private
 
-      def initialize(opts = {})
-        # TODO: stub for datatype checking
-        @datatype = opts[:datatype]
+      def type 
+        self.class.type
       end
 
       def attr_display_name(attr, print_level = :component)
@@ -39,237 +59,30 @@ module DTK
       end
 
       def hash_form_aux(hash)
-        hash.inject(type_and_display.merge(fix_action: fix_action, fix_text: fix_text)) do |h, (k, v)| 
+        hash.inject(type_and_display.merge(fix_text: fix_text)) do |h, (k, v)| 
           # This will remove nil values from hash
           v.nil? ? h : h.merge(k => v)
         end
       end
 
-      def attribute_ref
-        attr_display_name(@attr, @print_level)
+      def attribute_info
+        @attr.update_object!(:data_type, :hidden)
+        { 
+          attribute: {
+            ref: attr_display_name(@attr, @print_level),
+            datatype: @attr[:data_type],
+            hidden: @attr[:hidden]
+          }
+        }
       end
 
       def type_and_display
         { type: type, description: description }
       end
 
-      # could be overwritten
-      def type
-        Aux.underscore(Aux.demodulize(self.class.to_s)).to_sym
-      end
-
       # must be overwritten
-      def fix_action
-        fail Error, "Missing method '#{self.class}#fix_action'"
-      end
       def fix_text
         fail Error, "Missing method '#{self.class}#fix_text'"
-      end
-
-      ########## Violation classes
-      class ReqUnsetAttr < self
-        def initialize(attr, print_level)
-          super()
-          @attr              = attr
-          @print_level       = print_level
-          @attr_display_name = attr_display_name(attr, print_level)
-        end
-
-        def type
-          :required_unset_attribute
-        end
-
-        def hash_form
-          hash_form_aux(attribute_ref: attribute_ref)
-        end
-
-        def fix_action
-          :set_attribute
-        end
-
-        def fix_text
-          "Enter value for attribute '#{@attr_display_name}'"
-        end
-
-        def description
-          "Attribute '#{@attr_display_name}' is required, but unset"
-        end
-      end
-
-      class IllegalAttrValue < self
-        # opts can have keys
-        #  legal_values
-        def initialize(attr, value, opts = {})
-          super()
-          @attr              = attr
-          @attr_display_name = attr_display_name(attr)
-          @value             = value
-          @legal_values      = opts[:legal_values]
-        end
-
-        def type
-          :illegal_attribute_value
-        end
-
-        def fix_action
-          :set_attribute
-        end
-
-        def fix_text
-          "Enter value for attribute '#{@attr_display_name}'"
-        end
-
-        def hash_form
-          hash_form_aux(attribute_ref: attribute_ref, value: @value, legal_values: @legal_values)
-        end
-
-        def description
-          ret = "Attribute '#{@attr_display_name}' has illegal value '#{@value}'"
-          ret << "; legal values are: #{@legal_values.join(', ')}" if @legal_values
-          ret
-        end
-      end
-
-      # TODO: DTK-2525; got here in refining the violations to work with fix wizard
-
-      class ReqUnsetAttrs < self
-        def initialize(attrs, print_level)
-          opts_print = Opts.new(level: print_level)
-          @attr_display_names = attrs.map { |attr| attr_display_name(attr, print_level) }
-        end
-
-        def type
-          :required_unset_attributes
-        end
-
-        def description
-          aug_attrs_print_form = @attr_display_names.join(', ')
-          "At least one of the attributes (#{aug_attrs_print_form}) is required to be set"
-        end
-      end
-
-
-      class TargetServiceCmpsMissing < self
-        def initialize(component_types)
-          @component_types = component_types
-        end
-        
-        def type
-          :target_service_cmps_missing
-        end
-        
-        def description
-          cmp_or_cmps = (@component_types.size == 1) ? 'Component' : 'Components'
-          is_are = (@component_types.size == 1) ? 'is' : 'are'
-          
-          "#{cmp_or_cmps} of type (#{@component_types.join(', ')}) #{is_are} missing and #{is_are} required for a target service instance"
-        end
-      end
-
-      class ComponentConstraint < self
-        def initialize(constraint, node)
-          @constraint = constraint
-          @node = node
-        end
-
-        def type
-          :component_constraint
-        end
-
-        def description
-          "On assembly node (#{@node[:display_name]}): #{@constraint[:description]}"
-        end
-      end
-
-      class UnconnReqServiceRef < self
-        def initialize(aug_port)
-          @augmented_port = aug_port
-        end
-
-        def type
-          :unmet_dependency
-        end
-
-        def description
-          "Component (#{@augmented_port.display_name_print_form()}) has an unmet dependency"
-        end
-      end
-
-      class ComponentParsingError < self
-        def initialize(component, type)
-          @component = component
-          @type = type
-        end
-
-        def type
-          :parsing_error
-        end
-
-        def description
-          "#{@type} module '#{@component}' has one or more parsing errors."
-        end
-      end
-
-      class MissingIncludedModule < self
-        def initialize(included_module, namespace, version = nil)
-          @included_module = included_module
-          @namespace = namespace
-          @version = version
-        end
-
-        def type
-          :missing_included_module
-        end
-
-        def description
-          full_name = "#{@namespace}:#{@included_module}"
-          "Module '#{full_name}#{@version.nil? ? '' : '-' + @version}' is included in dsl, but not installed. Use 'print-includes' to see more details."
-        end
-      end
-
-      class MultipleNamespacesIncluded < self
-        def initialize(included_module, namespaces)
-          @included_module = included_module
-          @namespaces = namespaces
-        end
-
-        def type
-          :mapped_to_multiple_namespaces
-        end
-
-        def description
-          "Module '#{@included_module}' included in dsl is mapped to multiple namespaces: #{@namespaces.join(', ')}. Use 'print-includes' to see more details."
-        end
-      end
-
-      class HasItselfAsDependency < self
-        def initialize(message)
-          @message = message
-        end
-
-        def type
-          :has_itself_as_dependency
-        end
-
-        def description
-          @message
-        end
-      end
-
-      class NodesLimitExceeded < self
-        def initialize(new_nodes, running, node_limit)
-          @new        = new_nodes
-          @running    = running
-          @node_limit = node_limit
-        end
-
-        def type
-          :nodes_limit_exceeded
-        end
-
-        def description
-          "There are #{@running} nodes currently running in builtin target. Unable to create #{@new} new nodes because it will exceed number of nodes allowed in builtin target (#{@node_limit})"
-        end
       end
     end
   end
---
+++ application/model/assembly/instance/violation/sub_classes.rb
@@ -0,0 +1,221 @@
+#
+# Copyright (C) 2010-2016 dtk contributors
+#
+# This file is part of the dtk project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module DTK
+  class Assembly::Instance
+    class Violation
+      ########## Violation classes
+      class ReqUnsetAttr < self
+        impacted_by :unmet_dependency
+        def initialize(attr, print_level)
+          super()
+          @attr              = attr
+          @print_level       = print_level
+          @attr_display_name = attr_display_name(attr, print_level)
+        end
+
+        def self.type
+          :required_unset_attribute
+        end
+
+        def hash_form
+          hash_form_aux(attribute_info)
+        end
+
+        def fix_text
+          "Enter value for attribute '#{@attr_display_name}'"
+        end
+
+        def description
+          "Attribute '#{@attr_display_name}' is required, but unset"
+        end
+      end
+
+      class IllegalAttrValue < self
+        # opts can have keys
+        #  legal_values
+        def initialize(attr, value, opts = {})
+          super()
+          @attr              = attr
+          @attr_display_name = attr_display_name(attr)
+          @value             = value
+          @legal_values      = opts[:legal_values]
+        end
+
+        def self.type
+          :illegal_attribute_value
+        end
+
+        def fix_text
+          "Enter value for attribute '#{@attr_display_name}'"
+        end
+
+        def hash_form
+          hash_form_aux(attribute_info.merge(value: @value, legal_values: @legal_values))
+        end
+
+        def description
+          ret = "Attribute '#{@attr_display_name}' has illegal value '#{@value}'"
+          ret << "; legal values are: #{@legal_values.join(', ')}" if @legal_values
+          ret
+        end
+      end
+
+      # TODO: DTK-2525; got here in refining the violations to work with fix wizard
+
+      class ReqUnsetAttrs < self
+        def initialize(attrs, print_level)
+          opts_print = Opts.new(level: print_level)
+          @attr_display_names = attrs.map { |attr| attr_display_name(attr, print_level) }
+        end
+
+        def self.type
+          :required_unset_attributes
+        end
+
+        def description
+          aug_attrs_print_form = @attr_display_names.join(', ')
+          "At least one of the attributes (#{aug_attrs_print_form}) is required to be set"
+        end
+      end
+
+
+      class TargetServiceCmpsMissing < self
+        def initialize(component_types)
+          @component_types = component_types
+        end
+        
+        def self.type
+          :target_service_cmps_missing
+        end
+        
+        def description
+          cmp_or_cmps = (@component_types.size == 1) ? 'Component' : 'Components'
+          is_are = (@component_types.size == 1) ? 'is' : 'are'
+          
+          "#{cmp_or_cmps} of type (#{@component_types.join(', ')}) #{is_are} missing and #{is_are} required for a target service instance"
+        end
+      end
+
+      class ComponentConstraint < self
+        def initialize(constraint, node)
+          @constraint = constraint
+          @node = node
+        end
+
+        def self.type
+          :component_constraint
+        end
+
+        def description
+          "On assembly node (#{@node[:display_name]}): #{@constraint[:description]}"
+        end
+      end
+
+      class UnconnReqServiceRef < self
+        def initialize(aug_port)
+          @augmented_port = aug_port
+        end
+
+        def self.type
+          :unmet_dependency
+        end
+
+        def description
+          "Component (#{@augmented_port.display_name_print_form()}) has an unmet dependency"
+        end
+      end
+
+      class ComponentParsingError < self
+        def initialize(component, type)
+          @component = component
+          @type = type
+        end
+
+        def self.type
+          :parsing_error
+        end
+
+        def description
+          "#{@type} module '#{@component}' has one or more parsing errors."
+        end
+      end
+
+      class MissingIncludedModule < self
+        def initialize(included_module, namespace, version = nil)
+          @included_module = included_module
+          @namespace = namespace
+          @version = version
+        end
+
+        def self.type
+          :missing_included_module
+        end
+
+        def description
+          full_name = "#{@namespace}:#{@included_module}"
+          "Module '#{full_name}#{@version.nil? ? '' : '-' + @version}' is included in dsl, but not installed. Use 'print-includes' to see more details."
+        end
+      end
+
+      class MultipleNamespacesIncluded < self
+        def initialize(included_module, namespaces)
+          @included_module = included_module
+          @namespaces = namespaces
+        end
+
+        def self.type
+          :mapped_to_multiple_namespaces
+        end
+
+        def description
+          "Module '#{@included_module}' included in dsl is mapped to multiple namespaces: #{@namespaces.join(', ')}. Use 'print-includes' to see more details."
+        end
+      end
+
+      class HasItselfAsDependency < self
+        def initialize(message)
+          @message = message
+        end
+
+        def self.type
+          :has_itself_as_dependency
+        end
+
+        def description
+          @message
+        end
+      end
+
+      class NodesLimitExceeded < self
+        def initialize(new_nodes, running, node_limit)
+          @new        = new_nodes
+          @running    = running
+          @node_limit = node_limit
+        end
+
+        def self.type
+          :nodes_limit_exceeded
+        end
+
+        def description
+          "There are #{@running} nodes currently running in builtin target. Unable to create #{@new} new nodes because it will exceed number of nodes allowed in builtin target (#{@node_limit})"
+        end
+      end
+    end
+  end
+end

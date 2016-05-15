9bbd63a6a03293a8b906e0996062f087a485d177
Rich Pelavin <rich.reactor8.com>
2016-05-15 17:23:22 +0000

    DTK-2554: remove 'rest__' prefix from v1 controllers

--- application/controller/v1/authorization.rb
+++ application/controller/v1/authorization.rb
@@ -19,12 +19,12 @@ module DTK
   module V1
     class AuthorizationController < Controller
 
-      def rest__login
+      def login
         cred = { username: request.params['username'], password: DataEncryption.hash_it(request.params['password']), c: ret_session_context_id(), access_time: Time.now() }
         handle_and_return_authentication(cred)
       end
 
-      def rest__logout
+      def logout
         user_logout
         { content: nil }
       end

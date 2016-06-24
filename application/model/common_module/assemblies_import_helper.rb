9e495d32cd9edc7313927045822f66769a5556d1
Rich Pelavin <rich.reactor8.com>
2016-06-24 04:12:21 +0000

    DTK-2554: Working on encapsualting logic to import common module assemblies

--- application/model/common_module/assemblies_import_helper.rb
+++ application/model/common_module/assemblies_import_helper.rb
@@ -24,7 +24,7 @@ module DTK
         super(project.id_handle, module_branch, service_module, module_refs)
       end
 
-      def process_from_parse_hash(parse_hash)
+      def put_needed_info_into_import_helper!(parse_hash)
         parse_hash.each do |assembly|
           hash_content     = {}
           assembly_name    = assembly[:name]
@@ -37,7 +37,11 @@ module DTK
 
           hash_content.merge!('assembly' => assembly_content)
 
-          # hash_content.merge!('dsl_version' => '1.0.0')
+          # Aldin: 06/24/2016: no need to pass in dsl version; since parse_hash wil reflect all teh vesrion specific parsing and be
+          # in version independent 'canonical form'
+          # I put back in hash_content.merge!('dsl_version' => '1.0.0') so the code runs through v4 parsing
+          # all the equivalent rules to v4 parsing should be put in dtk-dsl under v1
+          hash_content.merge!('dsl_version' => '1.0.0')
           # TODO: Aldin - need to take dsl_version from hash;
           # currently there is issue with using version 1.0.0, error happens in
           # application/model/module/service_module/dsl/assembly_import/adapters/v4.rb:79:in `import_task_templates'",
@@ -49,12 +53,13 @@ module DTK
           @service_module.parse_assembly_wide_components!(hash_content)
 
           process_assembly(hash_content, opts)
-          @parent_class::SetParsedDSL.set_assembly_raw_hash?(assembly_name, hash_content, opts)
+          # TODO: dont need unless have opts[:ret_parsed_dsl] is set to true
+          # @parent_class::SetParsedDSL.set_assembly_raw_hash?(assembly_name, hash_content, opts)
         end
+      end
 
-        assembly_workflows = import
-
-        @parent_class::SetParsedDSL.set_module_refs_and_workflows?(@module_name, assembly_workflows, @component_module_refs)
+      def import_into_model
+        import
       end
 
       private
@@ -62,21 +67,32 @@ module DTK
       def process_assembly(assembly_hash, opts = {})
         assembly_ref = @service_module.assembly_ref(assembly_hash['name'], opts[:module_version])
         assembly_content = (assembly_hash['assembly'] || {}).merge(Aux.hash_subset(assembly_hash, ['name', 'description', 'workflow']))
-        # Aldin: 02/24/2016: want to remove need for version_proc_class and handle the version sepcfic parsing in dtk-dsl
+        # Aldin: 06/24/2016: remove need for version_proc_class  and instaed handle anything dsl version specfic in
+        # dtk-dsl
+        # version_proc_class.import_assembly_top and version_proc_class.import_nodes
+        # should be replaced by two phase processing:
+        # dtk-dsl should do fine grain parsing of assembly rather than course (i.e., returning just :name and :assembly
+        # This shoudl return hash with all keys in 'symbol form' we then have code in drk-server that converts this to
+        #  'db_update_hash form and add anythng that needs db lookup such as the ids'
+        # in the version_proc_class when see 'aggregate_errors.aggregate_errors!'; remove this; its purpose is to bulk up errors
+        # to make parsing simpler just throwing error on first error
         integer_version = determine_integer_version(assembly_hash, opts)
         version_proc_class = load_and_return_version_adapter_class(integer_version)
-
 
         db_updates_cmp = version_proc_class.import_assembly_top(assembly_ref, assembly_content, @module_branch, @module_name, opts)
         @db_updates_assemblies['component'].merge!(db_updates_cmp)
 
         # if bad node reference, return error and continue with module import
+        # Took at processing of node_bidnings_hash since assume common modules have node attributes instaed
+        node_bindings_hash = {}
         imported_nodes = version_proc_class.import_nodes(@container_idh, @module_branch, assembly_ref, assembly_content, node_bindings_hash, @component_module_refs, opts)
-        raise imported_nodes if ParsingError.is_error?(imported_nodes)
+        raise imported_nodes if ServiceModule::ParsingError.is_error?(imported_nodes)
 
         @db_updates_assemblies['node'].merge!(imported_nodes)
-        @ndx_assembly_hashes[ref] ||= assem
-        @ndx_version_proc_classes[ref] ||= version_proc_class
+
+        # Aldin: 06/24/2016: See if these can be removed
+        @ndx_assembly_hashes[assembly_ref] ||= assembly_content
+        @ndx_version_proc_classes[assembly_ref] ||= version_proc_class
       end
     end
   end

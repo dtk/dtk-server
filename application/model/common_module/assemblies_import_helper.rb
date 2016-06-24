c1c707ccbd38eef06f91d9fc5d8de374f2745034
Rich Pelavin <rich.reactor8.com>
2016-06-24 02:53:41 +0000

    DTK-2554: Working on encapsualting logic to import common module assemblies

--- application/model/common_module/assemblies_import_helper.rb
+++ application/model/common_module/assemblies_import_helper.rb
@@ -18,9 +18,9 @@
 module DTK
   class CommonModule
     class AssembliesImportHelper < ServiceModule::AssemblyImport
-      def initialize(project, module_branch)
-        service_module = module_branch.get_module
-        module_refs = ModuleRefs.get_component_module_refs(module_branch)
+      def initialize(project, service_module, module_branch)
+        module_refs   = ModuleRefs.get_component_module_refs(module_branch)
+        @parent_class = service_module.class
         super(project.id_handle, module_branch, service_module, module_refs)
       end
 
@@ -45,19 +45,39 @@ module DTK
 
           hash_content.merge!('name' => assembly_name)
 
-          service_module.create_ec2_properties?(hash_content)
-          service_module.parse_assembly_wide_components!(hash_content)
+          @service_module.create_ec2_properties?(hash_content)
+          @service_module.parse_assembly_wide_components!(hash_content)
 
-          process(service_module.module_name, hash_content, opts)
-          SetParsedDSL.set_assembly_raw_hash?(assembly_name, hash_content, opts)
+          process_assembly(hash_content, opts)
+          @parent_class::SetParsedDSL.set_assembly_raw_hash?(assembly_name, hash_content, opts)
         end
 
-        assembly_workflows = impor
+        assembly_workflows = import
 
-        module_refs = ModuleRefs.get_component_module_refs(module_branch)
-        SetParsedDSL.set_module_refs_and_workflows?(service_module.module_name, assembly_workflows, module_refs)
+        @parent_class::SetParsedDSL.set_module_refs_and_workflows?(@module_name, assembly_workflows, @component_module_refs)
       end
 
+      private
+
+      def process_assembly(assembly_hash, opts = {})
+        assembly_ref = @service_module.assembly_ref(assembly_hash['name'], opts[:module_version])
+        assembly_content = (assembly_hash['assembly'] || {}).merge(Aux.hash_subset(assembly_hash, ['name', 'description', 'workflow']))
+        # Aldin: 02/24/2016: want to remove need for version_proc_class and handle the version sepcfic parsing in dtk-dsl
+        integer_version = determine_integer_version(assembly_hash, opts)
+        version_proc_class = load_and_return_version_adapter_class(integer_version)
+
+
+        db_updates_cmp = version_proc_class.import_assembly_top(assembly_ref, assembly_content, @module_branch, @module_name, opts)
+        @db_updates_assemblies['component'].merge!(db_updates_cmp)
+
+        # if bad node reference, return error and continue with module import
+        imported_nodes = version_proc_class.import_nodes(@container_idh, @module_branch, assembly_ref, assembly_content, node_bindings_hash, @component_module_refs, opts)
+        raise imported_nodes if ParsingError.is_error?(imported_nodes)
+
+        @db_updates_assemblies['node'].merge!(imported_nodes)
+        @ndx_assembly_hashes[ref] ||= assem
+        @ndx_version_proc_classes[ref] ||= version_proc_class
+      end
     end
   end
 end

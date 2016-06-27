abdf926c3baa73da19e16cc3d74f052db280239d
Rich Pelavin <rich.reactor8.com>
2016-06-27 00:15:22 +0000

    DTK-2554: restructuring so can break version_proc_class.import_assembly methods into parsing part that put in dtk-dsl and server side processing to put info in import helper

---
+++ application/model/common_module/import/assemblies.rb
@@ -0,0 +1,55 @@
+#
+# Copyright (C) 2010-2016 dtk contributors
+#
+# This file is part of the dtk project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+module DTK
+  class CommonModule
+    module Import
+      class Assemblies < ServiceModule::AssemblyImport
+        require_relative('assemblies/assembly')
+        include AssemblyMixin
+
+        def initialize(project, service_module, module_branch)
+          module_refs   = ModuleRefs.get_component_module_refs(module_branch)
+          @parent_class = service_module.class
+          super(project.id_handle, module_branch, service_module, module_refs)
+        end
+
+        def put_needed_info_into_import_helper!(parsed_assemblies)
+          parsed_assemblies.each do |parsed_assembly|
+            
+            #  Aldin: 06/27/2016: write CommonModule::BaseService#create_ec2_properties?(parsed_assembly)
+            # to replace @service_module.create_ec2_properties?(hash_content)
+            # @service_module.create_ec2_properties?(hash_content)
+            
+            # Aldin: 06/27/2016: move this logic to the parser
+            # @service_module.parse_assembly_wide_components!(hash_content)
+            
+            process_assembly!(parsed_assembly)
+            # TODO: dont need unless have opts[:ret_parsed_dsl] is set to true
+            # @parent_class::SetParsedDSL.set_assembly_raw_hash?(assembly_name, hash_content, opts)
+          end
+        end
+        
+        def import_into_model
+          import
+        end
+
+      end
+    end
+  end
+end
+

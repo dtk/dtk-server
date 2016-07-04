7fb876e13ddc199448832e4da5698de871bee36b
Aldin Milanovic <aldin@atlantbh.com>
2016-07-04 16:59:45 +0200

    Partially finished import nodes

--- application/model/common_module/import/service_module/assembly.rb
+++ application/model/common_module/import/service_module/assembly.rb
@@ -48,33 +48,27 @@ module DTK
             # Aldin: 06/27/2016: There is alot of code in parsing workflow; I will handle conversion to
             # split between dtk-dsl and code I will put in assembly/workflows
             # add in workflows (task_templates)
+            #
             # workflows_db_update_hash  = version_proc_class.import_task_templates(parsed_assembly)
             # assembly_ref_pointer.merge!('task_template' => workflows_db_update_hash.mark_as_complete)
-            
+
             assembly_attrs_db_update_hash = Assembly::Attributes.db_update_hash(parsed_assembly.val(:Attributes) || [])
-            # Aldin: 06/27/2016: Moving mark as complete to this level
             assembly_ref_pointer.merge!('attribute' => assembly_attrs_db_update_hash.mark_as_complete)
 
-            # add to @db_updates_assemblies['node'] the db update hash that captures the nodes section
-            # Aldin: 06/27/2016: create new file assembly/nodes and put in replacement for @version_proc_class.import_nodes
-            # have this only return db hash updates and not error by raising ruby on first error
-            # this wil allow you to remove raise db_updates if ServiceModule::ParsingError.is_error?(db_updates)
-            # TODO: next line needed until convert from version_proc_class.import_nodes
-
-            if parsed_nodes = parsed_assembly.delete(:nodes)
-            # if parsed_nodes = parsed_assembly.val(:Nodes)
-              # nodes_db_update_hash = Assembly::Nodes.db_update_hash(@container_idh, assembly_ref, parsed_assembly.val(:Nodes) || [], node_bindings_hash, @component_module_refs, default_assembly_name: assembly_name)
-              # Aldin: 06/27/2016: Moving mark as complete to this level
-              # assembly_ref_pointer.merge!('nodes' => nodes_db_update_hash.mark_as_complete)
-
-              parsed_assembly['nodes'] = parsed_nodes.inject({}) { |h, parsed_node| h.merge(parsed_node.req(:Name) => parsed_node) }
+            # if parsed_nodes = parsed_assembly.delete(:nodes)
+            if parsed_nodes = parsed_assembly.val(:Nodes)
               node_bindings_hash = {}
-              db_updates = version_proc_class.import_nodes(@container_idh, @module_branch, assembly_ref, parsed_assembly, node_bindings_hash, @component_module_refs, default_assembly_name: assembly_name)
-              raise db_updates if ServiceModule::ParsingError.is_error?(db_updates)
-              @db_updates_assemblies['node'].merge!(db_updates)
+
+              # Aldin: 07/04/2016 - still not 100% completed
+              nodes_db_update_hash = Assembly::Nodes.db_update_hash(@container_idh, assembly_ref, parsed_nodes, node_bindings_hash, @component_module_refs, { default_assembly_name: assembly_name, version_proc_class: version_proc_class })
+              @db_updates_assemblies['node'].merge!(nodes_db_update_hash)
+
+              # parsed_assembly['nodes'] = parsed_nodes.inject({}) { |h, parsed_node| h.merge(parsed_node.req(:Name) => parsed_node) }
+              # db_updates = version_proc_class.import_nodes(@container_idh, @module_branch, assembly_ref, parsed_assembly, node_bindings_hash, @component_module_refs, default_assembly_name: assembly_name)
+              # raise db_updates if ServiceModule::ParsingError.is_error?(db_updates)
+              # @db_updates_assemblies['node'].merge!(db_updates)
             end
 
-            
             @ndx_assembly_hashes[assembly_ref] ||= parsed_assembly
             
             # Aldin: 06/27/2016: See how to remove this
--- application/model/common_module/import/service_module/assembly/nodes.rb
+++ application/model/common_module/import/service_module/assembly/nodes.rb
@@ -19,18 +19,85 @@ module DTK
   class CommonModule::Import::ServiceModule
     module Assembly
       module Nodes
+        extend FactoryObjectClassMixin
+
         def self.db_update_hash(container_idh, assembly_ref, parsed_nodes, node_bindings_hash, component_module_refs, opts = {})
-          # parsed_attributes.inject(DBUpdateHash.new) do |h, parsed_attribute|
-          #   attr_name    = parsed_attribute.req(:Name)
-          #   attr_val     = parsed_attribute.val(:Value)
-          #   attr_content = {
-          #     'display_name'   => attr_name,
-          #     'value_asserted' => attr_val,
-          #     'data_type'      => Attribute::Datatype.datatype_from_ruby_object(attr_val)
-          #   }
-          #   h.merge(attr_name => attr_content)
-          # end
-          # compute node_to_nb_rs and nb_rs_to_id
+          unless parsed_nodes.is_a?(Array)
+            fail ParsingError.new('Nodes section is ill-formed', opts_file_path(opts))
+          end
+
+          parsed_nodes.inject(DBUpdateHash.new) do |h, node_hash|
+            if node_hash && node_hash.is_a?(String)
+              node_hash = { name: node_hash }
+            end
+
+            node_hash ||= {}
+            node_hash_ref = node_hash.req(:Name)
+            node_ref      = assembly_template_node_ref(assembly_ref, node_hash_ref)
+
+            unless (node_hash || {}).is_a?(Hash)
+              fail ParsingError.new("The content associated with key (#{node_hash_ref}) should be a hash representing assembly node info", opts_file_path(opts))
+            end
+
+            attributes = Attributes.db_update_hash(node_hash.val(:Attributes) || []).mark_as_complete
+            type       = node_hash_ref.eql?(Node::Type::Node.assembly_wide) ? node_hash_ref : ret_node_type(attributes)
+
+            node_output = {
+              'display_name' => node_hash_ref,
+              'type' => type,
+              'attribute' => attributes,
+              '*assembly_id' => "/component/#{assembly_ref}"
+            }
+
+            # we are not going to use node-bindings in new dsl, instead will retreive node bindings from ec2_properties component
+            components = node_hash['components']
+
+            if components
+              node_output['node_binding_rs_id'] = node_binding_from_ec2_component_attributes(components, container_idh)
+
+              # Aldin: 07/04/2016 - need to rewrite this part without version_proc_class
+              if version_proc_class = opts[:version_proc_class]
+                cmps_output = version_proc_class.import_component_refs(container_idh, opts[:default_assembly_name], components, component_module_refs, opts)
+
+                unless cmps_output.empty?
+                  node_output['component_ref'] = cmps_output
+                end
+              end
+            end
+
+            h.merge(node_ref => node_output)
+          end
+        end
+
+        def self.node_binding_from_ec2_component_attributes(cmps, container_idh)
+          nb_name          = nil
+          node_binding     = nil
+          nb_rs_containter = Library.get_public_library(container_idh.createMH(:library))
+
+          cmps.each do |cmp|
+            if cmp.is_a?(Hash) && cmp.keys.first.eql?(CommandAndControl.node_property_component())
+              if attributes = cmp.values.first['attributes']
+                size = attributes['size']
+                image = attributes['image']
+                nb_name = "#{image}-#{size}" if size && image
+              end
+              break
+            end
+          end
+
+          if nb_name
+            filter = [:eq, :ref, nb_name]
+            node_bindings = nb_rs_containter.get_node_binding_rulesets(filter)
+            node_binding = node_bindings.first[:id] unless node_bindings.empty?
+          end
+
+          node_binding
+        end
+
+        def self.ret_node_type(attributes = {})
+          return Node::Type::Node.stub unless attributes['type']
+          attributes.delete('type')
+          Node::Type::NodeGroup.stub
         end
       end
     end

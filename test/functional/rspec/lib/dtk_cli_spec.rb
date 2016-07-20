d1f264cfba743b630c9cfc995fa5f5da749c5f54
bjusufbe <bakir@atlantbh.com>
2016-07-20 12:34:58 +0200

    added initial skeleton skript for new dtk cli

---
+++ test/functional/rspec/lib/dtk_cli_spec.rb
@@ -0,0 +1,58 @@
+require 'rubygems'
+require 'rest_client'
+require 'pp'
+require 'json'
+require 'awesome_print'
+
+STDOUT.sync = true
+
+shared_context 'Install module' do |module_name, dtk_cli_path|
+  it "installs #{module_name} module from local filesystem to server" do
+    puts 'Install module:', '----------------------'
+    pass = true
+    value = `#{dtk_cli_path}/bin/dtk module install`
+    puts value
+    pass = false if ((value.include? 'ERROR') || (value.include? 'exists already'))
+    puts "Install of module #{module_name} was completed successfully!" if pass == true
+    puts "Instlal of module #{module_name} did not complete successfully!" if pass == false
+    puts ''
+    expect(pass).to eq(true)
+  end
+end
+
+shared_context 'List assemblies' do |module_name, assembly_name, dtk_cli_path|
+  it "checks that assembly: #{assembly_name} exists as part of module: #{module_name}" do
+    puts 'List assemblies:', '----------------------'
+    assembly_found = false
+    # TODO: Add logic to search for assembly as part of module
+    expect(assembly_found).to eq(true)
+  end
+end
+
+shared_context 'Push module' do |module_name, dtk_cli_path|
+  it "push #{module_name} module changes from local filesystem to server" do
+    puts 'Push module:', '----------------------'
+    pass = true
+    value = `#{dtk_cli_path}/bin/dtk module push`
+    puts value
+    pass = false if value.include? 'ERROR'
+    puts "Push of module #{module_name} was completed successfully!" if pass == true
+    puts "Push of module #{module_name} did not complete successfully!" if pass == false
+    puts ''
+    expect(pass).to eq(true)
+  end
+end
+
+shared_context 'Delete module' do |module_name, dtk_cli_path|
+  it "deletes #{module_name} module from server" do
+    puts 'Delete module:', '----------------------'
+    pass = true
+    value = `#{dtk_cli_path}/bin/dtk module delete -y` # currently -y flag does not work
+    puts value
+    pass = false if ((value.include? 'ERROR') || (value.include? 'does not exist'))
+    puts "Delete of module #{module_name} was completed successfully!" if pass == true
+    puts "Delete of module #{module_name} did not complete successfully!" if pass == false
+    puts ''
+    expect(pass).to eq(true)
+  end
+end
---
+++ test/functional/rspec/spec/regression/dtk_cli/install_module_spec.rb
@@ -0,0 +1,31 @@
+require './lib/dtk_cli_spec'
+
+dtk_cli_path = '/Users/bjusufbe/R8-public/dtk-cli'
+module_name = 'rich/simple'
+assembly_name = 'simple'
+
+describe "(DTK CLI) Test Case 1: Install module with new dtk cli" do
+  before(:all) do
+    puts '******************************************************', ''
+  end
+
+  context "Install module" do
+    include_context "Install module", module_name, dtk_cli_path
+  end
+
+  context "List assemblies contained in this module" do
+    include_context "List assemblies", module_name, assembly_name, dtk_cli_path
+  end
+
+  context "Push module" do
+    include_context "Push module", module_name, dtk_cli_path
+  end
+
+  context "Delete module" do
+    include_context "Delete module", module_name, dtk_cli_path
+  end
+
+  after(:all) do
+    puts '', ''
+  end
+end

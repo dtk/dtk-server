c2af25231b17ee635513146473af96e64216d425
Aldin Milanovic <aldin@atlantbh.com>
2016-09-15 16:18:50 +0200

    Handle adding of new components

--- lib/common_dsl/object_logic/assembly/component/diff/add.rb
+++ lib/common_dsl/object_logic/assembly/component/diff/add.rb
@@ -22,9 +22,6 @@ module DTK; module CommonDSL
         include Mixin
 
         def process(result, opts = {})
-          # TODO: DTK-2665: look at whether should instead use Diff::DiffErrors.raise_error, rather than result.add_error_msg 
-          # and whether should trap errors, e.g., when call assembly_instance.add_component so can augment error message with
-          # diff change reference (i.e, qualified_key, change_type=component and operation=add)
           matching_aug_cmp_templates = ::DTK::Component::Template.find_matching_component_templates(assembly_instance, component_name) 
           aug_cmp_template = nil
 
@@ -44,22 +41,26 @@ module DTK; module CommonDSL
           end
 
           if aug_cmp_template
-            # TODO: DTK-2650:  use this and node to add component to node
-            # node is gotten by looking at qualified_key
-            # case on whether assembly or node level
             if node = parent_node?
               add_component_to_node(node, aug_cmp_template, component_title: component_title?)
               result.add_item_to_update(:workflow) # workflow will be updated with spliced in new component
             else
-              # TODO: DTK-2650:  need to call method that adds the component to top level assembly
+              # add component to assembly_wide node (to top level assembly)
+              node = assembly_instance.create_assembly_wide_node?
+              add_component_to_node(node, aug_cmp_template, component_title: component_title?)
+              result.add_item_to_update(:workflow)
             end
-            raise 'got here' # TODO: DTK-2650: This will undo transactions so can retest a change to dtk.service.yaml that adds a component
-            # TODO: DTK-2650: if comment this out and do push twiced second time wil not see a component added change, but wil see a diff in workflows unti            # logic is put in lib/common_dsl/diff.rb as noted that updates the dsl from the object model and signals the client to pull the changes
+
+            # raise 'got here'
+            # TODO: DTK-2650: This will undo transactions so can retest a change to dtk.service.yaml that adds a component
+            # TODO: DTK-2650: if comment this out and do push twiced second time wil not see a component added change, but wil see a diff in workflows until
+            # logic is put in lib/common_dsl/diff.rb as noted that updates the dsl from the object model and signals the client to pull the changes
             # a temporary thing to also consider is calling assembly_instance.add_component
             # as assembly_instance.add_component(node.id_handle, aug_cmp_template, opts[:component_title], :donot_update_workflow: true)
-            
+
+            # @parse_object.val(:Attributes)
             # TODO: DTK-2650: Put in logic to see if any attributes are part of the component add and if so then process; a sketch of this: adding method
-            # def add_component_attributes(result)
+            # add_component_attributes(result)
             # modeled after add_nested_components in node/add.rb
           end 
         end
@@ -70,6 +71,22 @@ module DTK; module CommonDSL
         #   :component_title
         def add_component_to_node(node, aug_cmp_template, opts = {})
           assembly_instance.add_component(node.id_handle, aug_cmp_template, opts[:component_title])
+        end
+
+        def add_component_attributes(result)
+           attributes_semantic_parse_array.each do |attribute|
+            # TODO: DTK-2650: Aldin: Need to implement attribute adding for component; similar to bellow:
+            # component_add_diff = Attribute::Diff::Modify.new(attribute, service_instance: @service_instance)#.qualified_key, parse_object: attribute, service_instance: @service_instance)
+            # component_add_diff.process(result)
+          end
+        end
+
+        def attributes_semantic_parse_hash
+          @parse_object.val(:Attributes) || {}
+        end
+
+        def attributes_semantic_parse_array
+          attributes_semantic_parse_hash.values
         end
 
         def find_matching_dependency(matching_aug_cmp_templates, dependencies = {})

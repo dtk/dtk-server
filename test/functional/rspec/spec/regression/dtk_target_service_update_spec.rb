453b7e1ebe390425560648530ae03d1ac584790b
bjusufbe <bakir@atlantbh.com>
2016-11-02 16:37:46 +0100

    fixed init script for treating targets/workspaces in docker regression environment

--- test/functional/rspec/spec/regression/dtk_target_service_update_spec.rb
+++ test/functional/rspec/spec/regression/dtk_target_service_update_spec.rb
@@ -1,11 +1,6 @@
 #!/usr/bin/env ruby
 # This RSpec script is used for creating service instance target with latest aws_images component module
 
-require 'rubygems'
-require 'rest_client'
-require 'pp'
-require 'json'
-require 'awesome_print'
 require './lib/dtk_common'
 require './lib/target_spec'
 require './lib/workspace_spec'
@@ -16,105 +11,70 @@ require './lib/component_module_versions_spec'
 require './lib/service_modules_spec'
 require './lib/service_module_versions_spec'
 
-
-target_name = 'target'
-workspace_assembly_template = 'workspace'
-module_dir = '~/dtk'
-namespace = 'aws'
-ec2_component_module_name = 'ec2'
-image_component_module_name = 'image_aws'
-network_component_module_name = 'network_aws'
-network_service_module_name = 'network'
-network_service_required_modules = [image_component_module_name]
-
 # Target specific properties
+target_service_module = 'aws/network'
+target_service_module_name = 'aws:network'
 aws_access_key = ENV['AWS_ACCESS_KEY']
 aws_secret_key = ENV['AWS_SECRET_KEY']
 default_keypair = 'testing_use1'
-security_group_name = 'default'
-security_group_id = 'sg-2282f747'
-subnet_id = 'subnet-af44b8d8'
 target_service_name = 'target'
-target_assembly_name = 'network::single-subnet'
+target_instance = 'network-target'
+target_assembly_name = 'network::target-v1.0.2'
 is_target = true
+target_location = "/tmp/network"
 
 target = Common.new(target_service_name, target_assembly_name, is_target)
 
-describe 'DTK Target Service Instance update' do
-         
+# Stage target service, set attributes and converge
+
+def setup_target(target, target_service_module_name, target_instance, target_service_name, aws_access_key, aws_secret_key, default_keypair)
+  target_staged = target.stage_service_instance(target_service_module_name, target_instance)
+  if target.check_if_service_exists(target.service_id)
+    puts "Set attributes for staged target..."
+    set_attributes_array = []
+    set_attributes_array << target.set_attribute(target.service_id, 'identity_aws::credentials/aws_access_key_id', aws_access_key)
+    set_attributes_array << target.set_attribute(target.service_id, 'identity_aws::credentials/aws_secret_access_key', aws_secret_key)
+    set_attributes_array << target.set_attribute(target.service_id, 'network_aws::vpc[vpc1]/default_keypair', default_keypair)
+    if !set_attributes_array.include? false
+      service_converged = target.converge_service(target.service_id, 10)
+      if service_converged == true
+        puts "#{target_service_name} service deployed!"
+        target.set_default_target(target_service_name)
+      else
+        fail "[ERROR] #{target_service_name} service was not deployed successfully!"
+      end
+    else
+      fail "[ERROR] Some of the attributes are not set correctly. Will not proceed with converge process!"
+    end
+  else
+    fail "[ERROR] Failed to stage target #{target_service_name}"
+  end
+end
+
+describe 'DTK Target Service Instance update' do    
   before :all do 
     puts '**********************************', ''
   end
 
-  #context 'Delete workspace service instances in default target' do
-  #  include_context 'Delete workspace instances in default target', target
-  #end
-
-  #context 'Delete default target service instance' do
-  #  include_context 'Delete default target', target
-  #end
-
-  #context "Delete #{namespace}/#{network_service_module_name} service module" do
-  #  include_context 'Delete all service module versions', target, "#{namespace}:#{network_service_module_name}"
-  #end
-
-  context "Delete #{namespace}/#{network_service_module_name} service module from local filesystem" do
-    include_context 'Delete all local service module versions', "#{module_dir}/service_modules/#{namespace}", "#{network_service_module_name}"
+  context 'Delete workspace service instances in default target' do
+    include_context 'Delete workspace instances in default target', target
   end
 
-  context "Delete #{namespace}/#{image_component_module_name} component modules" do
-    include_context 'Delete all component module versions', target, "#{namespace}:#{image_component_module_name}"
+  context 'Delete default target service instance' do
+    include_context 'Delete default target', target
   end
 
-  context "Delete #{namespace}/#{image_component_module_name} component modules from local filesystem" do
-    include_context 'Delete all local component module versions', "#{module_dir}/component_modules/#{namespace}/", "#{image_component_module_name}"
-  end
-  
-  context "Import #{namespace}#{network_service_module_name} service module" do
-    include_context 'Import remote service module', "#{namespace}:#{network_service_module_name}"
-  end
-
-   context "Stage target #{target_assembly_name}" do
-    include_context "Stage", target
-  end
-
-  context "List services after stage" do    
-    include_context "List services after stage", target
-  end
-
-  context "Set aws key attribute" do
-    include_context "Set attribute", target, 'network_aws::iam_user[default]/aws_access_key_id', aws_access_key
-  end
-  
-  context "Set aws secret attribute" do
-    include_context "Set attribute", target, 'network_aws::iam_user[default]/aws_secret_access_key', aws_secret_key
-  end
-
-  context "Set keypair attribute" do
-    include_context "Set attribute", target, 'network_aws::vpc[vpc1]/default_keypair', default_keypair
-  end
-
-  context "Set subnet id attribute" do
-    include_context "Set attribute", target, 'network_aws::vpc_subnet[vpc1-public]/subnet_id', subnet_id
-  end
-
-  context "Set security group name attribute" do
-    include_context "Set attribute", target, 'network_aws::security_group[vpc1-default]/group_name', security_group_name
-  end
-
-  context "Set security group id attribute" do
-    include_context "Set attribute", target, 'network_aws::security_group[vpc1-default]/group_id', security_group_id
-  end
-
-  context "Converge function" do
-    include_context "Converge service", target, 30
-  end
-
-  context "Set default target" do
-    include_context "Set default target", target, target_service_name
+  context 'Target setup' do
+    it 'creates new target and performs its setup' do
+      setup_target(target, target_service_module_name, target_instance, target_service_name, aws_access_key, aws_secret_key, default_keypair)
+    end
   end
 
   context "Create workspace" do
     include_context 'Create workspace instance', target, "workspace"
+  end
+
+  after :all do
+    puts ''
   end
 end
--- test/functional/rspec/utility/dtk_image_aws_pull.rb
+++ test/functional/rspec/utility/dtk_image_aws_pull.rb
@@ -3,12 +3,33 @@
 
 require './lib/dtk_common'
 require './lib/component_modules_spec'
+require './lib/service_modules_spec'
 
-component_module_name = 'aws:image_aws'
+component_1 = 'aws:image_aws'
+component_2 = 'aws:identity_aws'
+component_3 = 'aws:ec2'
+component_4 = 'aws:image_aws'
+service = 'aws:network'
 dtk_common = Common.new('', '')
 
 describe 'Pull aws:image_aws from repoman' do
   context "Pull-dtkn component module" do
-    include_context "Pull-dtkn component module", component_module_name
+    include_context "Pull-dtkn component module", component_1
+  end
+
+  context "Pull-dtkn component module" do
+    include_context "Pull-dtkn component module", component_2
+  end
+
+  context "Pull-dtkn component module" do
+    include_context "Pull-dtkn component module", component_3
+  end
+
+  context "Pull-dtkn component module" do
+    include_context "Pull-dtkn component module", component_4
+  end
+
+  context "Pull-dtkn service module" do
+    include_context "Pull-dtkn service module", service
   end
 end

167d69bb717b198c1ee36ad217eefb81a170a238
bjusufbe <bakir@atlantbh.com>
2016-12-27 11:12:16 +0100

    test scripts refactoring

--- test/functional/rspec/spec/regression/dslv3_component_modules_np/cmd_test_case_9_spec.rb
+++ test/functional/rspec/spec/regression/dslv3_component_modules_np/cmd_test_case_9_spec.rb
@@ -7,8 +7,26 @@ initial_module_location = "./spec/regression/dslv3_component_modules_np/resource
 module_location = '/tmp/cmd_test_case_9'
 module_name = 'test/cmd_test_case_9'
 assembly_name = 'test_assembly'
+service_name = 'cmd_test_case_9'
+service_location = "~/dtk/"
 original_module_name = 'cmd_test_case_9_dtk.module.yaml'
 delta_module_content = 'delta_cmd_test_case_9_dtk.module.yaml'
+
+attributes_to_check_1 = {
+  'node/cmd_test_case_9::first_component/hash_attr'    => "",
+  'node/cmd_test_case_9::first_component/hash_attr_2'  => "",
+  'node/cmd_test_case_9::first_component/hash_attr_3'  => "",
+  'node/cmd_test_case_9::first_component/hash_attr_4'  => "",
+  'node/cmd_test_case_9::first_component/hash_attr_5'  => "",
+}
+
+attributes_to_check_2 = {
+  'node/cmd_test_case_9::first_component/hash_attr'    => "{key1=>value1}",
+  'node/cmd_test_case_9::first_component/hash_attr_2'  => "",
+  'node/cmd_test_case_9::first_component/hash_attr_3'  => "{key1=>nil}",
+  'node/cmd_test_case_9::first_component/hash_attr_4'  => "{key1=>[element1, element2]}",
+  'node/cmd_test_case_9::first_component/hash_attr_5'  => "{key1=>value1, key2=>value2}",
+}
 
 dtk_common = Common.new("", "")
 
@@ -29,12 +47,44 @@ describe '(Component Module DSL) Test Case 9: Test adding verious default values
     include_context "List assemblies", module_name, assembly_name, dtk_common
   end
 
+  context "Stage assembly from module" do
+    include_context "Stage assembly from module", module_name, module_location, assembly_name, service_name
+  end
+
+  context "Check attributes correct in service instance" do
+    include_context "Check attributes correct in service instance", dtk_common, service_name, attributes_to_check_1
+  end
+
+  context "Delete service instance" do
+    include_context "Delete service instance", service_location, service_name, dtk_common
+  end
+
+  context "Uninstall service instance" do
+    include_context "Uninstall service instance", service_location, service_name
+  end
+
   context "Replace original content of dtk.module.yaml with delta content" do
     include_context "Replace original content of dtk.module.yaml with delta content", module_location, delta_module_content
   end
 
   context "Push module changes" do
     include_context "Push module changes", module_name, module_location
+  end
+
+  context "Stage assembly from module" do
+    include_context "Stage assembly from module", module_name, module_location, assembly_name, service_name
+  end
+
+  context "Check attributes correct in service instance" do
+    include_context "Check attributes correct in service instance", dtk_common, service_name, attributes_to_check_2
+  end
+
+  context "Delete service instance" do
+    include_context "Delete service instance", service_location, service_name, dtk_common
+  end
+
+  context "Uninstall service instance" do
+    include_context "Uninstall service instance", service_location, service_name
   end
 
   context "Uninstall module" do

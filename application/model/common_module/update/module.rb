dtk/dtk-server

commit b0a4045f3e7cd22209b9ae0725ad3089938f703e

Fix for module info message

application/model/common_module/update/module.rb
@@ -46,7 +46,7 @@ module DTK
 
         if repo_diffs_summary = @module_branch.pull_repo_changes_and_return_diffs_summary(@commit_sha, force: true) 
           ret.add_diffs_summary!(repo_diffs_summary)
-          top_dsl_file_changed = repo_diffs_summary.prune!(TOP_DSL_FILE_REGEXP)
+          top_dsl_file_changed = repo_diffs_summary.prune!(TOP_DSL_FILE_REGEXP) if repo_diffs_summary[:files_modified].nil?
           
           # TODO: make more efficient by just computing parsed_common_module if parsing
           parsed_common_module = dsl_file_obj_from_repo.parse_content(:common_module)

commit 914c0a79cc43e0697a50cb498498be705eba367b

Backed out commit b0a4045f3e7cd2 which broke parsing of changed dsl file when doing 'dtk module push'

application/model/common_module/update/module.rb
@@ -46,7 +46,7 @@ module DTK
 
         if repo_diffs_summary = @module_branch.pull_repo_changes_and_return_diffs_summary(@commit_sha, force: true) 
           ret.add_diffs_summary!(repo_diffs_summary)
-          top_dsl_file_changed = repo_diffs_summary.prune!(TOP_DSL_FILE_REGEXP) if repo_diffs_summary[:files_modified].nil?
+          top_dsl_file_changed = repo_diffs_summary.prune!(TOP_DSL_FILE_REGEXP)
           
           # TODO: make more efficient by just computing parsed_common_module if parsing
           parsed_common_module = dsl_file_obj_from_repo.parse_content(:common_module)
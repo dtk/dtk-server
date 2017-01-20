bb88e412407938c173c8debcf84f26331a9ea9e7
dduvnjak <dario@atlantbh.com>
2017-01-20 15:11:01 +0000

    DTK-2847: Sanitize potentially sensitive unhidden attributes

--- utils/internal/config_agent/adapter/dynamic.rb
+++ utils/internal/config_agent/adapter/dynamic.rb
@@ -121,7 +121,7 @@ module DTK; class ConfigAgent
       module Sanitize
         def self.sanitize_message(msg)
           sanitized_attributes = msg[:attributes].inject({}) do |h, (type, attributes_hash)| 
-            h.merge(type => attributes_hash.inject({}) { |h, (name, info)| h.merge(name => sanitize_attribute(info)) })
+            h.merge(type => attributes_hash.inject({}) { |h, (name, info)| h.merge(name => sanitize_attribute(name, info)) })
           end
           msg.merge(attributes: sanitized_attributes)
         end
@@ -129,8 +129,8 @@ module DTK; class ConfigAgent
         private
         
         HIDDEN_VALUE = '***'
-        def self.sanitize_attribute(attr_info)
-          attr_info[:hidden] ? attr_info.merge(value: HIDDEN_VALUE) : attr_info 
+        def self.sanitize_attribute(name, attr_info)
+          (attr_info[:hidden] || ['password', 'secret'].any? { |pattern| name.downcase.include? pattern }) ? attr_info.merge(value: HIDDEN_VALUE) : attr_info 
         end
       end
 

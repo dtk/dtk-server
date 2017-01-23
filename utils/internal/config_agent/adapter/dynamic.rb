f5cfa233b67f07ae73ee287d73e6dea08a11e332
dduvnjak <dario@atlantbh.com>
2017-01-23 16:02:24 +0000

    DTK-2847: Disable hack

--- utils/internal/config_agent/adapter/dynamic.rb
+++ utils/internal/config_agent/adapter/dynamic.rb
@@ -60,7 +60,7 @@ module DTK; class ConfigAgent
         Log.info_pp [:message_sent_to_dynamic_provider, Sanitize.sanitize_message(msg)]
 
         # TODO: DTK-2847: when switch over on arbiter side remove this line and remove the module HackForDTK2847
-        msg = HackForDTK2847.convert_message(msg)
+        #msg = HackForDTK2847.convert_message(msg)
 
         msg
       end
@@ -139,6 +139,7 @@ module DTK; class ConfigAgent
           converted_attributes = msg[:attributes].inject({}) do |h, (type, attributes_hash)|
             h.merge(type => attributes_hash.inject({}) { |h, (name, info)| h.merge(name => info[:value]) })
           end
+          msg.delete(:protocol_version)
           msg.merge(attributes: converted_attributes)
         end
       end

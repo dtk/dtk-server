25d14fe94e1d58848225d740295b04d4221a6bc6
dduvnjak <dario@atlantbh.com>
2017-02-13 12:42:38 +0000

    Remove dtk_passenger from manifests

--- docker/manifests/stage5.pp
+++
@@ -1,7 +0,0 @@
-$extlookup_datadir = '/etc/puppet/manifests/extdata'
-$extlookup_precedence = ['common']
-$dtk_assembly_node = "tenant"
-
-class {"dtk_nginx::base": }->
-
-dtk_nginx::vhost_for_tenant {"dtk1": instance_name => "dtk1", tenant_type => 'http'} 

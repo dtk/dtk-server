b325a5a525bfafdff2e1d406d47d123b214b7a07
bjusufbe <bakir@atlantbh.com>
2017-06-29 12:52:57 +0200

    added couple of tests for our existing representative assemblies + archived old scripts

---
+++ test/functional/rspec/spec/regression/different_node_templates/dnt_test_case_01_spec.rb
@@ -0,0 +1,58 @@
+# Test Case 01: Converge wordpress multi node scenario
+# This test script will test following: 
+# - converge multi node scenario
+# - test passing links between components that exist on different nodes
+
+require './lib/dtk_cli_spec'
+require './lib/dtk_common'
+
+assembly_name = 'wordpress_multi_node'
+service_name = 'wordpress_multi_node'
+remote_module = 'dtk-examples/wordpress'
+remote_module_location = '/tmp/wordpress'
+remote_module_version = '1.5.0'
+service_location = '~/dtk/'
+
+dtk_common = Common.new('', '')
+
+describe "Converge wordpress multi node scenario" do
+  before(:all) do
+    puts '**************************************', ''
+  end
+
+  context "Install module from dtkn" do
+    include_context "Install module from dtkn", remote_module, remote_module_location, remote_module_version
+  end
+
+  context "List assemblies contained in this module" do
+    include_context "List assemblies", remote_module, assembly_name, dtk_common
+  end
+
+  context "Stage assembly from module" do
+    include_context "Stage assembly from module", remote_module, remote_module_location, assembly_name, service_name
+  end
+
+  context "Converge service instance" do
+    include_context "Converge service instance", service_location, dtk_common, service_name
+  end
+
+  context "Delete service instance" do
+    include_context "Delete service instance", service_location, service_name, dtk_common
+  end
+
+  context "Uninstall service instance" do
+    include_context "Uninstall service instance", service_location, service_name
+  end
+
+  context "Uninstall module" do
+    include_context "Uninstall module", remote_module, remote_module_location
+  end
+
+  context "Delete initial module on filesystem" do
+    include_context "Delete initial module on filesystem", remote_module_location
+  end
+
+  after(:all) do
+    puts '', ''
+  end
+end
---
+++ test/functional/rspec/spec/regression/different_node_templates/dnt_test_case_02_spec.rb
@@ -0,0 +1,69 @@
+# Test Case 02: Converge rails local app with datadog integration
+# This test script will test following: 
+# - converge rails local app on nginx with postgres 
+# - establish integration with datadog in order to show postgres and rails metrics on datadog dashboards
+# - execute ruby provider actions for creating screenboard/timeboard on datadog and attaching metrics to it
+
+require './lib/dtk_cli_spec'
+require './lib/dtk_common'
+
+assembly_name = 'rails_app_local'
+service_name = 'rails_local_app_with_datadog'
+remote_module = 'dtk-examples/rails'
+remote_module_location = '/tmp/rails'
+remote_module_version = '1.0.0'
+service_location = '~/dtk/'
+api_key = 'ccb08354705864db6041d30fc4621529'
+app_key = 'af01ce7a8180b837f53b01f914ea7f0f82f4d6ba'
+
+dtk_common = Common.new('', '')
+
+describe "Converge rails local app with datadog integration" do
+  before(:all) do
+    puts '*************************************************', ''
+  end
+
+  context "Install module from dtkn" do
+    include_context "Install module from dtkn", remote_module, remote_module_location, remote_module_version
+  end
+
+  context "List assemblies contained in this module" do
+    include_context "List assemblies", remote_module, assembly_name, dtk_common
+  end
+
+  context "Stage assembly from module" do
+    include_context "Stage assembly from module", remote_module, remote_module_location, assembly_name, service_name
+  end
+
+  context "Set attribute for datadog api key" do
+    include_context "Set attribute", service_location, service_name, 'node/datadog_api::smoke_test/api_key', api_key
+  end
+
+  context "Set attribute for datadog app key" do
+    include_context "Set attribute", service_location, service_name, 'node/datadog_api::smoke_test/application_key', app_key
+  end
+
+  context "Converge service instance" do
+    include_context "Converge service instance", service_location, dtk_common, service_name
+  end
+
+  context "Delete service instance" do
+    include_context "Delete service instance", service_location, service_name, dtk_common
+  end
+
+  context "Uninstall service instance" do
+    include_context "Uninstall service instance", service_location, service_name
+  end
+
+  context "Uninstall module" do
+    include_context "Uninstall module", remote_module, remote_module_location
+  end
+
+  context "Delete initial module on filesystem" do
+    include_context "Delete initial module on filesystem", remote_module_location
+  end
+
+  after(:all) do
+    puts '', ''
+  end
+end

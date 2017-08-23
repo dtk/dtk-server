8335f018467e79a8063767fd8c15760b0106fa67
bjusufbe <bakir@atlantbh.com>
2017-08-23 13:17:38 +0200

    small fix in test scripts

--- test/functional/rspec/spec/regression/action_framework/af_test_case_7_spec.rb
+++ test/functional/rspec/spec/regression/action_framework/af_test_case_7_spec.rb
@@ -1,85 +1,57 @@
 #!/usr/bin/env ruby
 # Test Case 7: Using lambda function in dtk.model.yaml
+# This test script will test following: 
+# - converge service instance with component that contains ruby lambda that will be executed
 
-require 'rubygems'
-require 'rest_client'
-require 'pp'
-require 'json'
-require 'awesome_print'
+require './lib/dtk_cli_spec'
 require './lib/dtk_common'
-require './lib/assembly_and_service_operations_spec'
-require './lib/service_modules_spec'
-require './lib/component_modules_spec'
 
-namespace = 'dtk17'
-service_module_name = 'service_with_lambda'
-imported_service_module_name = 'dtk17:service_with_lambda'
-component_module_name = 'module_with_lambda'
-imported_component_module_name = 'dtk17:module_with_lambda'
-component_module_filesystem_location = '~/dtk/component_modules/dtk17'
-service_module_filesystem_location = '~/dtk/service_modules/dtk17'
+assembly_name = 'lambda'
+service_name = 'af_test_case_7_instance'
+module_location = '/tmp/module_with_lambda'
+module_name = 'r8/module_with_lambda'
+module_version = 'master'
+service_location = '~/dtk/'
+service_instance_location = '~/dtk/af_test_case_7_instance'
 
-service_name = 'stda_test_case_14_instance'
-assembly_name = 'service_with_lambda::lambda'
 dtk_common = Common.new(service_name, assembly_name)
 
 expected_output_1 = {
   command: 'cat /tmp/test | grep 55',
-  status: 0,
-  stderr: nil
+  status: 'succeeded',
+  stderr: 0
 }
 
 describe '(Action Framework) Test Case 7: Using lambda function in dtk.model.yaml' do
   before(:all) do
     puts '***********************************************************************', ''
+    # Install/clone r8:action_module module with required dependency modules if needed
+    location_exist = `ls #{module_location}`
+    if location_exist.include? "No such file or directory"
+      system("mkdir #{module_location}")
+      system("dtk module clone -v #{module_version} #{module_name} #{module_location}")
+      system("dtk module install --update-deps -d #{module_location} #{module_name}")
+    end
   end
 
-  context 'Import service module function' do
-    include_context 'Import remote service module', namespace + '/' + service_module_name
+  context "Stage assembly from module" do
+    include_context "Stage assembly from module", module_name, module_location, assembly_name, service_name
   end
 
-  context 'List all service modules' do
-    include_context 'List all service modules', dtk_common, imported_service_module_name
+  context "Converge service instance" do
+    include_context "Converge service instance", service_location, dtk_common, service_name
   end
 
-  context "Stage service function on #{assembly_name} assembly" do
-    include_context 'Stage', dtk_common
+  context "Get task status details for action with bash script command" do
+    include_context 'Get task status details', service_instance_location, "STAGE 4", [expected_output_1]
   end
 
-  context 'List services after stage' do
-    include_context 'List services after stage', dtk_common
+  context "Delete service instance" do
+    include_context "Delete service instance", service_location, service_name, dtk_common
   end
 
-  context 'Converge function' do
-    include_context 'Converge', dtk_common
-  end
-
-  context 'Get task action details for action with bash script command' do
-    include_context 'Get task action details', dtk_common, '4.1', [expected_output_1]
-  end
-
-  context 'Delete and destroy service function' do
-    include_context 'Delete services', dtk_common
-  end
-
-  context 'List services after delete' do
-    include_context 'List services after delete', dtk_common
-  end
-
-  context "Delete #{imported_service_module_name} service module" do
-    include_context 'Delete service module', dtk_common, imported_service_module_name
-  end
-
-  context "Delete #{service_module_name} service module from local filesystem" do
-    include_context 'Delete service module from local filesystem', service_module_filesystem_location, service_module_name
-  end
-
-  context 'Delete component module' do
-    include_context 'Delete component module', dtk_common, imported_component_module_name
-  end
-
-  context 'Delete component module from local filesystem' do
-    include_context 'Delete component module from local filesystem', component_module_filesystem_location, component_module_name
+  context "Uninstall service instance" do
+    include_context "Uninstall service instance", service_location, service_name
   end
 
   after(:all) do

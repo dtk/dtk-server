784e52d583faa529c9129db63c4e450336cf5b16
Rich Pelavin <rich.reactor8.com>
2017-08-30 22:55:50 +0000

    Fixed small bug that did not properly show errors when missing mustache variable in bash script

--- application/model/action_def/content/template_processor/adapter/mustache_template.rb
+++ application/model/action_def/content/template_processor/adapter/mustache_template.rb
@@ -24,11 +24,11 @@ module DTK; class ActionDef; class Content
 
       def bind_template_attributes(command_line, attr_val_pairs)
         ::DTK::MustacheTemplate.render(command_line, attr_val_pairs)
-       rescue MustacheTemplate::Error::MissingVar => e
+      rescue ::DTK::MustacheTemplate::Error::MissingVar => e
         ident = 4
         err_msg = "The mustache variable '#{e.missing_var}' in the following command is not set:\n#{' ' * ident}#{command_line}"
         raise ErrorUsage.new(err_msg)
-       rescue MustacheTemplate::Error => e
+      rescue ::DTK::MustacheTemplate::Error => e
         raise ErrorUsage.new("Template error in command (#{command_line}): #{e.error_message}")
       end
     end

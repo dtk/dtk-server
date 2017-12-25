295604099774b641e21aab83d7a3404186b78b9e
AlminABH <almin.islamovic@atlantbh.com>
2017-12-25 09:35:19 +0100

    Added workflow_delete variable where we store the order of delete workflow, this results in right order of deletion

--- application/model/assembly/instance/exec_delete.rb
+++ application/model/assembly/instance/exec_delete.rb
@@ -131,8 +131,9 @@ module DTK; class  Assembly
         if components = node.get_components
           cmp_opts = { method_name: 'delete', skip_running_check: true, delete_action: 'delete_component' }
 
+          workflow_delete = Task.get_delete_workflow_order(assembly_instance)
           # order components by 'delete' action inside assembly workflow if exists
-          ordered_components = order_components_by_workflow(components, Task.get_delete_workflow_order(assembly_instance))
+          ordered_components = order_components_by_workflow(components, workflow_delete)
           ordered_components.uniq.each do |component|
             next if component.get_field?(:component_type).eql?('ec2__node')
             cmp_action = nil
@@ -267,8 +268,9 @@ module DTK; class  Assembly
           if components = assembly_wide_node.get_components
             cmp_opts = { method_name: 'delete', skip_running_check: true, delete_action: 'delete_component' }
 
+            workflow_delete = Task.get_delete_workflow_order(assembly_instance)
             # order components by 'delete' action inside assembly workflow if exists
-            ordered_components = order_components_by_workflow(components, Task.get_delete_workflow_order(assembly_instance))
+            ordered_components = order_components_by_workflow(components, workflow_delete)
             ordered_components.each do |component|
               cmp_top_task = Task.create_top_level(model_handle(:task), assembly_instance, task_action: "delete component '#{component.display_name_print_form}'")
 

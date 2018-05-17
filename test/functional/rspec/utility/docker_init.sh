066a25f67ec84c1723ea10c9602fe27dbad762c9
bjusufbe <bakir@atlantbh.com>
2018-05-17 14:22:09 +0200

    small fix

--- test/functional/rspec/utility/docker_init.sh
+++ test/functional/rspec/utility/docker_init.sh
@@ -18,23 +18,14 @@
 #
 
 if [[ -z $1 ]]; then
-    echo "No public address provided."
-    exit 1
+  echo "No public address provided."
+  exit 1
 fi
 
 if [[ -z $2 ]]; then
   echo "Upgrade status not specified."
-    exit 1
+  exit 1
 fi
-
-DTK_IMAGE=${DTK_IMAGE-getdtk/dtk-server}
-ARBITER_IMAGE=${ARBITER_IMAGE-getdtk/dtk-arbiter}
-REPO_HOST=dtknet.servicecatalog.it
-REPO_PORT=7001
-MCO_PORT=6163
-SSH_PORT=2222
-HTTP_PORT=8080
-GIT_USERNAME=dtk1
 
 ADDRESS=${1}
 UPGRADE=${2}
@@ -46,10 +37,20 @@ ARBITER_CONTAINER=${7:-dtk-arbiter}
 USER=${8:-docker-test}
 PASS=${9:-r8server}
 
-if [[ $DTK_SERVER_BRANCH == 'master' ]]; then
-  DTK_SERVER_BRANCH_DOCKER='latest'
-else
-  DTK_SERVER_BRANCH_DOCKER=$DTK_SERVER_BRANCH
+SSH_PORT=2222
+
+docker ps -a | grep dtk
+EXISTS=$?
+if [[ $EXISTS -eq 0 ]]; then
+  echo -e "Removing Docker Container: ${CONTAINER}...\n"
+  docker rm -f ${CONTAINER}
+fi
+
+docker ps -a | grep dtk-arbiter
+EXISTS=$?
+if [[ $EXISTS -eq 0 ]]; then
+  echo -e "Removing Docker Container: ${ARBITER_CONTAINER}...\n"
+  docker rm -f ${ARBITER_CONTAINER}
 fi
 
 if [[ $UPGRADE -eq 0 ]]; then
@@ -59,64 +60,12 @@ if [[ $UPGRADE -eq 0 ]]; then
   echo -e "USERNAME=${USER}\nPASSWORD=${PASS}\nPUBLIC_ADDRESS=${ADDRESS}\nGIT_PORT=${SSH_PORT}\nLOG_LEVEL=debug\n" > "${HOST_VOLUME}/dtk.config"
 fi
 
-docker ps -a | grep dtk
-EXISTS=$?
-if [[ $EXISTS -eq 0 ]]; then
-    echo -e "Removing Docker Container: ${CONTAINER}...\n"
-    docker rm -f ${CONTAINER}
-fi
-
-docker ps -a | grep dtk-arbiter
-EXISTS=$?
-if [[ $EXISTS -eq 0 ]]; then
-    echo -e "Removing Docker Container: ${ARBITER_CONTAINER}...\n"
-    docker rm -f ${ARBITER_CONTAINER}
-fi
-
-echo -e "Pulling the latest DTK - Server image. \n"
-docker pull ${DTK_IMAGE}
-PULLED=$?
-if [[ $PULLED -eq 1 ]]; then
-  echo -e "Error while pulling dtk server image. \n"
-  exit 1
-fi
-
-echo -e "Pulling the latest DTK - Arbiter image. \n"
-docker pull ${ARBITER_IMAGE}
-PULLED=$?
-if [[ $PULLED -eq 1 ]]; then
-    echo -e "Error while pulling dtk arbiter image. \n"
-    exit 1
-fi
-
 echo -e "\nStarting a new Docker Container: ${CONTAINER}"
 echo -e "\nStarting a new Docker Container: ${ARBITER_CONTAINER}"
 
-# start the dtk-server container
+# start the dtk-server and dtk-arbiter container
 if [[ $DTK_SERVER_BRANCH == 'master' ]]; then
-  docker run --name ${CONTAINER} -v ${HOST_VOLUME}:/host_volume -p ${HTTP_PORT}:80 -p ${MCO_PORT}:6163 -p ${SSH_PORT}:22 -d ${DTK_IMAGE}
+  \curl -sSL https://getserver.dtk.io | bash -s -- -v latest ${HOST_VOLUME}
 else
-  docker run --name ${CONTAINER} -v ${HOST_VOLUME}:/host_volume -p ${HTTP_PORT}:80 -p ${MCO_PORT}:6163 -p ${SSH_PORT}:22 -d ${DTK_IMAGE}:${DTK_SERVER_BRANCH}
-fi
-
-# wait for dtk-arbiter ssh keypair to be generated
-while [[ ! -f $HOST_VOLUME/arbiter/arbiter_remote ]]; do
-  sleep 5
-done
-
-# check if docker daemon socket is available
-# and mount it inside dtk-arbiter contianer if so
-ADDITIONAL_ARGS=''
-if [[ -e /var/run/docker.sock ]]; then
-  ADDITIONAL_ARGS="-v /var/run/docker.sock:/var/run/docker.sock "
-fi
-
-# Give time to dtk server to be up and running
-sleep 30
-
-# start the dtk-arbiter container
-if [[ $DTK_ARBITER_BRANCH == 'master' ]]; then
-  docker run -e GIT_USERNAME=${GIT_USERNAME} --name ${ARBITER_CONTAINER} -v ${HOST_VOLUME}:/host_volume -e HOST_VOLUME=${HOST_VOLUME} ${ADDITIONAL_ARGS} --restart=on-failure -td ${ARBITER_IMAGE}
-else
-  docker run -e GIT_USERNAME=${GIT_USERNAME} --name ${ARBITER_CONTAINER} -v ${HOST_VOLUME}:/host_volume -e HOST_VOLUME=${HOST_VOLUME} ${ADDITIONAL_ARGS} --restart=on-failure -td ${ARBITER_IMAGE}:${DTK_ARBITER_BRANCH}
+  \curl -sSL https://getserver.dtk.io | bash -s -- -v ${DTK_SERVER_BRANCH} ${HOST_VOLUME}
 fi

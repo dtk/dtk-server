46b1af5f0d489e6eb4caf09792041017df0abae2
Aldin Milanovic <aldin@atlantbh.com>
2018-07-20 13:52:12 +0200

    Add component when added from new dependency

--- lib/common_dsl/diff/service_instance/dsl.rb
+++ lib/common_dsl/diff/service_instance/dsl.rb
@@ -33,7 +33,9 @@ module DTK; module CommonDSL
             if cmp_modules_with_namespaces = ret_cmp_modules_with_namespaces(service_instance_parse[:dependent_modules] || {})
               # component_module_refs = ModuleRefs.get_component_module_refs(module_branch)
               component_module_refs = service_instance.assembly_instance.component_module_refs
-              component_module_refs.update if component_module_refs.update_object_if_needed!(cmp_modules_with_namespaces)
+
+              # component_module_refs.update if component_module_refs.update_object_if_needed!(cmp_modules_with_namespaces)
+              component_module_refs.update_module_refs_if_needed!(cmp_modules_with_namespaces)
             end
 
             # compute base diffs

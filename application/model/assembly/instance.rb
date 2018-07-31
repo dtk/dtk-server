8dd9b3c8f5ca5fd68c718ecc09472f786111a8f9
AlminABH <almin.islamovic@atlantbh.com>
2018-07-31 15:56:03 +0200

    Fix for bug where actions would fail

--- application/model/assembly/instance.rb
+++ application/model/assembly/instance.rb
@@ -353,7 +353,7 @@ module DTK; class  Assembly
           end
         end
 
-        task[:retry] = {:attempts => params[:attempts], :sleep => params[:sleep]}
+        task[:retry] = {:attempts => params[:attempts], :sleep => params[:sleep]} unless params[:attempts].nil? || params[:sleep].nil?
         task = task.save_and_add_ids()
       rescue Task::Template::ParsingError => e
         return ret if params[:noop_if_no_action]

be02d8dcf550352ea03e1c2933205ac273fa5160
Rich Pelavin <rich@reactor8.com>
2019-01-21 05:28:38 +0000

    DTK-3645: added comments on how the workflow config adapter takes a workflow action and executes it

--- lib/config_agent/adapter/workflow.rb
+++ lib/config_agent/adapter/workflow.rb
@@ -23,6 +23,30 @@ module DTK; class ConfigAgent
         ret_msg_content(task_action)
         #We now have a task action that has templates substituted in its components
         #I suppose we should form the task hash here that will get executed
+        # You want to first create a hirerachical task and then execute a workflow on it. If you trace executing a 
+        # workflow you can trace at https://github.com/dtk/dtk-server/blob/master/application/model/assembly/instance.rb#L268
+        # AT This is point where a hierarchical task is created from pulling in the workflow to execute.
+        # On line 272 you see  task.save!() which is hack needed in this code base that faciliattes the next steps that 
+        # transalate the hierarchical task to the workflow executable language (ruote in this case).
+        # When we write a new workflow executor it wil have teh following high level functions 
+        # 1) One translate the workflow into the "hierarchical task form"
+        # 2) Map the hierarchical task form to the exact launguage that step 2 uses to step through teh workflow
+        # 3) Write code that steps through a workflow and sipatches actions to teh excutors (see arch slides I just posted) abd
+        #    collects rsults
+        # For the new executor we might be able to to make teh datastructure for teh hierarchical task the exact same that can be 
+        # used in step 3.
+        # Back tio teh curernt code. The key thing to look at after following https://github.com/dtk/dtk-server/blob/master/application/model/assembly/instance.rb#L268
+        # is the code that does part 2 and 3 above that starts at https://github.com/dtk/dtk-server/blob/master/application/model/assembly/instance.rb#L246
+        # You wil see you descend into code that does
+        # 
+        #  278:     def execute_service_action(task_id, params)
+        # => 279:       task_idh = id_handle().createIDH(id: task_id, model_name: :task)
+        # 280:       task     = Task::Hierarchical.get_and_reify(task_idh, params)
+        # 281:       workflow = Workflow.create(task)
+        # 282:       workflow.defer_execution()
+        # The task you get in step 280 is essntially teh same structure saved by instance.rb#L268
+        # Line 281 creates the ruote datastructure (step 2)
+        # Line 282 executes the workflow (step 3)
       end
 
       def ret_msg_content(task_info, opts = {})
